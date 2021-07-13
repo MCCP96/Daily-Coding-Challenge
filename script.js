a13fc089f034c6a05d76471999a5727f3121cb67
MCCP96 <petermccomb@live.com>
2021-07-13 13:26:20 -0400

    Day 28 - Gap in Primes

--- script.js
+++ script.js
@@ -1443,7 +1443,7 @@ console.log(decode(`h3 th2r2`));
 // Made for a very clean, very logical and readable one-liner solution */
 
 // Statistics for an Athletic Association         7/12/2021
-
+/* 
 // You are the "computer expert" of a local Athletic Association (C.A.A.). Many teams of runners come to compete. Each time you get a string of all race results of every team who has run. For example here is a string showing the individual results of a team of 5 runners:
 
 // "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
@@ -1502,7 +1502,8 @@ class teamStatistics {
     const date = new Date(null);
     date.setSeconds(timeInSeconds);
 
-    return date.toISOString().substr(11, 8).replace(/:\s*/g, "|");
+    // remove space between * / when uncommenting
+    return date.toISOString().substr(11, 8).replace(/:\s* /g, "|");
   }
 
   getAverage() {
@@ -1543,4 +1544,96 @@ class teamStatistics {
 
 console.log(bestPracticeStat("01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"));
 
-// This was a longer kata.
+// This was a longer kata. */
+
+// Gap in Primes          /7/13/2021
+
+// The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43
+
+// A prime gap of length n is a run of n-1 consecutive composite numbers between two successive primes (see: http://mathworld.wolfram.com/PrimeGaps.html).
+
+// We will write a function gap with parameters:
+// g (integer >= 2) which indicates the gap we are looking for
+// m (integer > 2) which gives the start of the search (m inclusive)
+// n (integer >= m) which gives the end of the search (n inclusive)
+//    n won't go beyond 1100000.
+
+// In the example above gap(2, 3, 50) will return [3, 5] which is the first pair between 3 and 50 with a 2-gap.
+
+// So this function should return the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise `null`
+
+// Examples:
+// gap(2, 5, 7) --> [5, 7]
+// gap(2, 5, 5) --> null
+// gap(4, 130, 200) --> [163, 167]
+//   ([193, 197] is also such a 4-gap primes between 130 and 200 but it's not the first pair)
+// gap(6,100,110) --> null : between 100 and 110 we have 101, 103, 107, 109 but 101-107is not a 6-gap because there is 103in between and 103-109is not a 6-gap because there is 107in between.
+
+// Ref
+// https://en.wikipedia.org/wiki/Prime_gap
+
+function gap(g, m, n) {
+  let lastPrime = 0;
+  for (m; m <= n; m++) {
+    if (isPrime(m)) {
+      if (m - lastPrime == g) return [lastPrime, m];
+      else lastPrime = m;
+    }
+  }
+  return null;
+}
+
+function isPrime(x) {
+  if (x <= 1) return false;
+  if (x % 2 == 0 && x > 2) return false;
+  const square = Math.sqrt(x);
+  for (let i = 3; i <= square; i += 2) {
+    if (x % i == 0) return false;
+  }
+  return true;
+}
+console.log(gap(2, 5, 7));
+console.log(gap(2, 5, 5));
+console.log(gap(4, 130, 200));
+
+// Fair solution, very readable.
+// Initially, isPrime function would time-out test so I added all the extra tests
+
+function oldIsPrime(x) {
+  for (let i = 1; i <= x; i++) {
+    if (x % i == 0) return false;
+  }
+  return true;
+}
+
+// This does the job if there's no time-out
+
+function bestPracticeGap(g, m, n) {
+  let lastPrime = 0;
+  function isPrime(x) {
+    for (let i = 2; i * i <= x; i++) {
+      if (x % i == 0) return false;
+    }
+    return true;
+  }
+
+  for (let i = m; i <= n; i++) {
+    if (isPrime(i)) {
+      if (i - lastPrime == g) return [lastPrime, i];
+      else lastPrime = i;
+    }
+  }
+  return null;
+}
+console.log(bestPracticeGap(2, 5, 7));
+console.log(bestPracticeGap(2, 5, 5));
+console.log(bestPracticeGap(4, 130, 200));
+
+// Pretty similar, but the isPrime function is optimized
+// This has more to do with math and knowledge of prime numbers, but the i*i<=x is basically the implementation of my
+
+const square = Math.sqrt(x);
+for (let i = 3; i <= square; i += 2) {}
+
+// just done cleaner...
+// Fun kata!
