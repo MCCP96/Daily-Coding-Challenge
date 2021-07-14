6526c50f554be570b7c64ed6c19cfee0733995da
MCCP96 <petermccomb@live.com>
2021-07-14 10:23:38 -0400

    Day 29 - Take a Number And Sum Its Digits Raised..

--- script.js
+++ script.js
@@ -1547,7 +1547,7 @@ console.log(bestPracticeStat("01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"));
 // This was a longer kata. */
 
 // Gap in Primes          /7/13/2021
-
+/* 
 // The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43
 
 // A prime gap of length n is a run of n-1 consecutive composite numbers between two successive primes (see: http://mathworld.wolfram.com/PrimeGaps.html).
@@ -1636,4 +1636,61 @@ const square = Math.sqrt(x);
 for (let i = 3; i <= square; i += 2) {}
 
 // just done cleaner...
-// Fun kata!
+// Fun kata! */
+
+// Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!          7/14/2021
+
+// The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+// In effect: 89 = 8^1 + 9^2
+
+// The next number in having this property is 135.
+// See this property again: 135 = 1^1 + 3^2 + 5^3
+
+// We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+// Let's see some cases:
+
+// sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+// If there are no numbers of this kind in the range [a, b] the function should output an empty list.
+
+// sumDigPow(90, 100) == []
+// Enjoy it!!
+
+function sumDigPow(a, b) {
+  let ans = [];
+  for (a; a <= b; a++) {
+    String(a)
+      .split(``)
+      .reduce((acc, cur, i) => {
+        acc += Number(cur ** (i + 1));
+        if (acc == a) ans.push(acc);
+        return acc;
+      }, 0);
+  }
+  return ans;
+}
+console.log(sumDigPow(88, 135));
+
+// I personally like this solution
+// converting to string, splitting, then using .reduce's index parameter allowed for clean power incrementation.
+
+function bestPracticeSumDigPow(a, b) {
+  let ans = [];
+  while (a <= b) {
+    if (
+      a
+        .toString()
+        .split(``)
+        .reduce((x, y, i) => x + y ** (i + 1), 0) == a
+    )
+      ans.push(a);
+    a++;
+  }
+  return ans;
+}
+console.log(bestPracticeSumDigPow(88, 135));
+
+// Seems I used all the correct steps.
+// Nice 👍
