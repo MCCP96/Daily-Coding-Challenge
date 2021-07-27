a0355e09c64e8002b34a17ae5df33cc6c7e8848b
MCCP96 <petermccomb@live.com>
2021-07-27 12:12:33 -0400

    Day 42 - Basic DeNico

--- script.js
+++ script.js
@@ -2329,7 +2329,7 @@ function solve(m) {
 console.log(solve(2.0)); */
 
 // k-Primes         7/26/2021
-
+/* 
 // A natural number is called k-prime if it has exactly k prime factors, counted with multiplicity. For example:
 
 // k = 2  -->  4, 6, 9, 10, 14, 15, 21, 22, ...
@@ -2384,4 +2384,60 @@ function puzzle(s) {
     }
   }
   return result;
+} */
+
+// Basic DeNico         7/27/2021
+
+// Write a function deNico/de_nico() that accepts two parameters:
+//    key/$key - string consists of unique letters and digits
+//    message/$message - string with encoded message
+//    and decodes the message using the key.
+
+// First create a numeric key basing on the provided key by assigning each letter position in which it is located after setting the letters from key in an alphabetical order.
+
+// For example, for the key crazy we will get 23154 because of acryz (sorted letters from the key).
+// Let's decode cseerntiofarmit on using our crazy key.
+
+// 1 2 3 4 5
+// ---------
+// c s e e r
+// n t i o f
+// a r m i t
+//   o n
+// After using the key:
+
+// 2 3 1 5 4
+// ---------
+// s e c r e
+// t i n f o
+// r m a t i
+// o n
+
+// Notes
+// The message is never shorter than the key.
+// Don't forget to remove trailing whitespace after decoding the message
+// Examples
+
+// deNico("crazy", "cseerntiofarmit on  ") => "secretinformation"
+// deNico("abc", "abcd") => "abcd"
+// deNico("ba", "2143658709") => "1234567890"
+// deNico("key", "eky") => "key"
+// Check the test cases for more examples.
+
+// Related Kata - https://www.codewars.com/kata/5968bb83c307f0bb86000015
+
+function deNico(key, m) {
+  let codder = key
+    .split(``)
+    .sort()
+    .map((e) => key.indexOf(e));
+  return m
+    .split(``)
+    .map(
+      (_, i) =>
+        m[
+          Math.floor(i / key.length) * key.length +
+            codder.indexOf(i % key.length)
+        ]
+    );
 }
