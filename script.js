10c7c733056b198214333adf2e1540b800ba5773
MCCP96 <petermccomb@live.com>
2021-08-02 12:55:46 -0400

    Day 48 - Merge Two Sorted Lists

--- script.js
+++ script.js
@@ -2706,7 +2706,7 @@ console.log(topVotedLongestCommonPrefix(["dog", "racecar", "car"]));
 //    repeat */
 
 // Valid Parentheses          8/1/2021
-
+/* 
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 // An input string is valid if:
@@ -2770,4 +2770,57 @@ console.log(isValid(`{[]}`));
 // Solution is adding to an array, then ensuring the correct closing bracket is being used
 // The default in the switch statement cleverly removes the closing bracket while testing it
 
-// If the array is empty, all opened brackets have been closed
+// If the array is empty, all opened brackets have been closed */
+
+// Merge Two Sorted Lists         8/2/2021
+
+// Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Example 1:
+//    Input: l1 = [1,2,4], l2 = [1,3,4]
+//    Output: [1,1,2,3,4,4]
+
+// Example 2:
+//    Input: l1 = [], l2 = []
+//    Output: []
+
+// Example 3:
+//    Input: l1 = [], l2 = [0]
+//    Output: [0]
+
+// Constraints:
+//    The number of nodes in both lists is in the range [0, 50].
+//    -100 <= Node.val <= 100
+//    Both l1 and l2 are sorted in non-decreasing order.
+
+const mergeTwoLists = function (l1, l2) {
+  let ans = [];
+  for (let i = 0; i <= 50; i++) {
+    if (!isFinite(l1[i]) && !isFinite(l2[i])) break;
+    l1[i] <= l2[i] ? ans.push(l1[i], l2[i]) : ans.push(l2[i], l1[i]);
+  }
+  return ans.filter((x) => x != undefined);
+};
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
+console.log(mergeTwoLists([], []));
+console.log(mergeTwoLists([], [0]));
+
+// Encountering some return type error
+// Current code answers the question, will be looking at top voted solution
+
+var topVotedMergeTwoLists = function (l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2;
+  if (l1.val < l2.val) {
+    l1.next = topVotedMergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = topVotedMergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
+console.log(topVotedMergeTwoLists([1, 2, 4], [1, 3, 4]));
+console.log(topVotedMergeTwoLists([], []));
+console.log(topVotedMergeTwoLists([], [0]));
+
+// Function feeds into itself, pretty clever and clean
+// Seems l1 and l2 are in fact objects and I had to return an object.
