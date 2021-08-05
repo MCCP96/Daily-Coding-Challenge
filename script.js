eb05c314755ae95f1f42bf02e65137586f35aa2b
MCCP96 <petermccomb@live.com>
2021-08-05 10:10:18 -0400

    Day 51 - Implement strStr()

--- script.js
+++ script.js
@@ -2885,7 +2885,7 @@ console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 // Works great! */
 
 // Remove Element         8/4/2021
-
+/* 
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
 
 // Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
@@ -2939,4 +2939,42 @@ console.log(removeElement([3, 2, 2, 3], 3));
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
 
 // Pretty straight-forward
-// While .indexOf still returns an index (not -1), splice said value
+// While .indexOf still returns an index (not -1), splice said value */
+
+// Implement strStr()         8/5/2021
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Clarification:
+
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+// Example 1:
+//    Input: haystack = "hello", needle = "ll"
+//    Output: 2
+
+// Example 2:
+//    Input: haystack = "aaaaa", needle = "bba"
+//    Output: -1
+
+// Example 3:
+//    Input: haystack = "", needle = ""
+//    Output: 0
+
+// Constraints:
+//    0 <= haystack.length, needle.length <= 5 * 104
+//    haystack and needle consist of only lower-case English characters.
+
+const strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+console.log(strStr("hello", "ll"));
+console.log(strStr("aaaaa", "bba"));
+console.log(strStr("", ""));
+
+// Simple and easy, faster than 95% of submissions
+
+// Reading discussion, people were attempting to solve without using built-in methods
+// Their 3-layer deep nested for loops proved to have slower runtime and higher memory usage
