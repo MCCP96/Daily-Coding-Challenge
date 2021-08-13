59d5e68d0ebdfb7f487cfa15abd3db7a498d5f74
MCCP96 <petermccomb@live.com>
2021-08-13 12:44:00 -0400

    Day 59 - Remove Duplicates from Sorted List

--- script.js
+++ script.js
@@ -3225,7 +3225,7 @@ console.log(mySqrt(8)); // 2
 // Basically loop over and over until x has been passed, then decrement once */
 
 // Climbing Stairs          8/12/2021
-
+/* 
 // You are climbing a staircase. It takes n steps to reach the top.
 
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
@@ -3280,4 +3280,66 @@ console.log(topVotedClimbStairs(3)); // 3
 //    5 + 8 = 13
 //    ...
 
-// Not the most obvious thing to me, but obviously Fibonacci sequence was designed to tackle this.
+// Not the most obvious thing to me, but obviously Fibonacci sequence was designed to tackle this. */
+
+// Remove Duplicates from Sorted List         8/13/2021
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Example 1:
+//    Input: head = [1,1,2]
+//    Output: [1,2]
+
+// Example 2:
+//    Input: head = [1,1,2,3,3]
+//    Output: [1,2,3]
+
+// Constraints:
+//    The number of nodes in the list is in the range [0, 300].
+//    -100 <= Node.val <= 100
+//    The list is guaranteed to be sorted in ascending order.
+
+const deleteDuplicates = function (head) {
+  return [...new Set(head)];
+};
+console.log(deleteDuplicates([1, 1, 2]));
+console.log(deleteDuplicates([1, 1, 2, 3, 3]));
+
+const secondDeleteDuplicates = function (head) {
+  return head.filter((cur, i, arr) => cur !== arr[i - 1]);
+};
+console.log(secondDeleteDuplicates([1, 1, 2]));
+console.log(secondDeleteDuplicates([1, 1, 2, 3, 3]));
+
+const thirdDeleteDuplicates = function (head) {
+  let i = 1;
+  while (i <= head.length) {
+    if (head[i] == head[i - 1]) {
+      head.splice(i, 1);
+    } else i++;
+  }
+  return head;
+};
+console.log(thirdDeleteDuplicates([1, 1, 2]));
+console.log(thirdDeleteDuplicates([1, 1, 2, 3, 3]));
+
+// Doesnt seem to want to accept any of my solution even though the browser clearly shows the correct output was returned
+
+var topVotedDeleteDuplicates = function (head) {
+  var current = head;
+  while (current) {
+    if (current.next !== null && current.val == current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
+};
+console.log(topVotedDeleteDuplicates([1, 1, 2]));
+console.log(topVotedDeleteDuplicates([1, 1, 2, 3, 3]));
+
+// LeetCode accepts it, but browser shows error...
+// Must be the verification process that's denying my results
+
+// I prefer the first solution provided. Very clear, very simple.
