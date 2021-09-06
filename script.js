63aac54ec762ed187dddf9ab6f21f317efc7c60a
MCCP96 <petermccomb@live.com>
2021-09-06 12:35:53 -0400

    Day 83 - Assign Cookies

--- script.js
+++ script.js
@@ -4217,7 +4217,7 @@ console.log(findDisappearedNumbers([1, 1])); // [2]
  */
 
 // Minimum Moves to Equal Array Elements          9/5/2021
-
+/* 
 // Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
 
 // In one move, you can increment n - 1 elements of the array by 1.
@@ -4261,4 +4261,63 @@ console.log(topVotedMinMoves([1, 1, 1])); // 0
 //      Add：[1,2,3] => [2,3,3] => [3,4,3] => [4,4,4]
 //      Subtracted：[1,2,3] =>  [1,2,2] => [1,1,2] => [1,1,1]
 //    ↑ Find the smallest element first, and find the sum of the differences between all elements and the smallest element,
-//    which is the minimum  moves."
+//    which is the minimum  moves." */
+
+// Assign Cookies         9/6/2021
+
+// Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+
+// Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+// Example 1:
+//    Input: g = [1,2,3], s = [1,1]
+//    Output: 1
+//    Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
+//    And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+//    You need to output 1.
+
+// Example 2:
+//    Input: g = [1,2], s = [1,2,3]
+//    Output: 2
+//    Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
+//    You have 3 cookies and their sizes are big enough to gratify all of the children,
+//    You need to output 2.
+
+// Constraints:
+//    1 <= g.length <= 3 * 104
+//    0 <= s.length <= 3 * 104
+//    1 <= g[i], s[j] <= 231 - 1
+
+const findContentChildren = function (g, s) {
+  const numChildren = g.length;
+  for (let i = g.length; i > 0; i--) {
+    if (
+      s.reduce((acc, cur) => (acc += cur)) >=
+      g.reduce((acc, cur) => (acc += cur))
+    )
+      return Math.max(...g);
+    else {
+      g = g.filter((x) => x != Math.max(...g));
+    }
+  }
+  return 0;
+};
+console.log(findContentChildren([1, 2, 3], [1, 1])); // 1
+console.log(findContentChildren([1, 2], [1, 2, 3])); // 2
+
+// [1,2,3], [3] didnt work with my solution
+
+const topVotedFindContentChildren = function (g, s) {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let j = 0,
+    res = 0;
+  for (let num of s) {
+    if (num >= g[j]) res++, j++;
+  }
+  return res;
+};
+
+// Sorts both arrays then finds the lowest possible answer
+
+// Not a big fan of this question...
