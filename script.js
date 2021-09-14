2dc728ea8b55a20b864f30901316f25f2ba86719
MCCP96 <petermccomb@live.com>
2021-09-14 09:47:13 -0400

    Day 91 - Teemo Attacking

--- script.js
+++ script.js
@@ -4675,7 +4675,7 @@ console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // 2
 // Nothing fancy */
 
 // Construct the Rectangle          9/13/2021
-
+/* 
 // A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
 
 // The area of the rectangular web page you designed must equal to the given target area.
@@ -4712,4 +4712,57 @@ console.log(topVotedConstructRectangle(122122)); // [427,286]
 // Opted to study top voted
 
 // Basically, this code cuts down the middle then works down from there
-// The first integer found breaks the loop and gives us our answer
+// The first integer found breaks the loop and gives us our answer */
+
+// Teemo Attacking          9/14/2021
+
+// Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
+
+// You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
+
+// Return the total number of seconds that Ashe is poisoned.
+
+// Example 1:
+//    Input: timeSeries = [1,4], duration = 2
+//    Output: 4
+// Explanation: Teemo's attacks on Ashe go as follows:
+// - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+// - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+// Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+
+// Example 2:
+//    Input: timeSeries = [1,2], duration = 2
+//    Output: 3
+// Explanation: Teemo's attacks on Ashe go as follows:
+// - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+// - At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
+// Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+
+// Constraints:
+//    1 <= timeSeries.length <= 104
+//    0 <= timeSeries[i], duration <= 107
+//    timeSeries is sorted in non-decreasing order.
+
+const findPoisonedDuration = function (t, d) {
+  return t.reverse().reduce((acc, cur, i, arr) => {
+    acc += d;
+    if (cur <= arr[i + 1] + d) acc -= arr[i + 1] + d - cur;
+    return acc;
+  }, 0);
+};
+console.log(findPoisonedDuration([1, 4], 2)); // 4
+console.log(findPoisonedDuration([1, 2], 2)); // 3
+console.log(findPoisonedDuration([1, 2, 5, 10], 2)); // 7
+
+// Faster runtime than 75%
+
+const findPoisonedDuration = function (timeSeries, duration) {
+  if (timeSeries.length === 0) return 0;
+  let res = duration;
+  for (let i = 1; i < timeSeries.length; i++) {
+    res += Math.min(timeSeries[i] - timeSeries[i - 1], duration);
+  }
+  return res;
+};
+
+// Good to have a guard clause for empty timeSeries array
