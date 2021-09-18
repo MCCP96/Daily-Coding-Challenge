fe5aabe7bf5c5c4de53a0a91adb4bfabf6dc284f
MCCP96 <petermccomb@live.com>
2021-09-18 11:09:50 -0400

    Day 95 - Relative Ranks

--- script.js
+++ script.js
@@ -4891,7 +4891,7 @@ console.log(topVotedFindWords(["abdfs", "cccd", "a", "qwwewm"])); // ["a"]
 // VERY nice solution */
 
 // Base 7         9/17/2021
-
+/* 
 // Given an integer num, return a string of its base 7 representation.
 
 // Constraints:
@@ -4904,4 +4904,70 @@ console.log(convertToBase7(100)); // 202
 console.log(convertToBase7(-7)); // -10
 
 // That was probably the easiest one to date
-// Identical to top voted
+// Identical to top voted */
+
+// Relative Ranks         9/18/2021
+
+// You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+// The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+// The 1st place athlete's rank is "Gold Medal".
+// The 2nd place athlete's rank is "Silver Medal".
+// The 3rd place athlete's rank is "Bronze Medal".
+// For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+// Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+// Example 1:
+//    Input: score = [5,4,3,2,1]
+//    Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+// Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+
+// Example 2:
+//    Input: score = [10,3,8,9,4]
+//    Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+// Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+// Constraints:
+//    n == score.length
+//    1 <= n <= 104
+//    0 <= score[i] <= 106
+//     All the values in score are unique.
+
+const findRelativeRanks = function (score) {
+  for (let i = 0; i < 3; i++) {
+    let curMax = score.indexOf(Math.max(...score.filter((x) => /\d/.test(x))));
+
+    if (i == 0) score[curMax] = "Gold Medal";
+    if (i == 1) score[curMax] = "Silver Medal";
+    if (i == 2) score[curMax] = "Bronze Medal";
+  }
+
+  score
+    .filter((x) => /\d/.test(x))
+    .sort((a, b) => b - a)
+    .forEach((num, i) => {
+      score[score.indexOf(num)] = `${i + 4}`;
+    });
+
+  return score;
+};
+console.log(findRelativeRanks([5, 4, 3, 2, 1])); // ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+console.log(findRelativeRanks([10, 3, 8, 9, 4])); // ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+
+// Definitely could've solved this using 1 For loop
+
+const topVotedFindRelativeRanks = function (nums) {
+  let ranks = nums.slice(0).sort((a, b) => b - a);
+
+  return nums.map((num, i) => {
+    if (num === ranks[0]) return "Gold Medal";
+    else if (num === ranks[1]) return "Silver Medal";
+    else if (num === ranks[2]) return "Bronze Medal";
+    else return (ranks.indexOf(num) + 1).toString();
+  });
+};
+console.log(topVotedFindRelativeRanks([5, 4, 3, 2, 1])); // ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+console.log(topVotedFindRelativeRanks([10, 3, 8, 9, 4])); // ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+
+// This makes so much more sense...
