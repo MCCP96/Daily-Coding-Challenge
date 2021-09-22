f26738bdf788590c387217c70914eab125767053
MCCP96 <petermccomb@live.com>
2021-09-22 11:44:52 -0400

    Day 99 - Longest Uncommon Subsequence I

--- script.js
+++ script.js
@@ -5068,7 +5068,7 @@ console.log(topVotedFib(4));
 // This formula can compute the solution in O(1) time as well as O(1) space. */
 
 // Detect Capital         9/21/2021
-
+/* 
 // We define the usage of capitals in a word to be right when one of the following cases holds:
 
 // All letters in this word are capitals, like "USA".
@@ -5097,4 +5097,45 @@ console.log(detectCapitalUse("leetcode")); // true
 console.log(detectCapitalUse("Google")); // true
 
 // Simple and better than most
-// Same as top voted
+// Same as top voted */
+
+// Longest Uncommon Subsequence I         9/22/2021
+
+// Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+// An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+
+// A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+
+// For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
+
+// Example 1:
+//    Input: a = "aba", b = "cdc"
+//    Output: 3
+// Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
+// Note that "cdc" is also a longest uncommon subsequence.
+
+// Example 2:
+//    Input: a = "aaa", b = "bbb"
+//    Output: 3
+// Explanation: The longest uncommon subsequences are "aaa" and "bbb".
+
+// Example 3:
+//    Input: a = "aaa", b = "aaa"
+//    Output: -1
+// Explanation: Every subsequence of string a is also a subsequence of string b. Similarly, every subsequence of string b is also a subsequence of string a.
+
+// Constraints:
+//    1 <= a.length, b.length <= 100
+//    a and b consist of lower-case English letters.
+
+const topVotedFindLUSlength = function (a, b) {
+  return a === b ? -1 : Math.max(a.length, b.length);
+};
+console.log(topVotedFindLUSlength("aba", "cdc")); // 3
+console.log(topVotedFindLUSlength("aaa", "bbb")); // 3
+console.log(topVotedFindLUSlength("aaa", "aaa")); // -1
+
+// Took top voted and turned into one-liner
+
+// I see. As soon as they're not directly equal to one another, the longest input is immediately the longest uncommon substring
