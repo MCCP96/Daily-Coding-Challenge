cd971209201997284b8d43531101cf1a5748ddff
MCCP96 <petermccomb@live.com>
2021-10-13 10:22:45 -0400

    Day 119 - Find Pivot Index

--- script.js
+++ script.js
@@ -5980,7 +5980,7 @@ console.log(toLowerCase(`LOVELY`));
 // As easy as they get */
 
 // 1-bit and 2-bit Characters         10/12/2021
-
+/* 
 // We have two special characters:
 
 // The first character can be represented by one bit 0.
@@ -6011,4 +6011,88 @@ const isOneBitCharacter = function (bits) {
 console.log(isOneBitCharacter([1, 0, 0])); // true
 console.log(isOneBitCharacter([1, 1, 1, 0])); // false
 
-// Identical to top voted
+// Identical to top voted */
+
+// Find Pivot Index
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+// Example 1:
+//    Input: nums = [1,7,3,6,5,6]
+//    Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+// Example 2:
+//    Input: nums = [1,2,3]
+//    Output: -1
+// Explanation:
+// There is no index that satisfies the conditions in the problem statement.
+
+// Example 3:
+//    Input: nums = [2,1,-1]
+//    Output: 0
+// Explanation:
+// The pivot index is 0.
+// Left sum = 0 (no elements to the left of index 0)
+// Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+// Constraints:
+//    1 <= nums.length <= 104
+//    -1000 <= nums[i] <= 1000
+
+const pivotIndex = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (
+      nums.slice(0, i).reduce((acc, cur) => (acc += cur), 0) ==
+      nums.slice(i + 1).reduce((acc, cur) => (acc += cur), 0)
+    )
+      return i;
+  }
+  return -1;
+};
+console.log(pivotIndex([1, 7, 3, 6, 5, 6])); // 3
+console.log(pivotIndex([1, 2, 3])); // -1
+console.log(pivotIndex([2, 1, -1])); // 0
+
+// Very slow, but super easy solution
+
+const topVotedPivotIndex = function (nums) {
+  let sum = 0;
+  let sumMap = new Map();
+  let result = -1;
+  const length = nums.length - 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum = sum + nums[i];
+    sumMap.set(i, sum);
+  }
+
+  if (nums[0] === sumMap.get(nums.length - 1)) {
+    return 0;
+  }
+
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (sumMap.get(i - 1) === sumMap.get(length) - sumMap.get(i)) {
+      result = i;
+      break;
+    }
+  }
+
+  if (sumMap.get(length - 1) === 0 && result === -1) {
+    return length;
+  }
+
+  return result;
+};
+console.log(topVotedPivotIndex([1, 7, 3, 6, 5, 6])); // 3
+console.log(topVotedPivotIndex([1, 2, 3])); // -1
+console.log(topVotedPivotIndex([2, 1, -1])); // 0
