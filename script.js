057ff88deb11d469539b007bf9c5991870477d2d
MCCP96 <petermccomb@live.com>
2021-10-14 12:46:23 -0400

    Day 120 - Self Dividing Numbers

--- script.js
+++ script.js
@@ -6013,8 +6013,8 @@ console.log(isOneBitCharacter([1, 1, 1, 0])); // false
 
 // Identical to top voted */
 
-// Find Pivot Index
-
+// Find Pivot Index         10/13/2021
+/* 
 // Given an array of integers nums, calculate the pivot index of this array.
 
 // The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
@@ -6095,4 +6095,73 @@ const topVotedPivotIndex = function (nums) {
 };
 console.log(topVotedPivotIndex([1, 7, 3, 6, 5, 6])); // 3
 console.log(topVotedPivotIndex([1, 2, 3])); // -1
-console.log(topVotedPivotIndex([2, 1, -1])); // 0
+console.log(topVotedPivotIndex([2, 1, -1])); // 0 */
+
+// Self Dividing Numbers          10/14/2021
+
+// A self-dividing number is a number that is divisible by every digit it contains.
+
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+// A self-dividing number is not allowed to contain the digit zero.
+
+// Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right].
+
+// Example 1:
+//    Input: left = 1, right = 22
+//    Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+
+// Example 2:
+//    Input: left = 47, right = 85
+//    Output: [48,55,66,77]
+
+// Constraints:
+//    1 <= left <= right <= 104
+
+const selfDividingNumbers = function (l, r) {
+  let ans = [];
+  for (l; l <= r; l++) {
+    if (l < 10) {
+      ans.push(l);
+      continue;
+    }
+
+    let isSelfDividing = true;
+    for (let digit of `${l}`.split(``)) {
+      if (l % digit !== 0) {
+        isSelfDividing = false;
+        break;
+      }
+    }
+    if (isSelfDividing) ans.push(l);
+  }
+
+  return ans;
+};
+console.log(selfDividingNumbers(1, 22)); // [1,2,3,4,5,6,7,8,9,11,12,15,22]
+console.log(selfDividingNumbers(47, 85)); // [48,55,66,77]
+
+// Not great runtime, pretty good memory usage
+// Very easy read
+
+var sdn = (num) => {
+  let n = "" + num;
+  return n.split("").every((item) => num % parseInt(item) === 0);
+};
+
+var topVotedSelfDividingNumbers = function (left, right) {
+  let result = [];
+  for (let i = left; i < right + 1; i++) {
+    if (sdn(i)) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+
+// The logic is pretty much identical
+
+// First time seeing .every:
+// "The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value."
+
+// Perfect use case for this method
