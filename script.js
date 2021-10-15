7e04ef1dc0cfc3cc4a0913f14131f0956d505e85
MCCP96 <petermccomb@live.com>
2021-10-15 18:23:31 -0400

    Day 121 - Flood Fill

--- script.js
+++ script.js
@@ -6098,7 +6098,7 @@ console.log(topVotedPivotIndex([1, 2, 3])); // -1
 console.log(topVotedPivotIndex([2, 1, -1])); // 0 */
 
 // Self Dividing Numbers          10/14/2021
-
+/* 
 // A self-dividing number is a number that is divisible by every digit it contains.
 
 // For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
@@ -6164,4 +6164,62 @@ var topVotedSelfDividingNumbers = function (left, right) {
 // First time seeing .every:
 // "The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value."
 
-// Perfect use case for this method
+// Perfect use case for this method */
+
+// Flood Fill         10/15/2021
+
+// An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+// You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+// To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
+
+// Return the modified image after performing the flood fill.
+
+// Example 1:
+//    Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+//    Output: [[2,2,2],[2,2,0],[2,0,1]]
+// Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+// Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+// Example 2:
+//    Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
+//    Output: [[2,2,2],[2,2,2]]
+
+// Constraints:
+//    m == image.length
+//    n == image[i].length
+//    1 <= m, n <= 50
+//    0 <= image[i][j], newColor < 216
+//    0 <= sr < m
+//    0 <= sc < n
+
+const topVotedFloodFill = function (image, sr, sc, newColor) {
+  const dfs = (x, y, base) => {
+    if (
+      x < 0 ||
+      y < 0 ||
+      x >= image.length ||
+      y >= image[0].length ||
+      image[x][y] != base ||
+      image[x][y] === newColor
+    )
+      return; // boundary conditions
+    image[x][y] = newColor;
+    dfs(x - 1, y, base);
+    dfs(x, y - 1, base);
+    dfs(x + 1, y, base);
+    dfs(x, y + 1, base);
+  };
+
+  dfs(sr, sc, image[sr][sc]);
+
+  return image;
+};
+// prettier-ignore
+console.log(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1,  1,  2)); // [[2,2,2],[2,2,0],[2,0,1]]
+// prettier-ignore
+console.log(floodFill([[0,0,0],[0,0,0]],  0,  0,  2)); // [[2,2,2],[2,2,2]]
+
+// dfs function seems to test all surrounding squares and adjust accordingly
+// Not the fastest, but very good memory usage
