647a5cbe0bd83dabbe0f755500ac4a3ac424b9f6
MCCP96 <petermccomb@live.com>
2021-10-17 10:05:13 -0400

    Day 123 - Min Cost Climbing Stairs

--- script.js
+++ script.js
@@ -6225,7 +6225,7 @@ console.log(floodFill([[0,0,0],[0,0,0]],  0,  0,  2)); // [[2,2,2],[2,2,2]]
 // Not the fastest, but very good memory usage */
 
 // Find Smallest Letter Greater Than Target         10/16/2021
-
+/* 
 // Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
 
 // Note that the letters wrap around.
@@ -6260,4 +6260,36 @@ const topVotedNextGreatestLetter = function (letters, target) {
   );
 };
 
-// Same thing as a one-liner essentially
+// Same thing as a one-liner essentially */
+
+// Min Cost Climbing Stairs         10/17/2021
+
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+// You can either start from the step with index 0, or the step with index 1.
+
+// Return the minimum cost to reach the top of the floor.
+
+// Example 1:
+//    Input: cost = [10,15,20]
+//    Output: 15
+// Explanation: Cheapest is: start on cost[1], pay that cost, and go to the top.
+
+// Example 2:
+//    Input: cost = [1,100,1,1,1,100,1,1,100,1]
+//    Output: 6
+// Explanation: Cheapest is: start on cost[0], and only step on 1s, skipping cost[3].
+
+// Constraints:
+//    2 <= cost.length <= 1000
+//    0 <= cost[i] <= 999
+
+const topVotedMinCostClimbingStairs = (cost) => {
+  for (let i = 2; i < cost.length; i++)
+    cost[i] = Math.min(cost[i - 1] + cost[i], cost[i - 2] + cost[i]);
+  return Math.min(cost.pop(), cost.pop());
+};
+console.log(topVotedMinCostClimbingStairs([10, 15, 20])); // 15
+console.log(
+  topVotedMinCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
+); // 6
