5fc0eb95640e45e41d7d5cdf02daebe1a434c3fc
MCCP96 <petermccomb@live.com>
2021-10-18 15:24:11 -0400

    Day 124 - Largest Number At Least Twice of Others

--- script.js
+++ script.js
@@ -6263,7 +6263,7 @@ const topVotedNextGreatestLetter = function (letters, target) {
 // Same thing as a one-liner essentially */
 
 // Min Cost Climbing Stairs         10/17/2021
-
+/* 
 // You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
 
 // You can either start from the step with index 0, or the step with index 1.
@@ -6292,4 +6292,55 @@ const topVotedMinCostClimbingStairs = (cost) => {
 console.log(topVotedMinCostClimbingStairs([10, 15, 20])); // 15
 console.log(
   topVotedMinCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
-); // 6
+); // 6 */
+
+// Largest Number At Least Twice of Others          10/18/2021
+
+// You are given an integer array nums where the largest integer is unique.
+
+// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+// Example 1:
+//    Input: nums = [3,6,1,0]
+//    Output: 1
+// Explanation: 6 is the largest integer.
+// For every other number in the array x, 6 is at least twice as big as x.
+// The index of value 6 is 1, so we return 1.
+
+// Example 2:
+//    Input: nums = [1,2,3,4]
+//    Output: -1
+// Explanation: 4 is less than twice the value of 3, so we return -1
+
+// Example 3:
+//    Input: nums = [1]
+//    Output: 0
+// Explanation: 1 is trivially at least twice the value as any other number because there are no other numbers.
+
+// Constraints:
+//    1 <= nums.length <= 50
+//    0 <= nums[i] <= 100
+//    The largest element in nums is unique.
+
+const dominantIndex = function (nums) {
+  const filtered = nums.filter((x) => x > Math.max(...nums) / 2);
+  return filtered.length > 1 ? -1 : nums.indexOf(filtered[0]);
+};
+console.log(dominantIndex([3, 6, 1, 0])); // 1
+console.log(dominantIndex([1, 2, 3, 4])); // -1
+console.log(dominantIndex([1])); // 0
+
+// Great memory, not great runtime
+// Simple solution
+
+const topVotedDominantIndex = (nums) => {
+  let max = Math.max(...nums);
+  let indexMax = nums.indexOf(max);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] * 2 > max && nums[i] !== max) {
+      return -1;
+    }
+  }
+  return indexMax;
+};
