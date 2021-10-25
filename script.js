349082bee7f9ebb247dfe23fdb8413964bc428d7
MCCP96 <petermccomb@live.com>
2021-10-25 12:06:02 -0400

    Day 131 - Number of Lines To Write String

--- script.js
+++ script.js
@@ -6627,7 +6627,7 @@ console.log(rotateString("abcde", "abced")); // false
 // Turned top voted into a one-liner */
 
 // Unique Morse Code Words          10/24/2024
-
+/* 
 // International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
 
 // For convenience, the full table for the 26 letters of the English alphabet is given below:
@@ -6679,4 +6679,76 @@ console.log(uniqueMorseRepresentations(["a"])); // 1
 // Less memory usage than 98%, not greatest runtime
 
 // Similar to top voted except I save memory by using .charCodeAt
-// Solid solution
+// Solid solution */
+
+// Number of Lines To Write String          10/25/2021
+
+// You are given a string s of lowercase English letters and an array widths denoting how many pixels wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
+
+// You are trying to write s across several lines, where each line is no longer than 100 pixels. Starting at the beginning of s, write as many letters on the first line such that the total width does not exceed 100 pixels. Then, from where you stopped in s, continue writing as many letters as you can on the second line. Continue this process until you have written all of s.
+
+// Return an array result of length 2 where:
+
+// result[0] is the total number of lines.
+// result[1] is the width of the last line in pixels.
+
+// Example 1:
+//    Input: widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], s = "abcdefghijklmnopqrstuvwxyz"
+//    Output: [3,60]
+// Explanation: You can write s as follows:
+// abcdefghij  // 100 pixels wide
+// klmnopqrst  // 100 pixels wide
+// uvwxyz      // 60 pixels wide
+// There are a total of 3 lines, and the last line is 60 pixels wide.
+
+// Example 2:
+//    Input: widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], s = "bbbcccdddaaa"
+//    Output: [2,4]
+// Explanation: You can write s as follows:
+// bbbcccdddaa  // 98 pixels wide
+// a            // 4 pixels wide
+// There are a total of 2 lines, and the last line is 4 pixels wide.
+
+// Constraints:
+//    widths.length == 26
+//    2 <= widths[i] <= 10
+//    1 <= s.length <= 1000
+//    s contains only lowercase English letters.
+
+const numberOfLines = function (widths, s) {
+  let ans = [1, 0];
+  s = s.split(``);
+
+  let curWidth = 0;
+  for (let i = 0; i < s.length; i++) {
+    const charPixels = widths[s[i].charCodeAt(0) - 97];
+    if (curWidth + charPixels > 100) {
+      ans[0]++;
+      curWidth = charPixels;
+    } else curWidth += charPixels;
+  }
+  ans[1] = curWidth;
+
+  return ans;
+};
+// prettier-ignore
+console.log(numberOfLines([10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "abcdefghijklmnopqrstuvwxyz")); // [3,60]
+// prettier-ignore
+console.log(numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "bbbcccdddaaa")); // [2,4]
+
+// Second problem using .charCodAt in a row
+// Decent solution, better memory than most & average runtime
+
+const topVotedNumberOfLines = (widths, s) =>
+  [...s]
+    .map((c) => widths[c.charCodeAt(0) - 97])
+    .reduce(
+      (acc, curr) => [
+        acc[0] + (100 < acc[1] + curr ? 1 : 0),
+        100 < acc[1] + curr ? curr : acc[1] + curr,
+      ],
+      [s.length ? 1 : 0, 0]
+    );
+
+// Sheeeeesh, very clean
+// Same process but optimized to the absolute max
