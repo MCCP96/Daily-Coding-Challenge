7b8dc9eb3fcf94fe7459b201639cc1634d3f056c
MCCP96 <petermccomb@live.com>
2021-10-28 14:20:43 -0400

    Day 134 - Shortest Distance to a Character

--- script.js
+++ script.js
@@ -6807,7 +6807,7 @@ console.log(topVotedLargestTriangleArea([[1,0],[0,0],[0,1]])); // 0.5
 // Seems he just loops through all possibilities using Math.max to get the largest */
 
 // Most Common Word         10/27/2021
-
+/* 
 // Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
 
 // The words in paragraph are case-insensitive and the answer should be returned in lowercase.
@@ -6875,4 +6875,52 @@ const topVotedMostCommonWord = function (paragraph, banned) {
   return frequentWord;
 };
 
-// Same logic, but his actually passes all test cases
+// Same logic, but his actually passes all test cases */
+
+// Shortest Distance to a Character         10/28/2021
+
+// Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+// The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+
+// Example 1:
+//    Input: s = "loveleetcode", c = "e"
+//    Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+// Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
+// The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
+// The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 2.
+// For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
+// The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
+
+// Example 2:
+//    Input: s = "aaab", c = "b"
+//    Output: [3,2,1,0]
+
+// Constraints:
+//    1 <= s.length <= 104
+//    s[i] and c are lowercase English letters.
+//    It is guaranteed that c occurs at least once in s.
+
+const topVotedShortestToChar = function (s, c) {
+  const ans = Array(s.length).fill(Infinity);
+  let l = Infinity,
+    r = Infinity;
+
+  for (let f = 0; f < s.length; f++) {
+    const b = s.length - 1 - f;
+
+    l = s[f] === c ? 0 : l + 1;
+    r = s[b] === c ? 0 : r + 1;
+
+    ans[f] = Math.min(ans[f], l);
+    ans[b] = Math.min(ans[b], r);
+  }
+  return ans;
+};
+console.log(topVotedShortestToChar("loveleetcode", "e")); // [3,2,1,0,1,0,0,1,2,2,1,0]
+console.log(topVotedShortestToChar("aaab", "b")); // [3,2,1,0]
+
+// Ended up studying top voted
+
+// Iterates over the string from both left and right at same time
+// Adding the smallest distance to the ans array
