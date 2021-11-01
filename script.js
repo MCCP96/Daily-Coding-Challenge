8c61834498a94096b805fb4ab6f0dc9df1a26564
MCCP96 <petermccomb@live.com>
2021-11-01 11:44:38 -0400

    Day 138 - Rectangle Overlap

--- script.js
+++ script.js
@@ -7025,7 +7025,7 @@ console.log(largeGroupPositions("aba")); // []
 // This runtime was better than 100% of submissions! */
 
 // Flipping an Image          10/31/2021
-
+/* 
 // Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
 
 // To flip an image horizontally means that each row of the image is reversed.
@@ -7073,4 +7073,44 @@ const topVotedFlipAndInvertImage = function (image) {
 };
 
 // This is what I was shooting for
-// Very nice!
+// Very nice! */
+
+// Rectangle Overlap          11/1/2021
+
+// An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
+
+// Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+// Given two axis-aligned rectangles rec1 and rec2, return true if they overlap, otherwise return false.
+
+// Example 1:
+//    Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+//    Output: true
+
+// Example 2:
+//    Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+//    Output: false
+
+// Example 3:
+//    Input: rec1 = [0,0,1,1], rec2 = [2,2,3,3]
+//    Output: false
+
+// Constraints:
+//    rect1.length == 4
+//    rect2.length == 4
+//    -109 <= rec1[i], rec2[i] <= 109
+//    rec1 and rec2 represent a valid rectangle with a non-zero area.
+
+const isRectangleOverlap = function (r1, r2) {
+  return r2[0] >= r1[2] || r2[1] >= r1[3] || r2[2] <= r1[0] || r2[3] <= r1[1]
+    ? false
+    : true;
+};
+console.log(isRectangleOverlap([0, 0, 2, 2], [1, 1, 3, 3])); // true
+console.log(isRectangleOverlap([0, 0, 1, 1], [1, 0, 2, 1])); // false
+console.log(isRectangleOverlap([0, 0, 1, 1], [2, 2, 3, 3])); // false
+
+// Hmm, surprised this actually did the trick
+// Better runtime than 80% and very low memory usage
+
+// All other user posted solutions seem to be less clean equivalents of what I have here
