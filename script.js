8c63e2bd0baed49efd53c7f386175fd2abd4cb64
MCCP96 <petermccomb@live.com>
2021-11-07 11:13:47 -0500

    Day 144 - Fair Candy Swap

--- script.js
+++ script.js
@@ -7376,7 +7376,7 @@ const topVotedLemonadeChange = function (bills) {
 // I'm off to study chem, maybe this midterm is taking over my mental RAM... lol */
 
 // Uncommon Words from Two Sentences          11/6/2021
-
+/* 
 // A sentence is a string of single-space separated words where each word consists only of lowercase letters.
 
 // A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
@@ -7420,4 +7420,69 @@ const topVotedUncommonFromSentences = (a, b) =>
 // Same idea but with plenty of optimization
 // Very clean
 
-// Tested them both and mine returned much better runtime somehow
+// Tested them both and mine returned much better runtime somehow */
+
+// Fair Candy Swap          11/7/2021
+
+// Alice and Bob have a different total number of candies. You are given two integer arrays aliceSizes and bobSizes where aliceSizes[i] is the number of candies of the ith box of candy that Alice has and bobSizes[j] is the number of candies of the jth box of candy that Bob has.
+
+// Since they are friends, they would like to exchange one candy box each so that after the exchange, they both have the same total amount of candy. The total amount of candy a person has is the sum of the number of candies in each box they have.
+
+// Return an integer array answer where answer[0] is the number of candies in the box that Alice must exchange, and answer[1] is the number of candies in the box that Bob must exchange. If there are multiple answers, you may return any one of them. It is guaranteed that at least one answer exists.
+
+// Example 1:
+//    Input: aliceSizes = [1,1], bobSizes = [2,2]
+//    Output: [1,2]
+
+// Example 2:
+//    Input: aliceSizes = [1,2], bobSizes = [2,3]
+//    Output: [1,2]
+
+// Example 3:
+//    Input: aliceSizes = [2], bobSizes = [1,3]
+//    Output: [2,3]
+
+// Example 4:
+//    Input: aliceSizes = [1,2,5], bobSizes = [2,4]
+//    Output: [5,4]
+
+// Constraints:
+//    1 <= aliceSizes.length, bobSizes.length <= 104
+//    1 <= aliceSizes[i], bobSizes[j] <= 105
+//    Alice and Bob have a different total number of candies.
+//    There will be at least one valid answer for the given input.
+
+const fairCandySwap = function (a, b) {
+  const count = (x) => x.reduce((acc, cur) => (acc += cur));
+
+  let dif = (count(a) - count(b)) / 2;
+
+  for (let size of b) {
+    if (a.includes(size + dif)) return [size + dif, size];
+  }
+};
+console.log(fairCandySwap([1, 1], [2, 2])); // [1,2]
+console.log(fairCandySwap([1, 2], [2, 3])); // [1,2]
+console.log(fairCandySwap([2], [1, 3])); // [2,3]
+console.log(fairCandySwap([1, 2, 5], [2, 4])); // [5,4]
+
+// OK
+
+const topVotedFairCandySwap = function (A, B) {
+  const sumA = A.reduce((acc, cur) => acc + cur);
+  const sumB = B.reduce((acc, cur) => acc + cur);
+  const diff = (sumA - sumB) >> 1;
+  const setA = new Set(A);
+  for (const candy of B) {
+    if (setA.has(candy + diff)) return [candy + diff, candy];
+  }
+};
+
+// Top voted has same idea
+// I believe using the Set's '.has' improves runtime over Array's '.includes'
+// Not familiar with '>>' used to obtain 'diff'
+
+// 'Right shift (>>)'
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Right_shift
+
+// Weird, I prefer my count(a) - count(b)) / 2
