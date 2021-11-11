6283b48d778c9f6eba6bfb400ee2ae946ccd6975
MCCP96 <petermccomb@live.com>
2021-11-11 11:18:14 -0500

    Day 148 - Smallest Range I

--- script.js
+++ script.js
@@ -7611,7 +7611,7 @@ const topVotedIsMonotonic = function (A) {
 // It's probably due to my breaks and the fact that I return half-way if increasing is true */
 
 // Sort Array By Parity         11/10/2021
-
+/* 
 //  Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
 
 // Return any array that satisfies this condition.
@@ -7644,4 +7644,47 @@ const topVotedSortArrayByParity = function (A) {
 };
 
 // Thought about this, but imagined it would have bad runtime given double .filter use
-// Better runtime than 74%, great memory
+// Better runtime than 74%, great memory */
+
+// Smallest Range I         11/11/2021
+
+// You are given an integer array nums and an integer k.
+
+// In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+
+// The score of nums is the difference between the maximum and minimum elements in nums.
+
+// Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+
+// Example 1:
+//    Input: nums = [1], k = 0
+//    Output: 0
+// Explanation: The score is max(nums) - min(nums) = 1 - 1 = 0.
+
+// Example 2:
+//    Input: nums = [0,10], k = 2
+//    Output: 6
+// Explanation: Change nums to be [2, 8]. The score is max(nums) - min(nums) = 8 - 2 = 6.
+
+// Example 3:
+//    Input: nums = [1,3,6], k = 3
+//    Output: 0
+// Explanation: Change nums to be [4, 4, 4]. The score is max(nums) - min(nums) = 4 - 4 = 0.
+
+// Constraints:
+//    1 <= nums.length <= 104
+//    0 <= nums[i] <= 104
+//    0 <= k <= 104
+
+const smallestRangeI = function (nums, k) {
+  let range = Math.max(...nums) - Math.min(...nums);
+  return range > 2 * k ? range - 2 * k : 0;
+};
+console.log(smallestRangeI([1], 0)); // 0
+console.log(smallestRangeI([0, 10], 2)); // 6
+console.log(smallestRangeI([1, 3, 6], 3)); // 0
+
+// Struggled with prompt at first, but ended up being pretty simple
+// Better runtime than 90%
+
+// Top voteds are all similar variations of this same logic
