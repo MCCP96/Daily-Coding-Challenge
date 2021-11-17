27b4c852a5f6943e2c6d6dcaacea19808e505b5a
MCCP96 <petermccomb@live.com>
2021-11-17 12:57:57 -0500

    Day 154 - Number of Recent Calls

--- script.js
+++ script.js
@@ -7865,7 +7865,7 @@ console.log(topVotedIsLongPressedName("laiden", "laiden")); // true
 // Very clean */
 
 // Unique Email Addresses         11/16/2021
-
+/* 
 // Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
 
 // For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
@@ -7923,4 +7923,53 @@ var topVotedNumUniqueEmails = function (emails) {
 };
 
 // This is what I was shooting for when writing my code
-// Small optimizations everywhere leads to slightly faster code
+// Small optimizations everywhere leads to slightly faster code */
+
+// Number of Recent Calls         11/17/2021
+
+// You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+// Implement the RecentCounter class:
+
+// RecentCounter() Initializes the counter with zero recent requests.
+// int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+// It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+
+// Example 1:
+//    Input
+//      ["RecentCounter", "ping", "ping", "ping", "ping"]
+//      [[], [1], [100], [3001], [3002]]
+//    Output
+//      [null, 1, 2, 3, 3]
+
+// Explanation
+// RecentCounter recentCounter = new RecentCounter();
+// recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
+// recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
+// recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
+// recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+
+// Constraints:
+//    1 <= t <= 109
+//    Each test case will call ping with strictly increasing values of t.
+//    At most 104 calls will be made to ping.
+
+var RecentCounter = function () {
+  this.oldCalls = [];
+};
+
+RecentCounter.prototype.ping = function (t) {
+  this.oldCalls.push(t);
+  while (this.oldCalls[0] < t - 3000) this.oldCalls.shift();
+  return this.oldCalls.length;
+};
+
+var obj = new RecentCounter();
+console.log(obj.ping([])); // null
+console.log(obj.ping([1])); // 1
+console.log(obj.ping([100])); // 2
+console.log(obj.ping([3001])); // 3
+console.log(obj.ping([3002])); // 3
+
+// This is the top voted solution
+// Not too sure how to deal with these yet
