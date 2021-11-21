9508e456c4bbfaa6b2be9353e28d19cda7fe98f5
MCCP96 <petermccomb@live.com>
2021-11-21 12:32:45 -0500

    Day 158 - Delete Columns to Make Sorted

--- script.js
+++ script.js
@@ -8101,7 +8101,7 @@ const topVotedValidMountainArray = function (A) {
 // I think most of my performance comes from the list of guard clauses I made */
 
 // DI String Match          11/20/2021
-
+/* 
 // A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
 
 // s[i] == 'I' if perm[i] < perm[i + 1], and
@@ -8154,4 +8154,67 @@ console.log(diStringMatch(`III`)); // [0,1,2,3]
 console.log(diStringMatch(`DDI`)); // [3,2,0,1]
 
 // Came up with this
-// Same runtime, but much better memory
+// Same runtime, but much better memory */
+
+// Delete Columns to Make Sorted          11/21/2021
+
+// You are given an array of n strings strs, all of the same length.
+
+// The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
+
+// abc
+// bce
+// cae
+// You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+// Return the number of columns that you will delete.
+
+// Example 1:
+//    Input: strs = ["cba","daf","ghi"]
+//    Output: 1
+// Explanation: The grid looks as follows:
+//   cba
+//   daf
+//   ghi
+// Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
+
+// Example 2:
+//    Input: strs = ["a","b"]
+//    Output: 0
+// Explanation: The grid looks as follows:
+//   a
+//   b
+// Column 0 is the only column and is sorted, so you will not delete any columns.
+
+// Example 3:
+//    Input: strs = ["zyx","wvu","tsr"]
+//    Output: 3
+// Explanation: The grid looks as follows:
+//   zyx
+//   wvu
+//   tsr
+// All 3 columns are not sorted, so you will delete all 3.
+
+// Constraints:
+//    n == strs.length
+//    1 <= n <= 100
+//    1 <= strs[i].length <= 1000
+//    strs[i] consists of lowercase English letters.
+
+const minDeletionSize = function (strs) {
+  let ans = 0;
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] < strs[j - 1][i]) {
+        ans++;
+        break;
+      }
+    }
+  }
+  return ans;
+};
+console.log(minDeletionSize(["cba", "daf", "ghi"])); // 1
+console.log(minDeletionSize(["a", "b"])); // 0
+console.log(minDeletionSize(["zyx", "wvu", "tsr"])); // 3
+
+// Matched top voted
