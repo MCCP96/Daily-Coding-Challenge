be1c61376f266c4e1e57fd0db75809f93dc4cf4b
MCCP96 <petermccomb@live.com>
2021-11-25 12:54:34 -0500

    Day 162 - Squares of a Sorted Array

--- script.js
+++ script.js
@@ -8319,7 +8319,7 @@ console.log(topVotedRepeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4])); // 5
 // I'm a fan of the one-liner, but worst performance than mine */
 
 // Largest Perimeter Triangle         11/24/2021
-
+/* 
 // Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
 
 // Constraints:
@@ -8343,4 +8343,33 @@ console.log(topVotedLargestPerimeter([3, 6, 2, 3])); // 8
 // Added the console.log line to help visualize a little
 // Makes sense
 
-// Terrible runtime, decent memory
+// Terrible runtime, decent memory */
+
+// Squares of a Sorted Array          11/25/2021
+
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+// Example 1:
+//    Input: nums = [-4,-1,0,3,10]
+//    Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+
+// Example 2:
+//    Input: nums = [-7,-3,2,3,11]
+//    Output: [4,9,9,49,121]
+
+// Constraints:
+//    1 <= nums.length <= 104
+//    -104 <= nums[i] <= 104
+//    nums is sorted in non-decreasing order.
+
+const sortedSquares = (nums) =>
+  nums.map((cur) => Math.pow(cur, 2)).sort((a, b) => a - b);
+console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0,1,9,16,100]
+console.log(sortedSquares([-7, -3, 2, 3, 11])); // [4,9,9,49,121]
+
+// Clean one-liner
+
+// Tested with both cur*cur and Math.pow(cur,2)
+// Math.pow gave better runtime but poorer memory
