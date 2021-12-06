3446d543dd46345f16c8818b8527e61d3a0960e1
MCCP96 <petermccomb@live.com>
2021-12-06 14:07:40 -0500

    Day 173 - Last Stone Weight

--- script.js
+++ script.js
@@ -8882,7 +8882,7 @@ console.log(divisorGame(3)); // false
 // Better runtime than 90% */
 
 // Valid Boomerang          12/5/2021
-
+/* 
 // Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
 
 // A boomerang is a set of three points that are all distinct and not in a straight line.
@@ -8897,4 +8897,39 @@ var topVotedIsBoomerang = ([[ax, ay], [bx, by], [cx, cy]]) =>
 // prettier-ignore
 console.log(topVotedIsBoomerang([[1,1],[2,3],[3,2]]) ); // true
 // prettier-ignore
-console.log(topVotedIsBoomerang([[1,1],[2,2],[3,3]])); // false
+console.log(topVotedIsBoomerang([[1,1],[2,2],[3,3]])); // false */
+
+// Last Stone Weight          12/6/2021
+
+//  You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+// We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+// If x == y, both stones are destroyed, and
+// If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+// At the end of the game, there is at most one stone left.
+
+// Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+
+// Example 1:
+//    Input: stones = [2,7,4,1,8,1]
+//    Output: 1
+// Explanation:
+//    We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+//    we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+//    we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+//    we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+// Constraints:
+//    1 <= stones.length <= 30
+//    1 <= stones[i] <= 1000
+
+const lastStoneWeight = function (s) {
+  if (1 === s.length) return s[0];
+  return lastStoneWeight(s.sort((a, b) => a - b).concat(s.pop() - s.pop()));
+};
+console.log(lastStoneWeight([2, 7, 4, 1, 8, 1])); // 1
+console.log(lastStoneWeight([1])); // 1
+
+// Inspired by top voted
+// Clever to call back function to sort after every substraction
