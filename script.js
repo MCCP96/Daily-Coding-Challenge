8f2e1baa79b50dc51c73ffe8fc44f09003d20cbe
MCCP96 <petermccomb@live.com>
2021-12-09 16:24:08 -0500

    Day 176 - Greatest Common Divisor of Strings

--- script.js
+++ script.js
@@ -8978,7 +8978,7 @@ var topVotedRemoveDuplicates = function (S) {
 // I prefer my solution, but it unfortunately doesn't work with leetcode */
 
 // Height Checker         12/8/2021
-
+/* 
 // A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
 
 // You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
@@ -9026,4 +9026,41 @@ console.log(heightChecker([1, 2, 3, 4, 5])); // 0
 
 // Terrible runtime
 
-// No time today, finals season!
+// No time today, finals season! */
+
+// Greatest Common Divisor of Strings         12/9/2021
+
+// For two strings s and t, we say "t divides s" if and only if s = t + ... + t  (t concatenated with itself 1 or more times)
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+// Example 1:
+//    Input: str1 = "ABCABC", str2 = "ABC"
+//    Output: "ABC"
+
+// Example 2:
+//    Input: str1 = "ABABAB", str2 = "ABAB"
+//    Output: "AB"
+
+// Example 3:
+//    Input: str1 = "LEET", str2 = "CODE"
+//    Output: ""
+
+// Example 4:
+//    Input: str1 = "ABCDEF", str2 = "ABC"
+//    Output: ""
+
+// Constraints:
+//    1 <= str1.length <= 1000
+//    1 <= str2.length <= 1000
+//    str1 and str2 consist of English uppercase letters.
+
+const gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) return "";
+  const gcd = (a, b) => (0 === b ? a : gcd(b, a % b));
+  return str1.substring(0, gcd(str1.length, str2.length));
+};
+console.log(gcdOfStrings("ABCABC", "ABC")); // "ABC"
+console.log(gcdOfStrings("ABABAB", "ABAB")); // "AB"
+console.log(gcdOfStrings("LEET", "CODE")); // ""
+console.log(gcdOfStrings("ABCDEF", "ABC")); // ""
