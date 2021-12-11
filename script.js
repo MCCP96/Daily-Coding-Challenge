164261f41fe1a74b51be01f8847eb0bf64ab16e0
MCCP96 <petermccomb@live.com>
2021-12-11 12:28:53 -0500

    Day 178 - Duplicate Zeros

--- script.js
+++ script.js
@@ -9066,7 +9066,7 @@ console.log(gcdOfStrings("LEET", "CODE")); // ""
 console.log(gcdOfStrings("ABCDEF", "ABC")); // "" */
 
 // Occurrences After Bigram         12/10/2021
-
+/* 
 // Given two strings first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
 
 // Return an array of all the words third for each occurrence of "first second third".
@@ -9097,4 +9097,29 @@ console.log(
 console.log(findOcurrences("we will we will rock you", "we", "will")); // ["we","rock"]
 
 // Better runtime than 85%, decent memory
+// Same as top voted */
+
+// Duplicate Zeros          12/11/2021
+
+// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+// Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+
+// Constraints:
+//    1 <= arr.length <= 104
+//    0 <= arr[i] <= 9
+
+const duplicateZeros = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+      arr.splice(i, 0, 0);
+      arr.pop();
+      i++;
+    }
+  }
+};
+console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0])); // [1,0,0,2,3,0,0,4]
+console.log(duplicateZeros([1, 2, 3])); // [1,2,3]
+
+// Ok runtime, great memory
 // Same as top voted
