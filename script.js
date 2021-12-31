9ad82557a98d924c1207c0227e13bab1c50ab6ae
MCCP96 <petermccomb@live.com>
2021-12-31 13:40:44 -0500

    Day 198 - Find Winner on a Tic Tac Toe Game

--- script.js
+++ script.js
@@ -9906,7 +9906,7 @@ var topVotedShiftGrid = function (grid, k) {
 // So much cleaner... I love it */
 
 // Minimum Time Visiting All Points         12/30/2021
-
+/* 
 // On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
 
 // You can move according to these rules:
@@ -9944,4 +9944,116 @@ const topVotedMinTimeToVisitAllPoints = (points) => {
 // prettier-ignore
 console.log(topVotedMinTimeToVisitAllPoints([[1,1],[3,4],[-1,0]])); // 7
 // prettier-ignore
-console.log(topVotedMinTimeToVisitAllPoints([[3,2],[-2,2]])); // 5
+console.log(topVotedMinTimeToVisitAllPoints([[3,2],[-2,2]])); // 5 */
+
+// Find Winner on a Tic Tac Toe Game          12/31/2021
+
+// Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules of Tic-Tac-Toe are:
+
+// Players take turns placing characters into empty squares ' '.
+// The first player A always places 'X' characters, while the second player B always places 'O' characters.
+// 'X' and 'O' characters are always placed into empty squares, never on filled ones.
+// The game ends when there are three of the same (non-empty) character filling any row, column, or diagonal.
+// The game also ends if all squares are non-empty.
+// No more moves can be played if the game is over.
+// Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that the ith move will be played on grid[rowi][coli]. return the winner of the game if it exists (A or B). In case the game ends in a draw return "Draw". If there are still movements to play return "Pending".
+
+// You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), the grid is initially empty, and A will play first.
+
+// Example 1:
+//    Input: moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
+//    Output: "A"
+// Explanation: A wins, they always play first.
+
+// Example 2:
+//    Input: moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
+//    Output: "B"
+// Explanation: B wins.
+
+// Example 3:
+//    Input: moves = [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]]
+//    Output: "Draw"
+// Explanation: The game ends in a draw since there are no moves to make.
+
+// Constraints:
+//    1 <= moves.length <= 9
+//    moves[i].length == 2
+//    0 <= rowi, coli <= 2
+//    There are no repeated elements on moves.
+//    moves follow the rules of tic tac toe.
+
+const tictactoe = function (moves) {
+  let f = [
+    [` `, ` `, ` `],
+    [` `, ` `, ` `],
+    [` `, ` `, ` `],
+  ];
+  let curPlayer = `A`;
+  for (let [row, col] of moves) {
+    f[row][col] = curPlayer;
+    curPlayer = curPlayer == `A` ? `B` : `A`;
+  }
+
+  if (
+    (f[0][0] === f[1][1] && f[1][1] === f[2][2] && f[1][1] !== " ") ||
+    (f[2][0] === f[1][1] && f[1][1] === f[0][2] && f[1][1] !== " ")
+  )
+    return f[1][1];
+
+  if (f[0][0] === f[0][1] && f[0][1] === f[0][2] && f[0][0] !== " ")
+    return f[0][0];
+  if (f[1][0] === f[1][1] && f[1][1] === f[1][2] && f[1][0] !== " ")
+    return f[1][0];
+  if (f[2][0] === f[2][1] && f[2][1] === f[2][2] && f[2][0] !== " ")
+    return f[2][0];
+
+  if (f[0][0] === f[1][0] && f[1][0] === f[2][0] && f[0][0] !== " ")
+    return f[0][0];
+  if (f[0][1] === f[1][1] && f[1][1] === f[2][1] && f[0][1] !== " ")
+    return f[0][1];
+  if (f[0][2] === f[1][2] && f[1][2] === f[2][2] && f[0][2] !== " ")
+    return f[0][2];
+
+  return f.find((item) => item.includes(" ")) ? "Pending" : "Draw";
+};
+// prettier-ignore
+console.log(tictactoe([[0,0],[2,0],[1,1],[2,1],[2,2]])); // A
+// prettier-ignore
+console.log(tictactoe([[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]])); // B
+// prettier-ignore
+console.log(tictactoe([[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]])); // Draw
+
+// Terrible runtime but amazing memory
+
+// Got the first half here, but didn't want to grind out all the possible win cases
+// Ended up copying them from another submission
+
+const topVotedTictactoe = (moves) => {
+  const cases = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const grid = new Uint8Array(9);
+  for (let i = 0; i < moves.length; ++i) {
+    grid[moves[i][0] * 3 + moves[i][1]] = (i % 2) + 1;
+  }
+  for (let i = 0; i < cases.length; ++i) {
+    const role = grid[cases[i][0]];
+    if (
+      role !== 0 &&
+      grid[cases[i][1]] === role &&
+      grid[cases[i][2]] === role
+    ) {
+      return role === 1 ? "A" : "B";
+    }
+  }
+  return moves.length === 9 ? "Draw" : "Pending";
+};
+
+// This seems to be the standard accepted solution, but it has equally terrible runtime
