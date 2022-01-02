18bfce1d3a7a6678bb0978948920a7da179483d9
MCCP96 <petermccomb@live.com>
2022-01-02 13:27:42 -0500

    Day 200 - Element Appearing More Than 25% In Arr

--- 2022.js
+++ 2022.js
@@ -1,5 +1,5 @@
 // Subtract the Product and Sum of Digits of an Integer         1/1/2022
-
+/* 
 // Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 
 // Example 1:
@@ -36,4 +36,34 @@ var topVotedSubtractProductAndSum = function (n) {
   return product - sum;
 };
 
-// Same idea, cleaner code
+// Same idea, cleaner code */
+
+// Element Appearing More Than 25% In Sorted Array          1/2/2021
+
+// Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+// Constraints:
+//    1 <= arr.length <= 104
+//    0 <= arr[i] <= 105
+
+const findSpecialInteger = function (arr) {
+  let count = {};
+  for (let n of arr) count[n] ? count[n]++ : (count[n] = 1);
+  let max = 0;
+  for (let n in count) if (count[n] > count[max]) max = n;
+  return max;
+};
+console.log(findSpecialInteger([1, 2, 2, 6, 6, 6, 6, 7, 10])); // 6
+console.log(findSpecialInteger([1, 1])); // 1
+
+// Not a fan of my solution
+
+var topVotedFindSpecialInteger = function (arr) {
+  const ws = Math.floor(arr.length / 4);
+  for (let i = 0; i < arr.length - ws; i++)
+    if (arr[i] === arr[i + ws]) return arr[i];
+  return -1;
+};
+
+// Much better
+// Great memory, not great runtime
