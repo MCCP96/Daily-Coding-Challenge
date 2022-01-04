5fce96faa4a448969a22d5d2a1ee6b653bfad338
MCCP96 <petermccomb@live.com>
2022-01-04 09:24:30 -0500

    Day 202 - Replace Elements with Greatest Element

--- 2022.js
+++ 2022.js
@@ -69,7 +69,7 @@ var topVotedFindSpecialInteger = function (arr) {
 // Great memory, not great runtime */
 
 // Find Numbers with Even Number of Digits          1/3/2021
-
+/* 
 // Given an array nums of integers, return how many of them contain an even number of digits.
 
 // Example 1:
@@ -96,4 +96,53 @@ console.log(findNumbers([12, 345, 2, 6, 7896])); // 2
 console.log(findNumbers([555, 901, 482, 1771])); // 1
 
 // Decent runtime & memory
-// Definitely a one-liner possible here
+// Definitely a one-liner possible here */
+
+// Replace Elements with Greatest Element on Right Side         1/4/2021
+
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+// Example 1:
+//    Input: arr = [17,18,5,4,6,1]
+//    Output: [18,6,6,6,1,-1]
+// Explanation:
+//    - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+//    - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+//    - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+//    - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+//    - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+//    - index 5 --> there are no elements to the right of index 5, so we put -1.
+
+// Example 2:
+//    Input: arr = [400]
+//    Output: [-1]
+// Explanation: There are no elements to the right of index 0.
+
+// Constraints:
+//    1 <= arr.length <= 104
+//    1 <= arr[i] <= 105
+
+const replaceElements = (arr) =>
+  arr.map((_, i, arr) =>
+    i == arr.length - 1 ? -1 : Math.max(...arr.slice(i + 1))
+  );
+console.log(replaceElements([17, 18, 5, 4, 6, 1])); // [18,6,6,6,1,-1]
+console.log(replaceElements([400])); // [-1]
+
+// Clean code
+// Not the greatest runtime nor memory
+
+const topVotedReplaceElements = (arr) => {
+  const result = new Array(arr.length);
+  result[arr.length - 1] = -1;
+
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    result[i - 1] = Math.max(arr[i], result[i]);
+  }
+
+  return result;
+};
+
+// Bit better runtime/memory
