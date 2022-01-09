35c7c1855aa932d20632f51fffeee18a83f6b9c2
MCCP96 <petermccomb@live.com>
2022-01-09 10:48:30 -0500

    Day 207 - Maximum 69 Number

--- 2022.js
+++ 2022.js
@@ -252,7 +252,7 @@ var topVotedDecompressRLElist = (nums) =>
 // Much worst runtime & memory */
 
 // Convert Integer to the Sum of Two No-Zero Integers         1/8/2022
-
+/* 
 // No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
 
 // Given an integer n, return a list of two integers [A, B] where:
@@ -283,4 +283,34 @@ var topVotedGetNoZeroIntegers = function (n) {
   return [];
 };
 
-// 'does not contain any 0 in its decimal representation', I see.
+// 'does not contain any 0 in its decimal representation', I see. */
+
+// Maximum 69 Number          1/9/2022
+
+// You are given a positive integer num consisting only of digits 6 and 9.
+
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+// Example 1:
+//    Input: num = 9669
+//    Output: 9969
+// Explanation:
+//    Changing the first digit results in 6669.
+//    Changing the second digit results in 9969.
+//    Changing the third digit results in 9699.
+//    Changing the fourth digit results in 9666.
+// The maximum number is 9969.
+
+// Constraints:
+//    1 <= num <= 104
+//    num consists of only 6 and 9 digits.
+
+const maximum69Number = (n) => n.toString(10).replace(6, 9);
+console.log(maximum69Number(9669)); // 9969
+console.log(maximum69Number(9996)); // 9999
+console.log(maximum69Number(9999)); // 9999
+
+// Perfect
+// Better runtime than 99.5%, top 50% memory
+
+// Identical to top voted
