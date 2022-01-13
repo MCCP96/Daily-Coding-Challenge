1ed5cc3e0ea98d7330fd94363afa49faf2b0e9d8
MCCP96 <petermccomb@live.com>
2022-01-13 10:37:41 -0500

    Day 211 - Number of Steps to Reduce a Number to 0

--- 2022.js
+++ 2022.js
@@ -406,7 +406,7 @@ const topVotedRemovePalindromeSub = (s) =>
 // Also not the greatest runtime */
 
 // The K Weakest Rows in a Matrix         1/12/2022
-
+/* 
 // You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
 
 // A row i is weaker than a row j if one of the following is true:
@@ -497,4 +497,43 @@ var topVotedKWeakestRows = function (mat, k) {
   //slice the result according to k number
 };
 
-// Nice!
+// Nice! */
+
+// Number of Steps to Reduce a Number to Zero         1/13/2022
+
+// Given an integer num, return the number of steps to reduce it to zero.
+
+// In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+// Example 1:
+//    Input: num = 14
+//    Output: 6
+// Explanation:
+//    Step 1) 14 is even; divide by 2 and obtain 7.
+//    Step 2) 7 is odd; subtract 1 and obtain 6.
+//    Step 3) 6 is even; divide by 2 and obtain 3.
+//    Step 4) 3 is odd; subtract 1 and obtain 2.
+//    Step 5) 2 is even; divide by 2 and obtain 1.
+//    Step 6) 1 is odd; subtract 1 and obtain 0.
+
+// Constraints:
+//    0 <= num <= 106
+
+const numberOfSteps = function (num) {
+  let count = 0;
+  while (num > 0) {
+    num % 2 ? num-- : (num = Math.abs(num / 2));
+    count++;
+  }
+  return count;
+};
+console.log(numberOfSteps(14)); // 6
+console.log(numberOfSteps(8)); // 4
+console.log(numberOfSteps(123)); // 12
+
+// Pretty ok, very basic
+
+const topVotedNumberOfSteps = (num, steps = 0) =>
+  num === 0 ? steps : numberOfSteps(num & 1 ? num - 1 : num >> 1, ++steps);
+
+// Clever
