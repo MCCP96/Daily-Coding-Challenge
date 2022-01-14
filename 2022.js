4d0205c7f45c33818a0a92db4d1c1e3160a2bfbc
MCCP96 <petermccomb@live.com>
2022-01-14 10:35:36 -0500

    Day 212 - Check If N and Its Double Exist

--- 2022.js
+++ 2022.js
@@ -500,7 +500,7 @@ var topVotedKWeakestRows = function (mat, k) {
 // Nice! */
 
 // Number of Steps to Reduce a Number to Zero         1/13/2022
-
+/* 
 // Given an integer num, return the number of steps to reduce it to zero.
 
 // In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
@@ -536,4 +536,76 @@ console.log(numberOfSteps(123)); // 12
 const topVotedNumberOfSteps = (num, steps = 0) =>
   num === 0 ? steps : numberOfSteps(num & 1 ? num - 1 : num >> 1, ++steps);
 
-// Clever
+// Clever */
+
+// Check If N and Its Double Exist          1/14/2021
+
+// Given an array arr of integers, check if there exists two integers N and M such that N is the double of M ( i.e. N = 2 * M).
+
+// More formally check if there exists two indices i and j such that :
+
+// i != j
+// 0 <= i, j < arr.length
+// arr[i] == 2 * arr[j]
+
+// Example 1:
+//    Input: arr = [10,2,5,3]
+//    Output: true
+// Explanation: N = 10 is the double of M = 5,that is, 10 = 2 * 5.
+
+// Example 2:
+//    Input: arr = [7,1,14,11]
+//    Output: true
+// Explanation: N = 14 is the double of M = 7,that is, 14 = 2 * 7.
+
+// Example 3:
+//    Input: arr = [3,1,7,11]
+//    Output: false
+// Explanation: In this case does not exist N and M, such that N = 2 * M.
+
+// Constraints:
+//    2 <= arr.length <= 500
+//    -10^3 <= arr[i] <= 10^3
+
+const checkIfExist = function (arr) {
+  for (let i = 0; i < arr.length; i++)
+    if (
+      arr.filter((x) => x != 0 && (x == arr[i] / 2 || x == arr[i] * 2))
+        .length >= 1
+    )
+      return true;
+
+  return false;
+};
+console.log(checkIfExist([10, 2, 5, 3])); // true
+console.log(checkIfExist([7, 1, 14, 11])); // true
+console.log(checkIfExist([3, 1, 7, 11])); // false
+console.log(checkIfExist([-2, 0, 10, -19, 4, 6, -8])); // false
+
+// Zeros causing trouble
+
+var topVotedCheckIfExist = function (A) {
+  let set = new Set();
+
+  for (let e of A) {
+    if (set.has(e * 2) || set.has(e / 2)) return true;
+    set.add(e);
+  }
+
+  return false;
+};
+// Set solution
+
+var topVoted2CheckIfExist = function (A) {
+  let map = new Map();
+
+  for (let e of A) {
+    if (map.has(e * 2) || map.has(e / 2)) return true;
+    map.set(e, true);
+  }
+
+  return false;
+};
+// Map solution
+
+// I'm a fan of the set solution, very simple and straightforward
