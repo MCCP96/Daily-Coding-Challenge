c80ab0193f8c1078366f6cb85ede7fdaaec6397f
MCCP96 <petermccomb@live.com>
2022-01-18 12:29:54 -0500

    Day 216 - How Many Nums Are Smaller Than Cur

--- 2022.js
+++ 2022.js
@@ -701,7 +701,7 @@ const newBitCount = (num) => num.toString(2).replaceAll(0, "").length;
 // A little mix of both solutions */
 
 // Number of Days Between Two Dates         1/17/2022
-
+/* 
 // Write a program to count the number of days between two dates.
 
 // The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
@@ -721,4 +721,39 @@ console.log(daysBetweenDates("2019-06-29", "2019-06-30")); // 1
 console.log(daysBetweenDates("2020-01-15", "2019-12-31")); // 15
 
 // Better runtime than 90%
-// Same as top voted
+// Same as top voted */
+
+// How Many Numbers Are Smaller Than the Current Number         1/18/2022
+
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+// Example 1:
+//    Input: nums = [8,1,2,2,3]
+//    Output: [4,0,1,1,3]
+// Explanation:
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1).
+// For nums[3]=2 there exist one smaller number than it (1).
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+// Constraints:
+//    2 <= nums.length <= 500
+//    0 <= nums[i] <= 100
+
+const smallerNumbersThanCurrent = (nums) =>
+  nums.map((x, _, arr) => arr.reduce((acc, cur) => (cur < x ? ++acc : acc), 0));
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3])); // [4,0,1,1,3]
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8])); // [2,1,0,3]
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7])); // [0,0,0,0]
+
+// Decent one-liner, good runtime
+
+var topVotedSmallerNumbersThanCurrent = function (nums) {
+  const sorted = [...nums].sort((a, b) => a - b);
+  return nums.map((num) => sorted.indexOf(num));
+};
+
+// Clever, but much worse runtime
