ac723efd93e00bfd0b1a2e2b6559e4352c544a08
MCCP96 <petermccomb@live.com>
2022-01-30 10:40:52 -0500

    Day 228 - Maximum Score After Splitting a String

--- 2022.js
+++ 2022.js
@@ -1305,7 +1305,7 @@ oneLinerMinStartValue = (A) =>
 // 👀 */
 
 // Reformat The String          1/29/2022
-
+/* 
 // You are given an alphanumeric string s. (Alphanumeric string is a string consisting of lowercase English letters and digits).
 
 // You have to find a permutation of the string where no letter is followed by another letter and no digit is followed by another digit. That is, no two adjacent characters have the same type.
@@ -1361,4 +1361,62 @@ var topVotedReformat = function (s) {
     : "";
 };
 
-// I like the use of regex to get a and b right off the bat
+// I like the use of regex to get a and b right off the bat */
+
+// Maximum Score After Splitting a String         1/30/2022
+
+// Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+// The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+// Example 1:
+//    Input: s = "011101"
+//    Output: 5
+// Explanation:
+//    All possible ways of splitting s into two non-empty substrings are:
+//    left = "0" and right = "11101", score = 1 + 4 = 5
+//    left = "01" and right = "1101", score = 1 + 3 = 4
+//    left = "011" and right = "101", score = 1 + 2 = 3
+//    left = "0111" and right = "01", score = 1 + 1 = 2
+//    left = "01110" and right = "1", score = 2 + 1 = 3
+
+// Example 2:
+//    Input: s = "00111"
+//    Output: 5
+// Explanation: When left = "00" and right = "111", we get the maximum score = 2 + 3 = 5
+
+// Constraints:
+//    2 <= s.length <= 500
+//    The string s consists of characters '0' and '1' only.
+
+const maxScore = function (s) {
+  let max = 0;
+  for (let i = 1; i < s.length; i++) {
+    max = Math.max(
+      max,
+      s.substring(0, i).replaceAll(1, "").length +
+        s.substring(i).replaceAll(0, "").length
+    );
+  }
+  return max;
+};
+console.log(maxScore("011101")); // 5
+console.log(maxScore("00111")); // 5
+console.log(maxScore("1111")); // 3
+console.log(maxScore("00")); // 1
+
+// Ok, not a fan of my substring.replaceAll approach
+
+var topVotedMaxScore = function (s) {
+  let max = 0;
+  for (let i = 1; i < s.length; i++) {
+    const left = s.slice(0, i).split("0").length - 1;
+    const right = s.slice(i).split("1").length - 1;
+    let currentSum = left + right;
+    max = Math.max(max, currentSum);
+  }
+  return max;
+};
+
+// Most top voted solutions look like this
+// Very similar
