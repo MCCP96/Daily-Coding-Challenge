8c6e3eeca1bc8f1a9394009589ca08038ea1d075
MCCP96 <petermccomb@live.com>
2022-01-31 10:15:36 -0500

    Day 229 - Kids With the Greatest Number of Candies

--- 2022.js
+++ 2022.js
@@ -1364,7 +1364,7 @@ var topVotedReformat = function (s) {
 // I like the use of regex to get a and b right off the bat */
 
 // Maximum Score After Splitting a String         1/30/2022
-
+/* 
 // Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
 
 // The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
@@ -1419,4 +1419,45 @@ var topVotedMaxScore = function (s) {
 };
 
 // Most top voted solutions look like this
-// Very similar
+// Very similar */
+
+// Kids With the Greatest Number of Candies         1/31/2022
+
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
+
+// Example 1:
+//    Input: candies = [2,3,5,1,3], extraCandies = 3
+//    Output: [true,true,true,false,true]
+// Explanation: If you give all extraCandies to:
+//    - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+//    - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+//    - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+//    - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+//    - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+// Example 2:
+//    Input: candies = [4,2,1,1,2], extraCandies = 1
+//    Output: [true,false,false,false,false]
+// Explanation: There is only 1 extra candy.
+// Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+
+// Constraints:
+//    n == candies.length
+//    2 <= n <= 100
+//    1 <= candies[i] <= 100
+//    1 <= extraCandies <= 50
+
+const kidsWithCandies = (candies, extra) => {
+  let max = Math.max(...candies);
+  return candies.map((cur) => cur + extra >= max);
+};
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3)); // [true,true,true,false,true]
+console.log(kidsWithCandies([4, 2, 1, 1, 2], 1)); // [true,false,false,false,false]
+console.log(kidsWithCandies([12, 1, 12], 10)); // [true,false,true]
+
+// Very good runtime
+// Same as top voted
