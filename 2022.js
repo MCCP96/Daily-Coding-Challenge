4bbf933edc76f6dac96a3b1767e537c4bd4addea
MCCP96 <petermccomb@live.com>
2022-02-04 11:19:43 -0500

    Day 233 - Num Students Doing Homework at a Time

--- 2022.js
+++ 2022.js
@@ -1558,7 +1558,7 @@ console.log(buildArray([1, 2], 4)); // ["Push","Push"]
 // Same as top voted */
 
 // Consecutive Characters         2/3/2022
-
+/* 
 // The power of the string is the maximum length of a non-empty substring that contains only one unique character.
 
 // Given a string s, return the power of s.
@@ -1600,4 +1600,45 @@ var topVotedMaxPower = function (s) {
     if (maxStr > accum) accum = maxStr;
   }
   return accum;
-};
+}; */
+
+// Number of Students Doing Homework at a Given Time          2/4/2022
+
+// Given two integer arrays startTime and endTime and given an integer queryTime.
+
+// The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+
+// Return the number of students doing their homework at time queryTime. More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
+// Example 1:
+//    Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+//    Output: 1
+// Explanation: We have 3 students where:
+// The first student started doing homework at time 1 and finished at time 3 and wasn't doing anything at time 4.
+// The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
+// The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
+
+// Example 2:
+//    Input: startTime = [4], endTime = [4], queryTime = 4
+//    Output: 1
+// Explanation: The only student was doing their homework at the queryTime.
+
+// Constraints:
+//    startTime.length == endTime.length
+//    1 <= startTime.length <= 100
+//    1 <= startTime[i] <= endTime[i] <= 1000
+//    1 <= queryTime <= 1000
+
+const busyStudent = (s, e, q) =>
+  e.reduce((a, c, i) => (c >= q && c - s[i] <= q ? a + 1 : a), 0);
+
+console.log(busyStudent([1, 2, 3], [3, 2, 7], 4)); // 1
+console.log(busyStudent([4], [4], 4)); // 1
+console.log(busyStudent([17], [86], 39)); // 1
+
+// Not sure why it's not working
+
+const topVotedBusyStudent = (s, e, q) =>
+  s.reduce((a, c, i) => (q >= c && q <= e[i] ? a + 1 : a), 0);
+
+// Top 1% runtime
