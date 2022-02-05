d28b137bc57a51e4268bb00eea125f938c8f7f86
MCCP96 <petermccomb@live.com>
2022-02-05 12:29:09 -0500

    Day 234 - Word Occurs As a Prefix of Any Word

--- 2022.js
+++ 2022.js
@@ -1603,7 +1603,7 @@ var topVotedMaxPower = function (s) {
 }; */
 
 // Number of Students Doing Homework at a Given Time          2/4/2022
-
+/* 
 // Given two integer arrays startTime and endTime and given an integer queryTime.
 
 // The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
@@ -1641,4 +1641,60 @@ console.log(busyStudent([17], [86], 39)); // 1
 const topVotedBusyStudent = (s, e, q) =>
   s.reduce((a, c, i) => (q >= c && q <= e[i] ? a + 1 : a), 0);
 
-// Top 1% runtime
+// Top 1% runtime */
+
+// Check If a Word Occurs As a Prefix of Any Word in a Sentence         2/5/2022
+
+// Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+
+// Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+
+// A prefix of a string s is any leading contiguous substring of s.
+
+// Example 1:
+//    Input: sentence = "i love eating burger", searchWord = "burg"
+//    Output: 4
+// Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+
+// Example 2:
+//    Input: sentence = "this problem is an easy problem", searchWord = "pro"
+//    Output: 2
+// Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
+
+// Example 3:
+//    Input: sentence = "i am tired", searchWord = "you"
+//    Output: -1
+// Explanation: "you" is not a prefix of any word in the sentence.
+
+// Constraints:
+//    1 <= sentence.length <= 100
+//    1 <= searchWord.length <= 10
+//    sentence consists of lowercase English letters and spaces.
+//    searchWord consists of lowercase English letters.
+
+const isPrefixOfWord = function (s, w) {
+  s = s.split(" ");
+  for (let i = 0; i < s.length; i++)
+    if (s[i].split(w).length > 1 && s[i].split(w)[0] == "") return i + 1;
+  return -1;
+};
+console.log(isPrefixOfWord("i love eating burger", "burg")); // 4
+console.log(isPrefixOfWord("this problem is an easy problem", "pro")); // 2
+console.log(isPrefixOfWord("i am tired", "you")); // -1
+console.log(isPrefixOfWord("hellohello hellohellohello", "ell")); // -1
+
+// Top 2% runtime
+
+const topVotedIsPrefixOfWord = (sentence, searchWord) => {
+  sentence += " ";
+  for (let i = 0, j = 1, word = ""; i < sentence.length; ++i) {
+    if (sentence[i] !== " ") {
+      word += sentence[i];
+      continue;
+    }
+    if (word.startsWith(searchWord)) return j;
+    ++j;
+    word = "";
+  }
+  return -1;
+};
