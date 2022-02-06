68fa4735e502028ad380d4cc7641e40ad25a1e1a
MCCP96 <petermccomb@live.com>
2022-02-06 11:13:37 -0500

    Day 235 - Make Two Arrs Equal by Reversing Sub-arr

--- 2022.js
+++ 2022.js
@@ -1644,7 +1644,7 @@ const topVotedBusyStudent = (s, e, q) =>
 // Top 1% runtime */
 
 // Check If a Word Occurs As a Prefix of Any Word in a Sentence         2/5/2022
-
+/* 
 // Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
 
 // Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
@@ -1697,4 +1697,45 @@ const topVotedIsPrefixOfWord = (sentence, searchWord) => {
     word = "";
   }
   return -1;
-};
+}; */
+
+// Make Two Arrays Equal by Reversing Sub-arrays          2/6/2022
+
+// You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty sub-array of arr and reverse it. You are allowed to make any number of steps.
+
+// Return true if you can make arr equal to target or false otherwise.
+
+// Example 1:
+//    Input: target = [1,2,3,4], arr = [2,4,1,3]
+//    Output: true
+// Explanation: You can follow the next steps to convert arr to target:
+//    1- Reverse sub-array [2,4,1], arr becomes [1,4,2,3]
+//    2- Reverse sub-array [4,2], arr becomes [1,2,4,3]
+//    3- Reverse sub-array [4,3], arr becomes [1,2,3,4]
+//    There are multiple ways to convert arr to target, this is not the only way to do so.
+
+// Example 2:
+//    Input: target = [7], arr = [7]
+//    Output: true
+// Explanation: arr is equal to target without any reverses.
+
+// Example 3:
+//    Input: target = [3,7,9], arr = [3,7,11]
+//    Output: false
+// Explanation: arr does not have value 9 and it can never be converted to target.
+
+// Constraints:
+//    target.length == arr.length
+//    1 <= target.length <= 1000
+//    1 <= target[i] <= 1000
+//    1 <= arr[i] <= 1000
+
+const canBeEqual = (target, arr) =>
+  target.sort().join("") == arr.sort().join("");
+
+console.log(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3])); // true
+console.log(canBeEqual([7], [7])); // true
+console.log(canBeEqual([3, 7, 9], [3, 7, 11])); // false
+
+// Super easy way to compare them
+// Same as top voted
