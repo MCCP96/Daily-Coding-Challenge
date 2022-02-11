f1d835ebc8fe4c644cfe5273dc00b77ce8545630
MCCP96 <petermccomb@live.com>
2022-02-11 12:08:48 -0500

    Day 240 - XOR Operation in an Array

--- 2022.js
+++ 2022.js
@@ -1892,7 +1892,7 @@ var topVotedFinalPrices = function (prices) {
 // Slower runtime, however avoids the use of 'p' array */
 
 // Running Sum of 1d Array          2/10/2022
-
+/* 
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 
 // Return the running sum of nums.
@@ -1942,4 +1942,48 @@ const topVotedRunningSum = (nums) => {
 };
 
 // Didn't think of this
-// I automatically defaulted to '.map' due to output being an array
+// I automatically defaulted to '.map' due to output being an array */
+
+// XOR Operation in an Array          2/11/2022
+
+// You are given an integer n and an integer start.
+
+// Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+// Return the bitwise XOR of all elements of nums.
+
+// Example 1:
+//    Input: n = 5, start = 0
+//    Output: 8
+// Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+// Where "^" corresponds to bitwise XOR operator.
+
+// Example 2:
+//    Input: n = 4, start = 3
+//    Output: 8
+// Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+
+// Constraints:
+//    1 <= n <= 1000
+//    0 <= start <= 1000
+//    n == nums.length
+
+const xorOperation = (n, start) => {
+  let acc = start;
+  return Array(n)
+    .fill(start)
+    .map((c, i) => (i == 0 ? c : (acc += 2)))
+    .reduce((a, c) => a ^ c);
+};
+console.log(xorOperation(5, 0)); // 8
+console.log(xorOperation(4, 3)); // 8
+
+// Im sure I can avoid using .map & .reduce, but the runtime is decent
+
+var topVotedXorOperation = function (n, start) {
+  let xor = 0;
+  for (let i = 0; i < n; i++) xor ^= start + 2 * i;
+  return xor;
+};
+
+// Very nice, amazing Runtime!
