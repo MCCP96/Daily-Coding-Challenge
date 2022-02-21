9008abd74ceb04ac57c75b9f717211506023d75e
MCCP96 <petermccomb@live.com>
2022-02-21 10:32:38 -0500

    Day 250 - Number of Good Pairs

--- 2022.js
+++ 2022.js
@@ -2349,7 +2349,7 @@ function topVotedCountOdds(low, high) {
 // Much better runtime, very nice */
 
 // Shuffle String         2/20/2022
-
+/* 
 // You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 
 // Return the shuffled string.
@@ -2381,4 +2381,55 @@ console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3])); // "leetcode"
 console.log(restoreString("abc", [0, 1, 2])); // "abc"
 
 // Better runtime than 90%
+// Same as top voted */
+
+// Count Good Triplets          2/21/2022
+
+// Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+// A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+// 0 <= i < j < k < arr.length
+// |arr[i] - arr[j]| <= a
+// |arr[j] - arr[k]| <= b
+// |arr[i] - arr[k]| <= c
+// Where |x| denotes the absolute value of x.
+
+// Return the number of good triplets.
+
+// Example 1:
+//    Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+//    Output: 4
+// Explanation: There are 4 good triplets: [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+
+// Example 2:
+//    Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+//    Output: 0
+// Explanation: No triplet satisfies all conditions.
+
+// Constraints:
+//    3 <= arr.length <= 100
+//    0 <= arr[i] <= 1000
+//    0 <= a, b, c <= 1000
+
+const countGoodTriplets = function (arr, a, b, c) {
+  let count = 0;
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      if (Math.abs(arr[i] - arr[j]) <= a) {
+        for (let k = j + 1; k < arr.length; k++) {
+          if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c)
+            count++;
+        }
+      }
+    }
+  }
+  return count;
+};
+console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3)); // 4
+console.log(countGoodTriplets([1, 1, 2, 2, 3], 0, 0, 1)); // 0
+
+// Better than 100% of runtimes!
+
+// Not a fan of solution but seems to be the standard
 // Same as top voted
