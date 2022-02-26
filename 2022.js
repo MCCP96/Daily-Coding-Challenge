c2245f33fd285b3d4821832374d59b03d8388d53
MCCP96 <petermccomb@live.com>
2022-02-26 12:15:42 -0500

    Day 255 - Most Visited Sector in a Circular Track

--- 2022.js
+++ 2022.js
@@ -2586,7 +2586,7 @@ var topVotedThreeConsecutiveOdds = (arr) =>
 // Very nice solution! */
 
 // Thousand Separator         2/25/2022
-
+/* 
 // Given an integer n, add a dot (".") as the thousands separator and return it in string format.
 
 // Constraints:
@@ -2609,4 +2609,46 @@ console.log(thousandSeparator(123456789)); // "123.456.789"
 
 const topVotedThousandSeparator = (n) => n.toLocaleString("de-DE");
 
-// Hah, nice hack
+// Hah, nice hack */
+
+// Most Visited Sector in a Circular Track          2/26/2022
+
+// Given an integer n and an integer array rounds. We have a circular track which consists of n sectors labeled from 1 to n. A marathon will be held on this track, the marathon consists of m rounds. The ith round starts at sector rounds[i - 1] and ends at sector rounds[i]. For example, round 1 starts at sector rounds[0] and ends at sector rounds[1]
+
+// Return an array of the most visited sectors sorted in ascending order.
+
+// Notice that you circulate the track in ascending order of sector numbers in the counter-clockwise direction (See the first example).
+
+// Example 1:
+//    Input: n = 4, rounds = [1,3,1,2]
+//    Output: [1,2]
+// Explanation: The marathon starts at sector 1. The order of the visited sectors is as follows:
+// 1 --> 2 --> 3 (end of round 1) --> 4 --> 1 (end of round 2) --> 2 (end of round 3 and the marathon)
+// We can see that both sectors 1 and 2 are visited twice and they are the most visited sectors. Sectors 3 and 4 are visited only once.
+
+// Constraints:
+//    2 <= n <= 100
+//    1 <= m <= 100
+//    rounds.length == m + 1
+//    1 <= rounds[i] <= n
+//    rounds[i] != rounds[i + 1] for 0 <= i < m
+
+const topVotedMostVisited = function (n, rounds) {
+  const start = rounds[0],
+    end = rounds[rounds.length - 1];
+  const count = [];
+
+  if (start <= end) {
+    for (let i = start; i <= end; i++) count.push(i);
+  } else {
+    for (let i = 1; i <= end; i++) count.push(i);
+    for (let i = start; i <= n; i++) count.push(i);
+  }
+  return count;
+};
+console.log(topVotedMostVisited(4, [1, 3, 1, 2])); // [1,2]
+console.log(topVotedMostVisited(2, [2, 1, 2, 1, 2, 1, 2, 1, 2])); // [2]
+console.log(topVotedMostVisited(7, [1, 3, 5, 7])); // [1,2,3,4,5,6,7]
+
+// No time today
+// Nice solution, better than 100% of runtimes
