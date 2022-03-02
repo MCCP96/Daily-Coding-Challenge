f1591f376a6b676efecc20d6e4b0298773962d6b
MCCP96 <petermccomb@live.com>
2022-03-02 11:21:18 -0500

    Day 259 - Special Positions in a Binary Matrix

--- 2022.js
+++ 2022.js
@@ -2757,7 +2757,7 @@ const topVotedDiagonalSum = (mat) => {
 // Same logic but cleaner */
 
 // Replace All ?'s to Avoid Consecutive Repeating Characters          3/1/2022
-
+/* 
 // Given a string s containing only lowercase English letters and the '?' character, convert all the '?' characters into lowercase letters such that the final string does not contain any consecutive repeating characters. You cannot modify the non '?' characters.
 
 // It is guaranteed that there are no consecutive repeating characters in the given string except for '?'.
@@ -2810,4 +2810,50 @@ const topVotedModifyString = function (s) {
 };
 
 // Keep it simple.
-// This top voted was titled as easy as 'abc' lol, love the logic
+// This top voted was titled as easy as 'abc' lol, love the logic */
+
+// Special Positions in a Binary Matrix         3/2/2022
+
+// Given an m x n binary matrix mat, return the number of special positions in mat.
+
+// A position (i, j) is called special if mat[i][j] == 1 and all other elements in row i and column j are 0 (rows and columns are 0-indexed).
+
+// Example 1:
+//    Input: mat = [[1,0,0],[0,0,1],[1,0,0]]
+//    Output: 1
+// Explanation: (1, 2) is a special position because mat[1][2] == 1 and all other elements in row 1 and column 2 are 0.
+
+// Example 2:
+//    Input: mat = [[1,0,0],[0,1,0],[0,0,1]]
+//    Output: 3
+// Explanation: (0, 0), (1, 1) and (2, 2) are special positions.
+
+// Constraints:
+//    m == mat.length
+//    n == mat[i].length
+//    1 <= m, n <= 100
+//    mat[i][j] is either 0 or 1.
+
+const numSpecial = function (mat) {
+  let count = 0;
+  for (let row = 0; row < mat.length; row++) {
+    const index = mat[row].indexOf(1);
+    if (index === mat[row].lastIndexOf(1)) {
+      let num = 0;
+      for (let i = 0; i < mat.length; i++) {
+        if (mat[i][index] === 1) num++;
+        if (num > 1) break;
+      }
+      if (num === 1) count++;
+    }
+  }
+  return count;
+};
+// prettier-ignore
+console.log(numSpecial([[1,0,0],[0,0,1],[1,0,0]])); // 1
+// prettier-ignore
+console.log(numSpecial([[1,0,0],[0,1,0],[0,0,1]])); // 3
+
+// Great runtime, feels like a very standard solution
+
+// All top voted answers are much much larger, sticking with mine
