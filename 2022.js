1928e3805c7b6cb1b6e5c37799f30215d9952913
MCCP96 <petermccomb@live.com>
2022-03-07 10:19:59 -0500

    Day 264 - Special Array With X Elements >= X

--- 2022.js
+++ 2022.js
@@ -3035,7 +3035,7 @@ const oneLinerMinOperations = (logs) =>
 // Made a one-liner out of both our solutions */
 
 // Design Parking System          3/6/2022
-
+/* 
 // Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
 
 // Implement the ParkingSystem class:
@@ -3073,4 +3073,59 @@ console.log(ex1.addCar(1), ex1.addCar(2), ex1.addCar(3), ex1.addCar(1)); // [tru
 
 // Definitely used top voted for this one
 // Still unfamiliar with this format of question
-// The logic makes sense, but they always catch me off guard
+// The logic makes sense, but they always catch me off guard */
+
+// Special Array With X Elements Greater Than or Equal X          3/7/2022
+
+// You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
+
+// Notice that x does not have to be an element in nums.
+
+// Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
+
+// Example 1:
+//    Input: nums = [3,5]
+//    Output: 2
+// Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
+
+// Example 2:
+//    Input: nums = [0,0]
+//    Output: -1
+// Explanation: No numbers fit the criteria for x.
+// If x = 0, there should be 0 numbers >= x, but there are 2.
+// If x = 1, there should be 1 number >= x, but there are 0.
+// If x = 2, there should be 2 numbers >= x, but there are 0.
+// x cannot be greater since there are only 2 numbers in nums.
+
+// Example 3:
+//    Input: nums = [0,4,3,0,4]
+//    Output: 3
+// Explanation: There are 3 values that are greater than or equal to 3.
+
+// Constraints:
+//    1 <= nums.length <= 100
+//    0 <= nums[i] <= 1000
+
+const specialArray = function (nums) {
+  for (let i = 0; i <= nums.length; i++)
+    if (nums.filter((x) => x >= i).length === i) return i;
+  return -1;
+};
+console.log(specialArray([3, 5])); // 2
+console.log(specialArray([0, 0])); // -1
+console.log(specialArray([0, 4, 3, 0, 4])); // 3
+console.log(specialArray([3, 6, 7, 7, 0])); // -1
+
+// Not the greatest runtime
+
+const topVotedSpecialArray = (nums) => {
+  for (let i = 0; i <= nums.length; ++i) {
+    let c = 0;
+    for (const num of nums) num >= i && ++c;
+    if (c === i) return i;
+  }
+  return -1;
+};
+
+// Much better runtime
+// Same logic, but uses for loop rather than .filter
