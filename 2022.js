97729af56a2a788267e3794e7f18be1e19fb9204
MCCP96 <petermccomb@live.com>
2022-03-11 11:30:53 -0500

    Day 268 - Slowest Key

--- 2022.js
+++ 2022.js
@@ -3239,7 +3239,7 @@ var topVotedTrimMean = function (arr) {
 // Not amazing runtime, but very readable */
 
 // Largest Substring Between Two Equal Characters         3/10/2022
-
+/* 
 // Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
 
 // A substring is a contiguous sequence of characters within a string.
@@ -3278,4 +3278,81 @@ console.log(topVotedMaxLengthBetweenEqualCharacters("abca")); // 2
 console.log(topVotedMaxLengthBetweenEqualCharacters("cbzxy")); // -1
 
 // No time today!
-// Great use of Map here
+// Great use of Map here */
+
+// Slowest Key          3/11/2022
+
+//  A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+
+// You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+
+// The tester wants to know the key of the keypress that had the longest duration. The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+
+// Note that the same key could have been pressed multiple times during the test, and these multiple presses of the same key may not have had the same duration.
+
+// Return the key of the keypress that had the longest duration. If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+
+// Example 1:
+//    Input: releaseTimes = [9,29,49,50], keysPressed = "cbcd"
+//    Output: "c"
+// Explanation: The keypresses were as follows:
+// Keypress for 'c' had a duration of 9 (pressed at time 0 and released at time 9).
+// Keypress for 'b' had a duration of 29 - 9 = 20 (pressed at time 9 right after the release of the previous character and released at time 29).
+// Keypress for 'c' had a duration of 49 - 29 = 20 (pressed at time 29 right after the release of the previous character and released at time 49).
+// Keypress for 'd' had a duration of 50 - 49 = 1 (pressed at time 49 right after the release of the previous character and released at time 50).
+// The longest of these was the keypress for 'b' and the second keypress for 'c', both with duration 20.
+// 'c' is lexicographically larger than 'b', so the answer is 'c'.
+
+// Example 2:
+//    Input: releaseTimes = [12,23,36,46,62], keysPressed = "spuda"
+//    Output: "a"
+// Explanation: The keypresses were as follows:
+// Keypress for 's' had a duration of 12.
+// Keypress for 'p' had a duration of 23 - 12 = 11.
+// Keypress for 'u' had a duration of 36 - 23 = 13.
+// Keypress for 'd' had a duration of 46 - 36 = 10.
+// Keypress for 'a' had a duration of 62 - 46 = 16.
+// The longest of these was the keypress for 'a' with duration 16.
+
+// Constraints:
+//    releaseTimes.length == n
+//    keysPressed.length == n
+//    2 <= n <= 1000
+//    1 <= releaseTimes[i] <= 109
+//    releaseTimes[i] < releaseTimes[i+1]
+//    keysPressed contains only lowercase English letters.
+
+const slowestKey = function (releaseTimes, keysPressed) {
+  let map = new Map();
+  keysPressed
+    .split("")
+    .forEach((key, i) =>
+      i === 0
+        ? map.set(releaseTimes[i], key)
+        : map.set(releaseTimes[i] - releaseTimes[i - 1], key)
+    );
+  return map.get(Math.max(...map.keys()));
+};
+console.log(slowestKey([9, 29, 49, 50], "cbcd")); // "c"
+console.log(slowestKey([12, 23, 36, 46, 62], "spuda")); // "a"
+
+// Doesn't work for all test cases
+
+const topVotedSlowestKey = function (releaseTimes, keysPressed) {
+  let maxDuration = releaseTimes[0],
+    char = keysPressed[0];
+  for (let i = 1; i < releaseTimes.length; i++) {
+    if (
+      releaseTimes[i] - releaseTimes[i - 1] == maxDuration &&
+      keysPressed[i] > char
+    )
+      char = keysPressed[i];
+    else if (releaseTimes[i] - releaseTimes[i - 1] > maxDuration) {
+      char = keysPressed[i];
+      maxDuration = releaseTimes[i] - releaseTimes[i - 1];
+    }
+  }
+  return char;
+};
+
+// Decent runtime, very logical code
