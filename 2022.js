79fbf24fcb843c73a4d3d0ee2c23a0ecd11fb201
MCCP96 <petermccomb@live.com>
2022-03-21 10:57:19 -0400

    Day 278 - Count of Matches in Tournament

--- 2022.js
+++ 2022.js
@@ -3795,7 +3795,7 @@ const oneLinerinterpret = (command) =>
 // More elegant, but both have worse runtime */
 
 // Count the Number of Consistent Strings         3/20/2022
-
+/* 
 // You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
 
 // Return the number of consistent strings in the array words.
@@ -3846,4 +3846,54 @@ const topVotedCountConsistentStrings = (allowed, words) => {
   }, 0);
 };
 
-// Nice, much better
+// Nice, much better */
+
+// Count of Matches in Tournament         3/21/2022
+
+// You are given an integer n, the number of teams in a tournament that has strange rules:
+
+// If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+
+// If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+// Return the number of matches played in the tournament until a winner is decided.
+
+// Example 1:
+//    Input: n = 7
+//    Output: 6
+// Explanation: Details of the tournament:
+// - 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
+// - 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
+// - 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+// Total number of matches = 3 + 2 + 1 = 6.
+
+// Example 2:
+//    Input: n = 14
+//    Output: 13
+// Explanation: Details of the tournament:
+// - 1st Round: Teams = 14, Matches = 7, and 7 teams advance.
+// - 2nd Round: Teams = 7, Matches = 3, and 4 teams advance.
+// - 3rd Round: Teams = 4, Matches = 2, and 2 teams advance.
+// - 4th Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+// Total number of matches = 7 + 3 + 2 + 1 = 13.
+
+// Constraints:
+//    1 <= n <= 200
+
+const numberOfMatches = function (n) {
+  let matches = 0;
+  while (n > 1) {
+    let isEven = n % 2 === 0;
+    matches += isEven ? n / 2 : (n - 1) / 2;
+    n = isEven ? n / 2 : (n - 1) / 2 + 1;
+  }
+  return matches;
+};
+console.log(numberOfMatches(7)); // 6
+console.log(numberOfMatches(14)); // 13
+
+// Very readable, decent runtime
+
+const topVotedNumberOfMatches = (n) => n - 1;
+
+// Woooow hahah
+// Work smart, not hard
