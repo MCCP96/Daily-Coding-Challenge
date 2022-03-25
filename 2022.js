047d4135668a3166a7037c6756fb51dc857bf429
MCCP96 <petermccomb@live.com>
2022-03-25 11:56:22 -0400

    Day 282 - Maximum Units on a Truck

--- 2022.js
+++ 2022.js
@@ -4057,7 +4057,7 @@ var topVotedCountStudents = function (students, sandwiches) {
 // Worse runtime somehow */
 
 // Determine if String Halves Are Alike         3/24/2022
-
+/* 
 // You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
 
 // Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
@@ -4106,4 +4106,68 @@ var toVotedHalvesAreAlike = function (S) {
   return ans === 0;
 };
 
-// Definitely an improvement on runtime to test both 'a' and 'b' within the same for loop
+// Definitely an improvement on runtime to test both 'a' and 'b' within the same for loop */
+
+// Maximum Units on a Truck         3/25/2022
+
+// You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+// numberOfBoxesi is the number of boxes of type i.
+// numberOfUnitsPerBoxi is the number of units in each box of the type i.
+// You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+// Return the maximum total number of units that can be put on the truck.
+
+// Example 1:
+//    Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+//    Output: 8
+// Explanation: There are:
+//    - 1 box of the first type that contains 3 units.
+//    - 2 boxes of the second type that contain 2 units each.
+//    - 3 boxes of the third type that contain 1 unit each.
+// You can take all the boxes of the first and second types, and one box of the third type.
+// The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+
+// Constraints:
+//    1 <= boxTypes.length <= 1000
+//    1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+//    1 <= truckSize <= 106
+
+const maximumUnits = function (boxTypes, truckSize) {
+  boxTypes.sort((a, b) => b[1] - a[1]);
+
+  let ans = 0;
+  while (truckSize > 0 && boxTypes.length > 0) {
+    if (boxTypes[0][0] <= truckSize) {
+      ans += boxTypes[0][0] * boxTypes[0][1];
+      truckSize -= boxTypes[0][0];
+      boxTypes.shift();
+    } else {
+      ans += truckSize * boxTypes[0][1];
+      truckSize = 0;
+    }
+  }
+  return ans;
+};
+// prettier-ignore
+console.log(maximumUnits([[1,3],[2,2],[3,1]], 4)); // 8
+// prettier-ignore
+console.log(maximumUnits([[5,10],[2,5],[4,7],[3,9]], 10)); // 91
+// prettier-ignore
+console.log(maximumUnits([[1,3],[5,5],[2,5],[4,2],[4,1],[3,1],[2,2],[1,3],[2,5],[3,2]], 35));
+
+// Works
+// Good memory, not so good runtime
+
+var topVotedMaximumUnits = function (B, T) {
+  B.sort((a, b) => b[1] - a[1]);
+  let ans = 0;
+  for (let i = 0; T && i < B.length; i++) {
+    let count = Math.min(B[i][0], T);
+    (ans += count * B[i][1]), (T -= count);
+  }
+  return ans;
+};
+
+// Slightly better runtime
+// Math.min instead of decrementing truckSize as I did
