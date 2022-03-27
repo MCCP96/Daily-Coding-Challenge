0d164c0c505bdfda380d64262784a71ca5ddd6a1
MCCP96 <petermccomb@live.com>
2022-03-27 11:14:14 -0400

    Day 284 - Decode XORed Array

--- 2022.js
+++ 2022.js
@@ -4173,7 +4173,7 @@ var topVotedMaximumUnits = function (B, T) {
 // Math.min instead of decrementing truckSize as I did */
 
 // Calculate Money in Leetcode Bank         3/26/2022
-
+/* 
 // Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
 
 // He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
@@ -4224,4 +4224,46 @@ function topVotedTotalMoney(n) {
   // (sum of k weeks) + (sum of x days)
 }
 
-// Slower runtime
+// Slower runtime */
+
+// Decode XORed Array         3/27/2022
+
+// There is a hidden integer array arr that consists of n non-negative integers.
+
+// It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+
+// You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+
+// Return the original array arr. It can be proved that the answer exists and is unique.
+
+// Example 1:
+//    Input: encoded = [1,2,3], first = 1
+//    Output: [1,0,2,1]
+// Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+
+// Constraints:
+//    2 <= n <= 104
+//    encoded.length == n - 1
+//    0 <= encoded[i] <= 105
+//    0 <= first <= 105
+
+const decode = function (encoded, first) {
+  let ans = [];
+  ans.push(first);
+  for (let i = 0; i < encoded.length; i++) {
+    ans.push(ans[ans.length - 1] ^ encoded[i]);
+  }
+  return ans;
+};
+console.log(decode([1, 2, 3], 1)); // [1,0,2,1]
+console.log(decode([6, 2, 7, 3], 4)); // [4,2,0,7,4]
+
+// Ok
+
+const topVotedDecode = (encoded, first) => {
+  const out = [first];
+  encoded.forEach((x, i) => out.push(out[i] ^ x));
+  return out;
+};
+
+// Same logic, cleaner code
