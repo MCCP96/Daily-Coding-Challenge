1eff047b8ef2bc4e95630eda5a4ed1f8d4bf4fc8
MCCP96 <petermccomb@live.com>
2022-03-28 10:53:02 -0400

    Day 285 - Num Of Rectangles Forming Largest Square

--- 2022.js
+++ 2022.js
@@ -4227,7 +4227,7 @@ function topVotedTotalMoney(n) {
 // Slower runtime */
 
 // Decode XORed Array         3/27/2022
-
+/* 
 // There is a hidden integer array arr that consists of n non-negative integers.
 
 // It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
@@ -4266,4 +4266,53 @@ const topVotedDecode = (encoded, first) => {
   return out;
 };
 
-// Same logic, cleaner code
+// Same logic, cleaner code */
+
+// Number Of Rectangles That Can Form The Largest Square          3/28/2022
+
+// You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+
+// You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+
+// Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+// Return the number of rectangles that can make a square with a side length of maxLen.
+
+// Example 1:
+//    Input: rectangles = [[5,8],[3,9],[5,12],[16,5]]
+//    Output: 3
+// Explanation: The largest squares you can get from each rectangle are of lengths [5,3,5,5].
+// The largest possible square is of length 5, and you can get it out of 3 rectangles.
+
+// Constraints:
+//    1 <= rectangles.length <= 1000
+//    rectangles[i].length == 2
+//    1 <= li, wi <= 109
+//    li != wi
+
+const countGoodRectangles = function (rectangles) {
+  const count = rectangles.map((c) => Math.min(...c));
+  const max = Math.max(...count);
+  return count.filter((x) => x === max).length;
+};
+// prettier-ignore
+console.log(countGoodRectangles([[5,8],[3,9],[5,12],[16,5]])); // 3
+// prettier-ignore
+console.log(countGoodRectangles([[2,3],[3,7],[4,3],[3,7]])); // 3
+
+// Top 10% runtime & memory
+
+const topVotedCountGoodRectangles = function (rectangles) {
+  let count = 0,
+    max = 0;
+  for (let rec of rectangles) {
+    let len = Math.min(rec[0], rec[1]);
+    if (len > max) {
+      count = 1;
+      max = len;
+    } else if (len == max) {
+      count++;
+    }
+  }
+  return count;
+};
