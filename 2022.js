1224b8c364ecd9b6980473af79be287297d53644
MCCP96 <petermccomb@live.com>
2022-04-02 10:55:16 -0400

    Day 290 - Check if Array Is Sorted and Rotated

--- 2022.js
+++ 2022.js
@@ -4474,7 +4474,7 @@ const oneLinerCountBalls = (lowLimit, highLimit) =>
   ); */
 
 // Sum of Unique Elements         4/1/2022
-
+/* 
 // You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
 
 // Return the sum of all the unique elements of nums.
@@ -4521,4 +4521,68 @@ const topVotedSumOfUnique = (nums) => {
   return calc;
 };
 
-// Map was definitely an option
+// Map was definitely an option */
+
+// Check if Array Is Sorted and Rotated         4/2/2022
+
+// Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+// There may be duplicates in the original array.
+
+// Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+// Example 1:
+//    Input: nums = [3,4,5,1,2]
+//    Output: true
+// Explanation: [1,2,3,4,5] is the original sorted array.
+// You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+
+// Example 2:
+//    Input: nums = [2,1,3,4]
+//    Output: false
+// Explanation: There is no sorted array once rotated that can make nums.
+
+// Example 3:
+//    Input: nums = [1,2,3]
+//    Output: true
+// Explanation: [1,2,3] is the original sorted array.
+// You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+
+// Constraints:
+//    1 <= nums.length <= 100
+//    1 <= nums[i] <= 100
+
+const check = function (nums) {
+  const first = nums.splice(0, nums.lastIndexOf(Math.max(...nums)) + 1);
+  const second = nums;
+
+  const arraysSorted =
+    first.join("") === first.sort((a, b) => a - b).join("") &&
+    second.join("") === second.sort((a, b) => a - b).join("");
+
+  return second.length > 0
+    ? first[0] >= second.pop() && arraysSorted
+    : arraysSorted;
+};
+console.log(check([3, 4, 5, 1, 2])); // true
+console.log(check([2, 1, 3, 4])); // false
+console.log(check([1, 2, 3])); // true
+console.log(check([6, 7, 7, 5])); // true
+
+// Doesn't work for all test cases
+
+var topVotedCheck = function (nums) {
+  let count = 0;
+  let len = nums.length - 1;
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > nums[i + 1]) {
+      count++;
+    }
+  }
+  if (count > 1 || (count == 1 && nums[0] < nums[len])) {
+    return false;
+  }
+  return true;
+};
+
+// I was definitely overcomplicating it
