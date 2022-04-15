d8b0d6404da5c5d03590203ebe8b7dc332fb39f7
MCCP96 <petermccomb@live.com>
2022-04-15 11:15:14 -0400

    Day 303 - Truncate Sentence

--- 2022.js
+++ 2022.js
@@ -5223,7 +5223,7 @@ var topVotedNumDifferentIntegers = function (word) {
 }; */
 
 // Determine Color of a Chessboard Square         4/14/2022
-
+/* 
 // You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
 
 // https://assets.leetcode.com/uploads/2021/02/19/screenshot-2021-02-20-at-22159-pm.png
@@ -5261,4 +5261,50 @@ const topVotedSquareIsWhite = (coordinates) => {
   return coordinates[0].charCodeAt(0) % 2 !== parseInt(coordinates[1]) % 2;
 };
 
-// Same logic but uses .charCodeAt instead of hardcodded array
+// Same logic but uses .charCodeAt instead of hardcodded array */
+
+// Truncate Sentence          4/15/2022
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+// Example 1:
+//    Input: s = "Hello how are you Contestant", k = 4
+//    Output: "Hello how are you"
+// Explanation:
+// The words in s are ["Hello", "how" "are", "you", "Contestant"].
+// The first 4 words are ["Hello", "how", "are", "you"].
+// Hence, you should return "Hello how are you".
+
+// Example 2:
+//    Input: s = "What is the solution to this problem", k = 4
+//    Output: "What is the solution"
+// Explanation:
+// The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+// The first 4 words are ["What", "is", "the", "solution"].
+// Hence, you should return "What is the solution".
+
+// Constraints:
+//    1 <= s.length <= 500
+//    k is in the range [1, the number of words in s].
+//    s consist of only lowercase and uppercase English letters and spaces.
+//    The words in s are separated by a single space.
+//    There are no leading or trailing spaces.
+
+const truncateSentence = function (s, k) {
+  s = s.split(" ");
+  let ans = [];
+  for (let i = 0; i < k; i++) ans.push(s[i]);
+  return ans.join(" ");
+};
+console.log(truncateSentence("Hello how are you Contestant", 4)); // "Hello how are you"
+console.log(truncateSentence("What is the solution to this problem", 4)); // "What is the solution"
+console.log(truncateSentence("chopper is not a tanuki", 5)); // "chopper is not a tanuki"
+
+// Faster than 98% of submissions
+
+const topVotedTruncateSentence = (s, k) => s.split(" ", k).join(" ");
+
+// Today I learned that .split has a second input variable for count
