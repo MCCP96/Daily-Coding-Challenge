1a9b682127d43ecc1972fcc46f8337dd9034e396
MCCP96 <petermccomb@live.com>
2022-04-17 10:55:14 -0400

    Day 305 - Min Operations to Make Array Increasing

--- 2022.js
+++ 2022.js
@@ -5310,7 +5310,7 @@ const topVotedTruncateSentence = (s, k) => s.split(" ", k).join(" ");
 // Today I learned that .split has a second input variable for count */
 
 // Sign of the Product of an Array          4/16/2022
-
+/* 
 // There is a function signFunc(x) that returns:
 
 // 1 if x is positive.
@@ -5356,4 +5356,56 @@ var topVotedArraySign = function (nums) {
   return negativeCount % 2 === 0 ? 1 : -1;
 };
 
-// Different logic, more of a math approach
+// Different logic, more of a math approach */
+
+// Minimum Operations to Make the Array Increasing          4//17/2022
+
+// You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+
+// For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+// Return the minimum number of operations needed to make nums strictly increasing.
+
+// An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+
+// Example 1:
+//    Input: nums = [1,1,1]
+//    Output: 3
+// Explanation: You can do the following operations:
+// 1) Increment nums[2], so nums becomes [1,1,2].
+// 2) Increment nums[1], so nums becomes [1,2,2].
+// 3) Increment nums[2], so nums becomes [1,2,3].
+
+// Constraints:
+//    1 <= nums.length <= 5000
+//    1 <= nums[i] <= 104
+
+const minOperations = function (nums) {
+  let acc = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      acc += nums[i - 1] - nums[i] + 1;
+      nums[i] = nums[i - 1] + 1;
+    }
+  }
+  return acc;
+};
+console.log(minOperations([1, 1, 1])); // 3
+console.log(minOperations([1, 5, 2, 4, 1])); // 14
+console.log(minOperations([8])); // 0
+
+// Top 10% runtimes
+
+var topVotedMinOperations = function (nums) {
+  if (nums.length < 2) return 0;
+  let count = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      let change = nums[i - 1] - nums[i] + 1;
+      count += change;
+      nums[i] += change;
+    }
+  }
+  return count;
+};
+
+// Same idea
