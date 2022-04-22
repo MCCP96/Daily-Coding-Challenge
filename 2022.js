33cad588a5e6395baf75145bc3f1b9e7c2f9652f
MCCP96 <petermccomb@live.com>
2022-04-22 11:46:20 -0400

    Day 310 - Maximum Population Year

--- 2022.js
+++ 2022.js
@@ -5537,7 +5537,7 @@ var topVotedReplaceDigits = function (s) {
 // Nice */
 
 // Minimum Distance to the Target Element         4/21/2022
-
+/* 
 // Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
 
 // Return abs(i - start).
@@ -5583,4 +5583,51 @@ var topVotedGetMinDistance = function (nums, target, start) {
     }
   }
   return min;
+}; */
+
+// Maximum Population Year          4/22/2022
+
+// You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
+
+// The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
+
+// Return the earliest year with the maximum population.
+
+// Example 1:
+//    Input: logs = [[1993,1999],[2000,2010]]
+//    Output: 1993
+// Explanation: The maximum population is 1, and 1993 is the earliest year with this population.
+
+// Example 2:
+//    Input: logs = [[1950,1961],[1960,1971],[1970,1981]]
+//    Output: 1960
+// Explanation:
+// The maximum population is 2, and it had happened in years 1960 and 1970.
+// The earlier year between them is 1960.
+
+// Constraints:
+//    1 <= logs.length <= 100
+//    1950 <= birthi < deathi <= 2050
+
+const topVotedMaximumPopulation = function (logs) {
+  const count = new Array(101).fill(0);
+
+  for (const [birth, death] of logs) {
+    count[birth - 1950]++;
+    count[death - 1950]--;
+  }
+
+  let max = 0;
+
+  for (let i = 1; i < 101; i++) {
+    count[i] += count[i - 1];
+
+    if (count[i] > count[max]) max = i;
+  }
+
+  return 1950 + max;
 };
+// prettier-ignore
+console.log(maximumPopulation([[1993,1999],[2000,2010]])); // 1993
+// prettier-ignore
+console.log(maximumPopulation([[1950,1961],[1960,1971],[1970,1981]])); // 1960
