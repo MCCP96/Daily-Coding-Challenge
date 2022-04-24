4732b499bbbb6005584a03ab54a60db2b882a56a
MCCP96 <petermccomb@live.com>
2022-04-24 11:50:56 -0400

    Day 312 - Sum of All Subset XOR Totals

--- 2022.js
+++ 2022.js
@@ -5633,7 +5633,7 @@ console.log(maximumPopulation([[1993,1999],[2000,2010]])); // 1993
 console.log(maximumPopulation([[1950,1961],[1960,1971],[1970,1981]])); // 1960 */
 
 // Sorting the Sentence         4/23/2022
-
+/* 
 // A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
 
 // A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
@@ -5679,4 +5679,69 @@ const topVotedSortSentence = function (s) {
     .join(" ");
 };
 
-// Same idea, different code
+// Same idea, different code */
+
+// Sum of All Subset XOR Totals         4/24/2022
+
+// The XOR total of an array is defined as the bitwise XOR of all its elements, or 0 if the array is empty.
+
+// For example, the XOR total of the array [2,5,6] is 2 XOR 5 XOR 6 = 1.
+// Given an array nums, return the sum of all XOR totals for every subset of nums.
+
+// Note: Subsets with the same elements should be counted multiple times.
+
+// An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b.
+
+// Example 1:
+//    Input: nums = [1,3]
+//    Output: 6
+// Explanation: The 4 subsets of [1,3] are:
+// - The empty subset has an XOR total of 0.
+// - [1] has an XOR total of 1.
+// - [3] has an XOR total of 3.
+// - [1,3] has an XOR total of 1 XOR 3 = 2.
+// 0 + 1 + 3 + 2 = 6
+
+// Example 2:
+//    Input: nums = [5,1,6]
+//    Output: 28
+// Explanation: The 8 subsets of [5,1,6] are:
+// - The empty subset has an XOR total of 0.
+// - [5] has an XOR total of 5.
+// - [1] has an XOR total of 1.
+// - [6] has an XOR total of 6.
+// - [5,1] has an XOR total of 5 XOR 1 = 4.
+// - [5,6] has an XOR total of 5 XOR 6 = 3.
+// - [1,6] has an XOR total of 1 XOR 6 = 7.
+// - [5,1,6] has an XOR total of 5 XOR 1 XOR 6 = 2.
+// 0 + 5 + 1 + 6 + 4 + 3 + 7 + 2 = 28
+
+// Example 3:
+//    Input: nums = [3,4,5,6,7,8]
+//    Output: 480
+// Explanation: The sum of all XOR totals for every subset is 480.
+
+// Constraints:
+//    1 <= nums.length <= 12
+//    1 <= nums[i] <= 20
+
+const topVotedSubsetXORSum = function (nums) {
+  let output = [];
+  backtrack();
+  return output.reduce((a, b) => a + b);
+
+  function backtrack(start = 0, arr = [nums[0]]) {
+    output.push([...arr].reduce((a, b) => a ^ b, 0));
+    for (let i = start; i < nums.length; i++) {
+      arr.push(nums[i]);
+      backtrack(i + 1, arr);
+      arr.pop();
+    }
+  }
+};
+console.log(topVotedSubsetXORSum([1, 3])); // 6
+console.log(topVotedSubsetXORSum([5, 1, 6])); // 28
+console.log(topVotedSubsetXORSum([3, 4, 5, 6, 7, 8])); // 480
+
+// Couldn't get it working so studied top voted
+// Nice solution!
