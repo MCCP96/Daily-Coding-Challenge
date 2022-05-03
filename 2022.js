10157e96243cdd2e6c3e08eed9da524c685582e2
MCCP96 <petermccomb@live.com>
2022-05-03 12:27:59 -0400

    Day 321 - Max Product Difference Between Two Pairs

--- 2022.js
+++ 2022.js
@@ -6189,7 +6189,7 @@ const topVotedLargestOddNumber = function (num) {
 // Same idea but actual working code */
 
 // Remove One Element to Make the Array Strictly Increasing         5/2/2022
-
+/* 
 // Given a 0-indexed integer array nums, return true if it can be made strictly increasing after removing exactly one element, or false otherwise. If the array is already strictly increasing, return true.
 
 // The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1 <= i < nums.length).
@@ -6245,4 +6245,41 @@ var topVotedCanBeIncreasing = function (nums) {
     (i === 1 || nums[i] > nums[i - 2]) && (prev = nums[i]);
   }
   return true;
+}; */
+
+// Maximum Product Difference Between Two Pairs         5/3/2022
+
+// The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+
+// For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+// Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+
+// Return the maximum such product difference.
+
+// Example 1:
+//    Input: nums = [5,6,2,7,4]
+//    Output: 34
+// Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+// The product difference is (6 * 7) - (2 * 4) = 34.
+
+// Example 2:
+//    Input: nums = [4,2,5,9,7,4,8]
+//    Output: 64
+// Explanation: We can choose indices 3 and 6 for the first pair (9, 8) and indices 1 and 5 for the second pair (2, 4).
+// The product difference is (9 * 8) - (2 * 4) = 64.
+
+// Constraints:
+//    4 <= nums.length <= 104
+//    1 <= nums[i] <= 104
+
+const maxProductDifference = function (nums) {
+  nums = nums.sort((a, b) => a - b);
+  return nums.pop() * nums.pop() - nums.shift() * nums.shift();
 };
+console.log(maxProductDifference([5, 6, 2, 7, 4])); // 34
+console.log(maxProductDifference([4, 2, 5, 9, 7, 4, 8])); // 64
+
+// Makes sense
+// 2 largest - 2 smallest
+
+// Same as all top voted solutions
