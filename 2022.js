559d7b9b6a08e4a6a9847bb11c148b65b25fd771
MCCP96 <petermccomb@live.com>
2022-05-04 12:35:45 -0400

    Day 322 - Build Array from Permutation

--- 2022.js
+++ 2022.js
@@ -6248,7 +6248,7 @@ var topVotedCanBeIncreasing = function (nums) {
 }; */
 
 // Maximum Product Difference Between Two Pairs         5/3/2022
-
+/* 
 // The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
 
 // For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
@@ -6282,4 +6282,38 @@ console.log(maxProductDifference([4, 2, 5, 9, 7, 4, 8])); // 64
 // Makes sense
 // 2 largest - 2 smallest
 
-// Same as all top voted solutions
+// Same as all top voted solutions */
+
+// Build Array from Permutation         5/4/2022
+
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+// Example 1:
+//    Input: nums = [0,2,1,5,3,4]
+//    Output: [0,1,2,4,5,3]
+// Explanation: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+//     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+//     = [0,1,2,4,5,3]
+
+// Example 2:
+//    Input: nums = [5,0,1,2,3,4]
+//    Output: [4,5,0,1,2,3]
+// Explanation: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+//     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+//     = [4,5,0,1,2,3]
+
+// Constraints:
+//    1 <= nums.length <= 1000
+//    0 <= nums[i] < nums.length
+//    The elements in nums are distinct.
+
+const buildArray = (nums) => nums.map((c, _, arr) => arr[c], []);
+console.log(buildArray([0, 2, 1, 5, 3, 4])); // [0,1,2,4,5,3]
+console.log(buildArray([5, 0, 1, 2, 3, 4])); // [4,5,0,1,2,3]
+
+// Slow runtime compared to other submissions
+// Same as top voteds
