4e2cc35b864c3e2a3135953e13db88d5e63177e9
MCCP96 <petermccomb@live.com>
2022-05-09 14:49:43 -0400

    Day 327 - Sum of Digits of String After Convert

--- 2022.js
+++ 2022.js
@@ -6447,7 +6447,7 @@ var topVotedCanBeTypedWords = function (text, brokenLetters) {
 // Filter approach is very valid here too */
 
 // Check if All Characters Have Equal Number of Occurrences         5/8/2022
-
+/* 
 // Given a string s, return true if s is a good string, or false otherwise.
 
 // A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
@@ -6490,4 +6490,70 @@ var topVotedAreOccurrencesEqual = (s) => {
 // Smart way of creating count here using .reduce
 // Using a Set to evaluate if all counts were the same too
 
-// Very nice
+// Very nice */
+
+// Sum of Digits of String After Convert          5/9/2022
+
+// You are given a string s consisting of lowercase English letters, and an integer k.
+
+// First, convert s into an integer by replacing each letter with its position in the alphabet (i.e., replace 'a' with 1, 'b' with 2, ..., 'z' with 26). Then, transform the integer by replacing it with the sum of its digits. Repeat the transform operation k times in total.
+
+// For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+// Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+// Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+// Transform #2: 17 ➝ 1 + 7 ➝ 8
+// Return the resulting integer after performing the operations described above.
+
+// Example 1:
+//    Input: s = "iiii", k = 1
+//    Output: 36
+// Explanation: The operations are as follows:
+// - Convert: "iiii" ➝ "(9)(9)(9)(9)" ➝ "9999" ➝ 9999
+// - Transform #1: 9999 ➝ 9 + 9 + 9 + 9 ➝ 36
+// Thus the resulting integer is 36.
+
+// Example 2:
+//    Input: s = "leetcode", k = 2
+//    Output: 6
+// Explanation: The operations are as follows:
+// - Convert: "leetcode" ➝ "(12)(5)(5)(20)(3)(15)(4)(5)" ➝ "12552031545" ➝ 12552031545
+// - Transform #1: 12552031545 ➝ 1 + 2 + 5 + 5 + 2 + 0 + 3 + 1 + 5 + 4 + 5 ➝ 33
+// - Transform #2: 33 ➝ 3 + 3 ➝ 6
+// Thus the resulting integer is 6.
+
+// Constraints:
+//    1 <= s.length <= 100
+//    1 <= k <= 10
+//    s consists of lowercase English letters.
+
+const getLucky = (s, k) => {
+  let nums = [...s].reduce((a, c) => (a += c.charCodeAt(0) - 96), "");
+  for (let i = 0; i < k; i++)
+    nums = String([...nums].reduce((a, c) => (a += +c), 0));
+  return nums;
+};
+console.log(getLucky("iiii", 1)); // 36
+console.log(getLucky("leetcode", 2)); // 6
+console.log(getLucky("zbax", 2)); // 8
+
+// Ok solution
+
+const topVotedGetLucky = (ss, k) => {
+  let s = "";
+  for (const c of ss) s += c.charCodeAt() - 96;
+  while (k--) {
+    s = go(s);
+  }
+  return s - "0";
+};
+
+const go = (s) => {
+  let sum = 0;
+  for (const c of s) {
+    sum += c - "0";
+  }
+  return sum + "";
+};
+
+// Much slower runtime
