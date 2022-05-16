fae11a80ae208257225b3597bb3ff6ee1817cdcd
MCCP96 <petermccomb@live.com>
2022-05-16 10:36:31 -0400

    Day 334 - Min Dif Between High & Low of K Scores

--- 2022.js
+++ 2022.js
@@ -6843,7 +6843,7 @@ console.log(improvedMinTimeToType("zjpc")); // 34
 // Slower runtime, more compressed code */
 
 // Find Greatest Common Divisor of Array          5/15/2022
-
+/* 
 // Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
 
 // The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
@@ -6894,4 +6894,56 @@ const topvotedFindGCD = (a) => {
 
 // gcd was the function I was missing
 
-// Google angel number 333 🙂
+// Google angel number 333 🙂 */
+
+// Minimum Difference Between Highest and Lowest of K Scores          5/16/2022
+
+// You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+
+// Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+
+// Return the minimum possible difference.
+
+// Example 1:
+//    Input: nums = [90], k = 1
+//    Output: 0
+// Explanation: There is one way to pick score(s) of one student:
+// - [90]. The difference between the highest and lowest score is 90 - 90 = 0.
+// The minimum possible difference is 0.
+
+// Example 2:
+//    Input: nums = [9,4,1,7], k = 2
+//    Output: 2
+// Explanation: There are six ways to pick score(s) of two students:
+// - [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
+// - [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
+// - [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
+// - [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
+// - [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
+// - [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
+// The minimum possible difference is 2.
+
+// Constraints:
+//    1 <= k <= nums.length <= 1000
+//    0 <= nums[i] <= 105
+
+const minimumDifference = (nums, k) => {
+  if (k === 1 || nums.length === 1) return 0;
+  nums = nums.sort((a, b) => a - b);
+  return nums.pop() - nums.pop();
+};
+console.log(minimumDifference([90], 1)); // 0
+console.log(minimumDifference([9, 4, 1, 7], 2)); // 2
+
+// Doesn't work for all test cases
+
+var topVotedMinimumDifference = function (nums, k) {
+  nums.sort((a, b) => a - b);
+  let min = nums[0],
+    max = nums[k - 1],
+    diff = max - min;
+  for (let i = k; i < nums.length; i++) {
+    diff = Math.min(diff, nums[i] - nums[i - k + 1]);
+  }
+  return diff;
+};
