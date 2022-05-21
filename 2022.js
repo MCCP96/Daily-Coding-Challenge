8e37c4026493f4ae5307ec4ac1cd72cb7d91af8c
MCCP96 <petermccomb@live.com>
2022-05-21 09:42:07 -0400

    Day 339 - Value of Var After Performing Operations

--- 2022.js
+++ 2022.js
@@ -7098,7 +7098,7 @@ var topVotedReversePrefix = function (word, ch) {
 }; */
 
 // Count Number of Pairs With Absolute Difference K         5/20/2022
-
+/* 
 // Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
 
 // The value of |x| is defined as:
@@ -7159,4 +7159,57 @@ const topVotedCountKDifference = function (nums, k) {
   return count;
 };
 
-// This is the logic I had in mind but I guess j=i+1 is what I was missing
+// This is the logic I had in mind but I guess j=i+1 is what I was missing */
+
+// Final Value of Variable After Performing Operations          5/21/2022
+
+// There is a programming language with only four operations and one variable X:
+
+// ++X and X++ increments the value of the variable X by 1.
+// --X and X-- decrements the value of the variable X by 1.
+// Initially, the value of X is 0.
+
+// Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+// Example 1:
+//    Input: operations = ["--X","X++","X++"]
+//    Output: 1
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// --X: X is decremented by 1, X =  0 - 1 = -1.
+// X++: X is incremented by 1, X = -1 + 1 =  0.
+// X++: X is incremented by 1, X =  0 + 1 =  1.
+
+// Example 2:
+//    Input: operations = ["++X","++X","X++"]
+//    Output: 3
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// ++X: X is incremented by 1, X = 0 + 1 = 1.
+// ++X: X is incremented by 1, X = 1 + 1 = 2.
+// X++: X is incremented by 1, X = 2 + 1 = 3.
+
+// Example 3:
+//    Input: operations = ["X++","++X","--X","X--"]
+//    Output: 0
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// X++: X is incremented by 1, X = 0 + 1 = 1.
+// ++X: X is incremented by 1, X = 1 + 1 = 2.
+// --X: X is decremented by 1, X = 2 - 1 = 1.
+// X--: X is decremented by 1, X = 1 - 1 = 0.
+
+// Constraints:
+//    1 <= operations.length <= 100
+//    operations[i] will be either "++X", "X++", "--X", or "X--".
+
+const finalValueAfterOperations = (operations) =>
+  operations.reduce((a, c) => (c.includes("+") ? ++a : --a), 0);
+console.log(finalValueAfterOperations(["--X", "X++", "X++"])); // 1
+console.log(finalValueAfterOperations(["++X", "++X", "X++"])); // 3
+console.log(finalValueAfterOperations(["X++", "++X", "--X", "X--"])); // 0
+
+// Easy peasy
+// Great runtime
+
+// Same as top voted
