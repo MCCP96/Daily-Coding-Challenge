ba156740ea2b78827c1c6eaa79bfa7be06f267f1
MCCP96 <petermccomb@live.com>
2022-05-22 10:50:35 -0400

    Day 340 - Max Diff Between Increasing Elements

--- 2022.js
+++ 2022.js
@@ -7162,7 +7162,7 @@ const topVotedCountKDifference = function (nums, k) {
 // This is the logic I had in mind but I guess j=i+1 is what I was missing */
 
 // Final Value of Variable After Performing Operations          5/21/2022
-
+/* 
 // There is a programming language with only four operations and one variable X:
 
 // ++X and X++ increments the value of the variable X by 1.
@@ -7212,4 +7212,66 @@ console.log(finalValueAfterOperations(["X++", "++X", "--X", "X--"])); // 0
 // Easy peasy
 // Great runtime
 
-// Same as top voted
+// Same as top voted */
+
+// Maximum Difference Between Increasing Elements         5/22/2022
+
+// Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+
+// Return the maximum difference. If no such i and j exists, return -1.
+
+// Example 1:
+//    Input: nums = [7,1,5,4]
+//    Output: 4
+// Explanation:
+// The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+// Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+
+// Example 2:
+//    Input: nums = [9,4,3,2]
+//    Output: -1
+// Explanation:
+// There is no i and j such that i < j and nums[i] < nums[j].
+
+// Example 3:
+//    Input: nums = [1,5,2,10]
+//    Output: 9
+// Explanation:
+// The maximum difference occurs with i = 0 and j = 3, nums[j] - nums[i] = 10 - 1 = 9.
+
+// Constraints:
+//    n == nums.length
+//    2 <= n <= 1000
+//    1 <= nums[i] <= 109
+
+const maximumDifference = (nums, max = 0) => {
+  for (let i = 0; i < nums.length; i++)
+    for (let j = i + 1; j < nums.length; j++)
+      max = Math.max(max, nums[j] - nums[i]);
+  return max ? max : -1;
+};
+console.log(maximumDifference([7, 1, 5, 4])); // 4
+console.log(maximumDifference([9, 4, 3, 2])); // -1
+console.log(maximumDifference([1, 5, 2, 10])); // 9
+
+// Functional and concise
+
+const topVotedMaximumDifference = (nums) => {
+  let res = -1;
+  let pointer1 = 0;
+  let pointer2 = 1;
+  while (pointer2 <= nums.length - 1) {
+    if (nums[pointer1] < nums[pointer2]) {
+      res =
+        nums[pointer2] - nums[pointer1] > res
+          ? nums[pointer2] - nums[pointer1]
+          : res;
+    } else {
+      pointer1 = pointer2;
+    }
+    pointer2++;
+  }
+  return res;
+};
+
+// O(n) solution with much better runtime
