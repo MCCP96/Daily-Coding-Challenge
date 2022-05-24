382173ac0455c4fecbbc31eff95a594f60211e98
MCCP96 <petermccomb@live.com>
2022-05-24 13:33:02 -0400

    Day 342 - Minimum Moves to Convert String

--- 2022.js
+++ 2022.js
@@ -7277,7 +7277,7 @@ const topVotedMaximumDifference = (nums) => {
 // O(n) solution with much better runtime */
 
 // Convert 1D Array Into 2D Array         5/23/2022
-
+/* 
 // You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with m rows and n columns using all the elements from original.
 
 // The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
@@ -7333,4 +7333,53 @@ var topVotedConstruct2DArray = (original, m, n) => {
     }
   }
   return result;
-};
+}; */
+
+// Minimum Moves to Convert String          5/24/2022
+
+// You are given a string s consisting of n characters which are either 'X' or 'O'.
+
+// A move is defined as selecting three consecutive characters of s and converting them to 'O'. Note that if a move is applied to the character 'O', it will stay the same.
+
+// Return the minimum number of moves required so that all the characters of s are converted to 'O'.
+
+// Example 1:
+//    Input: s = "XXX"
+//    Output: 1
+// Explanation: XXX -> OOO
+// We select all the 3 characters and convert them in one move.
+
+// Example 2:
+//    Input: s = "XXOX"
+//    Output: 2
+// Explanation: XXOX -> OOOX -> OOOO
+// We select the first 3 characters in the first move, and convert them to 'O'.
+// Then we select the last 3 characters and convert them so that the final string contains all 'O's.
+
+// Example 3:
+//    Input: s = "OOOO"
+//    Output: 0
+// Explanation: There are no 'X's in s to convert.
+
+// Constraints:
+//    3 <= s.length <= 1000
+//    s[i] is either 'X' or 'O'.
+
+const minimumMoves = (s) =>
+  s.match(/.{1,3}/g).reduce((a, c) => (c.includes("X") ? ++a : a), 0);
+
+console.log(minimumMoves("XXX")); // 1
+console.log(minimumMoves("XXOX")); // 2
+console.log(minimumMoves("OOOO")); // 0
+console.log(minimumMoves("OXOX")); // 1
+
+// Close but I'm not taking into account optimal grouping of 3 chars
+// Doesn't work for all test cases
+
+const topVotedMinimumMoves = (s) => s.match(/X.{0,2}/g)?.length ?? 0;
+
+// Oh wow, very nice
+// Amazing runtime & memory
+
+// His regex optimally groups Xs
+// Great use of optional chaining and nullish coalescing operators
