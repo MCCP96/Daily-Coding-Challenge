b0a3ee18c0ad225cf1c4ba1a379a8707ce878041
MCCP96 <petermccomb@live.com>
2022-05-25 13:26:48 -0400

    Day 343 - Two Out of Three

--- 2022.js
+++ 2022.js
@@ -7336,7 +7336,7 @@ var topVotedConstruct2DArray = (original, m, n) => {
 }; */
 
 // Minimum Moves to Convert String          5/24/2022
-
+/* 
 // You are given a string s consisting of n characters which are either 'X' or 'O'.
 
 // A move is defined as selecting three consecutive characters of s and converting them to 'O'. Note that if a move is applied to the character 'O', it will stay the same.
@@ -7382,4 +7382,59 @@ const topVotedMinimumMoves = (s) => s.match(/X.{0,2}/g)?.length ?? 0;
 // Amazing runtime & memory
 
 // His regex optimally groups Xs
-// Great use of optional chaining and nullish coalescing operators
+// Great use of optional chaining and nullish coalescing operators */
+
+// Two Out of Three         5/25/2022
+
+// Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
+
+// Example 1:
+//    Input: nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+//    Output: [3,2]
+// Explanation: The values that are present in at least two arrays are:
+// - 3, in all three arrays.
+// - 2, in nums1 and nums2.
+
+// Example 2:
+//    Input: nums1 = [3,1], nums2 = [2,3], nums3 = [1,2]
+//    Output: [2,3,1]
+// Explanation: The values that are present in at least two arrays are:
+// - 2, in nums2 and nums3.
+// - 3, in nums1 and nums2.
+// - 1, in nums1 and nums3.
+
+// Example 3:
+//    Input: nums1 = [1,2,2], nums2 = [4,3,3], nums3 = [5]
+//    Output: []
+// Explanation: No value is present in at least two arrays.
+
+// Constraints:
+//    1 <= nums1.length, nums2.length, nums3.length <= 100
+//    1 <= nums1[i], nums2[j], nums3[k] <= 100
+
+const twoOutOfThree = (nums1, nums2, nums3) => [
+  ...new Set(
+    [...new Set(nums1), ...new Set(nums2), ...new Set(nums3)].filter(
+      (x, _, arr) => arr.indexOf(x) !== arr.lastIndexOf(x)
+    )
+  ),
+];
+
+console.log(twoOutOfThree([1, 1, 3, 2], [2, 3], [3])); // [3,2]
+console.log(twoOutOfThree([3, 1], [2, 3], [1, 2])); // [2,3,1]
+console.log(twoOutOfThree([1, 2, 2], [4, 3, 3], [5])); // []
+
+// Gets the job done
+
+var topVotedTwoOutOfThree = function (nums1, nums2, nums3) {
+  let newArr = [];
+  newArr.push(
+    ...nums1.filter((num) => nums2.includes(num) || nums3.includes(num))
+  );
+  newArr.push(
+    ...nums2.filter((num) => nums1.includes(num) || nums3.includes(num))
+  );
+  return Array.from(new Set(newArr));
+};
+
+// Much simpler logic here
