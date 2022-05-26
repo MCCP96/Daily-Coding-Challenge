bf11600da596ddfa16365c6d71b65b61bda53123
MCCP96 <petermccomb@live.com>
2022-05-26 13:53:11 -0400

    Day 344 - Minimum Number of Moves to Seat Everyone

--- 2022.js
+++ 2022.js
@@ -7385,7 +7385,7 @@ const topVotedMinimumMoves = (s) => s.match(/X.{0,2}/g)?.length ?? 0;
 // Great use of optional chaining and nullish coalescing operators */
 
 // Two Out of Three         5/25/2022
-
+/* 
 // Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
 
 // Example 1:
@@ -7437,4 +7437,62 @@ var topVotedTwoOutOfThree = function (nums1, nums2, nums3) {
   return Array.from(new Set(newArr));
 };
 
-// Much simpler logic here
+// Much simpler logic here */
+
+// Minimum Number of Moves to Seat Everyone         5/26/2022
+
+// There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+
+// You may perform the following move any number of times:
+
+// Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+// Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+
+// Note that there may be multiple seats or students in the same position at the beginning.
+
+// Example 1:
+//    Input: seats = [3,1,5], students = [2,7,4]
+//    Output: 4
+// Explanation: The students are moved as follows:
+// - The first student is moved from from position 2 to position 1 using 1 move.
+// - The second student is moved from from position 7 to position 5 using 2 moves.
+// - The third student is moved from from position 4 to position 3 using 1 move.
+// In total, 1 + 2 + 1 = 4 moves were used.
+
+// Example 2:
+//    Input: seats = [4,1,5,9], students = [1,3,2,6]
+//    Output: 7
+// Explanation: The students are moved as follows:
+// - The first student is not moved.
+// - The second student is moved from from position 3 to position 4 using 1 move.
+// - The third student is moved from from position 2 to position 5 using 3 moves.
+// - The fourth student is moved from from position 6 to position 9 using 3 moves.
+// In total, 0 + 1 + 3 + 3 = 7 moves were used.
+
+// Example 3:
+//    Input: seats = [2,2,6,6], students = [1,3,2,6]
+//    Output: 4
+// Explanation: Note that there are two seats at position 2 and two seats at position 6.
+// The students are moved as follows:
+// - The first student is moved from from position 1 to position 2 using 1 move.
+// - The second student is moved from from position 3 to position 6 using 3 moves.
+// - The third student is not moved.
+// - The fourth student is not moved.
+// In total, 1 + 3 + 0 + 0 = 4 moves were used.
+
+// Constraints:
+//    n == seats.length == students.length
+//    1 <= n <= 100
+//    1 <= seats[i], students[j] <= 100
+
+const minMovesToSeat = (seats, students) => {
+  seats.sort((a, b) => a - b);
+  return students
+    .sort((a, b) => a - b)
+    .reduce((a, c) => (a += Math.abs(c - seats.shift())), 0);
+};
+console.log(minMovesToSeat([3, 1, 5], [2, 7, 4])); // 4
+console.log(minMovesToSeat([4, 1, 5, 9], [1, 3, 2, 6])); // 7
+console.log(minMovesToSeat([2, 2, 6, 6], [1, 3, 2, 6])); // 4
+
+// Same as top voted
