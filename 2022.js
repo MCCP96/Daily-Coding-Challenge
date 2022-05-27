016d39cec6b4ff1a41799db0b9d0906b32ccfc49
MCCP96 <petermccomb@live.com>
2022-05-27 13:46:29 -0400

    Day 345 - Check if Nums Ascending in a Sentence

--- 2022.js
+++ 2022.js
@@ -7440,7 +7440,7 @@ var topVotedTwoOutOfThree = function (nums1, nums2, nums3) {
 // Much simpler logic here */
 
 // Minimum Number of Moves to Seat Everyone         5/26/2022
-
+/* 
 // There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
 
 // You may perform the following move any number of times:
@@ -7495,4 +7495,66 @@ console.log(minMovesToSeat([3, 1, 5], [2, 7, 4])); // 4
 console.log(minMovesToSeat([4, 1, 5, 9], [1, 3, 2, 6])); // 7
 console.log(minMovesToSeat([2, 2, 6, 6], [1, 3, 2, 6])); // 4
 
-// Same as top voted
+// Same as top voted */
+
+// Check if Numbers Are Ascending in a Sentence         5/27/2022
+
+// A sentence is a list of tokens separated by a single space with no leading or trailing spaces. Every token is either a positive number consisting of digits 0-9 with no leading zeros, or a word consisting of lowercase English letters.
+
+// For example, "a puppy has 2 eyes 4 legs" is a sentence with seven tokens: "2" and "4" are numbers and the other tokens such as "puppy" are words.
+// Given a string s representing a sentence, you need to check if all the numbers in s are strictly increasing from left to right (i.e., other than the last number, each number is strictly smaller than the number on its right in s).
+
+// Return true if so, or false otherwise.
+
+// Example 1:
+//    Input: s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles"
+//    Output: true
+// Explanation: The numbers in s are: 1, 3, 4, 6, 12.
+// They are strictly increasing from left to right: 1 < 3 < 4 < 6 < 12.
+
+// Example 2:
+//    Input: s = "hello world 5 x 5"
+//    Output: false
+// Explanation: The numbers in s are: 5, 5. They are not strictly increasing.
+
+// Example 3:
+//    Input: s = "sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s"
+//    Output: false
+// Explanation: The numbers in s are: 7, 51, 50, 60. They are not strictly increasing.
+
+// Constraints:
+//    3 <= s.length <= 200
+//    s consists of lowercase English letters, spaces, and digits from 0 to 9, inclusive.
+//    The number of tokens in s is between 2 and 100, inclusive.
+//    The tokens in s are separated by a single space.
+//    There are at least two numbers in s.
+//    Each number in s is a positive number less than 100, with no leading zeros.
+//    s contains no leading or trailing spaces.
+
+const areNumbersAscending = (s) => {
+  s = s.match(/(\d+)/g);
+  for (let i = 1; i < s.length; i++) if (+s[i] <= +s[i - 1]) return false;
+  return true;
+};
+
+// prettier-ignore
+console.log(areNumbersAscending("1 box has 3 blue 4 red 6 green and 12 yellow marbles")); // true
+console.log(areNumbersAscending("hello world 5 x 5")); // false
+// prettier-ignore
+console.log(areNumbersAscending("sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s")); // false
+
+// Pretty good runtime
+
+const topVotedAreNumbersAscending = (s) => {
+  let items = s.split(" ");
+  let prev = Number.MIN_VALUE;
+  for (let str of items) {
+    let val = parseInt(str); // returns NaN if not valid number
+    if (!val) continue; // if NaN, go to next element
+    if (prev >= val) return false; // not ascending, return false
+    prev = val; // update placeholder
+  }
+  return true;
+};
+
+// I prefer the simplicity of mine, but this has better runtime
