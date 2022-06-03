6c18606e5b4e85bf4f0f825268ccff15fd4c6f9e
MCCP96 <petermccomb@live.com>
2022-06-03 12:54:32 -0400

    Day 352 - Two Furthest Houses With Different Color

--- 2022.js
+++ 2022.js
@@ -7887,7 +7887,7 @@ var topVotedCheckAlmostEquivalent = function (word1, word2) {
 // Same logic but using actual Map() */
 
 // Time Needed to Buy Tickets         6/2/2022
-
+/* 
 // There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
 
 // You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
@@ -7950,4 +7950,74 @@ var topVotedTimeRequiredToBuy = function (tickets, k) {
   return ans;
 };
 
-// Very similar
+// Very similar */
+
+// Two Furthest Houses With Different Colors          6/3/2022
+
+// There are n houses evenly lined up on the street, and each house is beautifully painted. You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+
+// Return the maximum distance between two houses with different colors.
+
+// The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+
+// Example 1:
+//    Input: colors = [1,1,1,6,1,1,1]
+//    Output: 3
+// Explanation: In the above image, color 1 is blue, and color 6 is red.
+// The furthest two houses with different colors are house 0 and house 3.
+// House 0 has color 1, and house 3 has color 6. The distance between them is abs(0 - 3) = 3.
+// Note that houses 3 and 6 can also produce the optimal answer.
+
+// Example 2:
+//    Input: colors = [1,8,3,8,3]
+//    Output: 4
+// Explanation: In the above image, color 1 is blue, color 8 is yellow, and color 3 is green.
+// The furthest two houses with different colors are house 0 and house 4.
+// House 0 has color 1, and house 4 has color 3. The distance between them is abs(0 - 4) = 4.
+
+// Example 3:
+//    Input: colors = [0,1]
+//    Output: 1
+// Explanation: The furthest two houses with different colors are house 0 and house 1.
+// House 0 has color 0, and house 1 has color 1. The distance between them is abs(0 - 1) = 1.
+
+// Constraints:
+//    n == colors.length
+//    2 <= n <= 100
+//    0 <= colors[i] <= 100
+//    Test data are generated such that at least two houses have different colors.
+
+const maxDistance = (colors) => {
+  let max = 0;
+  for (let i = 0; i < colors.length; i++) {
+    const cur = colors.shift();
+    max = Math.max(
+      max,
+      colors.reduce((a, c, i) => {
+        let diff = 0;
+        if (cur !== c) diff = i + 1;
+        return Math.max(a, diff);
+      }, 0)
+    );
+  }
+  return max;
+};
+console.log(maxDistance([1, 1, 1, 6, 1, 1, 1])); // 3
+console.log(maxDistance([1, 8, 3, 8, 3])); // 4
+console.log(maxDistance([0, 1])); // 1
+
+// I'm surprised this worked first try
+
+const topVotedMaxDistance = function (colors) {
+  let n = colors.length;
+  let max = 0;
+  for (let i = 0; i < n; i++) {
+    if (colors[i] !== colors[0]) {
+      max = Math.max(max, i);
+    }
+    if (colors[i] !== colors[n - 1]) {
+      max = Math.max(max, n - 1 - i);
+    }
+  }
+  return max;
+};
