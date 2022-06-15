632047b4c11909656fb0653981a9843c7124561b
MCCP96 <petermccomb@live.com>
2022-06-15 15:20:44 -0400

    Day 364 - Divide a String Into Groups of Size k

--- 2022.js
+++ 2022.js
@@ -8555,7 +8555,7 @@ const topVotedCapitalizeTitle = function (title) {
 // Same logic, different method */
 
 // Check if Every Row and Column Contains All Numbers          6/14/2022
-
+/* 
 // An n x n matrix is valid if every row and every column contains all the integers from 1 to n (inclusive).
 
 // Given an n x n integer matrix matrix, return true if the matrix is valid. Otherwise, return false.
@@ -8608,4 +8608,66 @@ const topVotedCheckValid = function (matrix) {
 };
 
 // I wasn't taking columns into account
-// My bad, didn't fully read the prompt
+// My bad, didn't fully read the prompt */
+
+// Divide a String Into Groups of Size k          6/15/2022
+
+// A string s can be partitioned into groups of size k using the following procedure:	The first group consists of the first k characters of the string, the second group consists of the next k characters of the string, and so on. Each character can be a part of exactly one group.	For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+
+// Note that the partition is done so that after removing the fill character from the last group (if it exists) and concatenating all the groups in order, the resultant string should be s.
+
+// Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
+
+// Example 1:
+//		 Input: s = "abcdefghi", k = 3, fill = "x"
+//		 Output: ["abc","def","ghi"]
+// Explanation:
+// The first 3 characters "abc" form the first group.
+// The next 3 characters "def" form the second group.
+// The last 3 characters "ghi" form the third group.
+// Since all groups can be completely filled by characters from the string, we do not need to use fill.
+// Thus, the groups formed are "abc", "def", and "ghi".
+
+// Example 2:
+//		 Input: s = "abcdefghij", k = 3, fill = "x"
+//		 Output: ["abc","def","ghi","jxx"]
+// Explanation:
+// Similar to the previous example, we are forming the first three groups "abc", "def", and "ghi".
+// For the last group, we can only use the character 'j' from the string. To complete this group, we add 'x' twice.
+// Thus, the 4 groups formed are "abc", "def", "ghi", and "jxx".
+
+// Constraints:
+//    1 <= s.length <= 100
+//    s consists of lowercase English letters only.
+//    1 <= k <= 100
+//    fill is a lowercase English letter.
+
+const divideString = (s, k, fill) => {
+  let ans = s.match(new RegExp(`.{1,${k}}`, "g"));
+  if (ans[ans.length - 1].length !== k) {
+    let filler = fill.repeat(k - ans[ans.length - 1].length);
+    ans[ans.length - 1] += filler;
+  }
+  return ans;
+};
+console.log(divideString("abcdefghi", 3, "x")); // ["abc","def","ghi"]
+console.log(divideString("abcdefghij", 3, "x")); // ["abc","def","ghi","jxx"]
+
+// I like the use of Regex here
+
+const topVotedDivideString = function (s, k, fill) {
+  var ans = [];
+  for (let i = 0; i < s.length; i += k) {
+    ans.push(s.substring(i, i + k));
+  }
+  let str = ans[ans.length - 1];
+  if (str.length == k) {
+    return ans;
+  }
+  for (let i = str.length; i < k; i++) {
+    ans[ans.length - 1] += fill;
+  }
+  return ans;
+};
+
+// Same idea, different method
