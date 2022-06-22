c51e44c96ec8b6e9231b3b4f71ce940422fb96be
MCCP96 <petermccomb@live.com>
2022-06-22 13:07:51 -0400

    Day 371 - Count Equal and Divisible Pairs in Array

--- 2022.js
+++ 2022.js
@@ -8958,7 +8958,7 @@ const topVotedSortEvenOdd = function (nums) {
 }; */
 
 // Count Operations to Obtain Zero          6/21/2022
-
+/* 
 // You are given two non-negative integers num1 and num2.
 
 // In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.	For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
@@ -9002,4 +9002,44 @@ const topVotedCountOperations = function (num1, num2) {
   if (num2 === 0) return 0;
   if (num1 < num2) countOperations(num2, num1);
   return Math.trunc(num1 / num2) + countOperations(num2, num1 % num2);
+}; */
+
+// Count Equal and Divisible Pairs in an Array          6/22/2022
+
+// Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
+
+// Example 1:
+//		 Input: nums = [3,1,2,2,2,1,3], k = 2
+//		 Output: 4
+// Explanation:
+// There are 4 pairs that meet all the requirements:
+// - nums[0] == nums[6], and 0 * 6 == 0, which is divisible by 2.
+// - nums[2] == nums[3], and 2 * 3 == 6, which is divisible by 2.
+// - nums[2] == nums[4], and 2 * 4 == 8, which is divisible by 2.
+// - nums[3] == nums[4], and 3 * 4 == 12, which is divisible by 2.
+
+// Example 2:
+//		 Input: nums = [1,2,3,4], k = 1
+//		 Output: 0
+// Explanation: Since no value in nums is repeated, there are no pairs (i,j) that meet all the requirements.
+
+// Constraints:
+//    1 <= nums.length <= 100
+//    1 <= nums[i], k <= 100
+
+const topVotedCountPairs = (nums, k) => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++)
+    for (let j = i + 1; j < nums.length; j++)
+      if (nums[i] == nums[j] && (i * j) % k == 0) count++;
+  return count;
 };
+
+console.log(topVotedCountPairs([3, 1, 2, 2, 2, 1, 3], 2)); // 4
+console.log(topVotedCountPairs([1, 2, 3, 4], 1)); // 0
+
+// I was trying to go the findIndex route but couldn't get it working
+// Ended up studying top voted
+
+// Nested for loop definitely saves a lot of trouble
+// Keep it simple
