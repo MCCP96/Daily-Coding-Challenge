89e8f4d722639b9eca963672449e2bdacb093b61
MCCP96 <petermccomb@live.com>
2022-06-26 11:47:22 -0400

    Day 375 - Cells in a Range on an Excel Sheet

--- 2022.js
+++ 2022.js
@@ -9119,7 +9119,7 @@ const topVotedPrefixCount = (words, pref) =>
 // Also nice */
 
 // Most Frequent Number Following Key In an Array          6/25/2022
-
+/* 
 // You are given a 0-indexed integer array nums. You are also given an integer key, which is present in nums.
 
 // For every unique integer target in nums, count the number of times target immediately follows an occurrence of key in nums. In other words, count the number of indices i such that:
@@ -9184,4 +9184,54 @@ const topVotedMostFrequent = (nums, key) => {
   return ret;
 };
 
-// Same idea, better execution
+// Same idea, better execution */
+
+// Cells in a Range on an Excel Sheet          6/26/2022
+
+// A cell (r, c) of an excel sheet is represented as a string "<col><row>" where:	<col> denotes the column number c of the cell. It is represented by alphabetical letters. For example, the 1^st column is denoted by 'A', the 2^nd by 'B', the 3^rd by 'C', and so on. <row> is the row number r of the cell. The r^th row is represented by the integer r.
+
+// You are given a string sinthe format "<col1><row1>:<col2><row2>", where <col1> represents the column c1, <row1> represents the row r1, <col2> represents the column c2, and <row2> represents the row r2, such that r1 <= r2 and c1 <= c2.
+
+// Return the list of cells (x, y) such that r1 <= x <= r2 and c1 <= y <= c2. The cells should be represented asstrings in the format mentioned above and be sorted in non-decreasing order first by columns and then by rows.
+
+// Example 1:
+//   https://assets.leetcode.com/uploads/2022/02/08/ex1drawio.png
+//		 Input: s = "K1:L2"
+//		 Output: ["K1","K2","L1","L2"]
+// Explanation:
+// The above diagram shows the cells which should be present in the list.
+// The red arrows denote the order in which the cells should be presented.
+
+// Example 2:
+//   https://assets.leetcode.com/uploads/2022/02/09/exam2drawio.png
+//		 Input: s = "A1:F1"
+//		 Output: ["A1","B1","C1","D1","E1","F1"]
+// Explanation:
+// The above diagram shows the cells which should be present in the list.
+// The red arrow denotes the order in which the cells should be presented.
+
+// Constraints:
+//    s.length == 5
+//    'A' <= s[0] <= s[3] <= 'Z'
+//    '1' <= s[1] <= s[4] <= '9'
+//    s consists of uppercase English letters, digits and ':'.
+
+const topVotedCellsInRange = (s) => {
+  const [fromLetter, fromNum, , toLetter, toNum] = s;
+  const ret = [];
+  for (
+    let l1 = fromLetter.charCodeAt(0), l2 = toLetter.charCodeAt(0);
+    l1 <= l2;
+    ++l1
+  ) {
+    for (let n1 = +fromNum, n2 = +toNum; n1 <= n2; ++n1)
+      ret.push(String.fromCharCode(l1) + n1);
+  }
+  return ret;
+};
+
+console.log(cellsInRange("K1:L2")); // ["K1","K2","L1","L2"]
+console.log(cellsInRange("A1:F1")); // ["A1","B1","C1","D1","E1","F1"]
+
+// Couldn't get it working, studying top voted
+// Was definitely going for the .charCodeAt approach
