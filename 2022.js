2fb0d66df1e8e91dfa4ee897f4534190bd6e8bd8
MCCP96 <petermccomb@live.com>
2022-06-29 22:25:04 -0400

    Day 378 - Count Hills and Valleys in an Array

--- 2022.js
+++ 2022.js
@@ -9280,7 +9280,7 @@ console.log(findKDistantIndices([3, 4, 9, 1, 3, 9, 5], 9, 1)); // [1,2,3,4,5,6]
 console.log(findKDistantIndices([2, 2, 2, 2, 2], 2, 2)); // [0,1,2,3,4] */
 
 // Divide Array Into Equal Pairs          6/28/2022
-
+/* 
 // You are given an integer array nums consisting of 2 * n integers.
 
 // You need to divide nums into n pairs such that:
@@ -9325,4 +9325,73 @@ const topVotedDivideArray = function (nums) {
   return numMap.size === 0;
 };
 
-// Thought about going the Map route
+// Thought about going the Map route */
+
+// Count Hills and Valleys in an Array          6/29/2022
+
+// You are given a 0-indexed integer array nums. An index i is part of a hill in nums if the closest non-equal neighbors of i are smaller than nums[i]. Similarly, an index i is part of a valley in nums if the closest non-equal neighbors of i are larger than nums[i]. Adjacent indices i and j are part of the same hill or valley if nums[i] == nums[j].
+
+// Note that for an index to be part of a hill or valley, it must have a non-equal neighbor on both the left and right of the index.
+
+// Return the number of hills and valleys in nums.
+
+// Example 1:
+//		 Input: nums = [2,4,1,1,6,5]
+//		 Output: 3
+// Explanation:
+// At index 0: There is no non-equal neighbor of 2 on the left, so index 0 is neither a hill nor a valley.
+// At index 1: The closest non-equal neighbors of 4 are 2 and 1. Since 4 > 2 and 4 > 1, index 1 is a hill.
+// At index 2: The closest non-equal neighbors of 1 are 4 and 6. Since 1 < 4 and 1 < 6, index 2 is a valley.
+// At index 3: The closest non-equal neighbors of 1 are 4 and 6. Since 1 < 4 and 1 < 6, index 3 is a valley, but note that it is part of the same valley as index 2.
+// At index 4: The closest non-equal neighbors of 6 are 1 and 5. Since 6 > 1 and 6 > 5, index 4 is a hill.
+// At index 5: There is no non-equal neighbor of 5 on the right, so index 5 is neither a hill nor a valley.
+// There are 3 hills and valleys so we return 3.
+
+// Example 2:
+//		 Input: nums = [6,6,5,5,4,1]
+//		 Output: 0
+// Explanation:
+// At index 0: There is no non-equal neighbor of 6 on the left, so index 0 is neither a hill nor a valley.
+// At index 1: There is no non-equal neighbor of 6 on the left, so index 1 is neither a hill nor a valley.
+// At index 2: The closest non-equal neighbors of 5 are 6 and 4. Since 5 < 6 and 5 > 4, index 2 is neither a hill nor a valley.
+// At index 3: The closest non-equal neighbors of 5 are 6 and 4. Since 5 < 6 and 5 > 4, index 3 is neither a hill nor a valley.
+// At index 4: The closest non-equal neighbors of 4 are 5 and 1. Since 4 < 5 and 4 > 1, index 4 is neither a hill nor a valley.
+// At index 5: There is no non-equal neighbor of 1 on the right, so index 5 is neither a hill nor a valley.
+// There are 0 hills and valleys so we return 0.
+
+// Constraints:
+//    3 <= nums.length <= 100
+//    1 <= nums[i] <= 100
+
+const countHillValley = (nums) => {
+  let count = 0;
+  nums = [...new Set(nums)];
+
+  for (let i = 1; i < nums.length - 1; i++)
+    if (
+      (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) ||
+      (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])
+    )
+      count++;
+
+  return count;
+};
+console.log(countHillValley([2, 4, 1, 1, 6, 5])); // 3
+console.log(countHillValley([6, 6, 5, 5, 4, 1])); // 0
+
+// Doesn't work for all test cases
+
+const topVotedCountHillValley = function (nums) {
+  let answer = 0;
+  nums = nums.filter((a, i, b) => a !== b[i + 1]); // Remove Flatlands
+
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1]) answer++; // Valley
+    if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) answer++; // Hill
+  }
+
+  return answer;
+};
+
+// I see my mistake
+// Set will remove all duplicates while filter only removes flatlands
