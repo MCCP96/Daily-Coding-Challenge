30c7795733cc35a09b2137f52d62676d77885c76
MCCP96 <petermccomb@live.com>
2022-07-16 07:16:44 -0400

    Day 395 - Min Max Game

--- 2022.js
+++ 2022.js
@@ -10229,7 +10229,7 @@ const topVotedDigitCount = (a) => {
 // I like the use of 'cnt' here, replaces my first if statement */
 
 // Rearrange Characters to Make Target String          7/15/2022
-
+/* 
 // You are given two 0-indexed strings s and target. You can take some letters from s and rearrange them to form new strings.
 
 // Return the maximum number of copies of target that can be formed by taking letters from s and rearranging them.
@@ -10297,4 +10297,54 @@ const topVotedRearrangeCharacters = function (s, target) {
 };
 
 // Very good use of Regex here
-// I much prefer this solution
+// I much prefer this solution */
+
+// Min Max Game          7/16/2022
+
+// You are given a 0-indexed integer array nums whose length is a power of 2.
+
+// Apply the following algorithm on nums:
+//    Let n be the length of nums. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n / 2.
+//    For every even index i where 0 <= i < n / 2, assign the value of newNums[i] as min(nums[2 * i], nums[2 * i + 1]).
+//    For every odd index i where 0 <= i < n / 2, assign the value of newNums[i] as max(nums[2 * i], nums[2 * i + 1]).
+//    Replace the array nums with newNums.
+//    Repeat the entire process starting from step 1.
+
+// Return the last number that remains in nums after applying the algorithm.
+
+// Example 1:
+//   https://assets.leetcode.com/uploads/2022/04/13/example1drawio-1.png
+//		 Input: nums = [1,3,5,2,4,8,2,2]
+//		 Output: 1
+// Explanation: The following arrays are the results of applying the algorithm repeatedly.
+// First: nums = [1,5,4,2]
+// Second: nums = [1,4]
+// Third: nums = [1]
+// 1 is the last remaining number, so we return 1.
+
+// Example 2:
+//		 Input: nums = [3]
+//		 Output: 3
+// Explanation: 3 is already the last remaining number, so we return 3.
+
+// Constraints:
+//    1 <= nums.length <= 1024
+//    1 <= nums[i] <= 10^9
+//    nums.length is a power of 2.
+
+const topVotedMinMaxGame = (nums) => {
+  if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.min(nums[0], nums[1]);
+
+  const newNums = [];
+  for (let i = 0; i < nums.length; i += 4) {
+    newNums.push(Math.min(nums[i], nums[i + 1]));
+    newNums.push(Math.max(nums[i + 2], nums[i + 3]));
+  }
+
+  return topVotedMinMaxGame(newNums);
+};
+console.log(topVotedMinMaxGame([1, 3, 5, 2, 4, 8, 2, 2])); // 1
+console.log(topVotedMinMaxGame([3])); // 3
+
+// No time today
