37df23bf858e09d477c0ac2692326586b0f9a33e
MCCP96 <petermccomb@live.com>
2022-07-17 09:38:58 -0400

    Day 395 - Strong Password Checker II

--- 2022.js
+++ 2022.js
@@ -10300,7 +10300,7 @@ const topVotedRearrangeCharacters = function (s, target) {
 // I much prefer this solution */
 
 // Min Max Game          7/16/2022
-
+/* 
 // You are given a 0-indexed integer array nums whose length is a power of 2.
 
 // Apply the following algorithm on nums:
@@ -10347,4 +10347,84 @@ const topVotedMinMaxGame = (nums) => {
 console.log(topVotedMinMaxGame([1, 3, 5, 2, 4, 8, 2, 2])); // 1
 console.log(topVotedMinMaxGame([3])); // 3
 
-// No time today
+// No time today */
+
+// Strong Password Checker II          7/17/2022
+
+// A password is said to be strong if it satisfies all the following criteria:
+//    It has at least 8 characters.
+//    It contains at least one lowercase letter.
+//    It contains at least one uppercase letter.
+//    It contains at least one digit.
+//    It contains at least one special character. The special characters are the characters in the following string: "!@#$%^&amp;*()-+".
+//    It does not contain 2 of the same character in adjacent positions (i.e., "aab" violates this condition, but "aba" does not).
+
+// Given a string password, return true if it is a strong password. Otherwise, return false.
+
+// Example 1:
+//		 Input: password = "IloveLe3tcode!"
+//		 Output: true
+// Explanation: The password meets all the requirements. Therefore, we return true.
+
+// Example 2:
+//		 Input: password = "Me+You--IsMyDream"
+//		 Output: false
+// Explanation: The password does not contain a digit and also contains 2 of the same character in adjacent positions. Therefore, we return false.
+
+// Example 3:
+//		 Input: password = "1aB!"
+//		 Output: false
+// Explanation: The password does not meet the length requirement. Therefore, we return false.
+
+// Constraints:
+//    1 <= password.length <= 100
+//    password consists of letters, digits, and special characters: "!@#$%^&amp;*()-+".
+
+const strongPasswordCheckerII = (password) =>
+  password.length >= 8 &&
+  password.toUpperCase() !== password &&
+  password.toLowerCase() !== password &&
+  /d/.test(password) &&
+  /!|@|#|\$|%|^|&|\*|(|)|-|\+/.test(password) &&
+  [...password].reduce((a, c, i, arr) => {
+    if (a === false) return false;
+    return c !== arr[i - 1];
+  });
+
+console.log(strongPasswordCheckerII("IloveLe3tcode!")); // true
+console.log(strongPasswordCheckerII("Me+You--IsMyDream")); // false
+console.log(strongPasswordCheckerII("1aB!")); // false
+
+// Doesn't work for all test cases
+
+const ord = (c) => c.charCodeAt();
+const isLowerCase = (c) => {
+  let x = ord(c);
+  return x >= 97 && x <= 122;
+};
+const isUpperCase = (c) => {
+  let x = ord(c);
+  return x >= 65 && x <= 90;
+};
+const isDigit = (c) => "0123456789".indexOf(c) != -1;
+const isSpecial = (c) => "!@#$%^&*()-+".indexOf(c) != -1;
+
+const topVotedStrongPasswordCheckerII = (s) => {
+  let n = s.length,
+    hasLower = false,
+    hasUpper = false,
+    hasDigit = false,
+    hasSpecial = false,
+    adjSame = false;
+  if (n < 8) return false;
+  for (let i = 0; i < n; i++) {
+    if (isLowerCase(s[i])) hasLower = true;
+    if (isUpperCase(s[i])) hasUpper = true;
+    if (isDigit(s[i])) hasDigit = true;
+    if (isSpecial(s[i])) hasSpecial = true;
+    if (i + 1 < n && s[i] == s[i + 1]) adjSame = true;
+  }
+  return hasLower && hasUpper && hasDigit && hasSpecial && !adjSame;
+};
+
+// Works, pretty bulky
