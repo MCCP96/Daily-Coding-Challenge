fa91c382096487fe5c6e0fcfee7e96f4deb5edae
MCCP96 <petermccomb@live.com>
2022-07-18 13:10:40 -0400

    Day 396 - Calculate Amount Paid in Taxes

--- 2022.js
+++ 2022.js
@@ -10350,7 +10350,7 @@ console.log(topVotedMinMaxGame([3])); // 3
 // No time today */
 
 // Strong Password Checker II          7/17/2022
-
+/* 
 // A password is said to be strong if it satisfies all the following criteria:
 //    It has at least 8 characters.
 //    It contains at least one lowercase letter.
@@ -10427,4 +10427,82 @@ const topVotedStrongPasswordCheckerII = (s) => {
   return hasLower && hasUpper && hasDigit && hasSpecial && !adjSame;
 };
 
-// Works, pretty bulky
+// Works, pretty bulky */
+
+// Calculate Amount Paid in Taxes          7/18/2022
+
+// You are given a 0-indexed 2D integer array brackets where brackets[i] = [upperi, percenti] means that the i^th tax bracket has an upper bound of upperi and is taxed at a rate of percenti. The brackets are sorted by upper bound (i.e. upperi-1 < upperi for 0 < i < brackets.length).
+
+// Tax is calculated as follows:
+//    The first upper0 dollars earned are taxed at a rate of percent0.
+//    The next upper1 - upper0 dollars earned are taxed at a rate of percent1.
+//    The next upper2 - upper1 dollars earned are taxed at a rate of percent2.
+//    And so on.
+
+// You are given an integer income representing the amount of money you earned. Return the amount of money that you have to pay in taxes. Answers within 10^-5 of the actual answer will be accepted.
+
+// Example 1:
+//		 Input: brackets = [[3,50],[7,10],[12,25]], income = 10
+//		 Output: 2.65000
+// Explanation:
+// Based on your income, you have 3 dollars in the 1^st tax bracket, 4 dollars in the 2^nd tax bracket, and 3 dollars in the 3^rd tax bracket.
+// The tax rate for the three tax brackets is 50%, 10%, and 25%, respectively.
+// In total, you pay $3 * 50% + $4 * 10% + $3 * 25% = $2.65 in taxes.
+
+// Example 2:
+//		 Input: brackets = [[1,0],[4,25],[5,50]], income = 2
+//		 Output: 0.25000
+// Explanation:
+// Based on your income, you have 1 dollar in the 1^st tax bracket and 1 dollar in the 2^nd tax bracket.
+// The tax rate for the two tax brackets is 0% and 25%, respectively.
+// In total, you pay $1 * 0% + $1 * 25% = $0.25 in taxes.
+
+// Example 3:
+//		 Input: brackets = [[2,50]], income = 0
+//		 Output: 0.00000
+// Explanation:
+// You have no income to tax, so you have to pay a total of $0 in taxes.
+
+// Constraints:
+//    1 <= brackets.length <= 100
+//    1 <= upperi <= 1000
+//    0 <= percenti <= 100
+//    0 <= income <= 1000
+//    upperi is sorted in ascending order.
+//    All the values of upperi are unique.
+//    The upper bound of the last tax bracket is greater than or equal to income.
+
+const calculateTax = (brackets, income, acc = 0) => {
+  let [amt, perc] = brackets.shift();
+  if (amt > income) amt = income;
+  if (brackets.length > 0) brackets[0][0] -= amt;
+  acc += (amt * perc) / 100;
+  income -= amt;
+  return income > 0 ? calculateTax(brackets, income, acc) : acc;
+};
+// prettier-ignore
+console.log(calculateTax([[3,50],[7,10],[12,25]],10)) // 2.65000
+// prettier-ignore
+console.log(calculateTax([[1,0],[4,25],[5,50]],2)) // 0.25000
+console.log(calculateTax([[2, 50]], 0)); // 0.00000
+
+// Doesn't work for all test cases
+// Seems there's a tiny rounding error somewhere
+
+const topVotedCalculateTax = (brackets, income) => {
+  let paid = 0;
+  let prev = 0;
+
+  for (const [amt, percent] of brackets) {
+    const current = Math.min(income, amt - prev);
+    const tax = current * (percent / 100);
+
+    income -= current;
+    paid += tax;
+    prev = amt;
+
+    if (income <= 0) return paid;
+  }
+};
+
+// Much cleaner and logical
