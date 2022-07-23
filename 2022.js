7577cc38a021d67e86510ef5d6985ed13158a0eb
MCCP96 <petermccomb@live.com>
2022-07-23 08:51:11 -0400

    Day 401 - Minimum Amount of Time to Fill Cups

--- 2022.js
+++ 2022.js
@@ -10683,7 +10683,7 @@ console.log(topVotedCheckXMatrix([[2,0,0,1],[0,3,1,0],[0,5,2,0],[4,0,0,2]])) // 
 console.log(topVotedCheckXMatrix([[5,7,0],[0,3,1],[0,5,0]])) // false */
 
 // Decode the Message          7/22/2022
-
+/* 
 // You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
 //    Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
 //    Align the substitution table with the regular English alphabet.
@@ -10757,4 +10757,59 @@ const topVotedDecodeMessage = function (key, message) {
 
 // Using Set to create the key is very clean
 
-// Day 4️⃣0️⃣0️⃣ 🎉
+// Day 4️⃣0️⃣0️⃣ 🎉 */
+
+// Minimum Amount of Time to Fill Cups          7/23/2022
+
+// You have a water dispenser that can dispense cold, warm, and hot water. Every second, you can either fill up 2 cups with different types of water, or 1 cup of any type of water.
+
+// You are given a 0-indexed integer array amount of length 3 where amount[0], amount[1], and amount[2] denote the number of cold, warm, and hot water cups you need to fill respectively. Return the minimum number of seconds needed to fill up all the cups.
+
+// Example 1:
+//		 Input: amount = [1,4,2]
+//		 Output: 4
+// Explanation: One way to fill up the cups is:
+// Second 1: Fill up a cold cup and a warm cup.
+// Second 2: Fill up a warm cup and a hot cup.
+// Second 3: Fill up a warm cup and a hot cup.
+// Second 4: Fill up a warm cup.
+// It can be proven that 4 is the minimum number of seconds needed.
+
+// Example 2:
+//		 Input: amount = [5,4,4]
+//		 Output: 7
+// Explanation: One way to fill up the cups is:
+// Second 1: Fill up a cold cup, and a hot cup.
+// Second 2: Fill up a cold cup, and a warm cup.
+// Second 3: Fill up a cold cup, and a warm cup.
+// Second 4: Fill up a warm cup, and a hot cup.
+// Second 5: Fill up a cold cup, and a hot cup.
+// Second 6: Fill up a cold cup, and a warm cup.
+// Second 7: Fill up a hot cup.
+
+// Example 3:
+//		 Input: amount = [5,0,0]
+//		 Output: 5
+// Explanation: Every second, we fill up a cold cup.
+
+// Constraints:
+//    amount.length == 3
+//    0 <= amount[i] <= 100
+
+const topVotedFillCups = function (amount) {
+  let result = 0;
+  amount.sort((a, b) => a - b);
+  while (amount[1] && amount[2]) {
+    result++;
+    amount[1]--;
+    amount[2]--;
+    amount.sort((a, b) => a - b);
+  }
+  result += amount[2];
+  return result;
+};
+console.log(fillCups([1, 4, 2])); // 4
+console.log(fillCups([5, 4, 4])); // 7
+console.log(fillCups([5, 0, 0])); // 5
+
+// No time today
