fd3d56d2c34157785ea0fabc764c0b44b9359b16
MCCP96 <petermccomb@live.com>
2022-07-27 19:38:21 -0400

    Day 405 - Add Two Numbers

--- 2022.js
+++ 2022.js
@@ -10954,7 +10954,7 @@ const topVotedBestHand = function (ranks, suits) {
 // Basically same logic */
 
 // First Letter to Appear Twice          7/26/2022
-
+/* 
 // Given a string s consisting of lowercase English letters, return the first letter to appear twice.
 
 // Note:
@@ -11004,4 +11004,70 @@ var topVotedRepeatedCharacter = function (s) {
   }
 };
 
-// Same idea
+// Same idea */
+
+// Add Two Numbers          7/27/2022
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Example 1:
+//   https://assets.leetcode.com/uploads/2020/10/02/addtwonumber1.jpg
+//		 Input: l1 = [2,4,3], l2 = [5,6,4]
+//		 Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+// Example 2:
+//		 Input: l1 = [0], l2 = [0]
+//		 Output: [0]
+
+// Example 3:
+//		 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//		 Output: [8,9,9,9,0,0,0,1]
+
+// Constraints:
+//    The number of nodes in each linked list is in the range [1, 100].
+//    0 <= Node.val <= 9
+//    It is guaranteed that the list represents a number that does not have leading zeros.
+
+const addTwoNumbers = (l1, l2) =>
+  [...`${+l1.reverse().join("") + +l2.reverse().join("")}`].reverse();
+
+console.log(addTwoNumbers([2, 4, 3], [5, 6, 4])); // [7,0,8]
+console.log(addTwoNumbers([0], [0])); // [0]
+console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9])); // [8,9,9,9,0,0,0,1]
+
+// Works but Leetcode is returning a runtime error
+
+const topVotedAddTwoNumbers = function (l1, l2) {
+  var List = new ListNode(0);
+  var head = List;
+  var sum = 0;
+  var carry = 0;
+
+  while (l1 !== null || l2 !== null || sum > 0) {
+    if (l1 !== null) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
+  }
+
+  return List.next;
+};
+
+// First time seeing ListNode
