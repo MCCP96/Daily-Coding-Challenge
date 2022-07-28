669baa292bb278f7f3639ed691240b77e67dd80c
MCCP96 <petermccomb@live.com>
2022-07-28 13:01:31 -0400

    Day 406 - Reverse Bits

--- 2022.js
+++ 2022.js
@@ -11007,7 +11007,7 @@ var topVotedRepeatedCharacter = function (s) {
 // Same idea */
 
 // Add Two Numbers          7/27/2022
-
+/* 
 // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
@@ -11070,4 +11070,40 @@ const topVotedAddTwoNumbers = function (l1, l2) {
   return List.next;
 };
 
-// First time seeing ListNode
+// First time seeing ListNode */
+
+// Reverse Bits          7/28/2022
+
+// Reverse bits of a given 32 bits unsigned integer.
+
+// Constraints:	The input must be a binary string of length 32
+
+// Note:
+// 	Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+
+// 	In Java, the compiler represents the signed integers using https://en.wikipedia.org/wiki/Two%27s_complement 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+
+// Example 1:
+//		 Input: n = 00000010100101000001111010011100
+//		 Output:    964176192 (00111001011110000010100101000000)
+// Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+
+// Example 2:
+//		 Input: n = 11111111111111111111111111111101
+//		 Output:   3221225471 (10111111111111111111111111111111)
+// Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+// Constraints:
+//    The input must be a binary string of length 32
+
+const topVotedReverseBits = (n) =>
+  Number.parseInt(
+    n.toString(2).split("").reverse().join("").padEnd(32, "0"),
+    2
+  );
+
+console.log(topVotedReverseBits(00000010100101000001111010011100)); // 964176192
+console.log(topVotedReverseBits(11111111111111111111111111111101)); // 3221225471
+
+// Couldn't get it working
+// The test cases here don't seem to work, but leetcode sees this as a valid solution
