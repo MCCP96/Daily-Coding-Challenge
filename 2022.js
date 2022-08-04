73de3c0fbd5763a248330db2977a8990a5f3eb08
MCCP96 <petermccomb@live.com>
2022-08-04 12:52:39 -0400

    Day 413 - Summary Ranges

--- 2022.js
+++ 2022.js
@@ -11355,7 +11355,7 @@ var otherContainsDuplicate = (nums) =>
   nums.sort().some((a, i) => a === nums[i - 1]); */
 
 // Contains Duplicate II          8/3/2022
-
+/* 
 // Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
 // Example 1:
@@ -11397,4 +11397,80 @@ var topVotedContainsNearbyDuplicate = function (nums, k) {
   return false;
 };
 
-// Very good runtime
+// Very good runtime */
+
+// Summary Ranges          8/4/2022
+
+// You are given a sorted unique integer array nums.
+
+// A range [a,b] is the set of all integers from a to b (inclusive).
+
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range [a,b] in the list should be output as:
+//    "a->b" if a != b
+//    "a" if a == b
+
+// Example 1:
+//		 Input: nums = [0,1,2,4,5,7]
+//		 Output: ["0->2","4->5","7"]
+// Explanation: The ranges are:
+// [0,2] --> "0->2"
+// [4,5] --> "4->5"
+// [7,7] --> "7"
+
+// Example 2:
+//		 Input: nums = [0,2,3,4,6,8,9]
+//		 Output: ["0","2->4","6","8->9"]
+// Explanation: The ranges are:
+// [0,0] --> "0"
+// [2,4] --> "2->4"
+// [6,6] --> "6"
+// [8,9] --> "8->9"
+
+// Constraints:
+//    0 <= nums.length <= 20
+//    -2^31 <= nums[i] <= 2^31 - 1
+//    All the values of nums are unique.
+//    nums is sorted in ascending order.
+
+const summaryRanges = (nums) => {
+  if (nums.length < 1) return [];
+  let prev = nums.shift();
+  let count = prev;
+  return nums
+    .reduce(
+      (a, c, i, arr) => {
+        if (c - 1 !== count) {
+          if (a[a.length - 1] !== count) a.push(`${a.pop()}->${count}`);
+          a.push(c);
+          count = c;
+        } else if (i === arr.length - 1 && c - 1 === count) {
+          a.push(`${a.pop()}->${c}`);
+        } else count++;
+        return a;
+      },
+      [prev]
+    )
+    .map((x) => `${x}`);
+};
+console.log(summaryRanges([0, 1, 2, 4, 5, 7])); // ["0->2","4->5","7"]
+console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9])); // ["0","2->4","6","8->9"]
+console.log(summaryRanges([])); // []
+
+// Quite a cluster, but works
+
+var topVotedSummaryRanges = function (nums) {
+  var t = 0;
+  var ans = [];
+  nums.push("#");
+  for (var i = 1; i < nums.length; i++)
+    if (nums[i] - nums[t] !== i - t) {
+      if (i - t > 1) ans.push(nums[t] + "->" + nums[i - 1]);
+      else ans.push(nums[t].toString());
+      t = i;
+    }
+  return ans;
+};
+
+// Much cleaner
