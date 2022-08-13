6325e3af02084ac9da4957a8eff727571ca938da
MCCP96 <petermccomb@live.com>
2022-08-13 18:51:46 -0400

    Day 422 - Nim Game

--- 2022.js
+++ 2022.js
@@ -11762,7 +11762,7 @@ const topVotedMoveZeroes = (nums) => {
 // Very clean */
 
 // Word Pattern          8/12/2022
-
+/* 
 // Given a pattern and a string s, find if s follows the same pattern.
 
 // Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
@@ -11821,4 +11821,43 @@ const topVotedWordPattern = function (pattern, str) {
   return true;
 };
 
-// Same idea, cleaner code
+// Same idea, cleaner code */
+
+// Nim Game          8/13/2022
+
+// You are playing the following Nim Game with your friend:
+//    Initially, there is a heap of stones on the table.
+//    You and your friend will alternate taking turns, and you go first.
+//    On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+//    The one who removes the last stone is the winner.
+
+// Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.
+
+// Example 1:
+//		 Input: n = 4
+//		 Output: false
+// Explanation: These are the possible outcomes:
+// 1. You remove 1 stone. Your friend removes 3 stones, including the last stone. Your friend wins.
+// 2. You remove 2 stones. Your friend removes 2 stones, including the last stone. Your friend wins.
+// 3. You remove 3 stones. Your friend removes the last stone. Your friend wins.
+// In all outcomes, your friend wins.
+
+// Example 2:
+//		 Input: n = 1
+//		 Output: true
+
+// Example 3:
+//		 Input: n = 2
+//		 Output: true
+
+// Constraints:
+//    1 <= n <= 2^31 - 1
+
+const canWinNim = (n) => n % 4 !== 0;
+
+console.log(canWinNim(4)); // false
+console.log(canWinNim(1)); // true
+console.log(canWinNim(2)); // true
+
+// Multiples of 4 are the only count that promise a loss
+// Same as top voted
