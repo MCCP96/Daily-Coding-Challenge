1b5d1e9286ddae6d20ac27a7cd511ee517872647
MCCP96 <petermccomb@live.com>
2022-08-22 14:37:23 -0400

    Day 431 - Valid Perfect Square

--- 2022.js
+++ 2022.js
@@ -12153,7 +12153,7 @@ const topVotedIntersect = (nums1, nums2) => {
 // Clean */
 
 // Intersection of Two Arrays II          8/21/2022
-
+/* 
 // Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 // Example 1:
@@ -12203,4 +12203,38 @@ const topVotedIntersect = function (nums1, nums2) {
 };
 
 // Wouldn't be my approach, but works
-// Worse runtime
+// Worse runtime */
+
+// Valid Perfect Square          8/22/2022
+
+// Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+// Follow up: Do not use any built-in library function such as sqrt.
+
+// Example 1:
+//		 Input: num = 16
+//		 Output: true
+
+// Example 2:
+//		 Input: num = 14
+//		 Output: false
+
+// Constraints:
+//    1 <= num <= 2^31 - 1
+
+const isPerfectSquare = (num) => {
+  for (let i = 1; i <= num; i++) if (i * i === num) return true;
+  return false;
+};
+console.log(isPerfectSquare(1)); // true
+console.log(isPerfectSquare(16)); // true
+console.log(isPerfectSquare(14)); // false
+
+// Pretty basic solution
+// Definitely not the best runtime
+
+const topVotedIsPerfectSquare = function (num) {
+  return num ** 0.5 == parseInt(num ** 0.5);
+};
+
+// Much better
