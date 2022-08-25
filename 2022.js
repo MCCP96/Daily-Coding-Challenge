b7db1c7d6bb571d36c5321983a0d58cc16b4bb81
MCCP96 <petermccomb@live.com>
2022-08-25 12:57:46 -0400

    Day 434 - Convert a Number to Hexadecimal

--- 2022.js
+++ 2022.js
@@ -12302,7 +12302,7 @@ const topVotedGuessNumber = function (n) {
 // Not great runtime */
 
 // Ransom Note          8/24/2022
-
+/* 
 // Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
 // Each letter in magazine can only be used once in ransomNote.
@@ -12354,4 +12354,52 @@ const topVotedCanConstruct = function (ransomNote, magazine) {
     map[letter]--;
   }
   return true;
+}; */
+
+// Convert a Number to Hexadecimal          8/25/2022
+
+// Given an integer num, return a string representing its hexadecimal representation. For negative integers, https://en.wikipedia.org/wiki/Two%27s_complement two’s complement method is used.
+
+// All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
+
+// Note: You are not allowed to use any built-in library method to directly solve this problem.
+
+// Example 1:
+//		 Input: num = 26
+//		 Output: "1a"
+
+// Example 2:
+//		 Input: num = -1
+//		 Output: "ffffffff"
+
+// Constraints:
+//    -2^31 <= num <= 2^31 - 1
+
+const toHex = (num) =>
+  num >= 0 ? num.toString(16) : (num += Math.pow(2, 32)).toString(16);
+
+console.log(toHex(26)); // "1a"
+console.log(toHex(-1)); // "ffffffff"
+
+// Stole 'num += Math.pow(2, 32)' from top voted submission
+
+const topVotedToHex = function (num) {
+  // prettier-ignore
+  var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  if (num == 0) return "0";
+
+  if (num < 0) {
+    num += Math.pow(2, 32);
+  }
+  var res = "";
+
+  while (num > 0) {
+    var digit = num % 16;
+    res = arr[digit] + res;
+    num = Math.floor(num / 16);
+  }
+  return res;
 };
+
+// I rather '.toString(16)' over having an array
