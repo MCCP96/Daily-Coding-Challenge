53d5845de4646c03770e6c81dcd3a1df21e31ad3
MCCP96 <petermccomb@live.com>
2022-08-31 13:11:08 -0400

    Day 440 - Number of Arithmetic Triplets

--- 2022.js
+++ 2022.js
@@ -12584,7 +12584,7 @@ const topVotedMinimumOperations = (nums) =>
 // Hah, didn't think of that, makes total sense */
 
 // Merge Similar Items          8/30/2022
-
+/* 
 // You are given two 2D integer arrays, items1 and items2, representing two sets of items. Each array items has the following properties:
 //    items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the i^th item.
 //    The value of each item in items is unique.
@@ -12657,4 +12657,59 @@ console.log(oneLineMergeSimilarItems([[1,1],[3,2],[2,3]],[[2,1],[3,2],[1,3]])) /
 // prettier-ignore
 console.log(oneLineMergeSimilarItems([[1,3],[2,2]],[[7,1],[2,2],[1,4]])) // [[1,7],[2,4],[7,1]]
 
-// Top voteds were all very bulky so decided to make a one liner
+// Top voteds were all very bulky so decided to make a one liner */
+
+// Number of Arithmetic Triplets          8/31/2022
+
+// You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. A triplet (i, j, k) is an arithmetic triplet if the following conditions are met:
+//    i < j < k,
+//    nums[j] - nums[i] == diff, and
+//    nums[k] - nums[j] == diff.
+
+// Return the number of unique arithmetic triplets.
+
+// Example 1:
+//		 Input: nums = [0,1,4,6,7,10], diff = 3
+//		 Output: 2
+// Explanation:
+// (1, 2, 4) is an arithmetic triplet because both 7 - 4 == 3 and 4 - 1 == 3.
+// (2, 4, 5) is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3.
+
+// Example 2:
+//		 Input: nums = [4,5,6,7,8,9], diff = 2
+//		 Output: 2
+// Explanation:
+// (0, 2, 4) is an arithmetic triplet because both 8 - 6 == 2 and 6 - 4 == 2.
+// (1, 3, 5) is an arithmetic triplet because both 9 - 7 == 2 and 7 - 5 == 2.
+
+// Constraints:
+//    3 <= nums.length <= 200
+//    0 <= nums[i] <= 200
+//    1 <= diff <= 50
+//    nums is strictly increasing.
+
+const arithmeticTriplets = (nums, diff) => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++)
+    for (let j = i + 1; j < nums.length; j++)
+      if (nums[j] - nums[i] === diff)
+        for (let k = j + 1; k < nums.length; k++)
+          if (nums[k] - nums[j] === diff) count++;
+  return count;
+};
+console.log(arithmeticTriplets([0, 1, 4, 6, 7, 10], 3)); // 2
+console.log(arithmeticTriplets([4, 5, 6, 7, 8, 9], 2)); // 2
+
+// Good memory, ok runtime
+
+const topVotedArithmeticTriplets = function (nums, diff) {
+  let count = 0;
+  for (let num of nums) {
+    if (nums.includes(num + diff) && nums.includes(num + diff * 2)) {
+      count++;
+    }
+  }
+  return count;
+};
+
+// Hah, nice
