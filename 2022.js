d2cb00b7e250b15a0e5c05adef19b90a63b9d12f
MCCP96 <petermccomb@live.com>
2022-09-01 14:10:32 -0400

    Day 441 - Largest Local Values in a Matrix

--- 2022.js
+++ 2022.js
@@ -12660,7 +12660,7 @@ console.log(oneLineMergeSimilarItems([[1,3],[2,2]],[[7,1],[2,2],[1,4]])) // [[1,
 // Top voteds were all very bulky so decided to make a one liner */
 
 // Number of Arithmetic Triplets          8/31/2022
-
+/* 
 // You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. A triplet (i, j, k) is an arithmetic triplet if the following conditions are met:
 //    i < j < k,
 //    nums[j] - nums[i] == diff, and
@@ -12712,4 +12712,79 @@ const topVotedArithmeticTriplets = function (nums, diff) {
   return count;
 };
 
-// Hah, nice
+// Hah, nice */
+
+// Largest Local Values in a Matrix          9/1/2022
+
+// You are given an n x n integer matrix grid.
+
+// Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:	maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+
+// In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+
+// Return the generated matrix.
+
+// Example 1:
+//   https://assets.leetcode.com/uploads/2022/06/21/ex1.png
+//		 Output: [[9,9],[8,6]]
+// Explanation: The diagram above shows the original matrix and the generated matrix.
+// Notice that each value in the generated matrix corresponds to the largest value of a contiguous 3 x 3 matrix in grid.
+
+// Example 2:
+//   https://assets.leetcode.com/uploads/2022/07/02/ex2new2.png
+//		 Input: grid = [[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]]
+//		 Output: [[2,2,2],[2,2,2],[2,2,2]]
+// Explanation: Notice that the 2 is contained within every contiguous 3 x 3 matrix in grid.
+
+// Constraints:
+//    n == grid.length == grid[i].length
+//    3 <= n <= 100
+//    1 <= grid[i][j] <= 100
+
+const largestLocal = (grid) => {
+  let ans = [];
+  for (let i = 1; i < grid.length - 1; i++) {
+    let row = [];
+    for (let j = 1; j < grid.length - 1; j++) {
+      row.push(
+        Math.max(
+          grid[i - 1][j - 1],
+          grid[i - 1][j],
+          grid[i - 1][j + 1],
+          grid[i][j - 1],
+          grid[i][j],
+          grid[i][j + 1],
+          grid[i + 1][j - 1],
+          grid[i + 1][j],
+          grid[i + 1][j + 1]
+        )
+      );
+    }
+    ans.push(row);
+  }
+  return ans;
+};
+// prettier-ignore
+console.log(largestLocal([[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]])) // [[9,9],[8,6]]
+// prettier-ignore
+console.log(largestLocal([[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]])) // [[2,2,2],[2,2,2],[2,2,2]]
+
+// pretty bulky Math.max, but works
+// 90% runtime
+
+const topVotedLargestLocal = function (grid, count = 2) {
+  let n = grid.length;
+  let arr = [];
+  for (let i = 0; i < n - 1; i++) arr[i] = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      arr[i][j] = Math.max(
+        grid[i][j],
+        Math.max(grid[i][j + 1], Math.max(grid[i + 1][j], grid[i + 1][j + 1]))
+      );
+    }
+  }
+  return --count == 0 ? arr : largestLocal(arr, count);
+};
+
+// Most top voteds were identical to mine except this one
