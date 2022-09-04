0a2477e82f60e935fc9c2a27e9f42aaee840714c
MCCP96 <petermccomb@live.com>
2022-09-04 08:58:00 -0400

    Day 444 - Longest Subsequence With Limited Sum

--- 2022.js
+++ 2022.js
@@ -12854,7 +12854,7 @@ const topVotedMinimumRecolors = function (blocks, k) {
 // min_count optimizes recolors but impacts runtime */
 
 // Minimum Hours of Training to Win a Competition          9/3/2022
-
+/* 
 // You are entering a competition, and are given two positive integers initialEnergy and initialExperience denoting your initial energy and initial experience respectively.
 
 // You are also given two 0-indexed integer arrays energy and experience, both of length n.
@@ -12932,4 +12932,52 @@ const topVotedMinNumberOfHours = function(initialEnergy, initialExperience, ener
   return count
 };
 
-// Not a fan of this question
+// Not a fan of this question */
+
+// Longest Subsequence With Limited Sum          9/4/2022
+
+// You are given an integer array nums of length n, and an integer array queries of length m.
+
+// Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+
+// A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+// Example 1:
+//		 Input: nums = [4,5,2,1], queries = [3,10,21]
+//		 Output: [2,3,4]
+// Explanation: We answer the queries as follows:
+// - The subsequence [2,1] has a sum less than or equal to 3. It can be proven that 2 is the maximum size of such a subsequence, so answer[0] = 2.
+// - The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+// - The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven that 4 is the maximum size of such a subsequence, so answer[2] = 4.
+
+// Example 2:
+//		 Input: nums = [2,3,4,5], queries = [1]
+//		 Output: [0]
+// Explanation: The empty subsequence is the only subsequence that has a sum less than or equal to 1, so answer[0] = 0.
+
+// Constraints:
+//    n == nums.length
+//    m == queries.length
+//    1 <= n, m <= 1000
+//    1 <= nums[i], queries[i] <= 10^6
+
+const topVotedAnswerQueries = (nums, queries) => {
+  nums.sort((a, b) => a - b);
+  let arr = [];
+  for (let query of queries) {
+    let count = 0,
+      sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (sum + nums[i] <= query) {
+        sum += nums[i];
+        count++;
+      }
+    }
+    arr.push(count);
+  }
+  return arr;
+};
+console.log(answerQueries([4, 5, 2, 1], [3, 10, 21])); // [2,3,4]
+console.log(answerQueries([2, 3, 4, 5], [1])); // [0]
+
+// No time today
