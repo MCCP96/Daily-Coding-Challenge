8401d992aee373aed4498bf20fb1226f7ebf80ab
MCCP96 <petermccomb@live.com>
2022-09-05 16:55:38 -0400

    Day 445 - Find Subarrays With Equal Sum

--- 2022.js
+++ 2022.js
@@ -12935,7 +12935,7 @@ const topVotedMinNumberOfHours = function(initialEnergy, initialExperience, ener
 // Not a fan of this question */
 
 // Longest Subsequence With Limited Sum          9/4/2022
-
+/* 
 // You are given an integer array nums of length n, and an integer array queries of length m.
 
 // Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
@@ -12980,4 +12980,61 @@ const topVotedAnswerQueries = (nums, queries) => {
 console.log(answerQueries([4, 5, 2, 1], [3, 10, 21])); // [2,3,4]
 console.log(answerQueries([2, 3, 4, 5], [1])); // [0]
 
-// No time today
+// No time today */
+
+// Find Subarrays With Equal Sum          9/5/2022
+
+// Given a 0-indexed integer array nums, determine whether there exist two subarrays of length 2 with equal sum. Note that the two subarrays must begin at different indices.
+
+// Return true if these subarrays exist, and false otherwise.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+//		 Input: nums = [4,2,4]
+//		 Output: true
+// Explanation: The subarrays with elements [4,2] and [2,4] have the same sum of 6.
+
+// Example 2:
+//		 Input: nums = [1,2,3,4,5]
+//		 Output: false
+// Explanation: No two subarrays of size 2 have the same sum.
+
+// Example 3:
+//		 Input: nums = [0,0,0]
+//		 Output: true
+// Explanation: The subarrays [nums[0],nums[1]] and [nums[1],nums[2]] have the same sum of 0.
+// Note that even though the subarrays have the same content, the two subarrays are considered different because they are in different positions in the original array.
+
+// Constraints:
+//    2 <= nums.length <= 1000
+//    -10^9 <= nums[i] <= 10^9
+
+const findSubarrays = (nums) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    const cur = nums[i] + nums[i + 1];
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      if (nums[j] + nums[j + 1] === cur) return true;
+    }
+  }
+  return false;
+};
+console.log(findSubarrays([4, 2, 4])); // true
+console.log(findSubarrays([1, 2, 3, 4, 5])); // false
+console.log(findSubarrays([0, 0, 0])); // true
+console.log(findSubarrays([0, 0])); // false
+console.log(findSubarrays([1, 2, 3, 1, 0, 2])); // false
+
+// used for loop to return true asap
+
+const topVotedFindSubarrays = function (nums) {
+  const sums = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    let tot = nums[i] + nums[i + 1];
+    if (sums.has(tot)) return true;
+    sums.add(tot);
+  }
+  return false;
+};
+
+// Nice
