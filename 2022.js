f131c0b5f63d168a80c8f45338d9a358b5077915
MCCP96 <petermccomb@live.com>
2022-09-07 10:19:37 -0400

    Day 447 - Longest Substring Without Repeating Char

--- 2022.js
+++ 2022.js
@@ -13040,7 +13040,7 @@ const topVotedFindSubarrays = function (nums) {
 // Nice */
 
 // Check Distances Between Same Letters          9/6/2022
-
+/* 
 // You are given a 0-indexed string s consisting of only lowercase English letters, where each letter in s appears exactly twice. You are also given a 0-indexed integer array distance of length 26.
 
 // Each letter in the alphabet is numbered from 0 to 25 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, ... , 'z' -> 25).
@@ -13102,4 +13102,59 @@ const topVotedCheckDistances = function (s, distance) {
   return true;
 };
 
-// Bit bulkier, but works
+// Bit bulkier, but works */
+
+// Longest Substring Without Repeating Characters          9/7/2022
+
+// Given a string s, find the length of the longest substring without repeating characters.
+
+// Example 1:
+//		 Input: s = "abcabcbb"
+//		 Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+//		 Input: s = "bbbbb"
+//		 Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+//		 Input: s = "pwwkew"
+//		 Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+// Constraints:
+//    0 <= s.length <= 5 * 10^4
+//    s consists of English letters, digits, symbols and spaces.
+
+const lengthOfLongestSubstring = (s) => {
+  let ans = [];
+  for (let i = 0; i < s.length; i++) {
+    let cur = [];
+    for (let j = i; j < s.length; j++) {
+      if (cur.includes(s[j])) break;
+      cur.push(s[j]);
+    }
+    if (cur.length > ans.length) ans = cur;
+  }
+  return ans.length;
+};
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
+
+// Terrible runtime
+
+const topVotedLengthOfLongestSubstring = (s) => {
+  const map = {};
+  var left = 0;
+
+  return s.split("").reduce((max, v, i) => {
+    left = map[v] >= left ? map[v] + 1 : left;
+    map[v] = i;
+    return Math.max(max, i - left + 1);
+  }, 0);
+};
+
+// Very nice!
