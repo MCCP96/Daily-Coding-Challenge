b24ab9e44616a6d5d176f61e1df625de2557ae45
MCCP96 <petermccomb@live.com>
2022-09-13 11:25:42 -0400

    Day 453 - 3Sum

--- 2022.js
+++ 2022.js
@@ -13410,7 +13410,7 @@ const topVotedMaxArea = (H) => {
 // Smart to work in from left and right */
 
 // Integer to Roman          9/12/2022
-
+/* 
 // Roman numerals are represented by seven different symbols:I, V, X, L, C, D and M.
 // Symbol       Value
 // I             1
@@ -13469,7 +13469,7 @@ const intToRoman = (num, ans = "") => {
 console.log(intToRoman(3)); // "III"
 console.log(intToRoman(58)); // "LVIII"
 console.log(intToRoman(1994)); // "MCMXCIV"
-console.log(intToRoman(1000)); // "Mm"
+console.log(intToRoman(1000)); // "M"
 
 // Disgusting block of if/elses and return statement
 // Better than 100% of submissions runtimes
@@ -13483,4 +13483,95 @@ const topVotedIntToRoman = (N) => {
   return ans;
 };
 
-// Much more concise
+// Much more concise */
+
+// 3Sum          9/13/2022
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+// Example 1:
+//		 Input: nums = [-1,0,1,2,-1,-4]
+//		 Output: [[-1,-1,2],[-1,0,1]]
+// Explanation:
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+
+// Example 2:
+//		 Input: nums = [0,1,1]
+//		 Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+
+// Example 3:
+//		 Input: nums = [0,0,0]
+//		 Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+// Constraints:
+//    3 <= nums.length <= 3000
+//    -10^5 <= nums[i] <= 10^5
+
+const threeSum = (nums) => {
+  let [acc, ans] = [{}, []];
+  if (nums.length < 3) return ans;
+
+  for (let i = 0; i < nums.length - 2; i++)
+    for (let j = i + 1; j < nums.length - 1; j++)
+      if (
+        nums.slice(j + 1).includes(-(nums[i] + nums[j])) &&
+        !acc[
+          [nums[i], nums[j], -(nums[i] + nums[j])]
+            .sort((a, b) => a - b)
+            .join("")
+        ]
+      ) {
+        ans.push([nums[i], nums[j], -(nums[i] + nums[j])]);
+        acc[ans[ans.length - 1].sort((a, b) => a - b).join("")] = true;
+      }
+
+  return ans;
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); // [[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0, 1, 1])); // []
+console.log(threeSum([0, 0, 0])); // [[0,0,0]]
+
+// Exceeds runtime limit
+// Pretty nasty if statement here
+
+const topVotedThreeSum = (nums) => {
+  const results = [];
+
+  if (nums.length < 3) return results;
+  nums = nums.sort((a, b) => a - b);
+  let target = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > target) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let j = i + 1;
+    let k = nums.length - 1;
+
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+      if (sum === target) {
+        results.push([nums[i], nums[j], nums[k]]);
+        while (nums[j] === nums[j + 1]) j++;
+        while (nums[k] === nums[k - 1]) k--;
+        j++;
+        k--;
+      } else if (sum < target) j++;
+      else k--;
+    }
+  }
+
+  return results;
+};
+
+// Amazing explanation:
+// https://leetcode.com/problems/3sum/discuss/281302/JavaScript-with-lots-of-explanatory-comments!
