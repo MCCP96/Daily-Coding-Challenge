0e5a9e2adcab38923773696da8bbda984bbc3f6c
mccp96 <petermccomb@live.com>
2022-09-26 08:19:07 -0400

    Day 466 - Goat Latin

--- 2022.js
+++ 2022.js
@@ -14194,7 +14194,7 @@ console.log(minTimeToVisitAllPoints([[3,2],[-2,2]])) // 5
 // same as top voted */
 
 // Binary Prefix Divisible By 5          9/25/2022
-
+/* 
 // You are given a binary array nums (0-indexed).
 
 // We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).	For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
@@ -14225,4 +14225,57 @@ console.log(prefixesDivBy5([1, 1, 1])); // [false,false,false]
 
 const prefixesDivBy5 = (A, acc = 0) => A.map((d) => !(acc = (acc * 2 + d) % 5));
 
-// * 2 instead of parseInt is nice
+// * 2 instead of parseInt is nice */
+
+// Goat Latin          9/26/2022
+
+// You are given a string sentence that consist of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+// We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.) The rules of Goat Latin are as follows:	If a word begins with a vowel ('a', 'e', 'i', 'o', or 'u'), append "ma" to the end of the word.			For example, the word "apple" becomes "applema".			If a word begins with a consonant (i.e., not a vowel), remove the first letter and append it to the end, then add "ma".			For example, the word "goat" becomes "oatgma".			Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.			For example, the first word gets "a" added to the end, the second word gets "aa" added to the end, and so on.
+
+// Return the final sentence representing the conversion from sentence to Goat Latin.
+
+// Example 1:
+//		 Input: sentence = "I speak Goat Latin"
+//		 Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+
+// Example 2:
+//		 Input: sentence = "The quick brown fox jumped over the lazy dog"
+//		 Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+
+// Constraints:
+//    1 <= sentence.length <= 150
+//    sentence consists of English letters and spaces.
+//    sentence has no leading or trailing spaces.
+//    All the words in sentence are separated by a single space.
+
+const vowels = ["a", "e", "i", "o", "u"];
+const toGoatLatin = (sentence) =>
+  sentence.split(" ").reduce((a, c, i, arr) => {
+    const ma = "maa" + "a".repeat(i);
+    if (vowels.includes(c[0].toLowerCase())) c += ma;
+    else {
+      c = [...c];
+      const first = c.shift();
+      c = c.join("") + first + ma;
+    }
+    return `${i === 0 ? c : a + " " + c}`;
+  }, "");
+
+console.log(toGoatLatin("I speak Goat Latin")); // "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+console.log(toGoatLatin("The quick brown fox jumped over the lazy dog")); // "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+
+// Bad runtime, but works
+
+const topVotedToGoatLatin = function (S) {
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  return S.split(" ")
+    .map((w, i) =>
+      vowels.has(w[0])
+        ? w + "ma" + "a".repeat(i + 1)
+        : w.slice(1) + w[0] + "ma" + "a".repeat(i + 1)
+    )
+    .join(" ");
+};
+
+// Pretty much identical logic
