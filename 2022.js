bc8208f18c6749bd401065034b25d5cd9d747e5e
mccp96 <petermccomb@live.com>
2022-11-15 10:28:08 -0500

    Day 516 - Number of Distinct Averages

--- 2022.js
+++ 2022.js
@@ -16872,7 +16872,7 @@ const topVotedAverageValue = function (nums) {
 }; */
 
 // Apply Operations to an Array          11/14/2022
-
+/* 
 // You are given a 0-indexed array nums of size n consisting of non-negative integers.
 
 // You need to apply n - 1 operations to this array where, in the i^th operation (0-indexed), you will apply the following on the i^th element of nums:	If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
@@ -16921,4 +16921,51 @@ const topVotedApplyOperations = function (nums) {
   return nums.sort((a, b) => !a - !b);
 };
 
-// '!a - !b' is what I was looking for
+// '!a - !b' is what I was looking for */
+
+// Number of Distinct Averages          11/15/2022
+
+// You are given a 0-indexed integer array nums of even length.
+
+// As long as nums is not empty, you must repetitively:	Find the minimum number in nums and remove it.	Find the maximum number in nums and remove it.	Calculate the average of the two removed numbers.
+
+// The average of two numbers a and b is (a + b) / 2.	For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+
+// Return the number of distinct averages calculated using the above process.
+
+// Note that when there is a tie for a minimum or maximum number, any can be removed.
+
+// Example 1:
+//		 Input: nums = [4,1,4,0,3,5]
+//		 Output: 2
+// Explanation:
+// 1. Remove 0 and 5, and the average is (0 + 5) / 2 = 2.5. Now, nums = [4,1,4,3].
+// 2. Remove 1 and 4. The average is (1 + 4) / 2 = 2.5, and nums = [4,3].
+// 3. Remove 3 and 4, and the average is (3 + 4) / 2 = 3.5.
+// Since there are 2 distinct numbers among 2.5, 2.5, and 3.5, we return 2.
+
+// Example 2:
+//		 Input: nums = [1,100]
+//		 Output: 1
+// Explanation:
+// There is only one average to be calculated after removing 1 and 100, so we return 1.
+
+// Constraints:
+//    2 <= nums.length <= 100
+//    nums.length is even.
+//    0 <= nums[i] <= 100
+
+const distinctAverages = (nums) => {
+  let avgs = new Set();
+  nums.sort((a, b) => a - b);
+  while (nums.length > 1) {
+    const cur = [nums.shift(), nums.pop()];
+    avgs.add((cur[0] + cur[1]) / 2);
+  }
+  return avgs.size;
+};
+console.log(distinctAverages([4, 1, 4, 0, 3, 5])); // 2
+console.log(distinctAverages([1, 100])); // 1
+
+// Ok runtime
+// Same as top voted
