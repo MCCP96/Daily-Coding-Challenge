ca613e512ddef68fc3fa8abe3e5bd866a7b56d16
MCCP96 <petermccomb@live.com>
2022-11-17 20:47:43 -0500

    Day 518 - Convert the Temperature

--- 2022.js
+++ 2022.js
@@ -16971,7 +16971,7 @@ console.log(distinctAverages([1, 100])); // 1
 // Same as top voted */
 
 // Count Numbers with Unique Digits          11/16/2022
-
+/* 
 // Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10^n.
 
 // Example 1:
@@ -17000,4 +17000,34 @@ console.log(countNumbersWithUniqueDigits(3)); // 739
 console.log(countNumbersWithUniqueDigits(2)); // 91
 console.log(countNumbersWithUniqueDigits(0)); // 1
 
-// Math solution
+// Math solution */
+
+// Convert the Temperature          11/17/2022
+
+// You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+
+// You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+
+// Return the array ans. Answers within 10^-5 of the actual answer will be accepted.
+
+// Note that:	Kelvin = Celsius + 273.15	Fahrenheit = Celsius * 1.80 + 32.00
+
+// Example 1:
+//		 Input: celsius = 36.50
+//		 Output: [309.65000,97.70000]
+// Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+// Example 2:
+//		 Input: celsius = 122.11
+//		 Output: [395.26000,251.79800]
+// Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+
+// Constraints:
+//    0 <= celsius <= 1000
+
+const convertTemperature = (celsius) => [celsius + 273.15, celsius * 1.8 + 32];
+
+console.log(convertTemperature(36.5)); // [309.65000,97.70000]
+console.log(convertTemperature(122.11)); // [395.26000,251.79800]
+
+// Same as top voted
