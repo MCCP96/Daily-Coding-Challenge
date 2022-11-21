6ab5dbbafe39e59d33b5028bc522f7bf3eeb44fc
MCCP96 <petermccomb@live.com>
2022-11-20 21:13:54 -0500

    Day 521 - Find Peak Element

--- 2022.js
+++ 2022.js
@@ -17082,7 +17082,7 @@ function topVotedIsSameTree(p, q) {
 // I will learn to do these questions and prototype questions over winter break (Dec 19th), After my last final exam */
 
 // Sort Colors          11/19/2022
-
+/* 
 // Given an array nums with n objects colored red, white, or blue, sort them https://en.wikipedia.org/wiki/In-place_algorithm so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
 // We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
@@ -17147,4 +17147,48 @@ const revisedSortColors = (nums) => {
     (a, c) => [...a, ...Array(c[1]).fill(+c[0])],
     []
   );
+}; */
+
+// Find Peak Element          11/20/2022
+
+// A peak element is an element that is strictly greater than its neighbors.
+
+// Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+// You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+// You must write an algorithm that runs in O(log n) time.
+
+// Example 1:
+//		 Input: nums = [1,2,3,1]
+//		 Output: 2
+// Explanation: 3 is a peak element and your function should return the index number 2.
+
+// Example 2:
+//		 Input: nums = [1,2,1,3,5,6,4]
+//		 Output: 5
+// Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+// Constraints:
+//    1 <= nums.length <= 1000
+//    -2^31 <= nums[i] <= 2^31 - 1
+//    nums[i] != nums[i + 1] for all valid i.
+
+const findPeakElement = (nums) => {
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+  }
+  return nums[0] > nums[1] ? 0 : nums.length - 1;
+};
+console.log(findPeakElement([1, 2, 3, 1])); // 2
+console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4])); // 5
+console.log(findPeakElement([1])); // 0
+
+// Faster than 90%
+
+const topVotedFindPeakElement = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i + 1]) return i;
+  }
+  return nums.length - 1;
 };
