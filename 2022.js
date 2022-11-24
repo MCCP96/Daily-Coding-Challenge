db56ff4a00d554803122c30dd323f25c280aa960
MCCP96 <petermccomb@live.com>
2022-11-23 23:44:00 -0500

    Day 524 - Path Sum II

--- 2022.js
+++ 2022.js
@@ -17253,7 +17253,7 @@ var topVotedMaxProduct = function (nums) {
 // Clean */
 
 // Group Shifted Strings          11/22/2022
-
+/* 
 // We can shift a string by shifting each of its letters to its successive letter.	For example, "abc" can be shifted to be "bcd".
 
 // We can keep shifting the string to form a sequence.	For example, we can keep shifting "abc" to form the sequence: "abc" -> "bcd" -> ... -> "xyz".
@@ -17300,4 +17300,45 @@ const topVotedGroupStrings = (strs) => {
   return Object.values(res);
 };
 
-// Close, but not really
+// Close, but not really */
+
+// Path Sum II          11/23/2022
+
+// Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+// A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+// Example 1:
+//   https://assets.leetcode.com/uploads/2021/01/18/pathsumii1.jpg
+//		 Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+//		 Output: [[5,4,11,2],[5,8,4,5]]
+// Explanation: There are two paths whose sum equals targetSum:
+// 5 + 4 + 11 + 2 = 22
+// 5 + 8 + 4 + 5 = 22
+
+// Example 2:
+//   https://assets.leetcode.com/uploads/2021/01/18/pathsum2.jpg
+//		 Input: root = [1,2,3], targetSum = 5
+//		 Output: []
+
+// Example 3:
+//		 Input: root = [1,2], targetSum = 0
+//		 Output: []
+
+// Constraints:
+//    The number of nodes in the tree is in the range [0, 5000].
+//    -1000 <= Node.val <= 1000
+//    -1000 <= targetSum <= 1000
+
+const topVotedPathSum = function (root, sum, res = [], path = []) {
+  if (root) {
+    path.push(root.val);
+    if (!root.left && !root.right && sum - root.val === 0) res.push([...path]);
+    topVotedPathSum(root.left, sum - root.val, res, path);
+    topVotedPathSum(root.right, sum - root.val, res, path);
+    path.pop();
+  }
+  return res;
+};
+
+// No time today
