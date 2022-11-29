2f2e64c1d8f907733853b2faee37569d47f8eacd
MCCP96 <petermccomb@live.com>
2022-11-28 23:16:02 -0500

    Day 529 - Find the Pivot Integer

--- 2022.js
+++ 2022.js
@@ -17459,7 +17459,7 @@ console.log(topVotedIntegerBreak(2)); // 1
 console.log(topVotedIntegerBreak(10)); // 36 */
 
 // Top K Frequent Words          11/27/2022
-
+/*
 // Given an array of strings words and an integer k, return the k most frequent strings.
 
 // Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
@@ -17515,4 +17515,54 @@ const topVotedTopKFrequent = (words, k) => {
   return result.slice(0, k);
 };
 
-// Much cleaner
+// Much cleaner */
+
+// Find the Pivot Integer          11/28/2022
+
+// Given a positive integer n, find the pivot integer x such that:	The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+
+// Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+
+// Example 1:
+//		 Input: n = 8
+//		 Output: 6
+// Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+
+// Example 2:
+//		 Input: n = 1
+//		 Output: 1
+// Explanation: 1 is the pivot integer since: 1 = 1.
+
+// Example 3:
+//		 Input: n = 4
+//		 Output: -1
+// Explanation: It can be proved that no such integer exist.
+
+// Constraints:
+//    1 <= n <= 1000
+
+const pivotInteger = (n) => {
+  let r = 0;
+  for (let i = 1; i <= n; i++) r += i;
+  let l = 0;
+  for (let i = 1, j = n; i <= n; i++, j--) {
+    (l += i), (r -= i - 1);
+    if (l === r) return i;
+  }
+  return -1;
+};
+console.log(pivotInteger(8)); // 6
+console.log(pivotInteger(1)); // 1
+console.log(pivotInteger(4)); // -1
+
+// Sliding window
+
+const sumOfRange = (l, r) => ((l + r) * (r - l + 1)) / 2;
+const topVotedPivotInteger = (n) => {
+  for (let x = 1; x <= n; x++) {
+    if (sumOfRange(1, x) == sumOfRange(x, n)) return x;
+  }
+  return -1;
+};
+
+// bit slower
