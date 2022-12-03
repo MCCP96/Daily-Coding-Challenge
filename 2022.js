2e57b3bcad0369f96f14684ab1c45cb038c8d226
MCCP96 <petermccomb@live.com>
2022-12-03 14:26:28 -0500

    Day 534 - Advent of code - Day 3: Rucksack Reorg

--- 2022.js
+++ 2022.js
@@ -17738,7 +17738,7 @@ console.log(
 // Challenges become available everyday at midnight */
 
 // Advent of code - Day 2: Rock Paper Scissors         12/2/2022
-
+/* 
 // Rock Paper Scissors is a game between two players. Each game contains many rounds; in each round, the players each simultaneously choose one of Rock, Paper, or Scissors using a hand shape. Then, a winner for that round is selected: Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock. If both players choose the same shape, the round instead ends in a draw.
 
 // Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input) that they say will be sure to help you win. "The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors. The second column--" Suddenly, the Elf is called away to help with someone's tent.
@@ -17807,4 +17807,118 @@ console.log(rps2(["AY", "BX", "CZ"])); // 12
 // There are 2 problems available every day
 // Finish the first to unlock the second
 
-// Wish I could see how others are answering these!
+// Wish I could see how others are answering these! */
+
+// Advent of code - Day 3: Rucksack Reorganization          12/3/2022
+
+// One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey. Unfortunately, that Elf didn't quite follow the packing instructions, and so a few items now need to be rearranged.
+
+// Each rucksack has two large compartments. All items of a given type are meant to go into exactly one of the two compartments. The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.
+
+// The Elves have made a list of all of the items currently in each rucksack (your puzzle input), but they need your help finding the errors. Every item type is identified by a single lowercase or uppercase letter (that is, a and A refer to different types of items).
+
+// The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the same number of items in each of its two compartments, so the first half of the characters represent items in the first compartment, while the second half of the characters represent items in the second compartment.
+
+// For example, suppose you have the following list of contents from six rucksacks:
+
+// vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw
+
+// The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that appears in both compartments is lowercase p.
+// The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that appears in both compartments is uppercase L.
+// The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
+// The fourth rucksack's compartments only share item type v.
+// The fifth rucksack's compartments only share item type t.
+// The sixth rucksack's compartments only share item type s.
+// To help prioritize item rearrangement, every item type can be converted to a priority:
+
+// Lowercase item types a through z have priorities 1 through 26.
+// Uppercase item types A through Z have priorities 27 through 52.
+// In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
+
+// Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+
+const rucksack = (supplies) =>
+  supplies.reduce((a, c) => {
+    let sharedItem = null;
+    const [p1, p2] = [c.substring(0, c.length / 2), c.substring(c.length / 2)];
+    for (let i = 0; i < p1.length; i++)
+      if (p1.includes(p2[i])) {
+        sharedItem = p2[i];
+        break;
+      }
+    sharedItem = sharedItem.charCodeAt(0) - 96;
+    if (sharedItem < 0) sharedItem += 58;
+    return (a += sharedItem);
+  }, 0);
+
+console.log(
+  rucksack([
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw",
+  ])
+); // 157
+
+// As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+// For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+// The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+// Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+// Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+// vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// And the second group's rucksacks are the next three lines:
+
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw
+// In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+// Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+// Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+
+const badges = (supplies) => {
+  let badgeList = [];
+  for (let i = 0; i < supplies.length - 2; i += 3) {
+    const r = [supplies[i], supplies[i + 1], supplies[i + 2]];
+    r.sort((a, b) => a.length - b.length);
+    for (let j = 0; j < r[0].length; j++)
+      if (r[1].includes(r[0][j]) && r[2].includes(r[0][j])) {
+        badgeList.push(r[0][j]);
+        break;
+      }
+  }
+  return badgeList.reduce((a, badge) => {
+    badge = badge.charCodeAt(0) - 96;
+    if (badge < 0) badge += 58;
+    return (a += badge);
+  }, 0);
+};
+
+console.log(
+  badges([
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw",
+  ])
+); // 157
+
+// I feel like this one had a bit more depth than the last two, which is nice
+// Solving problems is painting an ASCII picture on the website!
