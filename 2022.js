82723c9ad43b94642197c2ac0e713777044e432f
MCCP96 <petermccomb@live.com>
2022-12-06 11:29:41 -0500

    Day 537 - Advent of code - Day 6: Tuning Trouble

--- 2022.js
+++ 2022.js
@@ -18027,7 +18027,7 @@ console.log(
 ); // 4 */
 
 // Advent of code - Day 5: Supply Stacks          12/5/2022
-
+/* 
 // The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in stacks of marked crates, but because the needed supplies are buried under many other crates, the crates need to be rearranged.
 
 // The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
@@ -18161,4 +18161,70 @@ console.log(
       [1, 1, 2],
     ]
   )
-); // MCD
+); // MCD */
+
+// Advent of code - Day 6: Tuning Trouble          12/6/2022
+
+// The preparations are finally complete; you and the Elves leave camp on foot and begin to make your way toward the star fruit grove.
+
+// As you move through the dense undergrowth, one of the Elves gives you a handheld device. He says that it has many fancy features, but the most important one to set up right now is the communication system.
+
+// However, because he's heard you have significant experience dealing with signal-based systems, he convinced the other Elves that it would be okay to give you their one malfunctioning device - surely you'll have no problem fixing it.
+
+// As if inspired by comedic timing, the device emits a few colorful sparks.
+
+// To be able to communicate with the Elves, the device needs to lock on to their signal. The signal is a series of seemingly-random characters that the device receives one at a time.
+
+// To fix the communication system, you need to add a subroutine to the device that detects a start-of-packet marker in the datastream. In the protocol being used by the Elves, the start of a packet is indicated by a sequence of four characters that are all different.
+
+// The device will send your subroutine a datastream buffer (your puzzle input); your subroutine needs to identify the first position where the four most recently received characters were all different. Specifically, it needs to report the number of characters from the beginning of the buffer to the end of the first such four-character marker.
+
+// For example, suppose you receive the following datastream buffer:
+
+// mjqjpqmgbljsphdztnvjfqwrcgsmlb
+// After the first three characters (mjq) have been received, there haven't been enough characters received yet to find the marker. The first time a marker could occur is after the fourth character is received, making the most recent four characters mjqj. Because j is repeated, this isn't a marker.
+
+// The first time a marker appears is after the seventh character arrives. Once it does, the last four characters received are jpqm, which are all different. In this case, your subroutine should report the value 7, because the first start-of-packet marker is complete after 7 characters have been processed.
+
+// Here are a few more examples:
+
+// bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+// nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
+// nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
+// zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
+// How many characters need to be processed before the first start-of-packet marker is detected?
+
+const marker = (packet) => {
+  for (let i = 0; i < packet.length - 4; i++)
+    if (new Set(packet.slice(i, i + 4).split("")).size === 4) return i + 4;
+};
+console.log(marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")); // 7
+console.log(marker("bvwbjplbgvbhsrlpgdmjqwftvncz")); // 5
+console.log(marker("nppdvjthqldpwncqszvftbrmjlhg")); // 6
+console.log(marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")); // 10
+console.log(marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")); // 11
+
+// Your device's communication system is correctly detecting packets, but still isn't working. It looks like it also needs to look for messages.
+
+// A start-of-message marker is just like a start-of-packet marker, except it consists of 14 distinct characters rather than 4.
+
+// Here are the first positions of start-of-message markers for all of the above examples:
+
+// mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
+// bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
+// nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
+// nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
+// zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
+// How many characters need to be processed before the first start-of-message marker is detected?
+
+const message = (packet) => {
+  for (let i = 0; i < packet.length - 14; i++)
+    if (new Set(packet.slice(i, i + 14).split("")).size === 14) return i + 14;
+};
+console.log(message("mjqjpqmgbljsphdztnvjfqwrcgsmlb")); // 19
+console.log(message("bvwbjplbgvbhsrlpgdmjqwftvncz")); // 23
+console.log(message("nppdvjthqldpwncqszvftbrmjlhg")); // 23
+console.log(message("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")); // 29
+console.log(message("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")); // 26
+
+// Was easy to scale between part 1 and 2 with this solution
