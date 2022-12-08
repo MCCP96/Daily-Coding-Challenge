842c20eb3e9e70d14a25eab5385f895c719d3ea9
MCCP96 <petermccomb@live.com>
2022-12-08 11:26:56 -0500

    Day 539 - Advent of code - Day 8: Treetop House

--- 2022.js
+++ 2022.js
@@ -18230,7 +18230,7 @@ console.log(message("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")); // 26
 // Was easy to scale between part 1 and 2 with this solution */
 
 // Advent of code - Day 7: No Space Left On Device          12/7/2022
-
+/* 
 // You can hear birds chirping and raindrops hitting leaves as the expedition proceeds. Occasionally, you can even hear much louder sounds in the distance; how big do the animals get out here, anyway?
 
 // The device the Elves gave you has problems with more than just its communication system. You try to run a system update:
@@ -18403,4 +18403,145 @@ console.log(update(example)); // 24933642
 
 // That took a while
 // Couldn't find the error caused by duplicate folder names in different depths of the folder tree
-// Resolved it with 'path += `${c}/`'
+// Resolved it with 'path += `${c}/`' */
+
+// Advent of code - Day 8: Treetop Tree House         12/8/2022
+
+// The expedition comes across a peculiar patch of tall trees all planted carefully in a grid. The Elves explain that a previous expedition planted these trees as a reforestation effort. Now, they're curious if this would be a good location for a tree house.
+
+// First, determine whether there is enough tree cover here to keep a tree house hidden. To do this, you need to count the number of trees that are visible from outside the grid when looking directly along a row or column.
+
+// The Elves have already launched a quadcopter to generate a map with the height of each tree (your puzzle input). For example:
+
+// 30373
+// 25512
+// 65332
+// 33549
+// 35390
+
+// Each tree is represented as a single digit whose value is its height, where 0 is the shortest and 9 is the tallest.
+
+// A tree is visible if all of the other trees between it and an edge of the grid are shorter than it. Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
+
+// All of the trees around the edge of the grid are visible - since they are already on the edge, there are no trees to block the view. In this example, that only leaves the interior nine trees to consider:
+
+// The top-left 5 is visible from the left and top. (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+// The top-middle 5 is visible from the top and right.
+// The top-right 1 is not visible from any direction; for it to be visible, there would need to only be trees of height 0 between it and an edge.
+// The left-middle 5 is visible, but only from the right.
+// The center 3 is not visible from any direction; for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
+// The right-middle 3 is visible from the right.
+// In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
+// With 16 trees visible on the edge and another 5 visible in the interior, a total of 21 trees are visible in this arrangement.
+
+// Consider your map; how many trees are visible from outside the grid?
+
+const heights = (grid) => {
+  let count = 2 * (grid[0].length + grid.length - 2);
+
+  const isTaller = (tree, adj) => {
+    let isTaller = true;
+    for (let i = 0; i < adj.length; i++)
+      if (adj[i] >= tree) {
+        isTaller = false;
+        break;
+      }
+    return isTaller;
+  };
+
+  for (let i = 1; i < grid.length - 1; i++) {
+    for (let j = 1; j < grid[i].length - 1; j++) {
+      const tree = +grid[i][j];
+
+      let l = isTaller(tree, grid[i].slice(0, j));
+      let r = isTaller(tree, grid[i].slice(j + 1));
+
+      let t = "",
+        b = "";
+      for (let k = 0; k < i; k++) t += grid[k][j];
+      for (let k = i + 1; k < grid.length; k++) b += grid[k][j];
+
+      t = isTaller(tree, t);
+      b = isTaller(tree, b);
+
+      count += l || r || t || b;
+    }
+  }
+  return count;
+};
+
+console.log(heights(["30373", "25512", "65332", "33549", "35390"])); // 21
+
+// --- Part Two ---
+
+// Content with the amount of tree cover available, the Elves just need to know the best spot to build their tree house: they would like to be able to see a lot of trees.
+
+// To measure the viewing distance from a given tree, look up, down, left, and right from that tree; stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration. (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+// The Elves don't care about distant trees taller than those found by the rules above; the proposed tree house has large eaves to keep it dry, so they wouldn't be able to see higher than the tree house anyway.
+
+// In the example above, consider the middle 5 in the second row:
+
+// 30373
+// 25512
+// 65332
+// 33549
+// 35390
+// Looking up, its view is not blocked; it can see 1 tree (of height 3).
+// Looking left, its view is blocked immediately; it can see only 1 tree (of height 5, right next to it).
+// Looking right, its view is not blocked; it can see 2 trees.
+// Looking down, its view is blocked eventually; it can see 2 trees (one of height 3, then the tree of height 5 that blocks its view).
+// A tree's scenic score is found by multiplying together its viewing distance in each of the four directions. For this tree, this is 4 (found by multiplying 1 * 1 * 2 * 2).
+
+// However, you can do even better: consider the tree of height 5 in the middle of the fourth row:
+
+// 30373
+// 25512
+// 65332
+// 33549
+// 35390
+// Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+// Looking left, its view is not blocked; it can see 2 trees.
+// Looking down, its view is also not blocked; it can see 1 tree.
+// Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+// This tree's scenic score is 8 (2 * 2 * 1 * 2); this is the ideal spot for the tree house.
+
+// Consider each tree on your map. What is the highest scenic score possible for any tree?
+
+const scenic = (grid) => {
+  let scores = [];
+
+  const countScore = (tree, adj) => {
+    let count = 0;
+    for (let i = 0; i < adj.length; i++) {
+      count++;
+      if (tree <= adj[i]) break;
+    }
+    if (count === 0) count++;
+    return count;
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const tree = +grid[i][j];
+
+      let l = countScore(tree, grid[i].slice(0, j).split("").reverse());
+      let r = countScore(tree, grid[i].slice(j + 1));
+
+      let t = "",
+        b = "";
+      for (let k = 0; k < i; k++) t += grid[k][j];
+      for (let k = i + 1; k < grid.length; k++) b += grid[k][j];
+      t = countScore(tree, [...t].reverse().join(""));
+      b = countScore(tree, b);
+
+      scores.push(l * r * t * b);
+    }
+  }
+  return Math.max(...scores);
+};
+
+console.log(scenic(["30373", "25512", "65332", "33549", "35390"])); // 16
+
+// Definitely some runtime optimization to be had, but works
+// If I had to redo it, I would append to the top and bottom accumulators instead of rebuilding them on every iteration
