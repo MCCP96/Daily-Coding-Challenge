b9cf862f73fa15bee408505727ea060114b66fae
MCCP96 <petermccomb@live.com>
2022-12-15 23:30:13 -0500

    Day 546 - Maximum Value of a String in an Array

--- 2022.js
+++ 2022.js
@@ -19102,7 +19102,7 @@ function checkPackets(leftPacket, rightPacket) {
 // May spend a couple days studying some of the previously missed days */
 
 // Circular Sentence          12/14/2022
-
+/* 
 // A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
 
 // For example, "Hello World", "HELLO", "hello world hello world" are all sentences.
@@ -19182,4 +19182,53 @@ const topVotedIsCircularSentence = function (sentence) {
   return true;
 };
 
-// So much cleaner
+// So much cleaner */
+
+// Maximum Value of a String in an Array          12/15/2022
+
+// The value of an alphanumeric string can be defined as:
+
+// The numeric representation of the string in base 10, if it comprises of digits only.
+// The length of the string, otherwise.
+// Given an array strs of alphanumeric strings, return the maximum value of any string in strs.
+
+// Example 1:
+// Input: strs = ["alic3","bob","3","4","00000"]
+// Output: 5
+// Explanation:
+// - "alic3" consists of both letters and digits, so its value is its length, i.e. 5.
+// - "bob" consists only of letters, so its value is also its length, i.e. 3.
+// - "3" consists only of digits, so its value is its numeric equivalent, i.e. 3.
+// - "4" also consists only of digits, so its value is 4.
+// - "00000" consists only of digits, so its value is 0.
+// Hence, the maximum value is 5, of "alic3".
+
+// Example 2:
+// Input: strs = ["1","01","001","0001"]
+// Output: 1
+// Explanation:
+// Each string in the array has value 1. Hence, we return 1.
+
+// Constraints:
+// 1 <= strs.length <= 100
+// 1 <= strs[i].length <= 9
+// strs[i] consists of only lowercase English letters and digits.
+
+const maximumValue = (strs) =>
+  strs.reduce((a, c) => Math.max(isNaN(+c) ? c.length : +c, a), 0);
+
+console.log(maximumValue(["alic3", "bob", "3", "4", "00000"])); // 5
+console.log(maximumValue(["1", "01", "001", "0001"])); // 1
+
+// Clean
+
+const topVotedMaximumValue = function (strs) {
+  let result = 0;
+  const regex = /^\d+$/;
+  for (let i = 0; i < strs.length; ++i) {
+    result = Math.max(result, regex.test(strs[i]) ? +strs[i] : strs[i].length);
+  }
+  return result;
+};
+
+// Thought about using Regex, but didn't want to overcomplicate things
