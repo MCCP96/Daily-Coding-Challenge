825e80bec126d3e78b0abb81fac25d93b6430774
MCCP96 <petermccomb@live.com>
2022-12-16 14:31:00 -0500

    Day 547 -  Minimum Cuts to Divide a Circle

--- 2022.js
+++ 2022.js
@@ -19185,7 +19185,7 @@ const topVotedIsCircularSentence = function (sentence) {
 // So much cleaner */
 
 // Maximum Value of a String in an Array          12/15/2022
-
+/* 
 // The value of an alphanumeric string can be defined as:
 
 // The numeric representation of the string in base 10, if it comprises of digits only.
@@ -19231,4 +19231,42 @@ const topVotedMaximumValue = function (strs) {
   return result;
 };
 
-// Thought about using Regex, but didn't want to overcomplicate things
+// Thought about using Regex, but didn't want to overcomplicate things */
+
+// Minimum Cuts to Divide a Circle          12/16/2022
+
+// A valid cut in a circle can be:
+
+// A cut that is represented by a straight line that touches two points on the edge of the circle and passes through its center, or
+// A cut that is represented by a straight line that touches one point on the edge of the circle and its center.
+// Some valid and invalid cuts are shown in the figures below.
+// https://assets.leetcode.com/uploads/2022/10/29/alldrawio.png
+
+// Given the integer n, return the minimum number of cuts needed to divide a circle into n equal slices.
+
+// Example 1:
+// https://assets.leetcode.com/uploads/2022/10/24/11drawio.png
+//    Input: n = 4
+//    Output: 2
+// Explanation:
+// The above figure shows how cutting the circle twice through the middle divides it into 4 equal slices.
+
+// Example 2:
+// https://assets.leetcode.com/uploads/2022/10/24/22drawio.png
+//    Input: n = 3
+//    Output: 3
+// Explanation:
+// At least 3 cuts are needed to divide the circle into 3 equal slices.
+// It can be shown that less than 3 cuts cannot result in 3 slices of equal size and shape.
+// Also note that the first cut will not divide the circle into distinct parts.
+
+// Constraints:
+//  1 <= n <= 100
+
+const numberOfCuts = (n) => (n === 1 ? 0 : n % 2 ? n : n / 2);
+
+console.log(numberOfCuts(4)); // 2
+console.log(numberOfCuts(3)); // 3
+
+// Faster than 90% of submissions
+// Same as top voted
