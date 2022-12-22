f1dec90eb9ecbeb83c7f068e0ae6145f23345e12
MCCP96 <petermccomb@live.com>
2022-12-22 11:44:49 -0500

    Day 553 - Valid Sudoku

--- 2022.js
+++ 2022.js
@@ -19474,7 +19474,7 @@ console.log(topKFrequent([1], 1)); // [1]
 // Not sure I'm respecting the O(n log n) complexity */
 
 // Compare Version Numbers          12/21/2022
-
+/* 
 // Given two version numbers, version1 and version2, compare them.
 
 // Version numbers consist of one or more revisions joined by a dot '.'. Each revision consists of digits and may contain leading zeros. Every revision contains at least one character. Revisions are 0-indexed from left to right, with the leftmost revision being revision 0, the next revision being revision 1, and so on. For example 2.5.33 and 0.1 are valid version numbers.
@@ -19558,4 +19558,157 @@ const topVotedCompareVersion = function (version1, version2) {
   return 0;
 };
 
-// Same as mine, but bit bulkier
+// Same as mine, but bit bulkier */
+
+// Valid Sudoku         12/22/2022
+
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+// Example 1:
+//    Input: board =
+//    [["5","3",".",".","7",".",".",".","."]
+//    ,["6",".",".","1","9","5",".",".","."]
+//    ,[".","9","8",".",".",".",".","6","."]
+//    ,["8",".",".",".","6",".",".",".","3"]
+//    ,["4",".",".","8",".","3",".",".","1"]
+//    ,["7",".",".",".","2",".",".",".","6"]
+//    ,[".","6",".",".",".",".","2","8","."]
+//    ,[".",".",".","4","1","9",".",".","5"]
+//    ,[".",".",".",".","8",".",".","7","9"]]
+//    Output: true
+
+// Example 2:
+//    Input: board =
+//    [["8","3",".",".","7",".",".",".","."]
+//    ,["6",".",".","1","9","5",".",".","."]
+//    ,[".","9","8",".",".",".",".","6","."]
+//    ,["8",".",".",".","6",".",".",".","3"]
+//    ,["4",".",".","8",".","3",".",".","1"]
+//    ,["7",".",".",".","2",".",".",".","6"]
+//    ,[".","6",".",".",".",".","2","8","."]
+//    ,[".",".",".","4","1","9",".",".","5"]
+//    ,[".",".",".",".","8",".",".","7","9"]]
+//    Output: false
+//    Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+// Constraints:
+//    board.length == 9
+//    board[i].length == 9
+//    board[i][j] is a digit 1-9 or '.'.
+
+const isValidSudoku = (board) => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const cur = board[i][j];
+      if (cur === ".") continue;
+
+      const row = [...board[i].slice(0, j), ...board[i].slice(j + 1)];
+      if (row.includes(cur)) return false;
+
+      let col = [];
+      for (let k = 0; k < board.length; k++) {
+        if (k === i) continue;
+        col.push(board[k][j]);
+      }
+      if (col.includes(cur)) return false;
+    }
+  }
+
+  let squares = {};
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === ".") continue;
+      const cur = `${Math.floor(i / 3)}${Math.floor(j / 3)}`;
+      squares[cur]
+        ? squares[cur].push(board[i][j])
+        : (squares[cur] = [board[i][j]]);
+    }
+  }
+  squares = Object.values(squares);
+  for (let i = 0; i < squares.length; i++)
+    if (squares[i].length !== new Set(squares[i]).size) return false;
+
+  return true;
+};
+
+console.log(
+  isValidSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ])
+); // true
+console.log(
+  isValidSudoku([
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ])
+); // false
+console.log(
+  isValidSudoku([
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["2", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ])
+); // false
+
+// Better than 90% of runtimes
+// Thought of doing squares at same time, but couldn't get it going
+
+const topVotedIsValidSudoku = function (board) {
+  for (let i = 0; i < 9; i++) {
+    let row = new Set(),
+      col = new Set(),
+      box = new Set();
+
+    for (let j = 0; j < 9; j++) {
+      let _row = board[i][j];
+      let _col = board[j][i];
+      let _box =
+        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
+
+      if (_row != ".") {
+        if (row.has(_row)) return false;
+        row.add(_row);
+      }
+      if (_col != ".") {
+        if (col.has(_col)) return false;
+        col.add(_col);
+      }
+      if (_box != ".") {
+        if (box.has(_box)) return false;
+        box.add(_box);
+      }
+    }
+  }
+  return true;
+};
+
+// Saves a lot of effort by going incrementally, not checking later values
