cf5b71913e131024c263f3f5d6690141cdf45184
MCCP96 <petermccomb@live.com>
2022-12-23 09:25:14 -0500

    Day 554 - Rotate Array

--- 2022.js
+++ 2022.js
@@ -19561,7 +19561,7 @@ const topVotedCompareVersion = function (version1, version2) {
 // Same as mine, but bit bulkier */
 
 // Valid Sudoku         12/22/2022
-
+/* 
 // Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
 // Each row must contain the digits 1-9 without repetition.
@@ -19711,4 +19711,63 @@ const topVotedIsValidSudoku = function (board) {
   return true;
 };
 
-// Saves a lot of effort by going incrementally, not checking later values
+// Saves a lot of effort by going incrementally, not checking later values */
+
+// Rotate Array         12/23/2022
+
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+//     Input: nums = [1,2,3,4,5,6,7], k = 3
+//     Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+// Example 2:
+//     Input: nums = [-1,-100,3,99], k = 2
+//     Output: [3,99,-1,-100]
+// Explanation:
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+// Constraints:
+//     1 <= nums.length <= 105
+//     -231 <= nums[i] <= 231 - 1
+//     0 <= k <= 105
+
+// Follow up:
+//     Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+//     Could you do it in-place with O(1) extra space?
+
+const rotate = (nums, k) => {
+  for (let i = 0; i < k; i++) nums.unshift(nums.pop());
+};
+const rotate1 = (nums, k) =>
+  nums.unshift(...nums.splice(nums.length - (k % nums.length)));
+
+console.log(rotate1([1, 2, 3, 4, 5, 6, 7], 3)); // [5,6,7,1,2,3,4]
+console.log(rotate1([-1, -100, 3, 99], 2)); // [3,99,-1,-100]
+console.log(rotate1([1, 2], 5)); // [3,99,-1,-100]
+
+// 2nd solution is nice
+
+var topVotedRotate = function (nums, k) {
+  k %= nums.length;
+  let reverse = function (i, j) {
+    while (i < j) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      i++;
+      j--;
+    }
+  }; // suppose  ----->--->
+  reverse(0, nums.length - 1); // reverse   <--<------
+  reverse(0, k - 1); // reverse first part ---><----
+  reverse(k, nums.length - 1); // reverse second part --->----->
+};
+
+// Thought about the temp approach
+// Better runtime than mine
