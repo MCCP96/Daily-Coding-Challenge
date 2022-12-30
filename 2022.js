1051286036d0ee81bd30ad6d87736ef7d26bf249
MCCP96 <petermccomb@live.com>
2022-12-30 12:07:11 -0500

    Day 561 - H-Index

--- 2022.js
+++ 2022.js
@@ -20024,7 +20024,7 @@ console.log(subsetsWithDup([0])); // [[],[0]]
 // I'll have to do my research on backtracking */
 
 // Boats to Save People         12/29/2022
-
+/* 
 // You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
 
 // Return the minimum number of boats to carry every given person.
@@ -20084,4 +20084,49 @@ var topVotedNumRescueBoats = function (people, limit) {
   return count;
 };
 
-// Two pointers drastically improves runtime here
+// Two pointers drastically improves runtime here */
+
+// H-Index          12/30/2022
+
+// Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return compute the researcher's h-index.
+
+// According to the definition of h-index on Wikipedia: A scientist has an index h if h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
+
+// If there are several possible values for h, the maximum one is taken as the h-index.
+
+// Example 1:
+//    Input: citations = [3,0,6,1,5]
+//    Output: 3
+// Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+// Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+// Example 2:
+//    Input: citations = [1,3,1]
+//    Output: 1
+
+// Constraints:
+//    n == citations.length
+//    1 <= n <= 5000
+//    0 <= citations[i] <= 1000
+
+const hIndex = (cite) => {
+  for (let h = cite.length; h > 0; h--)
+    if (h <= cite.filter((x) => x >= h).length) return h;
+  return cite.pop();
+};
+
+console.log(hIndex([3, 0, 6, 1, 5])); // 3
+console.log(hIndex([1, 3, 1])); // 1
+console.log(hIndex([0])); // 0
+
+// Pretty concise
+
+const topVotedHIndex = (citations) => {
+  citations.sort((a, b) => b - a);
+  for (i = 0; i < citations.length; i++) {
+    if (citations[i] < i + 1) return i;
+  }
+  return i;
+};
+
+// Having it sorted beforehand allows you to save a lot of .filters()
