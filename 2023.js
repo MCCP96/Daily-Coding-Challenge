6e2494c82f60306c625edb920a90a8c365999658
MCCP96 <petermccomb@live.com>
2023-01-02 17:20:27 -0500

    Day 564 - Maximum Enemy Forts That Can Be Captured

--- 2023.js
+++ 2023.js
@@ -1,5 +1,5 @@
 // H-Index II         1/1/2023
-
+/* 
 // Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper and citations is sorted in an ascending order, return compute the researcher's h-index.
 
 // According to the definition of h-index on Wikipedia: A scientist has an index h if h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
@@ -48,4 +48,61 @@ var topVotedHIndex = function (citations) {
   return citations.length - start;
 };
 
-// Nice!
+// Nice! */
+
+// Maximum Enemy Forts That Can Be Captured         1/2/2023
+
+// You are given a 0-indexed integer array forts of length n representing the positions of several forts. forts[i] can be -1, 0, or 1 where:
+
+// -1 represents there is no fort at the ith position.
+// 0 indicates there is an enemy fort at the ith position.
+// 1 indicates the fort at the ith the position is under your command.
+// Now you have decided to move your army from one of your forts at position i to an empty position j such that:
+
+// 0 <= i, j <= n - 1
+// The army travels over enemy forts only. Formally, for all k where min(i,j) < k < max(i,j), forts[k] == 0.
+// While moving the army, all the enemy forts that come in the way are captured.
+
+// Return the maximum number of enemy forts that can be captured. In case it is impossible to move your army, or you do not have any fort under your command, return 0.
+
+// Example 1:,
+//    Input: forts = [1,0,0,-1,0,0,0,0,1]
+//    Output: 4
+// Explanation:
+// - Moving the army from position 0 to position 3 captures 2 enemy forts, at 1 and 2.
+// - Moving the army from position 8 to position 3 captures 4 enemy forts.
+// Since 4 is the maximum number of enemy forts that can be captured, we return 4.
+
+// Example 2:
+//    Input: forts = [0,0,1,-1]
+//    Output: 0
+// Explanation: Since no enemy fort can be captured, 0 is returned.
+
+// Constraints:
+//    1 <= forts.length <= 1000
+//    -1 <= forts[i] <= 1
+
+const captureForts = (forts, count = 0) =>
+  forts.slice(Math.min(forts.indexOf(1), forts.indexOf(-1))).reduce((a, c) => {
+    if (c == 1 || c == -1) {
+      a = Math.max(a, count);
+      count = 0;
+    } else count++;
+    return a;
+  }, 0);
+
+console.log(captureForts([1, 0, 0, -1, 0, 0, 0, 0, 1])); // 4
+console.log(captureForts([0, 0, 1, -1])); // 0
+console.log(captureForts([-1, 0, -1, 0, 1, 1, 1, -1, -1, -1])); // 1
+
+// OK, doesn't work for all test cases
+
+var topVotedCaptureForts = function (forts) {
+  forts = forts.map((e) => e - -1).join("");
+  const front = forts.match(/2(1+)0/g) || [];
+  const back = forts.match(/0(1+)2/g) || [];
+  const res = [...front, ...back].map((e) => e.length - 2);
+  return res.length ? Math.max(...res) : 0;
+};
+
+// Though about Regex, but didn't want to mess with the expressions
