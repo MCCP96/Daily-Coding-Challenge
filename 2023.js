cf7498f4f7725f8dd2c072ea201859ddf9ee5bc0
MCCP96 <petermccomb@live.com>
2023-01-03 11:09:12 -0500

    Day 565 - Walking Robot Simulation

--- 2023.js
+++ 2023.js
@@ -51,7 +51,7 @@ var topVotedHIndex = function (citations) {
 // Nice! */
 
 // Maximum Enemy Forts That Can Be Captured         1/2/2023
-
+/* 
 // You are given a 0-indexed integer array forts of length n representing the positions of several forts. forts[i] can be -1, 0, or 1 where:
 
 // -1 represents there is no fort at the ith position.
@@ -105,4 +105,139 @@ var topVotedCaptureForts = function (forts) {
   return res.length ? Math.max(...res) : 0;
 };
 
-// Though about Regex, but didn't want to mess with the expressions
+// Though about Regex, but didn't want to mess with the expressions */
+
+// Walking Robot Simulation         1/3/2023
+
+// A robot on an infinite XY-plane starts at point (0, 0) facing north. The robot can receive a sequence of these three possible types of commands:
+
+// -2: Turn left 90 degrees.
+// -1: Turn right 90 degrees.
+// 1 <= k <= 9: Move forward k units, one unit at a time.
+// Some of the grid squares are obstacles. The ith obstacle is at grid point obstacles[i] = (xi, yi). If the robot runs into an obstacle, then it will instead stay in its current location and move on to the next command.
+
+// Return the maximum Euclidean distance that the robot ever gets from the origin squared (i.e. if the distance is 5, return 25).
+
+// Note:
+//    North means +Y direction.
+//    East means +X direction.
+//    South means -Y direction.
+//    West means -X direction.
+
+// Example 1:
+//    Input: commands = [4,-1,3], obstacles = []
+//    Output: 25
+// Explanation: The robot starts at (0, 0):
+// 1. Move north 4 units to (0, 4).
+// 2. Turn right.
+// 3. Move east 3 units to (3, 4).
+// The furthest point the robot ever gets from the origin is (3, 4), which squared is 32 + 42 = 25 units away.
+
+// Example 2:
+//    Input: commands = [4,-1,4,-2,4], obstacles = [[2,4]]
+//    Output: 65
+// Explanation: The robot starts at (0, 0):
+// 1. Move north 4 units to (0, 4).
+// 2. Turn right.
+// 3. Move east 1 unit and get blocked by the obstacle at (2, 4), robot is at (1, 4).
+// 4. Turn left.
+// 5. Move north 4 units to (1, 8).
+// The furthest point the robot ever gets from the origin is (1, 8), which squared is 12 + 82 = 65 units away.
+
+// Example 3:
+//    Input: commands = [6,-1,-1,6], obstacles = []
+//    Output: 36
+// Explanation: The robot starts at (0, 0):
+// 1. Move north 6 units to (0, 6).
+// 2. Turn right.
+// 3. Turn right.
+// 4. Move south 6 units to (0, 0).
+// The furthest point the robot ever gets from the origin is (0, 6), which squared is 62 = 36 units away.
+
+// Constraints:
+//    1 <= commands.length <= 104
+//    commands[i] is either -2, -1, or an integer in the range [1, 9].
+//    0 <= obstacles.length <= 104
+//    -3 * 104 <= xi, yi <= 3 * 104
+//    The answer is guaranteed to be less than 231.
+
+const robotSim = (cmd, obst) => {
+  obst = obst.map((c) => ({ x: c[0], y: c[1] }));
+  const opp = { x: "y", y: "x" };
+  const turnRight = { "+y": "+x", "+x": "-y", "-y": "-x", "-x": "+y" };
+  const turnLeft = { "+y": "-x", "-x": "-y", "-y": "+x", "+x": "+y" };
+  let dir = "+y";
+
+  let furthest = 0;
+  const end = cmd.reduce(
+    (a, c) => {
+      if (c > 0) {
+        for (let i = 0; i < obst.length; i++) {
+          if (obst[i][opp[dir[1]]] == a[opp[dir[1]]]) {
+            c = Math.abs(c - obst[i][dir[1]] - 1);
+            break;
+          }
+        }
+        a[dir[1]] += +`${dir[0]}${c}`;
+        furthest = Math.max(furthest, a.x ** 2 + a.y ** 2);
+      } else if (c == -1) dir = turnRight[dir];
+      else if (c == -2) dir = turnLeft[dir];
+      return a;
+    },
+    { x: 0, y: 0 }
+  );
+
+  return furthest;
+};
+console.log(robotSim([4, -1, 3], [])); // 25
+console.log(robotSim([4, -1, 4, -2, 4], [[2, 4]])); // 65
+console.log(robotSim([6, -1, -1, 6], [])); // 36
+
+// Gave it my best shot, doesn't work for all test cases
+// My obstacle detection is pretty janky
+
+const topVotedRobotSim = (commands, obstacles) => {
+  let obstacle = {};
+  let x = 0;
+  let y = 0;
+  let max = 0;
+
+  /* 0 = north
+   * 1 = east
+   * 2 = south
+   * 3 = west
+   */
+
+  let direction = 0;
+  for (let i = 0; i < obstacles.length; i++) {
+    obstacle[obstacles[i]] = true;
+  }
+  console.log(obstacle);
+
+  for (let i = 0; i < commands.length; i++) {
+    if (commands[i] == -1) {
+      direction = (direction + 1) % 4; // updated
+    } else if (commands[i] == -2) {
+      direction = (direction - 1 + 4) % 4;
+    } else {
+      while (commands[i]--) {
+        let previousX = x;
+        let previousY = y;
+
+        if (direction === 0) y++;
+        if (direction === 1) x++;
+        if (direction === 2) y--;
+        if (direction === 3) x--;
+        if (obstacle[x + "," + y]) {
+          [x, y] = [previousX, previousY];
+          break;
+        }
+      }
+    }
+    max = Math.max(max, x ** 2 + y ** 2);
+  }
+  return max;
+};
+
+// Could've definitely made it easier on myself by doing the if(direction === x) statements instead of my dictionnary approach
+// Such simple yet effective obstacle detection too
