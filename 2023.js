d0c407597aaeb7239bb190eff9866c2e6f93f471
MCCP96 <petermccomb@live.com>
2023-01-08 18:31:13 -0500

    Day 570 - Shortest Dist to String in Circular Arr

--- 2023.js
+++ 2023.js
@@ -459,7 +459,7 @@ var topVotedIsNStraightHand = function (hand, W) {
 }; */
 
 // Rabbits in Forest          1/7/2023
-
+/* 
 // There is a forest with an unknown number of rabbits. We asked n rabbits "How many rabbits have the same color as you?" and collected the answers in an integer array answers where answers[i] is the answer of the ith rabbit.
 
 // Given the array answers, return the minimum number of rabbits that could be in the forest.
@@ -521,4 +521,73 @@ var topVotedNumRabbits = function (answers) {
     }
   }
   return tot;
+}; */
+
+// Shortest Distance to Target String in a Circular Array         1/8/2023
+
+// You are given a 0-indexed circular string array words and a string target. A circular array means that the array's end connects to the array's beginning.
+
+// Formally, the next element of words[i] is words[(i + 1) % n] and the previous element of words[i] is words[(i - 1 + n) % n], where n is the length of words.
+// Starting from startIndex, you can move to either the next word or the previous word with 1 step at a time.
+
+// Return the shortest distance needed to reach the string target. If the string target does not exist in words, return -1.
+
+// Example 1:
+//    Input: words = ["hello","i","am","leetcode","hello"], target = "hello", startIndex = 1
+//    Output: 1
+// Explanation: We start from index 1 and can reach "hello" by
+// - moving 3 units to the right to reach index 4.
+// - moving 2 units to the left to reach index 4.
+// - moving 4 units to the right to reach index 0.
+// - moving 1 unit to the left to reach index 0.
+// The shortest distance to reach "hello" is 1.
+
+// Example 2:
+//    Input: words = ["a","b","leetcode"], target = "leetcode", startIndex = 0
+//    Output: 1
+// Explanation: We start from index 0 and can reach "leetcode" by
+// - moving 2 units to the right to reach index 3.
+// - moving 1 unit to the left to reach index 3.
+// The shortest distance to reach "leetcode" is 1.
+
+// Example 3:
+//    Input: words = ["i","eat","leetcode"], target = "ate", startIndex = 0
+//    Output: -1
+// Explanation: Since "ate" does not exist in words, we return -1.
+
+// Constraints:
+//    1 <= words.length <= 100
+//    1 <= words[i].length <= 100
+//    words[i] and target consist of only lowercase English letters.
+//    0 <= startIndex < words.length
+
+const closetTarget = (w, t, s) => {
+  w.push(...w.splice(0, s));
+  return Math.min(w.indexOf(t), w.length - w.lastIndexOf(t));
+};
+
+console.log(
+  closetTarget(["hello", "i", "am", "leetcode", "hello"], "hello", 1)
+); // 1
+console.log(closetTarget(["a", "b", "leetcode"], "leetcode", 0)); // 1
+console.log(closetTarget(["i", "eat", "leetcode"], "ate", 0)); // -1
+
+// Short, but not great runtime
+
+var topVotedClosetTarget = function (words, target, startIndex) {
+  let left = startIndex;
+  let right = startIndex;
+  let step = 0;
+  let n = words.length;
+
+  while (step <= n) {
+    if (words[left] === target || words[right] === target) {
+      return step;
+    } else {
+      right = (right + 1) % n;
+      left = (left - 1 + n) % n;
+    }
+    step++;
+  }
+  return -1;
 };
