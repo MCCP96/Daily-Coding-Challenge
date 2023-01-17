9851d85768ecaa329cec09a478a8110a1bd8d7d4
MCCP96 <petermccomb@live.com>
2023-01-17 10:00:07 -0500

    Day 579 - Successful Pairs of Spells and Potions

--- 2023.js
+++ 2023.js
@@ -1104,7 +1104,7 @@ const revisedSolution = (s1, s2, base) => {
 console.log(revisedSolution("parker", "morris", "parser")); // "makkek" */
 
 // Sender With Largest Word Count         1/16/2023
-
+/* 
 // You have a chat log of n messages. You are given two string arrays messages and senders where messages[i] is a message sent by senders[i].
 
 // A message is list of words that are separated by a single space with no leading or trailing spaces. The word count of a sender is the total number of words sent by the sender. Note that a sender may send more than one message.
@@ -1181,4 +1181,67 @@ console.log(
 ); // "Charlie"
 
 // Better runtime than 95%
-// Same as top voted
+// Same as top voted */
+
+// Successful Pairs of Spells and Potions         1/17/2023
+
+// You are given two positive integer arrays spells and potions, of length n and m respectively, where spells[i] represents the strength of the ith spell and potions[j] represents the strength of the jth potion.
+
+// You are also given an integer success. A spell and potion pair is considered successful if the product of their strengths is at least success.
+
+// Return an integer array pairs of length n where pairs[i] is the number of potions that will form a successful pair with the ith spell.
+
+// Example 1:
+//    Input: spells = [5,1,3], potions = [1,2,3,4,5], success = 7
+//    Output: [4,0,3]
+// Explanation:
+// - 0th spell: 5 * [1,2,3,4,5] = [5,10,15,20,25]. 4 pairs are successful.
+// - 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
+// - 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are successful.
+// Thus, [4,0,3] is returned.
+
+// Example 2:
+//    Input: spells = [3,1,2], potions = [8,5,8], success = 16
+//    Output: [2,0,2]
+// Explanation:
+// - 0th spell: 3 * [8,5,8] = [24,15,24]. 2 pairs are successful.
+// - 1st spell: 1 * [8,5,8] = [8,5,8]. 0 pairs are successful.
+// - 2nd spell: 2 * [8,5,8] = [16,10,16]. 2 pairs are successful.
+// Thus, [2,0,2] is returned.
+
+// Constraints:
+//    n == spells.length
+//    m == potions.length
+//    1 <= n, m <= 105
+//    1 <= spells[i], potions[i] <= 105
+//    1 <= success <= 1010
+
+const successfulPairs = (spells, pots, success) =>
+  spells.reduce((a, spell) => {
+    a.push(pots.map((pot) => spell * pot).filter((x) => x >= success).length);
+    return a;
+  }, []);
+
+console.log(successfulPairs([5, 1, 3], [1, 2, 3, 4, 5], 7)); // [4,0,3]
+console.log(successfulPairs([3, 1, 2], [8, 5, 8], 16)); // [2,0,2]
+
+// Exceeds runtime limit on large testcases
+
+var topVotedSuccessfulPairs = function (spells, potions, success) {
+  let res = [];
+  potions.sort((a, b) => a - b);
+  for (let i = 0; i < spells.length; i++) {
+    let h = potions.length - 1,
+      l = 0,
+      mid;
+    while (l <= h) {
+      mid = ~~(l + (h - l) / 2);
+      if (spells[i] * potions[mid] >= success) h = mid - 1;
+      else l = mid + 1;
+    }
+    res[i] = potions.length - 1 - h;
+  }
+  return res;
+};
+
+// Binary search would be the way to go here
