8754088e2da5771cd4efdb1eb933efe9f40fc73d
MCCP96 <petermccomb@live.com>
2023-01-20 09:05:40 -0500

    Day 582 - Longest Binary Subsequence <= to K

--- 2023.js
+++ 2023.js
@@ -1310,7 +1310,7 @@ function topVotedDiscountPrices(sentence, discount) {
 // Seems the only valid approach was Regex */
 
 // Replace Elements in an Array         1/19/2023
-
+/* 
 //  You are given a 0-indexed array nums that consists of n distinct positive integers. Apply m operations to this array, where in the ith operation you replace the number operations[i][0] with operations[i][1].
 
 // It is guaranteed that in the ith operation:
@@ -1386,4 +1386,56 @@ var topVotedArrayChange = function (nums, operations) {
   }
 
   return nums;
+}; */
+
+// Longest Binary Subsequence Less Than or Equal to K         1/20/2023
+
+// You are given a binary string s and a positive integer k.
+
+// Return the length of the longest subsequence of s that makes up a binary number less than or equal to k.
+
+// Note:
+
+// The subsequence can contain leading zeroes.
+// The empty string is considered to be equal to 0.
+// A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+// Example 1:
+//    Input: s = "1001010", k = 5
+//    Output: 5
+// Explanation: The longest subsequence of s that makes up a binary number less than or equal to 5 is "00010", as this number is equal to 2 in decimal.
+// Note that "00100" and "00101" are also possible, which are equal to 4 and 5 in decimal, respectively.
+// The length of this subsequence is 5, so 5 is returned.
+
+// Example 2:
+//    Input: s = "00101001", k = 1
+//    Output: 6
+// Explanation: "000001" is the longest subsequence of s that makes up a binary number less than or equal to 1, as this number is equal to 1 in decimal.
+// The length of this subsequence is 6, so 6 is returned.
+
+// Constraints:
+//    1 <= s.length <= 1000
+//    s[i] is either '0' or '1'.
+//    1 <= k <= 109
+
+var topVotedLongestSubsequence = function (s, k) {
+  let count = 0;
+  let j = s.length - 1; // starting from the last digit
+  let i = 0; // binary number position
+  let acc = 0;
+
+  while (j >= 0) {
+    let positionNumber = Number(s[j]) * Math.pow(2, i);
+    j--;
+    i++;
+    if (acc + positionNumber > k) continue;
+
+    acc += positionNumber;
+    count++;
+  }
+
+  return count;
 };
+
+console.log(topVotedLongestSubsequence("1001010", 5)); // 5
+console.log(topVotedLongestSubsequence("00101001", 1)); // 6
