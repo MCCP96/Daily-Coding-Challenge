b649ae346d90dc5d971acc2093dd77748658c14a
MCCP96 <petermccomb@live.com>
2023-01-21 14:11:05 -0500

    Day 583 - Categorize Box According to Criteria

--- 2023.js
+++ 2023.js
@@ -1389,7 +1389,7 @@ var topVotedArrayChange = function (nums, operations) {
 }; */
 
 // Longest Binary Subsequence Less Than or Equal to K         1/20/2023
-
+/* 
 // You are given a binary string s and a positive integer k.
 
 // Return the length of the longest subsequence of s that makes up a binary number less than or equal to k.
@@ -1438,4 +1438,61 @@ var topVotedLongestSubsequence = function (s, k) {
 };
 
 console.log(topVotedLongestSubsequence("1001010", 5)); // 5
-console.log(topVotedLongestSubsequence("00101001", 1)); // 6
+console.log(topVotedLongestSubsequence("00101001", 1)); // 6 */
+
+// Categorize Box According to Criteria         1/21/2023
+
+// Given four integers length, width, height, and mass, representing the dimensions and mass of a box, respectively, return a string representing the category of the box.
+
+// The box is "Bulky" if:
+//    Any of the dimensions of the box is greater or equal to 10^4.
+//    Or, the volume of the box is greater or equal to 10^9.
+//  If the mass of the box is greater or equal to 100, it is "Heavy".
+//  If the box is both "Bulky" and "Heavy", then its category is "Both".
+//  If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+//  If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+//  If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+// Note that the volume of the box is the product of its length, width and height.
+
+// Example 1:
+//    Input: length = 1000, width = 35, height = 700, mass = 300
+//    Output: "Heavy"
+// Explanation:
+// None of the dimensions of the box is greater or equal to 104.
+// Its volume = 24500000 <= 109. So it cannot be categorized as "Bulky".
+// However mass >= 100, so the box is "Heavy".
+// Since the box is not "Bulky" but "Heavy", we return "Heavy".
+
+// Example 2:
+//    Input: length = 200, width = 50, height = 800, mass = 50
+//    Output: "Neither"
+// Explanation:
+// None of the dimensions of the box is greater or equal to 104.
+// Its volume = 8 * 106 <= 109. So it cannot be categorized as "Bulky".
+// Its mass is also less than 100, so it cannot be categorized as "Heavy" either.
+// Since its neither of the two above categories, we return "Neither".
+
+// Constraints:
+//    1 <= length, width, height <= 10^5
+//    1 <= mass <= 10^3
+
+const categorizeBox = (l, w, h, m) => {
+  let res = {
+    truetrue: "Both",
+    falsefalse: "Neither",
+    truefalse: "Bulky",
+    falsetrue: "Heavy",
+  };
+
+  const vol = l * w * h;
+  const isBulky =
+    l >= 10 ** 4 || w >= 10 ** 4 || h >= 10 ** 4 || vol >= 10 ** 9;
+  const isHeavy = m >= 100;
+
+  return res[`${isBulky}${isHeavy}`];
+};
+
+console.log(categorizeBox(1000, 35, 700, 300)); // "Heavy"
+console.log(categorizeBox(200, 50, 800, 50)); // "Neither"
+
+// Same as top voted
