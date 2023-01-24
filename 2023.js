a77d1bee66e1d225e63381f45587353fd94a8e5d
mccp96 <petermccomb@live.com>
2023-01-24 09:48:06 -0500

    586 - Maximum of Absolute Value Expression

--- 2023.js
+++ 2023.js
@@ -1564,7 +1564,7 @@ console.log(
 // Same as top voted */
 
 // Find Three Consecutive Integers That Sum to a Given Number         1/23/2023
-
+/* 
 // Given an integer num, return three consecutive integers (as a sorted array) that sum to num. If num cannot be expressed as the sum of three consecutive integers, return an empty array.
 
 // Example 1:
@@ -1596,4 +1596,51 @@ console.log(sumOfThree(6675975537)); // [2225325178,2225325179,2225325180]
 var topVotedSumOfThree = (num) =>
   num % 3 === 0 ? [num / 3 - 1, num / 3, num / 3 + 1] : [];
 
-// You only need to check for numbers divisible by 3
+// You only need to check for numbers divisible by 3 */
+
+// Maximum of Absolute Value Expression         1/24/2023
+
+// Given two arrays of integers with equal lengths, return the maximum value of:
+
+// |arr1[i] - arr1[j]| + |arr2[i] - arr2[j]| + |i - j|
+
+// where the maximum is taken over all 0 <= i, j < arr1.length.
+
+// Example 1:
+//    Input: arr1 = [1,2,3,4], arr2 = [-1,4,5,6]
+//    Output: 13
+
+// Example 2:
+//    Input: arr1 = [1,-2,-5,0,10], arr2 = [0,-2,-1,-7,-4]
+//    Output: 20
+
+// Constraints:
+//    2 <= arr1.length == arr2.length <= 40000
+//    -10^6 <= arr1[i], arr2[i] <= 10^6
+
+const topVotedMaxAbsValExpr = function (arr1, arr2) {
+  const l1 = [],
+    l2 = [],
+    l3 = [],
+    l4 = [],
+    res = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    l1.push(arr1[i] + arr2[i] + i);
+    l2.push(arr1[i] - arr2[i] + i);
+    l3.push(-arr1[i] + arr2[i] + i);
+    l4.push(-arr1[i] - arr2[i] + i);
+  }
+
+  res.push(Math.max(...l1) - Math.min(...l1));
+  res.push(Math.max(...l2) - Math.min(...l2));
+  res.push(Math.max(...l3) - Math.min(...l3));
+  res.push(Math.max(...l4) - Math.min(...l4));
+
+  return Math.max(...res);
+};
+
+console.log(topVotedMaxAbsValExpr([1, 2, 3, 4], [-1, 4, 5, 6])); // 13
+console.log(topVotedMaxAbsValExpr([1, -2, -5, 0, 10], [0, -2, -1, -7, -4])); // 20
+
+// Couldn't get it working, but this makes total sense
