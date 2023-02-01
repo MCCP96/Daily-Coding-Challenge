2be35a9f38061fcde04bef6ade372a45c80b1840
MCCP96 <petermccomb@live.com>
2023-02-01 08:59:16 -0500

    Day 594 - Max Sum of Pair With Equal Sum of Digits

--- 2023.js
+++ 2023.js
@@ -2091,7 +2091,7 @@ var topVotedShiftingLetters = function (s, shifts) {
 }; */
 
 // Maximum White Tiles Covered by a Carpet          1/31/2023
-
+/* 
 // You are given a 2D integer array tiles where tiles[i] = [li, ri] represents that every tile j in the range li <= j <= ri is colored white.
 
 // You are also given an integer carpetLen, the length of a single carpet that can be placed anywhere.
@@ -2166,4 +2166,74 @@ var topVotedMaximumWhiteTiles = function (tiles, carpetLen) {
   }
 
   return maxCovered;
+}; */
+
+// Max Sum of a Pair With Equal Sum of Digits         2/1/2023
+
+// You are given a 0-indexed array nums consisting of positive integers. You can choose two indices i and j, such that i != j, and the sum of digits of the number nums[i] is equal to that of nums[j].
+
+// Return the maximum value of nums[i] + nums[j] that you can obtain over all possible indices i and j that satisfy the conditions.
+
+// Example 1:
+//    Input: nums = [18,43,36,13,7]
+//    Output: 54
+// Explanation: The pairs (i, j) that satisfy the conditions are:
+// - (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54.
+// - (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50.
+// So the maximum sum that we can obtain is 54.
+
+// Example 2:
+//    Input: nums = [10,12,19,14]
+//    Output: -1
+// Explanation: There are no two numbers that satisfy the conditions, so we return -1.
+
+// Constraints:
+//    1 <= nums.length <= 105
+//    1 <= nums[i] <= 109
+
+const maximumSum = (nums) => {
+  nums.sort((a, b) => b - a);
+  let map = {};
+  let dups = [];
+
+  nums.map((c) => {
+    const digits = [...`${c}`].reduce((a, c) => (a += +c), 0);
+    map[digits] ? map[digits].push(c) : (map[digits] = [c]);
+    if (map[digits].length == 2) dups.push(digits);
+  });
+  if (dups.length == 0) return -1;
+
+  let max = 0;
+  for (const key of dups) {
+    const cur = map[key];
+    max = Math.max(max, cur[0] + cur[1]);
+  }
+  return max;
+};
+
+console.log(maximumSum([18, 43, 36, 13, 7])); // 54
+console.log(maximumSum([10, 12, 19, 14])); // -1
+
+const topVotedMaximumSum = (A) => {
+  let m = new Map(),
+    res = -1;
+  for (const x of A) {
+    // accumulate same digit sum in a hashmap
+    let sum = sumOfDigit(x);
+    if (!m.has(sum)) m.set(sum, []);
+    m.get(sum).push(x);
+  }
+  for (const [sum, a] of m) {
+    // two sum max of each same digit sum
+    a.sort((x, y) => y - x);
+    if (a.length >= 2) res = Math.max(res, a[0] + a[1]);
+  }
+  return res;
+};
+
+const sumOfDigit = (x) => {
+  let s = x + "",
+    res = 0;
+  for (const c of s) res += c - "0";
+  return res;
 };
