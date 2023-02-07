7a3bf9007cf7e46d3edf5cdb59e6a0763f21a9fb
MCCP96 <petermccomb@live.com>
2023-02-07 09:25:19 -0500

    Day 600 - Minimum Lines to Represent a Line Chart

--- 2023.js
+++ 2023.js
@@ -2483,7 +2483,7 @@ var topVotedRobotWithString = function (s) {
 }; */
 
 // Intervals Between Identical Elements         2/6/2023
-
+/* 
 // You are given a 0-indexed array of n integers arr.
 
 // The interval between two elements in arr is defined as the absolute difference between their indices. More formally, the interval between arr[i] and arr[j] is |i - j|.
@@ -2573,4 +2573,98 @@ var topVotedGetDistances = function (arr) {
   }
 
   return result;
+}; */
+
+// Minimum Lines to Represent a Line Chart          2/7/2023
+
+// You are given a 2D integer array stockPrices where stockPrices[i] = [dayi, pricei] indicates the price of the stock on day dayi is pricei. A line chart is created from the array by plotting the points on an XY plane with the X-axis representing the day and the Y-axis representing the price and connecting adjacent points. One such example is shown below:
+
+// Return the minimum number of lines needed to represent the line chart.
+
+// Example 1:
+//    Input: stockPrices = [[1,7],[2,6],[3,5],[4,4],[5,4],[6,3],[7,2],[8,1]]
+//    Output: 3
+// Explanation:
+// The diagram above represents the input, with the X-axis representing the day and Y-axis representing the price.
+// The following 3 lines can be drawn to represent the line chart:
+// - Line 1 (in red) from (1,7) to (4,4) passing through (1,7), (2,6), (3,5), and (4,4).
+// - Line 2 (in blue) from (4,4) to (5,4).
+// - Line 3 (in green) from (5,4) to (8,1) passing through (5,4), (6,3), (7,2), and (8,1).
+// It can be shown that it is not possible to represent the line chart using less than 3 lines.
+
+// Example 2:
+//    Input: stockPrices = [[3,4],[1,2],[7,8],[2,3]]
+//    Output: 1
+// Explanation:
+// As shown in the diagram above, the line chart can be represented with a single line.
+
+// Constraints:
+//    1 <= stockPrices.length <= 105
+//    stockPrices[i].length == 2
+//    1 <= dayi, pricei <= 109
+//    All dayi are distinct.
+
+const minimumLines = (stockPrices) => {
+  if (stockPrices.length == 1) return 0;
+  stockPrices.sort((a, b) => a[0] - b[0]);
+  let prevslope;
+  let count = 0;
+  for (let i = 0; i < stockPrices.length - 1; i++) {
+    const [x1, y1] = stockPrices[i];
+    const [x2, y2] = stockPrices[i + 1];
+    const slope = (y2 - y1) / (x2 - x1);
+    if (slope !== prevslope) {
+      prevslope = slope;
+      count++;
+    }
+  }
+  return count;
+};
+
+// prettier-ignore
+console.log(minimumLines([[1,7],[2,6],[3,5],[4,4],[5,4],[6,3],[7,2],[8,1]])); // 3
+// prettier-ignore
+console.log(minimumLines([[3,4],[1,2],[7,8],[2,3]])); // 1
+// prettier-ignore
+console.log(minimumLines([[1,1],[500000000,499999999],[1000000000,999999998]])); // 2
+
+// Doesn't pass for larger test cases
+
+const topVotedMinimumLines = function (stockPrices) {
+  if (stockPrices.length === 1) return 0;
+  stockPrices.sort(function (a, b) {
+    return a[0] - b[0];
+  });
+  let count = 1;
+  for (let i = 2; i < stockPrices.length; i++) {
+    let x1 = BigInt(stockPrices[i - 2][0]);
+    let x2 = BigInt(stockPrices[i - 1][0]);
+    let x3 = BigInt(stockPrices[i][0]);
+    let y1 = BigInt(stockPrices[i - 2][1]);
+    let y2 = BigInt(stockPrices[i - 1][1]);
+    let y3 = BigInt(stockPrices[i][1]);
+    if ((y2 - y1) * (x3 - x2) !== (y3 - y2) * (x2 - x1)) {
+      //as  (y3-y2)/(x3-x2) = (y2-y1)/(x2-x1) for same slope
+      count++;
+    }
+  }
+  return count;
+};
+
+// Tested it and the comparison needs to be BigInts and a multiplication for this to work
+// As seen in the top voted's if statement
+
+const revisedMinimumLines = function (stockPrices) {
+  if (stockPrices.length == 1) return 0;
+  const stocks = stockPrices
+    .sort((a, b) => a[0] - b[0])
+    .map((day) => day.map((c) => BigInt(c)));
+  let count = 1;
+  for (let i = 0; i < stocks.length - 2; i++) {
+    const [x1, y1] = stocks[i];
+    const [x2, y2] = stocks[i + 1];
+    const [x3, y3] = stocks[i + 2];
+    if ((y2 - y1) * (x3 - x2) !== (y3 - y2) * (x2 - x1)) count++;
+  }
+  return count;
 };
