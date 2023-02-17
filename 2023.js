b0ea2a5df576db47ae819c0cd83678b501d82b43
MCCP96 <petermccomb@live.com>
2023-02-17 09:01:23 -0500

    Day 610 - Diff Between 1s and 0s in Row and Column

--- 2023.js
+++ 2023.js
@@ -3128,7 +3128,7 @@ console.log(rangeAddQueries(2, [[0,0,1,1]])); // [[1,1],[1,1]]
 // Anything faster is very bulky */
 
 // Maximum Sum of Distinct Subarrays With Length K          2/16/2023
-
+/* 
 // You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
 
 // The length of the subarray is k, and
@@ -3240,4 +3240,81 @@ console.log(revisedMaximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3)); // 15
 console.log(revisedMaximumSubarraySum([4, 4, 4], 3)); // 0
 console.log(revisedMaximumSubarraySum([9, 9, 9, 1, 2, 3], 3)); // 12
 
-// ♥ -Hannah
+// ♥ -Hannah */
+
+// Difference Between Ones and Zeros in Row and Column          2/17/2023
+
+// You are given a 0-indexed m x n binary matrix grid.
+
+// A 0-indexed m x n difference matrix diff is created with the following procedure:
+
+// Let the number of ones in the ith row be onesRowi.
+// Let the number of ones in the jth column be onesColj.
+// Let the number of zeros in the ith row be zerosRowi.
+// Let the number of zeros in the jth column be zerosColj.
+// diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj
+// Return the difference matrix diff.
+
+// Example 1:
+//    Input: grid = [[0,1,1],[1,0,1],[0,0,1]]
+//    Output: [[0,0,4],[0,0,4],[-2,-2,2]]
+// Explanation:
+// - diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 2 + 1 - 1 - 2 = 0
+// - diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 2 + 1 - 1 - 2 = 0
+// - diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 2 + 3 - 1 - 0 = 4
+// - diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 2 + 1 - 1 - 2 = 0
+// - diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 2 + 1 - 1 - 2 = 0
+// - diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 2 + 3 - 1 - 0 = 4
+// - diff[2][0] = onesRow2 + onesCol0 - zerosRow2 - zerosCol0 = 1 + 1 - 2 - 2 = -2
+// - diff[2][1] = onesRow2 + onesCol1 - zerosRow2 - zerosCol1 = 1 + 1 - 2 - 2 = -2
+// - diff[2][2] = onesRow2 + onesCol2 - zerosRow2 - zerosCol2 = 1 + 3 - 2 - 0 = 2
+
+// Example 2:
+//    Input: grid = [[1,1,1],[1,1,1]]
+//    Output: [[5,5,5],[5,5,5]]
+// Explanation:
+// - diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+// - diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+// - diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+// - diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+// - diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+// - diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+
+// Constraints:
+//    m == grid.length
+//    n == grid[i].length
+//    1 <= m, n <= 105
+//    1 <= m * n <= 105
+//    grid[i][j] is either 0 or 1.
+
+const onesMinusZeros = (grid) => {
+  let [row1, col1] = [
+    new Array(grid.length).fill(0),
+    new Array(grid[0].length).fill(0),
+  ];
+  let [row0, col0] = [
+    new Array(grid.length).fill(0),
+    new Array(grid[0].length).fill(0),
+  ];
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        row1[i]++;
+        col1[j]++;
+      } else {
+        row0[i]++;
+        col0[j]++;
+      }
+    }
+  }
+  return grid.map((row, i) =>
+    row.map((_, j) => row1[i] + col1[j] - row0[i] - col0[j])
+  );
+};
+
+// prettier-ignore
+console.log(onesMinusZeros([[0,1,1],[1,0,1],[0,0,1]])); // [[0,0,4],[0,0,4],[-2,-2,2]]
+// prettier-ignore
+console.log(onesMinusZeros([[1,1,1],[1,1,1]])); // [[5,5,5],[5,5,5]]
+
+// Same logic as top voted solutions
