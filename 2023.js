c947fc1e60f0f113fba959ad6f198b3e84c9285b
MCCP96 <petermccomb@live.com>
2023-02-19 09:44:14 -0500

    Day 612 - Total Cost to Hire K Workers

--- 2023.js
+++ 2023.js
@@ -3320,7 +3320,7 @@ console.log(onesMinusZeros([[1,1,1],[1,1,1]])); // [[5,5,5],[5,5,5]]
 // Same logic as top voted solutions */
 
 // Count Vowel Strings in Ranges          2/18/2023
-
+/* 
 // You are given a 0-indexed array of strings words and a 2D array of integers queries.
 
 // Each query queries[i] = [li, ri] asks us to find the number of strings present in the range li to ri (both inclusive) of words that start and end with a vowel.
@@ -3418,4 +3418,124 @@ const revisedVowelStrings = (w, q) => {
 };
 
 // This approach avoids the nested loop at the end, making it much faster
-// First time seeing the addition of a boolean used in this way
+// First time seeing the addition of a boolean used in this way */
+
+// Total Cost to Hire K Workers         2/19/2023
+
+// You are given a 0-indexed integer array costs where costs[i] is the cost of hiring the ith worker.
+
+// You are also given two integers k and candidates. We want to hire exactly k workers according to the following rules:
+
+// You will run k sessions and hire exactly one worker in each session.
+// In each hiring session, choose the worker with the lowest cost from either the first candidates workers or the last candidates workers. Break the tie by the smallest index.
+// For example, if costs = [3,2,7,7,1,2] and candidates = 2, then in the first hiring session, we will choose the 4th worker because they have the lowest cost [3,2,7,7,1,2].
+// In the second hiring session, we will choose 1st worker because they have the same lowest cost as 4th worker but they have the smallest index [3,2,7,7,2]. Please note that the indexing may be changed in the process.
+// If there are fewer than candidates workers remaining, choose the worker with the lowest cost among them. Break the tie by the smallest index.
+// A worker can only be chosen once.
+// Return the total cost to hire exactly k workers.
+
+// Example 1:
+//    Input: costs = [17,12,10,2,7,2,11,20,8], k = 3, candidates = 4
+//    Output: 11
+// Explanation: We hire 3 workers in total. The total cost is initially 0.
+// - In the first hiring round we choose the worker from [17,12,10,2,7,2,11,20,8]. The lowest cost is 2, and we break the tie by the smallest index, which is 3. The total cost = 0 + 2 = 2.
+// - In the second hiring round we choose the worker from [17,12,10,7,2,11,20,8]. The lowest cost is 2 (index 4). The total cost = 2 + 2 = 4.
+// - In the third hiring round we choose the worker from [17,12,10,7,11,20,8]. The lowest cost is 7 (index 3). The total cost = 4 + 7 = 11. Notice that the worker with index 3 was common in the first and last four workers.
+// The total hiring cost is 11.
+
+// Example 2:
+//    Input: costs = [1,2,4,1], k = 3, candidates = 3
+//    Output: 4
+// Explanation: We hire 3 workers in total. The total cost is initially 0.
+// - In the first hiring round we choose the worker from [1,2,4,1]. The lowest cost is 1, and we break the tie by the smallest index, which is 0. The total cost = 0 + 1 = 1. Notice that workers with index 1 and 2 are common in the first and last 3 workers.
+// - In the second hiring round we choose the worker from [2,4,1]. The lowest cost is 1 (index 2). The total cost = 1 + 1 = 2.
+// - In the third hiring round there are less than three candidates. We choose the worker from the remaining workers [2,4]. The lowest cost is 2 (index 0). The total cost = 2 + 2 = 4.
+// The total hiring cost is 4.
+
+// Constraints:
+//    1 <= costs.length <= 105
+//    1 <= costs[i] <= 105
+//    1 <= k, candidates <= costs.length
+
+const totalCost = (costs, k, candidates) => {
+  let res = 0;
+  for (let i = 0; i < k; i++) {
+    if (costs.length <= 2 * candidates) {
+      res += +costs.splice(costs.indexOf(Math.min(...costs)), 1);
+    } else {
+      let min = { index: Infinity, val: Infinity };
+
+      for (let j = 0; j < candidates; j++) {
+        const [l, r] = [costs[j], costs[costs.length - 1 - j]];
+
+        if (
+          (l > min.val && r > min.val) ||
+          (l == min.val && j > min.index) ||
+          (r == min.val && costs.length - 1 - j > min.index)
+        )
+          continue;
+
+        min =
+          l <= r
+            ? { index: j, val: l }
+            : { index: costs.length - 1 - j, val: r };
+      }
+      res += +costs.splice(min.index, 1);
+    }
+  }
+  return res;
+};
+
+console.log(totalCost([17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4)); // 11
+console.log(totalCost([1, 2, 4, 1], 3, 3)); // 4
+
+// Exceeds runtime limitations
+
+const topVotedTotalCost = (a, k, m) => {
+  let pq = new MinPriorityQueue({
+    compare: (x, y) => {
+      if (x[0] != y[0]) return x[0] - y[0];
+      return x[1] - y[1];
+    },
+  });
+
+  let n = a.length,
+    l = 0,
+    r = n - 1,
+    res = 0;
+
+  for (let i = 0; i < m; i++) {
+    if (l <= r) {
+      pq.enqueue([a[l], l]);
+      l++;
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    if (l <= r) {
+      pq.enqueue([a[r], r]);
+      r--;
+    }
+  }
+
+  for (let i = 0; i < k; i++) {
+    let cur = pq.dequeue();
+    res += cur[0];
+    if (cur[1] < l && l <= r) {
+      pq.enqueue([a[l], l]);
+      l++;
+    } else if (cur[1] > r && l <= r) {
+      pq.enqueue([a[r], r]);
+      r--;
+    }
+  }
+  return res;
+};
+
+// Today I learnt that the LeetCode javascript environment includes a couple packages:
+
+//    lodash
+//    datastructures-js/priority-queue
+//    datastructures-js/queue
+
+// https://support.leetcode.com/hc/en-us/articles/360011833974-What-are-the-environments-for-the-programming-languages-
