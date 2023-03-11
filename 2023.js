3e1272c33b3662c10b96e72f8cd928ffcfa66407
MCCP96 <petermccomb@live.com>
2023-03-11 11:19:20 -0500

    Day 632 - Sum of Subarray Ranges

--- 2023.js
+++ 2023.js
@@ -4796,7 +4796,7 @@ console.log(minimumDeletions([101])); // 1
 // Probably better runtime */
 
 // Find Good Days to Rob the Bank					3/10/2023
-
+/* 
 // You and a gang of thieves are planning on robbing a bank. You are given a 0-indexed integer array security, where security[i] is the number of guards on duty on the ith day. The days are numbered starting from 0. You are also given an integer time.
 
 // The ith day is a good day to rob the bank if:
@@ -4885,4 +4885,66 @@ var topVotedGoodDaysToRobBank = function (security, time) {
   return output;
 };
 
-// Much better runtime
+// Much better runtime */
+
+// Sum of Subarray Ranges					3/11/2023
+
+// You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
+
+// Return the sum of all subarray ranges of nums.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+// 		Input: nums = [1,2,3]
+// 		Output: 4
+// Explanation: The 6 subarrays of nums are the following:
+// 		[1], range = largest - smallest = 1 - 1 = 0
+// 		[2], range = 2 - 2 = 0
+// 		[3], range = 3 - 3 = 0
+// 		[1,2], range = 2 - 1 = 1
+// 		[2,3], range = 3 - 2 = 1
+// 		[1,2,3], range = 3 - 1 = 2
+// 		So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+
+// Example 2:
+// 		Input: nums = [1,3,3]
+// 		Output: 4
+// Explanation: The 6 subarrays of nums are the following:
+// 		[1], range = largest - smallest = 1 - 1 = 0
+// 		[3], range = 3 - 3 = 0
+// 		[3], range = 3 - 3 = 0
+// 		[1,3], range = 3 - 1 = 2
+// 		[3,3], range = 3 - 3 = 0
+// 		[1,3,3], range = 3 - 1 = 2
+// 		So the sum of all ranges is 0 + 0 + 0 + 2 + 0 + 2 = 4.
+
+// Example 3:
+// 		Input: nums = [4,-2,-3,4,1]
+// 		Output: 59
+// Explanation: The sum of all subarray ranges of nums is 59.
+
+// Constraints:
+//		1 <= nums.length <= 1000
+//		-109 <= nums[i] <= 109
+
+// Follow-up: Could you find a solution with O(n) time complexity?
+
+const subArrayRanges = (nums) => {
+  let res = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    let [min, max] = [nums[i], nums[i]];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < min) min = nums[j];
+      if (nums[j] > max) max = nums[j];
+      res += max - min;
+    }
+  }
+  return res;
+};
+
+console.log(subArrayRanges([1, 2, 3])); // 4
+console.log(subArrayRanges([1, 3, 3])); // 4
+console.log(subArrayRanges([4, -2, -3, 4, 1])); // 59
+
+// Same as top voted
