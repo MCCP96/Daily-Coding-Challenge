f2b0a584c0fd8a088fdb9d2d9ce2f1f012cedf27
MCCP96 <petermccomb@live.com>
2023-03-12 10:10:14 -0400

    Day 633 - Find All Possible Recipes from Supplies

--- 2023.js
+++ 2023.js
@@ -4888,7 +4888,7 @@ var topVotedGoodDaysToRobBank = function (security, time) {
 // Much better runtime */
 
 // Sum of Subarray Ranges					3/11/2023
-
+/* 
 // You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
 
 // Return the sum of all subarray ranges of nums.
@@ -4947,4 +4947,110 @@ console.log(subArrayRanges([1, 2, 3])); // 4
 console.log(subArrayRanges([1, 3, 3])); // 4
 console.log(subArrayRanges([4, -2, -3, 4, 1])); // 59
 
-// Same as top voted
+// Same as top voted */
+
+// Find All Possible Recipes from Given Supplies					3/12/2023
+
+// You have information about n different recipes. You are given a string array recipes and a 2D string array ingredients. The ith recipe has the name recipes[i], and you can create it if you have all the needed ingredients from ingredients[i]. Ingredients to a recipe may need to be created from other recipes, i.e., ingredients[i] may contain a string that is in recipes.
+
+// You are also given a string array supplies containing all the ingredients that you initially have, and you have an infinite supply of all of them.
+
+// Return a list of all the recipes that you can create. You may return the answer in any order.
+
+// Note that two recipes may contain each other in their ingredients.
+
+// Example 1:
+// 		Input: recipes = ["bread"], ingredients = [["yeast","flour"]], supplies = ["yeast","flour","corn"]
+// 		Output: ["bread"]
+// Explanation:
+// 		We can create "bread" since we have the ingredients "yeast" and "flour".
+
+// Example 2:
+// 		Input: recipes = ["bread","sandwich"], ingredients = [["yeast","flour"],["bread","meat"]], supplies = ["yeast","flour","meat"]
+// 		Output: ["bread","sandwich"]
+// Explanation:
+// 		We can create "bread" since we have the ingredients "yeast" and "flour".
+// 		We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
+
+// Example 3:
+// 		Input: recipes = ["bread","sandwich","burger"], ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]], supplies = ["yeast","flour","meat"]
+// 		Output: ["bread","sandwich","burger"]
+// Explanation:
+// 		We can create "bread" since we have the ingredients "yeast" and "flour".
+// 		We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
+// 		We can create "burger" since we have the ingredient "meat" and can create the ingredients "bread" and "sandwich".
+
+// Constraints:
+//		n == recipes.length == ingredients.length
+//		1 <= n <= 100
+//		1 <= ingredients[i].length, supplies.length <= 100
+//		1 <= recipes[i].length, ingredients[i][j].length, supplies[k].length <= 10
+//		recipes[i], ingredients[i][j], and supplies[k] consist only of lowercase English letters.
+//		All the values of recipes and supplies combined are unique.
+//		Each ingredients[i] does not contain any duplicate values.
+
+const topVotedFindAllRecipes = function (recipes, ingredients, supplies) {
+  const suppliesSet = new Set(supplies),
+    res = [],
+    recipesMap = new Map(),
+    cantMake = new Set();
+  // Reverse map all recipes
+  for (let i = 0; i < recipes.length; i++) recipesMap.set(recipes[i], i);
+
+  // Check if we can make the given food name
+  const canMake = (name, visited = new Set()) => {
+    if (suppliesSet.has(name)) return true;
+    if (!recipesMap.has(name) || cantMake.has(name) || visited.has(name))
+      return false;
+
+    // We can't make it using our current supplies, find its recipe
+    const idx = recipesMap.get(name);
+    visited.add(name); // Prevent loops
+    for (let i = 0; i < ingredients[idx].length; i++) {
+      const ingredient = ingredients[idx][i];
+      if (!canMake(ingredient, visited)) {
+        cantMake.add(name);
+        return false;
+      }
+    }
+
+    // Add the food to our list of supplies
+    suppliesSet.add(name);
+    return true;
+  };
+  for (let i = 0; i < recipes.length; i++)
+    if (canMake(recipes[i])) res.push(recipes[i]);
+  return res;
+};
+
+console.log(
+  topVotedFindAllRecipes(
+    ["bread"],
+    [["yeast", "flour"]],
+    ["yeast", "flour", "corn"]
+  )
+); // ["bread"]
+console.log(
+  topVotedFindAllRecipes(
+    ["bread", "sandwich"],
+    [
+      ["yeast", "flour"],
+      ["bread", "meat"],
+    ],
+    ["yeast", "flour", "meat"]
+  )
+); // ["bread","sandwich"]
+console.log(
+  topVotedFindAllRecipes(
+    ["bread", "sandwich", "burger"],
+    [
+      ["yeast", "flour"],
+      ["bread", "meat"],
+      ["sandwich", "meat", "bread"],
+    ],
+    ["yeast", "flour", "meat"]
+  )
+); // ["bread","sandwich","burger"]
+
+// Fell down a different train of thought and couldn't get it working
+// Very nice code, felt very easy to read and understand
