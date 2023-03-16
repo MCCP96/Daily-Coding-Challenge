1d3b220ec717510dc70924531d338f257141741d
MCCP96 <petermccomb@live.com>
2023-03-16 11:12:22 -0400

    Day 637 - Minimum Swaps to Group All 1's Together

--- 2023.js
+++ 2023.js
@@ -5169,7 +5169,7 @@ console.log(asteroidsDestroyed(5, [4, 9, 23, 4])); // false
 // Same logic as top voteds */
 
 // Longest Palindrome by Concatenating Two Letter Words					3/15/2023
-
+/* 
 // You are given an array of strings words. Each element of words consists of two lowercase English letters.
 
 // Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
@@ -5250,4 +5250,73 @@ const revisedLongestPalindrome = (words) => {
   return count + (centers > 0 && 2);
 };
 
-// Somehow slower than previous version
+// Somehow slower than previous version */
+
+// Minimum Swaps to Group All 1's Together II					3/16/2023
+
+// A swap is defined as taking two distinct positions in an array and swapping the values in them.
+
+// A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+
+// Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+
+// Example 1:
+// 		Input: nums = [0,1,0,1,1,0,0]
+// 		Output: 1
+// Explanation: Here are a few of the ways to group all the 1's together:
+// 		[0,0,1,1,1,0,0] using 1 swap.
+// 		[0,1,1,1,0,0,0] using 1 swap.
+// 		[1,1,0,0,0,0,1] using 2 swaps (using the circular property of the array).
+// 		There is no way to group all 1's together with 0 swaps.
+// 		Thus, the minimum number of swaps required is 1.
+
+// Example 2:
+// 		Input: nums = [0,1,1,1,0,0,1,1,0]
+// 		Output: 2
+// Explanation: Here are a few of the ways to group all the 1's together:
+// 		[1,1,1,0,0,0,0,1,1] using 2 swaps (using the circular property of the array).
+// 		[1,1,1,1,1,0,0,0,0] using 2 swaps.
+// 		There is no way to group all 1's together with 0 or 1 swaps.
+// 		Thus, the minimum number of swaps required is 2.
+
+// Example 3:
+// 		Input: nums = [1,1,0,0,1]
+// 		Output: 0
+// Explanation: All the 1's are already grouped together due to the circular property of the array.
+// 		Thus, the minimum number of swaps required is 0.
+
+// Constraints:
+//		1 <= nums.length <= 105
+//		nums[i] is either 0 or 1.
+
+const topVotedMinSwaps = (nums) => {
+  let ones = nums.reduce((a, bit) => a + bit, 0);
+  const doubledNums = nums.concat(nums);
+
+  let min = Number.MAX_SAFE_INTEGER;
+  let [left, withinWindow] = [0, 0];
+
+  for (let i = 0; i < doubledNums.length; ++i) {
+    if (doubledNums[i] === 1) withinWindow += 1;
+    if (i >= ones) {
+      if (doubledNums[left] === 1) withinWindow -= 1;
+      left++;
+    }
+    if (i + 1 >= ones) min = Math.min(min, ones - withinWindow);
+  }
+
+  return min;
+};
+
+console.log(topVotedMinSwaps([0, 1, 0, 1, 1, 0, 0])); // 1
+console.log(topVotedMinSwaps([0, 1, 1, 1, 0, 0, 1, 1, 0])); // 2
+console.log(topVotedMinSwaps([1, 1, 0, 0, 1])); // 0
+console.log(
+  topVotedMinSwaps([
+    1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1,
+    0, 0, 1, 1, 0, 0, 1, 0, 0,
+  ])
+); // 7
+
+// Couldn't get it working
+// Sliding window makes sense
