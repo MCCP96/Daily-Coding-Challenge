7e34dad8ea950ddc7798ed7fa67970b0ae8dea09
MCCP96 <petermccomb@live.com>
2023-03-20 08:40:05 -0400

    Day 641 - Rearrange Array Elements by Sign

--- 2023.js
+++ 2023.js
@@ -5497,7 +5497,7 @@ var topVotedMostPoints = function (questions) {
 }; */
 
 // Minimum Moves to Reach Target Score					3/19/2023
-
+/* 
 // You are playing a game with integers. You start with the integer 1 and you want to reach the integer target.
 
 // In one move, you can either:
@@ -5581,4 +5581,70 @@ const revisedMinMoves = (t, dubs) => {
 };
 
 // Cleaned up top voted
-// Beats 100%
+// Beats 100% */
+
+// Rearrange Array Elements by Sign					3/20/2023
+
+// You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+
+// You should rearrange the elements of nums such that the modified array follows the given conditions:
+
+// Every consecutive pair of integers have opposite signs.
+
+// For all integers with the same sign, the order in which they were present in nums is preserved.
+
+// The rearranged array begins with a positive integer.
+
+// Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+// Example 1:
+// 		Input: nums = [3,1,-2,-5,2,-4]
+// 		Output: [3,-2,1,-5,2,-4]
+// Explanation:
+// 		The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+// 		The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+// 		Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.
+
+// Example 2:
+// 		Input: nums = [-1,1]
+// 		Output: [1,-1]
+// Explanation:
+// 		1 is the only positive integer and -1 the only negative integer in nums.
+// 		So nums is rearranged to [1,-1].
+
+// Constraints:
+//		2 <= nums.length <= 2 * 105
+//		nums.length is even
+//		1 <= |nums[i]| <= 105
+//		nums consists of equal number of positive and negative integers.
+
+const rearrangeArray = (nums) => {
+  let [pos, neg] = [[], []];
+  nums.forEach((num) => (num > 0 ? pos.push(num) : neg.push(num)));
+
+  return pos.reduce((res, c, i) => {
+    res.push(c, neg[i]);
+    return res;
+  }, []);
+};
+
+console.log(rearrangeArray([3, 1, -2, -5, 2, -4])); // [3,-2,1,-5,2,-4]
+console.log(rearrangeArray([-1, 1])); // [1,-1]
+
+// Nothing fancy
+
+function topVotedRearrangeArray(nums) {
+  const result = [];
+  const index = { pos: 0, neg: 1 };
+
+  for (const num of nums) {
+    const sign = num >= 0 ? "pos" : "neg";
+
+    result[index[sign]] = num;
+    console.log(result);
+    index[sign] += 2;
+  }
+  return result;
+}
+
+// This is more what I was looking for
