66f88556914db67348a75d6f4e9d83196e1833ce
MCCP96 <petermccomb@live.com>
2023-03-23 09:54:40 -0400

    Day 644 - Partition Array According to Given Pivot

--- 2023.js
+++ 2023.js
@@ -5704,7 +5704,7 @@ console.log(findLonely([1, 3, 5, 3])); // [1,5]
 // Same as top voted */
 
 // All Divisions With the Highest Score of a Binary Array					3/22/2023
-
+/* 
 // You are given a 0-indexed binary array nums of length n. nums can be divided at index i (where 0 <= i <= n) into two arrays (possibly empty) numsleft and numsright:
 
 // numsleft has all the elements of nums between index 0 and i - 1 (inclusive), while numsright has all the elements of nums between index i and n - 1 (inclusive).
@@ -5801,4 +5801,98 @@ const topVotedMaxScoreIndices = function (nums) {
     maxScore = Math.max(score, maxScore);
   }
   return map[maxScore];
+}; */
+
+// Partition Array According to Given Pivot					3/23/2023
+
+// You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+// Every element less than pivot appears before every element greater than pivot.
+
+// Every element equal to pivot appears in between the elements less than and greater than pivot.
+
+// The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+
+// More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. For elements less than pivot, if i < j and nums[i] < pivot and nums[j] < pivot, then pi < pj. Similarly for elements greater than pivot, if i < j and nums[i] > pivot and nums[j] > pivot, then pi < pj.
+
+// Return nums after the rearrangement.
+
+// Example 1:
+// 		Input: nums = [9,12,5,10,14,3,10], pivot = 10
+// 		Output: [9,5,3,10,10,12,14]
+// Explanation:
+// 		The elements 9, 5, and 3 are less than the pivot so they are on the left side of the array.
+// 		The elements 12 and 14 are greater than the pivot so they are on the right side of the array.
+// 		The relative ordering of the elements less than and greater than pivot is also maintained. [9, 5, 3] and [12, 14] are the respective orderings.
+
+// Example 2:
+// 		Input: nums = [-3,4,3,2], pivot = 2
+// 		Output: [-3,2,4,3]
+// Explanation:
+// 		The element -3 is less than the pivot so it is on the left side of the array.
+// 		The elements 4 and 3 are greater than the pivot so they are on the right side of the array.
+// 		The relative ordering of the elements less than and greater than pivot is also maintained. [-3] and [4, 3] are the respective orderings.
+
+// Constraints:
+//		1 <= nums.length <= 105
+//		-106 <= nums[i] <= 106
+//		pivot equals to an element of nums.
+
+const pivotArray = (nums, p) => {
+  let [l, m, r] = [[], [], []];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < p) l.push(nums[i]);
+    else if (nums[i] > p) r.push(nums[i]);
+    else m.push(nums[i]);
+  }
+
+  return [l, m, r].flat();
 };
+
+console.log(pivotArray([9, 12, 5, 10, 14, 3, 10], 10)); // [9,5,3,10,10,12,14]
+console.log(pivotArray([-3, 4, 3, 2], 2)); // [-3,2,4,3]
+
+// Easy code
+// Slow runtime
+
+const topVotedPivotArray = (nums, pivot) => {
+  let N = nums.length;
+  let output = [];
+
+  let i = 0;
+  let j = N - 1;
+
+  let L = 0;
+  let R = N - 1;
+
+  while (i < nums.length) {
+    if (nums[i] < pivot) output[L++] = nums[i];
+    if (nums[j] > pivot) output[R--] = nums[j];
+
+    i++;
+    j--;
+  }
+
+  while (L <= R) {
+    output[L++] = pivot;
+  }
+
+  return output;
+};
+
+const revisedPivotArray = (nums, p) => {
+  let res = [];
+  let [l, r] = [0, nums.length - 1];
+
+  for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+    if (nums[i] < p) res[l++] = nums[i];
+    if (nums[j] > p) res[r--] = nums[j];
+  }
+  while (l <= r) res[l++] = p;
+
+  return res;
+};
+
+// Equally poor runtime, but
+// I like how the ++ and --s were built into their respective statements
