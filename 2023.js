758cee6e0b77ff19a40b6c4260f7490e5d725f86
MCCP96 <petermccomb@live.com>
2023-04-04 10:17:33 -0400

    Day 656 - Minimum Operations to Halve Array Sum

--- 2023.js
+++ 2023.js
@@ -6569,7 +6569,7 @@ const topVotedMaximumTop = function (nums, k) {
 // Makes much more sense */
 
 // Maximize Number of Subsequences in a String					4/3/2023
-
+/* 
 // You are given a 0-indexed string text and another 0-indexed string pattern of length 2, both of which consist of only lowercase English letters.
 
 // You can add either pattern[0] or pattern[1] anywhere in text exactly once. Note that the character can be added even at the beginning or at the end of text.
@@ -6646,4 +6646,87 @@ const topVotedMaximumSubsequenceCount = function (text, pattern) {
 };
 
 // '(x * (x + 1)) / 2' counts cummulative occurrences of x
-// Saw it a couple problems ago and allows to break early from repetitive counting
+// Saw it a couple problems ago and allows to break early from repetitive counting */
+
+// Minimum Operations to Halve Array Sum					4/4/2023
+
+// You are given an array nums of positive integers. In one operation, you can choose any number from nums and reduce it to exactly half the number. (Note that you may choose this reduced number in future operations.)
+
+// Return the minimum number of operations to reduce the sum of nums by at least half.
+
+// Example 1:
+// 		Input: nums = [5,19,8,1]
+// 		Output: 3
+// Explanation: The initial sum of nums is equal to 5 + 19 + 8 + 1 = 33.
+// 		The following is one of the ways to reduce the sum by at least half:
+// 		Pick the number 19 and reduce it to 9.5.
+// 		Pick the number 9.5 and reduce it to 4.75.
+// 		Pick the number 8 and reduce it to 4.
+// 		The final array is [5, 4.75, 4, 1] with a total sum of 5 + 4.75 + 4 + 1 = 14.75.
+// 		The sum of nums has been reduced by 33 - 14.75 = 18.25, which is at least half of the initial sum, 18.25 >= 33/2 = 16.5.
+// 		Overall, 3 operations were used so we return 3.
+// 		It can be shown that we cannot reduce the sum by at least half in less than 3 operations.
+
+// Example 2:
+// 		Input: nums = [3,8,20]
+// 		Output: 3
+// Explanation: The initial sum of nums is equal to 3 + 8 + 20 = 31.
+// 		The following is one of the ways to reduce the sum by at least half:
+// 		Pick the number 20 and reduce it to 10.
+// 		Pick the number 10 and reduce it to 5.
+// 		Pick the number 3 and reduce it to 1.5.
+// 		The final array is [1.5, 8, 5] with a total sum of 1.5 + 8 + 5 = 14.5.
+// 		The sum of nums has been reduced by 31 - 14.5 = 16.5, which is at least half of the initial sum, 16.5 >= 31/2 = 15.5.
+// 		Overall, 3 operations were used so we return 3.
+// 		It can be shown that we cannot reduce the sum by at least half in less than 3 operations.
+
+// Constraints:
+//		1 <= nums.length <= 105
+//		1 <= nums[i] <= 107
+
+const halveArray = (nums) => {
+  let operations = 0;
+  let tot = nums.reduce((a, c) => a + c, 0);
+  let half = tot / 2;
+
+  while (tot > half) {
+    nums.sort((a, b) => b - a);
+    const div = nums[0] / 2;
+    tot -= div;
+    nums[0] = div;
+    operations++;
+  }
+
+  return operations;
+};
+
+console.log(halveArray([5, 19, 8, 1])); // 3
+console.log(halveArray([3, 8, 20])); // 3
+
+// Sorting on every iteration is heavy
+
+var topVotedHalveArray = function (nums) {
+  let sum = nums.reduce((a, b) => a + b, 0);
+  const pq = new MaxPriorityQueue();
+
+  for (let i = 0; i < nums.length; i++) {
+    pq.enqueue(nums[i]);
+  }
+
+  let n = sum / 2,
+    c = 0;
+
+  while (sum > n) {
+    let x = pq.dequeue();
+    x = x.element / 2;
+    sum -= x;
+    pq.enqueue(x);
+    c++;
+  }
+
+  return c;
+};
+
+// Similar logic to my solution
+// MaxPriorityQueue() keeps the largest value accessible with .dequeue()
+// Saves from sorting on every iteration
