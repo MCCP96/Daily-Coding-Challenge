154f3af985859da13fb66e942dfe6719411b2b04
MCCP96 <petermccomb@live.com>
2023-04-05 10:46:03 -0400

    Day 657 - Maximum Points in an Archery Competition

--- 2023.js
+++ 2023.js
@@ -6649,7 +6649,7 @@ const topVotedMaximumSubsequenceCount = function (text, pattern) {
 // Saw it a couple problems ago and allows to break early from repetitive counting */
 
 // Minimum Operations to Halve Array Sum					4/4/2023
-
+/* 
 // You are given an array nums of positive integers. In one operation, you can choose any number from nums and reduce it to exactly half the number. (Note that you may choose this reduced number in future operations.)
 
 // Return the minimum number of operations to reduce the sum of nums by at least half.
@@ -6729,4 +6729,85 @@ var topVotedHalveArray = function (nums) {
 
 // Similar logic to my solution
 // MaxPriorityQueue() keeps the largest value accessible with .dequeue()
-// Saves from sorting on every iteration
+// Saves from sorting on every iteration */
+
+// Maximum Points in an Archery Competition					4/5/2023
+
+// Alice and Bob are opponents in an archery competition. The competition has set the following rules:
+
+// Alice first shoots numArrows arrows and then Bob shoots numArrows arrows.
+
+// The points are then calculated as follows:
+
+// The target has integer scoring sections ranging from 0 to 11 inclusive.
+
+// For each section of the target with score k (in between 0 to 11), say Alice and Bob have shot ak and bk arrows on that section respectively. If ak >= bk, then Alice takes k points. If ak < bk, then Bob takes k points.
+
+// However, if ak == bk == 0, then nobody takes k points.
+
+// For example, if Alice and Bob both shot 2 arrows on the section with score 11, then Alice takes 11 points. On the other hand, if Alice shot 0 arrows on the section with score 11 and Bob shot 2 arrows on that same section, then Bob takes 11 points.
+
+// You are given the integer numArrows and an integer array aliceArrows of size 12, which represents the number of arrows Alice shot on each scoring section from 0 to 11. Now, Bob wants to maximize the total number of points he can obtain.
+
+// Return the array bobArrows which represents the number of arrows Bob shot on each scoring section from 0 to 11. The sum of the values in bobArrows should equal numArrows.
+
+// If there are multiple ways for Bob to earn the maximum total points, return any one of them.
+
+// Example 1:
+// 		Input: numArrows = 9, aliceArrows = [1,1,0,1,0,0,2,1,0,1,2,0]
+// 		Output: [0,0,0,0,1,1,0,0,1,2,3,1]
+// Explanation: The table above shows how the competition is scored.
+// 		Bob earns a total point of 4 + 5 + 8 + 9 + 10 + 11 = 47.
+// 		It can be shown that Bob cannot obtain a score higher than 47 points.
+
+// Example 2:
+// 		Input: numArrows = 3, aliceArrows = [0,0,1,0,0,0,0,0,0,0,0,2]
+// 		Output: [0,0,0,0,0,0,0,0,1,1,1,0]
+// Explanation: The table above shows how the competition is scored.
+// 		Bob earns a total point of 8 + 9 + 10 = 27.
+// 		It can be shown that Bob cannot obtain a score higher than 27 points.
+
+// Constraints:
+//		1 <= numArrows <= 105
+//		aliceArrows.length == bobArrows.length == 12
+//		0 <= aliceArrows[i], bobArrows[i] <= numArrows
+//		sum(aliceArrows[i]) == numArrows
+
+const maximumBobPoints = (numArrows, aliceArrows) => {
+  let res = new Array(12).fill(0);
+  let max = 0;
+
+  const backtrack = (i, arrows, tot, bobArrows) => {
+    if (arrows == 0 || i == 0) {
+      if (tot > max) {
+        bobArrows[0] = arrows; // leftover arrows
+        max = tot;
+        res = bobArrows;
+      }
+      return;
+    }
+
+    // skip
+    backtrack(i - 1, arrows, tot, [...bobArrows]);
+
+    // take
+    let arrowsToBeatAlice = aliceArrows[i] + 1;
+    if (arrowsToBeatAlice <= arrows) {
+      bobArrows[i] = arrowsToBeatAlice;
+      backtrack(i - 1, arrows - arrowsToBeatAlice, tot + i, bobArrows);
+    }
+  };
+
+  backtrack(11, numArrows, 0, new Array(12).fill(0));
+
+  return res;
+};
+
+console.log(maximumBobPoints(9, [1, 1, 0, 1, 0, 0, 2, 1, 0, 1, 2, 0])); // [0,0,0,0,1,1,0,0,1,2,3,1]
+console.log(maximumBobPoints(3, [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2])); // [0,0,0,0,0,0,0,0,1,1,1,0]
+console.log(maximumBobPoints(89, [3, 2, 28, 1, 7, 1, 16, 7, 3, 13, 3, 5])); // [21,3,0,2,8,2,17,8,4,14,4,6]
+
+// Same as top voted
+
+// Really getting comfortable with backtracking
+// took me a while to find a bug, fixed by returning [...bobArrows] in skip
