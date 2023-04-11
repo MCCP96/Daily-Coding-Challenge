a55509359a4055c33b527c0a0ff3dc6775d2a649
MCCP96 <petermccomb@live.com>
2023-04-11 09:46:39 -0400

    Day 663 - Find Players With Zero or One Losses

--- 2023.js
+++ 2023.js
@@ -7073,7 +7073,7 @@ const op = (s, t) => {
 // Very nice */
 
 // Minimum Common Value					4/10/2023
-
+/* 
 // Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
 
 // Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
@@ -7103,4 +7103,95 @@ console.log(getCommon([1, 2, 3], [2, 4])); // 2
 console.log(getCommon([1, 2, 3, 6], [2, 3, 4, 5])); // 2
 
 // Was too slow at first
-// Worked once array was cast as Set
+// Worked once array was cast as Set */
+
+// Find Players With Zero or One Losses					4/11/2023
+
+// You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+
+// Return a list answer of size 2 where:
+
+// answer[0] is a list of all players that have not lost any matches.
+
+// answer[1] is a list of all players that have lost exactly one match.
+
+// The values in the two lists should be returned in increasing order.
+
+// Note:
+
+// You should only consider the players that have played at least one match.
+
+// The testcases will be generated such that no two matches will have the same outcome.
+
+// Example 1:
+// 		Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+// 		Output: [[1,2,10],[4,5,7,8]]
+// Explanation:
+// 		Players 1, 2, and 10 have not lost any matches.
+// 		Players 4, 5, 7, and 8 each have lost one match.
+// 		Players 3, 6, and 9 each have lost two matches.
+// 		Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+
+// Example 2:
+// 		Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+// 		Output: [[1,2,5,6],[]]
+// Explanation:
+// 		Players 1, 2, 5, and 6 have not lost any matches.
+// 		Players 3 and 4 each have lost two matches.
+// 		Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+
+// Constraints:
+//		1 <= matches.length <= 10^5
+//		matches[i].length == 2
+//		1 <= winneri, loseri <= 10^5
+//		winneri != loseri
+//		All matches[i] are unique.
+
+const findWinners = (matches) => {
+  const winners = new Set();
+  const lossMap = matches.reduce((map, [winner, loser]) => {
+    winners.add(winner);
+    return map.set(loser, map.get(loser) + 1 || 1);
+  }, new Map());
+
+  const noLoss = [...winners]
+    .filter((player) => !lossMap.has(player))
+    .sort((a, b) => a - b);
+
+  const oneLoss = [...lossMap]
+    .reduce((a, [player, losses]) => {
+      if (losses == 1) a.push(player);
+      return a;
+    }, [])
+    .sort((a, b) => a - b);
+
+  return [noLoss, oneLoss];
+};
+
+// prettier-ignore
+console.log(findWinners([[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]])) // [[1,2,10],[4,5,7,8]]
+// prettier-ignore
+console.log(findWinners([[2,3],[1,3],[5,4],[6,4]])) // [[1,2,5,6],[]]
+
+// Very readable I find
+
+var topVotedFindWinners = function (matches) {
+  let p = new Set(); // all players
+  let ml = new Map(); // players who also lost
+
+  for (let [w, l] of matches) {
+    p.add(w);
+    p.add(l);
+    ml.set(l, (ml.get(l) || 0) + 1);
+  }
+
+  let ans = [[], []]; // [winners, players with 1 loss]
+  for (let x of p) {
+    if (ml.has(x) === false) ans[0].push(x);
+    if (ml.get(x) === 1) ans[1].push(x);
+  }
+  return ans.map((arr) => arr.sort((a, b) => a - b));
+};
+
+// Same logic
+// Cleaner code
