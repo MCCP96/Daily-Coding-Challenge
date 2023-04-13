4bbba2e7e7c0860b17d2d32b0f917857ac782a17
MCCP96 <petermccomb@live.com>
2023-04-13 09:20:23 -0400

    Day 665 - Counter

--- 2023.js
+++ 2023.js
@@ -7197,7 +7197,7 @@ var topVotedFindWinners = function (matches) {
 // Cleaner code */
 
 // Array Prototype Last					4/12/2023
-
+/* 
 // Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
 
 // Example 1:
@@ -7231,4 +7231,39 @@ Array.prototype.topVotedLast = function () {
 // First time seeing .at
 
 // Leetcode dropped a new category today for Javascript Front-end questions only
-// Very cool, will be doing more of these definitely
+// Very cool, will be doing more of these definitely */
+
+// Counter					4/13/2023
+
+// Write a function that accepts an integer n and returns a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+// Example 1:
+// 		Input:
+// 		n = 10
+// 		["call","call","call"]
+// 		Output: [10,11,12]
+// Explanation:
+// 		counter() = 10 // The first time counter() is called, it returns n.
+// 		counter() = 11 // Returns 1 more than the previous time.
+// 		counter() = 12 // Returns 1 more than the previous time.
+
+// Example 2:
+// 		Input:
+// 		n = -2
+// 		["call","call","call","call","call"]
+// 		Output: [-2,-1,0,1,2]
+// Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+
+// Constraints:
+//		-1000 <= n <= 1000
+//		At most 1000 calls to counter() will be made
+
+var createCounter = (n) => () => n++;
+
+const call1 = createCounter(10);
+console.log(call1(), call1(), call1());
+
+const call2 = createCounter(-2);
+console.log(call2(), call2(), call2(), call2(), call2());
+
+// Same as top voted
