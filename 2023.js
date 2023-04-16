febcc9b096153066b4db6dcd5e334c607658f273
MCCP96 <petermccomb@live.com>
2023-04-16 10:13:53 -0400

    Day 668 - Function Composition

--- 2023.js
+++ 2023.js
@@ -7309,7 +7309,7 @@ const revisedSleep = async (millis) =>
 // Day 666 🤡 */
 
 // Array Reduce Transformation					4/15/2023
-
+/* 
 // Given an integer array nums, a reducer function fn, and an intial value init, return a reduced array.
 
 // A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, arr[2]), ... until every element in the array has been processed. The final value of val is returned.
@@ -7376,4 +7376,66 @@ const fn3 = (a, c) => 0;
 console.log(reduce([], fn3, 25)); // 25
 
 // Faster than 100% runtimes
-// Same as top voted
+// Same as top voted */
+
+// Function Composition					4/16/2023
+
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+// The function composition of an empty list of functions is the identity function f(x) = x.
+
+// You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+// Example 1:
+// 		Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+// 		Output: 65
+// Explanation:
+// 		Evaluating from right to left ...
+// 		Starting with x = 4.
+// 		2 * (4) = 8
+// 		(8) * (8) = 64
+// 		(64) + 1 = 65
+
+// Example 2:
+// 		Input: functions = [x => 10 * x, x => 10 * x, x => 10 * x], x = 1
+// 		Output: 1000
+// Explanation:
+// 		Evaluating from right to left ...
+// 		10 * (1) = 10
+// 		10 * (10) = 100
+// 		10 * (100) = 1000
+
+// Example 3:
+// 		Input: functions = [], x = 42
+// 		Output: 42
+// Explanation:
+// 		The composition of zero functions is the identity function
+
+// Constraints:
+//		-1000 <= x <= 1000
+//		0 <= functions.length <= 1000
+//		all functions accept and return a single integer
+
+const compose = (fns) => (x) => {
+  let res = x;
+  for (let i = fns.length - 1; i >= 0; i--) {
+    res = fns[i](res);
+  }
+  return res;
+};
+
+console.log(compose([(x) => x + 1, (x) => x * x, (x) => 2 * x])(4)); // 65
+console.log(compose([(x) => 10 * x, (x) => 10 * x, (x) => 10 * x])(1)); // 1000
+console.log(compose([])(42)); // 42
+
+// Took me a while, couldn't seem to get the syntax right
+
+var topVotedCompose = function (functions) {
+  return (x) => functions.reduceRight((acc, f) => f(acc), x);
+};
+
+// First time seeing .reduceRight
+
+const revisedCompose = (fns) => (x) => fns.reduceRight((a, fn) => fn(a), x);
