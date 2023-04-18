9c4747a1d7ce692a1bb6b7e944a51744a6b046aa
MCCP96 <petermccomb@live.com>
2023-04-18 10:12:56 -0400

    Day 670 - Apply Transform Over Each Element in Arr

--- 2023.js
+++ 2023.js
@@ -7441,7 +7441,7 @@ var topVotedCompose = function (functions) {
 const revisedCompose = (fns) => (x) => fns.reduceRight((a, fn) => fn(a), x); */
 
 // Filter Elements from Array					4/17/2023
-
+/* 
 // Given an integer array arr and a filtering function fn, return a new array with a fewer or equal number of elements.
 
 // The returned array should only contain elements where fn(arr[i], i) evaluated to a truthy value.
@@ -7482,4 +7482,48 @@ console.log(filter([0, 10, 20, 30], (n) => n > 10)); // [20,30]
 console.log(filter([1, 2, 3], (n, i) => i === 0)); // [1]
 console.log(filter([-2, -1, 0, 1, 2], (n) => n + 1)); // [-2,0,1,2]
 
-// Same as top voted
+// Same as top voted */
+
+// Apply Transform Over Each Element in Array					4/18/2023
+
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+// Example 1:
+// 		Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// 		Output: [2,3,4]
+// Explanation:
+// 		const newArray = map(arr, plusone); // [2,3,4]
+// 		The function increases each value in the array by one.
+
+// Example 2:
+// 		Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+// 		Output: [1,3,5]
+// Explanation: The function increases each value by the index it resides in.
+
+// Example 3:
+// 		Input: arr = [10,20,30], fn = function constant() { return 42; }
+// 		Output: [42,42,42]
+// Explanation: The function always returns 42.
+
+// Constraints:
+//		0 <= arr.length <= 1000
+//		-109 <= arr[i] <= 109
+//		fn returns a number
+
+const map = (arr, fn) => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+};
+
+console.log(map([1, 2, 3], (n) => n + 1)); // [2,3,4]
+console.log(map([1, 2, 3], (n, i) => n + i)); // [1,3,5]
+console.log(map([10, 20, 30], () => 42)); // [42,42,42]
+
+// 100% Runtime
+// Very similar to yesterday's question
