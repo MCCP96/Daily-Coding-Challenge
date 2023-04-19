3eb2d9442462fbaf61152a90354716e376c0e338
MCCP96 <petermccomb@live.com>
2023-04-19 11:04:51 -0400

    Day 671 - Promise Time Limit

--- 2023.js
+++ 2023.js
@@ -7485,7 +7485,7 @@ console.log(filter([-2, -1, 0, 1, 2], (n) => n + 1)); // [-2,0,1,2]
 // Same as top voted */
 
 // Apply Transform Over Each Element in Array					4/18/2023
-
+/* 
 // Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
 
 // The returned array should be created such that returnedArray[i] = fn(arr[i], i).
@@ -7526,4 +7526,100 @@ console.log(map([1, 2, 3], (n, i) => n + i)); // [1,3,5]
 console.log(map([10, 20, 30], () => 42)); // [42,42,42]
 
 // 100% Runtime
-// Very similar to yesterday's question
+// Very similar to yesterday's question */
+
+// Promise Time Limit					4/19/2023
+
+// Given an asyncronous function fn and a time t in milliseconds, return a new time limited version of the input function.
+
+// A time limited function is a function that is identical to the original unless it takes longer than t milliseconds to fullfill. In that case, it will reject with "Time Limit Exceeded".  Note that it should reject with a string, not an Error.
+
+// Example 1:
+// 		Input:
+// 		fn = async (n) => {
+// 		await new Promise(res => setTimeout(res, 100));
+// 		return n * n;
+// 		}
+// 		inputs = [5]
+// 		t = 50
+// 		Output: {"rejected":"Time Limit Exceeded","time":50}
+// Explanation:
+// 		The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
+
+// Example 2:
+// 		Input:
+// 		fn = async (n) => {
+// 		await new Promise(res => setTimeout(res, 100));
+// 		return n * n;
+// 		}
+// 		inputs = [5]
+// 		t = 150
+// 		Output: {"resolved":25,"time":100}
+// Explanation:
+// 		The function resolved 5 * 5 = 25 at t=100ms. The time limit is never reached.
+
+// Example 3:
+// 		Input:
+// 		fn = async (a, b) => {
+// 		await new Promise(res => setTimeout(res, 120));
+// 		return a + b;
+// 		}
+// 		inputs = [5,10]
+// 		t = 150
+// 		Output: {"resolved":15,"time":120}
+// Explanation:
+// 		The function resolved 5 + 10 = 15 at t=120ms. The time limit is never reached.
+
+// Example 4:
+// 		Input:
+// 		fn = async () => {
+// 		throw "Error";
+// 		}
+// 		inputs = []
+// 		t = 1000
+// 		Output: {"rejected":"Error","time":0}
+// Explanation:
+// 		The function immediately throws an error.
+
+// Constraints:
+//		0 <= inputs.length <= 10
+//		0 <= t <= 1000
+//		fn returns a promise
+
+const topVotedTimeLimit = (fn, t) => {
+  return async (...args) => {
+    const timeout = new Promise((_, rej) => {
+      setTimeout(() => {
+        rej("Time Limit Exceeded");
+      }, t);
+    });
+
+    return Promise.race([fn(...args), timeout]);
+  };
+};
+
+const fn1 = topVotedTimeLimit(async (n) => {
+  await new Promise((res) => setTimeout(res, 100));
+  return n * n;
+}, 50);
+// console.log(await fn1(5)); // {"rejected":"Time Limit Exceeded","time":50}
+
+const fn2 = topVotedTimeLimit(async (n) => {
+  await new Promise((res) => setTimeout(res, 100));
+  return n * n;
+}, 150);
+console.log(await fn2(5)); // {"resolved":25,"time":100}
+
+const fn3 = topVotedTimeLimit(async (a, b) => {
+  await new Promise((res) => setTimeout(res, 120));
+  return a + b;
+}, 150);
+console.log(await fn3(5, 10)); // {"resolved":15,"time":120}
+
+const fn4 = topVotedTimeLimit(async () => {
+  throw "Error";
+}, 1000);
+console.log(await fn4()); // {"rejected":"Error","time":0}
+
+// I'm very out of practice with Promises
+// Seems like a perfect situation for .race here
