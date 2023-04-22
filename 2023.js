302dc389603567eae34221b2951091d9fea0fe11
MCCP96 <petermccomb@live.com>
2023-04-22 09:25:33 -0400

    Day 674 - Cache With Time Limit

--- 2023.js
+++ 2023.js
@@ -7683,7 +7683,7 @@ const topVotedCheckIfInstanceOf = (obj, classFn) => {
 // Turns out I had to cast it as an Object first */
 
 // Generate Fibonacci Sequence					4/21/2023
-
+/* 
 // Write a generator function that returns a generator object which yields the fibonacci sequence.
 
 // The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
@@ -7762,4 +7762,132 @@ const revisedFibGenerator = function* () {
   }
 };
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function* */
+
+// Cache With Time Limit					4/22/2023
+
+// Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
+
+// The class has three public methods:
+
+// set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds. Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
+
+// get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+
+// count(): returns the count of un-expired keys.
+
+// Example 1:
+// 		Input:
+// 		["TimeLimitedCache", "set", "get", "count", "get"]
+// 		[[], [1, 42, 100], [1], [], [1]]
+// 		[0, 0, 50, 50, 150]
+// 		Output: [null, false, 42, 1, -1]
+// Explanation:
+// 		At t=0, the cache is constructed.
+// 		At t=0, a key-value pair (1: 42) is added with a time limit of 100ms. The value doesn't exist so false is returned.
+// 		At t=50, key=1 is requested and the value of 42 is returned.
+// 		At t=50, count() is called and there is one active key in the cache.
+// 		At t=100, key=1 expires.
+// 		At t=150, get(1) is called but -1 is returned because the cache is empty.
+
+// Example 2:
+// 		Input:
+// 		["TimeLimitedCache", "set", "set", "get", "get", "get", "count"]
+// 		[[], [1, 42, 50], [1, 50, 100], [1], [1], [1], []]
+// 		[0, 0, 40, 50, 120, 200, 250]
+// 		Output: [null, false, true, 50, 50, -1]
+// Explanation:
+// 		At t=0, the cache is constructed.
+// 		At t=0, a key-value pair (1: 42) is added with a time limit of 50ms. The value doesn't exist so false is returned.
+// 		At t=40, a key-value pair (1: 50) is added with a time limit of 100ms. A non-expired value already existed so true is returned and the old value was overwritten.
+// 		At t=50, get(1) is called which returned 50.
+// 		At t=120, get(1) is called which returned 50.
+// 		At t=140, key=1 expires.
+// 		At t=200, get(1) is called but the cache is empty so -1 is returned.
+// 		At t=250, count() returns 0 because the cache is empty.
+
+// Constraints:
+//		0 <= key <= 109
+//		0 <= value <= 109
+//		0 <= duration <= 1000
+//		total method calls will not exceed 100
+
+class TimeLimitedCache {
+  constructor() {
+    this.obj = {};
+  }
+
+  set(key, value, duration) {
+    this.obj[key] = value;
+    setTimeout(() => delete this.obj[key], duration);
+  }
+
+  get(key) {
+    return this.obj[key] ?? -1;
+  }
+
+  count() {
+    return Object.keys(this.obj).length;
+  }
+}
+
+let obj = new TimeLimitedCache();
+console.log(obj.set(1, 42, 50));
+console.log(obj.set(1, 50, 100));
+console.log(obj.get(1));
+await new Promise((res) => setTimeout(res, 120));
+console.log(obj.get(1));
+
+// Doesn't work for keys being overwritten before expiring
+// Timeout remains and clears the new one
+
+const topVotedTimeLimitedCache = function () {
+  this.cache = new Map(); // Using Map so we don't need a size variable
+};
+
+topVotedTimeLimitedCache.prototype.set = function (key, value, duration) {
+  let found = this.cache.has(key);
+  if (found) clearTimeout(this.cache.get(key).ref); // Cancel previous timeout
+  this.cache.set(key, {
+    value, // Equivalent to `value: value`
+    ref: setTimeout(() => this.cache.delete(key), duration),
+  });
+  return found;
+};
+
+topVotedTimeLimitedCache.prototype.get = function (key) {
+  return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+topVotedTimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+// clearTimeout is what I was looking for
+// Also ref
+
+class revisedTimeLimitedCache {
+  constructor() {
+    this.obj = new Map();
+  }
+
+  set(key, value, duration) {
+    let exists = this.obj.has(key);
+    if (exists) clearTimeout(this.obj.get(key).timer);
+
+    this.obj.set(key, {
+      value,
+      timer: setTimeout(() => this.obj.delete(key), duration),
+    });
+
+    return exists;
+  }
+
+  get(key) {
+    return this.obj.get(key)?.value || -1;
+  }
+
+  count() {
+    return this.obj.size;
+  }
+}
