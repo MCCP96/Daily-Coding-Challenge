5cffeab9939fb28c5b513ed07c05c97a1e098b7a
MCCP96 <petermccomb@live.com>
2023-04-24 08:49:28 -0400

    Day 676 - Snail Traversal

--- 2023.js
+++ 2023.js
@@ -7893,7 +7893,7 @@ class revisedTimeLimitedCache {
 } */
 
 // Memoize					4/23/2023
-
+/* 
 // Given a function fn, return a memoized version of that function.
 
 // A memoized function is a function that will never be called twice with the same inputs. Instead it will returned a cached value.
@@ -7996,4 +7996,93 @@ function topVotedMemoize(fn) {
   };
 }
 
-// Same logic
+// Same logic */
+
+// Snail Traversal					4/24/2023
+
+// Write code that enhances all arrays such that you can call the snail(rowsCount, colsCount) method that transforms the 1D array into a 2D array organised in the pattern known as snail traversal order. Invalid input values should output an empty array. If rowsCount * colsCount !== nums.length, the input is considered invalid.
+
+// Snail traversal order starts at the top left cell with the first value of the current array. It then moves through the entire first column from top to bottom, followed by moving to the next column on the right and traversing it from bottom to top. This pattern continues, alternating the direction of traversal with each column, until the entire current array is covered. For example, when given the input array [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15] with rowsCount = 5 and colsCount = 4, the desired output matrix is shown below. Note that iterating the matrix following the arrows corresponds to the order of numbers in the original array.
+
+// https://assets.leetcode.com/uploads/2023/04/10/screen-shot-2023-04-10-at-100006-pm.png
+
+// Example 1:
+// 		Input:
+// 		nums = [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15]
+// 		rowsCount = 5
+// 		colsCount = 4
+// 		Output:
+// 		[
+// 		[19,17,16,15],
+// 		[10,1,14,4],
+// 		[3,2,12,20],
+// 		[7,5,18,11],
+// 		[9,8,6,13]
+// 		]
+
+// Example 2:
+// 		Input:
+// 		nums = [1,2,3,4]
+// 		rowsCount = 1
+// 		colsCount = 4
+// 		Output: [[1, 2, 3, 4]]
+
+// Example 3:
+// 		Input:
+// 		nums = [1,3]
+// 		rowsCount = 2
+// 		colsCount = 2
+// 		Output: []
+// Explanation: 2 multiplied by 2 is 4, and the original array [1,3] has a length of 2; therefore, the input is invalid.
+
+// Constraints:
+//		0 <= nums.length <= 250
+//		1 <= nums[i] <= 1000
+//		1 <= rowsCount <= 250
+//		1 <= colsCount <= 250
+
+Array.prototype.snail = function (rows, cols) {
+  if (rows * cols !== this.length) return [];
+
+  let res = new Array(rows).fill().map(() => []);
+  let invert = false;
+
+  for (let i = 0; i < this.length; i++) {
+    if (!invert) {
+      res[i % rows].push(this[i]);
+    } else {
+      res[rows - (i % rows) - 1].push(this[i]);
+    }
+
+    if (i % rows == rows - 1) {
+      invert = !invert;
+    }
+  }
+
+  return res;
+};
+
+// prettier-ignore
+console.log([19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15].snail(5, 4));
+console.log([1, 2, 3, 4].snail(1, 4));
+console.log([1, 3].snail(2, 2));
+
+// Great runtime, pretty readable
+
+Array.prototype.topVotedSnail = function (rowsCount, colsCount) {
+  if (rowsCount * colsCount !== this.length) return [];
+
+  return this.reduce((res, item, index) => {
+    let col = index % rowsCount;
+    let row = Math.floor(index / rowsCount);
+
+    if (row % 2 !== 0) {
+      col = rowsCount - col - 1;
+    }
+
+    res[col] = [...(res[col] || []), item];
+    return res;
+  }, []);
+};
+
+// Clean
