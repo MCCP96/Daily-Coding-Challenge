a7ef5a450b0cc3330131ee0c3a16cbeecd131540
MCCP96 <petermccomb@live.com>
2023-04-26 08:18:55 -0400

    Day 678 - Debounce

--- 2023.js
+++ 2023.js
@@ -8088,7 +8088,7 @@ Array.prototype.topVotedSnail = function (rowsCount, colsCount) {
 // Clean */
 
 // Flatten Deeply Nested Array					4/25/2023
-
+/* 
 // Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
 
 // A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
@@ -8166,4 +8166,94 @@ var topVotedFlat = function (arr, n) {
   return answer;
 };
 
-// Much better
+// Much better */
+
+// Debounce					4/26/2023
+
+// Given a function fn and a time in milliseconds t, return a debounced version of that function.
+
+// A debounced function is a function whose execution is delayed by t milliseconds and whose execution is cancelled if it is called again within that window of time. The debounced function should also recieve the passed parameters.
+
+// For example, let's say t = 50ms, and the function was called at 30ms, 60ms, and 100ms. The first 2 function calls would be cancelled, and the 3rd function call would be executed at 150ms. If instead t = 35ms, The 1st call would be cancelled, the 2nd would be executed at 95ms, and the 3rd would be executed at 135ms.
+
+// Debounce Schematic
+
+// The above diagram shows how debounce will transform events. Each rectangle represents 100ms and the debounce time is 400ms. Each color represents a different set of inputs.
+
+// Please solve it without using lodash's _.debounce() function.
+
+// Example 1:
+// 		Input:
+// 		t = 50
+// 		calls = [
+// 		{"t": 50, inputs: [1]},
+// 		{"t": 75, inputs: [2]}
+// 		]
+// 		Output: [{"t": 125, inputs: [2]}]
+// Explanation:
+// 		let start = Date.now();
+// 		function log(...inputs) {
+// 		console.log([Date.now() - start, inputs ])
+// 		}
+// 		const dlog = debounce(log, 50);
+// 		setTimeout(() => dlog(1), 50);
+// 		setTimeout(() => dlog(2), 75);
+// 		The 1st call is cancelled by the 2nd call because the 2nd call occurred before 100ms
+// 		The 2nd call is delayed by 50ms and executed at 125ms. The inputs were (2).
+
+// Example 2:
+// 		Input:
+// 		t = 20
+// 		calls = [
+// 		{"t": 50, inputs: [1]},
+// 		{"t": 100, inputs: [2]}
+// 		]
+// 		Output: [{"t": 70, inputs: [1]}, {"t": 120, inputs: [2]}]
+// Explanation:
+// 		The 1st call is delayed until 70ms. The inputs were (1).
+// 		The 2nd call is delayed until 120ms. The inputs were (2).
+
+// Example 3:
+// 		Input:
+// 		t = 150
+// 		calls = [
+// 		{"t": 50, inputs: [1, 2]},
+// 		{"t": 300, inputs: [3, 4]},
+// 		{"t": 300, inputs: [5, 6]}
+// 		]
+// 		Output: [{"t": 200, inputs: [1,2]}, {"t": 450, inputs: [5, 6]}]
+// Explanation:
+// 		The 1st call is delayed by 150ms and ran at 200ms. The inputs were (1, 2).
+// 		The 2nd call is cancelled by the 3rd call
+// 		The 3rd call is delayed by 150ms and ran at 450ms. The inputs were (5, 6).
+
+// Constraints:
+//		0 <= t <= 1000
+//		1 <= calls.length <= 10
+//		0 <= calls[i].t <= 1000
+//		0 <= calls[i].inputs.length <= 10
+
+const debounce = function (fn, t) {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(args), t);
+  };
+};
+
+const fn1 = (a) => console.log(a);
+const dlog = debounce(fn1, 50);
+setTimeout(() => dlog(1), 50);
+setTimeout(() => dlog(2), 75);
+
+// Same as top voted
+// They wrote a nice description of why debouncing is useful:
+
+// "Why to use Debouncing?
+// Have you ever encountered a situation where a function gets called multiple times within a short amount of time, leading to performance issues or unexpected behavior? This is a common problem in JavaScript, especially when working with events like scrolling, resizing, or typing. Fortunately, there's a simple technique called "debouncing" that can help you control the frequency of function calls and avoid these issues."
+
+// "What is Debouncing?
+// Debouncing is a method that limits the rate at which a function gets called. It works by delaying the execution of a function until a certain amount of time has passed without any additional function calls. If another function call happens within this time frame, the timer resets and the function execution is delayed again. Debouncing is useful in situations where you want to prevent a function from being called too frequently, such as:
+// Handling user input events like keypresses, mouse movements, or button clicks
+// Handling expensive computations or network requests that don't need to be performed on every function call"
