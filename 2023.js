6fdc137d7e442ef3cbdebfe7acd158859e09e72b
MCCP96 <petermccomb@live.com>
2023-05-02 08:23:20 -0400

    Day 684 - Promise Pool

--- 2023.js
+++ 2023.js
@@ -8581,7 +8581,7 @@ var oneLineCurry = (c = (f, ...a) =>
     : f(...a)); */
 
 // Convert Object to JSON String					5/1/2023
-
+/* 
 // Given an object, return a valid JSON string of that object. You may assume the object only inludes strings, integers, arrays, objects, booleans, and null. The returned string should not include extra spaces. The order of keys should be the same as the order returned by Object.keys().
 
 // Please solve it without using the built-in JSON.stringify method.
@@ -8650,4 +8650,105 @@ console.log(topVotedJsonStringify({ key: { a: 1, b: [{}, null, "Hello"] } })); /
 console.log(topVotedJsonStringify(true)); // true
 
 // Felt similar to areDeeplyEqual from last week
-// Not so much a fan of this one, JSON.stringify would be the way to go!
+// Not so much a fan of this one, JSON.stringify would be the way to go! */
+
+// Promise Pool					5/2/2023
+
+// Given an array of asyncronous functions functions and a pool limit n, return an asyncronous function promisePool. It should return a promise that resolves when all the input functions resolve.
+
+// Pool limit is defined as the maximum number promises that can be pending at once. promisePool should begin execution of as many functions as possible and continue executing new functions when old promises resolve. promisePool should execute functions[i] then functions[i + 1] then functions[i + 2], etc. When the last promise resolves, promisePool should also resolve.
+
+// For example, if n = 1, promisePool will execute one function at a time in series. However, if n = 2, it first executes two functions. When either of the two functions resolve, a 3rd function should be executed (if available), and so on until there are no functions left to execute.
+
+// You can assume all functions never reject. It is acceptable for promisePool to return a promise that resolves any value.
+
+// Example 1:
+// 		Input:
+// 		functions = [
+// 		() => new Promise(res => setTimeout(res, 300)),
+// 		() => new Promise(res => setTimeout(res, 400)),
+// 		() => new Promise(res => setTimeout(res, 200))
+// 		]
+// 		n = 2
+// 		Output: [[300,400,500],500]
+// Explanation:
+// 		Three functions are passed in. They sleep for 300ms, 400ms, and 200ms respectively.
+// 		At t=0, the first 2 functions are executed. The pool size limit of 2 is reached.
+// 		At t=300, the 1st function resolves, and the 3rd function is executed. Pool size is 2.
+// 		At t=400, the 2nd function resolves. There is nothing left to execute. Pool size is 1.
+// 		At t=500, the 3rd function resolves. Pool size is zero so the returned promise also resolves.
+
+// Example 2:
+// 		Input:
+// 		functions = [
+// 		() => new Promise(res => setTimeout(res, 300)),
+// 		() => new Promise(res => setTimeout(res, 400)),
+// 		() => new Promise(res => setTimeout(res, 200))
+// 		]
+// 		n = 5
+// 		Output: [[300,400,200],400]
+// Explanation:
+// 		At t=0, all 3 functions are executed. The pool limit of 5 is never met.
+// 		At t=200, the 3rd function resolves. Pool size is 2.
+// 		At t=300, the 1st function resolved. Pool size is 1.
+// 		At t=400, the 2nd function resolves. Pool size is 0, so the returned promise also resolves.
+
+// Example 3:
+// 		Input:
+// 		functions = [
+// 		() => new Promise(res => setTimeout(res, 300)),
+// 		() => new Promise(res => setTimeout(res, 400)),
+// 		() => new Promise(res => setTimeout(res, 200))
+// 		]
+// 		n = 1
+// 		Output: [[300,700,900],900]
+// Explanation:
+// 		At t=0, the 1st function is executed. Pool size is 1.
+// 		At t=300, the 1st function resolves and the 2nd function is executed. Pool size is 1.
+// 		At t=700, the 2nd function resolves and the 3rd function is executed. Pool size is 1.
+// 		At t=900, the 3rd function resolves. Pool size is 0 so the returned promise resolves.
+
+// Constraints:
+//		0 <= functions.length <= 10
+//		1 <= n <= 10
+
+const promisePool = async (fns, n) => {
+  let active = 0;
+  return Promise.allSettled(fns).then((res) =>
+    res.forEach((res) => {
+      // something
+    })
+  );
+};
+
+// Couldn't get it working
+// Too new with Promises
+
+var topVotedPromisePool = async function (functions, n) {
+  let next = () => functions[n++]?.().then(next);
+  return Promise.all(functions.slice(0, n).map((f) => f().then(next)));
+};
+
+// prettier-ignore
+console.log(topVotedPromisePool([
+  () => new Promise((res) => setTimeout(res, 300)),
+  () => new Promise((res) => setTimeout(res, 400)),
+  () => new Promise((res) => setTimeout(res, 200)),
+], 2));
+// prettier-ignore
+console.log(topVotedPromisePool([
+  () => new Promise((res) => setTimeout(res, 300)),
+  () => new Promise((res) => setTimeout(res, 400)),
+  () => new Promise((res) => setTimeout(res, 200)),
+], 5));
+// prettier-ignore
+console.log(topVotedPromisePool([
+  () => new Promise((res) => setTimeout(res, 300)),
+  () => new Promise((res) => setTimeout(res, 400)),
+  () => new Promise((res) => setTimeout(res, 200)),
+], 1));
+
+// Start promises from 0 to n, start the next once one fulfills
+// Seems so easy in retrospect
+
+// Didn't know you had to call a Promise like you would a function, using ()
