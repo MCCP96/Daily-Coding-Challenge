b1dd077c85c83c0aa52710a92e5b1bfec0dde829
MCCP96 <petermccomb@live.com>
2023-05-04 10:07:46 -0400

    Day 686 - Counter II

--- 2023.js
+++ 2023.js
@@ -8754,7 +8754,7 @@ console.log(topVotedPromisePool([
 // Didn't know you had to call a Promise like you would a function, using () */
 
 // Alternating Digit Sum					5/3/2023
-
+/* 
 // You are given a positive integer n. Each digit of n has a sign according to the following rules:
 
 // The most significant digit is assigned a positive sign.
@@ -8796,4 +8796,58 @@ var topVotedAlternateDigitSum = function (n) {
     else sum -= +str[i];
   }
   return sum;
+}; */
+
+// Counter II					5/4/2023
+
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+
+// decrement() reduces the current value by 1 and then returns it.
+
+// reset() sets the current value to init and then returns it.
+
+// Example 1:
+// 		Input: init = 5, calls = ["increment","reset","decrement"]
+// 		Output: [6,5,4]
+// Explanation:
+// 		const counter = createCounter(5);
+// 		counter.increment(); // 6
+// 		counter.reset(); // 5
+// 		counter.decrement(); // 4
+
+// Example 2:
+// 		Input: init = 0, calls = ["increment","increment","decrement","reset","reset"]
+// 		Output: [1,2,1,0,0]
+// Explanation:
+// 		const counter = createCounter(0);
+// 		counter.increment(); // 1
+// 		counter.increment(); // 2
+// 		counter.decrement(); // 1
+// 		counter.reset(); // 0
+// 		counter.reset(); // 0
+
+// Constraints:
+//		-1000 <= init <= 1000
+//		total calls not to exceed 1000
+
+const createCounter = (init) => {
+  let val = init;
+  return {
+    increment: () => ++val,
+    decrement: () => --val,
+    reset: () => (val = init),
+  };
 };
+
+let ex1 = createCounter(5);
+console.log(ex1);
+console.log(ex1.increment());
+console.log(ex1.decrement());
+console.log(ex1.reset());
+console.log(ex1.increment());
+
+// Same as top voteds
