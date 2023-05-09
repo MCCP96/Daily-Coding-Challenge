c254a34ad0367bc026dc8835cd6add432d528cf9
MCCP96 <petermccomb@live.com>
2023-05-09 07:57:02 -0400

    Day 691 - Number of Ways to Buy Pens and Pencils

--- 2023.js
+++ 2023.js
@@ -8985,7 +8985,7 @@ var topVotedInorderTraversal = function* (arr) {
 // yield* is what I was looking for */
 
 // Design an ATM Machine					5/8/2023
-
+/* 
 // There is an ATM machine that stores banknotes of 5 denominations: 20, 50, 100, 200, and 500 dollars. Initially the ATM is empty. The user can use the machine to deposit or withdraw any amount of money.
 
 // When withdrawing, the machine prioritizes using banknotes of larger values.
@@ -9147,4 +9147,60 @@ console.log(ex1.deposit([0, 1, 0, 1, 1]));
 console.log(ex1.withdraw(600));
 console.log(ex1.withdraw(550));
 
-// Passes all test cases now
+// Passes all test cases now */
+
+// Number of Ways to Buy Pens and Pencils					5/9/2023
+
+// You are given an integer total indicating the amount of money you have. You are also given two integers cost1 and cost2 indicating the price of a pen and pencil respectively. You can spend part or all of your money to buy multiple quantities (or none) of each kind of writing utensil.
+
+// Return the number of distinct ways you can buy some number of pens and pencils.
+
+// Example 1:
+// 		Input: total = 20, cost1 = 10, cost2 = 5
+// 		Output: 9
+// Explanation: The price of a pen is 10 and the price of a pencil is 5.
+// 		- If you buy 0 pens, you can buy 0, 1, 2, 3, or 4 pencils.
+// 		- If you buy 1 pen, you can buy 0, 1, or 2 pencils.
+// 		- If you buy 2 pens, you cannot buy any pencils.
+// 		The total number of ways to buy pens and pencils is 5 + 3 + 1 = 9.
+
+// Example 2:
+// 		Input: total = 5, cost1 = 10, cost2 = 10
+// 		Output: 1
+// Explanation: The price of both pens and pencils are 10, which cost more than total, so you cannot buy any writing utensils. Therefore, there is only 1 way: buy 0 pens and 0 pencils.
+
+// Constraints:
+//		1 <= total, cost1, cost2 <= 106
+
+const backtrackWaysToBuyPensPencils = (tot, c1, c2) => {
+  let count = 1;
+  if (c1 > tot && c2 > tot) return count;
+
+  const backtrack = (acc, c1, c2) => {
+    if (acc >= tot) return ++count;
+    backtrack(acc + c1, c1, c2);
+    backtrack(acc + c2, c1, c2);
+  };
+  backtrack(0, c1, c2);
+
+  return count;
+};
+
+// Too slow
+
+const waysToBuyPensPencils = (tot, c1, c2) => ~~((tot / c1) * (tot / c2) + 1);
+
+console.log(waysToBuyPensPencils(20, 10, 5)); // 9
+console.log(waysToBuyPensPencils(5, 10, 10)); // 1
+console.log(waysToBuyPensPencils(100, 1, 1)); // 5151
+
+// Doesn't work
+// There must be a formula
+
+var topVotedWaysToBuyPensPencils = function (total, cost1, cost2) {
+  var count = 0;
+  for (var i = 0; i <= total / cost1; ++i) {
+    count += Math.trunc((total - i * cost1) / cost2 + 1);
+  }
+  return count;
+};
