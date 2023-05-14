436e512628a76a170e261275263ea9a4a7b871aa
MCCP96 <petermccomb@live.com>
2023-05-14 09:41:32 -0400

    Day 696 - Find The Original Array of Prefix Xor

--- 2023.js
+++ 2023.js
@@ -9393,7 +9393,7 @@ console.log(executeInstructions(1, [0, 0], "LRUD")); // [0,0,0,0]
 // Same logic as top voted solutions */
 
 // Minimum Rounds to Complete All Tasks					5/13/2023
-
+/* 
 // You are given a 0-indexed integer array tasks, where tasks[i] represents the difficulty level of a task. In each round, you can complete either 2 or 3 tasks of the same difficulty level.
 
 // Return the minimum rounds required to complete all the tasks, or -1 if it is not possible to complete all the tasks.
@@ -9451,4 +9451,57 @@ const revisedMinimumRounds = (tasks) => {
   return res;
 };
 
-// Looking at top voted, realized I could just round up instead of down then adding
+// Looking at top voted, realized I could just round up instead of down then adding */
+
+// Find The Original Array of Prefix Xor					5/14/2023
+
+// You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+
+// pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+
+// Note that ^ denotes the bitwise-xor operation.
+
+// It can be proven that the answer is unique.
+
+// Example 1:
+// 		Input: pref = [5,2,0,3,1]
+// 		Output: [5,7,2,3,2]
+// Explanation: From the array [5,7,2,3,2] we have the following:
+// 		- pref[0] = 5.
+// 		- pref[1] = 5 ^ 7 = 2.
+// 		- pref[2] = 5 ^ 7 ^ 2 = 0.
+// 		- pref[3] = 5 ^ 7 ^ 2 ^ 3 = 3.
+// 		- pref[4] = 5 ^ 7 ^ 2 ^ 3 ^ 2 = 1.
+
+// Example 2:
+// 		Input: pref = [13]
+// 		Output: [13]
+// Explanation: We have pref[0] = arr[0] = 13.
+
+// Constraints:
+//		1 <= pref.length <= 105
+//		0 <= pref[i] <= 106
+
+const findArray = (pref) => {
+  let res = [pref[0]];
+  let acc = res[0];
+  for (let i = 1; i < pref.length; i++) {
+    res[i] = acc ^ pref[i];
+    acc ^= res[i];
+  }
+  return res;
+};
+
+console.log(findArray([5, 2, 0, 3, 1])); // [5,7,2,3,2]
+console.log(findArray([13])); // [13]
+
+// Same logic as top voted
+
+var topVotedFindArray = function (pref) {
+  let xor = pref[0];
+  for (let i = 1; i < pref.length; i++) {
+    pref[i] ^= xor;
+    xor ^= pref[i];
+  }
+  return pref;
+};
