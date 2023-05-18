168ae1051a6d3cdb6a619d16c580e4890817a83a
MCCP96 <petermccomb@live.com>
2023-05-18 08:36:16 -0400

    Day 700 - Throttle

--- 2023.js
+++ 2023.js
@@ -9614,7 +9614,7 @@ console.log(revisedCountVowelStrings(2)); // 15
 console.log(revisedCountVowelStrings(33)); // 66045 */
 
 // Array of Objects to Matrix					5/17/2023
-
+/* 
 // Write a function that converts an array of objects arr into a matrix m.
 
 // arr is an array of objects or arrays. Each item in the array can be deeply nested with child arrays and child objects. It can also contain numbers, strings, booleans, and null values.
@@ -9770,4 +9770,69 @@ const topVotedJsonToMatrix = (a) => {
   );
 };
 
-// Same idea, much more concise
+// Same idea, much more concise */
+
+// Throttle					5/18/2023
+
+// Given a function fn and a time in milliseconds t, return a throttled version of that function.
+
+// A throttled function is first called without delay and then, for a time interval of t milliseconds, can't be executed but should store the latest function arguments provided to call fn with them after the end of the delay.
+
+// For instance, t = 50ms, and the function was called at 30ms, 40ms, and 60ms. The first function call would block calling functions for the following t milliseconds. The second function call would save arguments, and the third call arguments should overwrite currently stored arguments from the second call because the second and third calls are called before 80ms. Once the delay has passed, the throttled function should be called with the latest arguments provided during the delay period, and it should also create another delay period of 80ms + t.
+
+// Throttle DiagramThe above diagram shows how throttle will transform events. Each rectangle represents 100ms and the throttle time is 400ms. Each color represents a different set of inputs.
+
+// Example 1:
+// 		Input: t = 100, calls = [{"t":20,"inputs":[1]}]
+// 		Output: [{"t":20,"inputs":[1]}]
+// Explanation: The 1st call is always called without delay
+
+// Example 2:
+// 		Input: t = 50, calls = [{"t":50,"inputs":[1]},{"t":75,"inputs":[2]}]
+// 		Output: [{"t":50,"inputs":[1]},{"t":100,"inputs":[2]}]
+// Explanation:
+// 		The 1st is called a function with arguments (1) without delay.
+// 		The 2nd is called at 75ms, within the delay period because 50ms + 50ms = 100ms, so the next call can be reached at 100ms. Therefore, we save arguments from the 2nd call to use them at the callback of the 1st call.
+
+// Constraints:
+//		0 <= t <= 1000
+//		1 <= calls.length <= 10
+//		0 <= calls[i].t <= 1000
+//		0 <= calls[i].inputs[i], calls[i].inputs.length <= 10
+
+const throttle = (fn, t) => {
+  let input;
+  let throttleComplete = true;
+
+  const run = () => {
+    if (input && throttleComplete) {
+      fn(...input);
+      input = null;
+      throttleComplete = false;
+
+      setTimeout(() => {
+        throttleComplete = true;
+        run();
+      }, t);
+    }
+  };
+
+  return function (...args) {
+    input = args;
+    run();
+  };
+};
+
+const ex1 = throttle(() => console.log("e1"), 100);
+console.log(ex1([1]));
+
+const ex2 = throttle(() => console.log("e2"), 50);
+console.log(ex2([1]));
+await setTimeout(() => console.log("25ms"), 25);
+console.log(ex2([2]));
+
+// Wasn't fully working, had to exclude run() from returned function to be able to call it after timeout
+// Got that from top voted
+
+// Day 700!
+// See day 730 for my 2 year recap :^)
