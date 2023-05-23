060bbeba5f233901232071e069e1750391e1c912
MCCP96 <petermccomb@live.com>
2023-05-23 11:19:05 -0400

    Day 705 - Find the Prefix Common Array of 2 Arrays

--- 2023.js
+++ 2023.js
@@ -10044,7 +10044,7 @@ console.log(emitter.emit("onClick")); // []
 // More readable than top voteds */
 
 // Lexicographically Smallest Palindrome					5/22/2023
-
+/* 
 // You are given a string s consisting of lowercase English letters, and you are allowed to perform operations on it. In one operation, you can replace a character in s with another lowercase English letter.
 
 // Your task is to make s a palindrome with the minimum number of operations possible. If there are multiple palindromes that can be made using the minimum number of operations, make the lexicographically smallest one.
@@ -10103,4 +10103,55 @@ const topVotedMakeSmallestPalindrome = function (s) {
     }
   }
   return s.join("");
+}; */
+
+// Find the Prefix Common Array of Two Arrays					5/23/2023
+
+// You are given two 0-indexed integer permutations A and B of length n.
+
+// A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
+
+// Return the prefix common array of A and B.
+
+// A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
+
+// Example 1:
+// 		Input: A = [1,3,2,4], B = [3,1,2,4]
+// 		Output: [0,2,3,4]
+// Explanation: At i = 0: no number is common, so C[0] = 0.
+// 		At i = 1: 1 and 3 are common in A and B, so C[1] = 2.
+// 		At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+// 		At i = 3: 1, 2, 3, and 4 are common in A and B, so C[3] = 4.
+
+// Example 2:
+// 		Input: A = [2,3,1], B = [3,1,2]
+// 		Output: [0,1,3]
+// Explanation: At i = 0: no number is common, so C[0] = 0.
+// 		At i = 1: only 3 is common in A and B, so C[1] = 1.
+// 		At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+
+// Constraints:
+//		1 <= A.length == B.length == n <= 50
+//		1 <= A[i], B[i] <= n
+//		It is guaranteed that A and B are both a permutation of n integers.
+
+const findThePrefixCommonArray = (A, B) => {
+  let map = new Map();
+  let commons = 0;
+
+  const check = (n) => {
+    if (map.has(n)) {
+      if (map.get(n) == 1) commons++;
+      map.set(n, map.get(n) + 1);
+    } else map.set(n, 1);
+  };
+
+  return new Array(A.length).fill(0).map((_, i) => {
+    check(B[i]);
+    check(A[i]);
+    return commons;
+  });
 };
+
+console.log(findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4])); // [0,2,3,4]
+console.log(findThePrefixCommonArray([2, 3, 1], [3, 1, 2])); // [0,1,3]
