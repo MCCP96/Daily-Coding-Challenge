935128af817313e61f644ffde349ecac64aa0fdc
MCCP96 <petermccomb@live.com>
2023-05-25 09:29:41 -0400

    Day 707 - Unique Paths

--- 2023.js
+++ 2023.js
@@ -10157,7 +10157,7 @@ console.log(findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4])); // [0,2,3,4]
 console.log(findThePrefixCommonArray([2, 3, 1], [3, 1, 2])); // [0,1,3] */
 
 // Permutations					5/24/2023
-
+/* 
 // Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
 // Example 1:
@@ -10218,4 +10218,77 @@ var topVotedPermute = function (nums) {
   dfs([], nums);
 
   return output;
+}; */
+
+// Unique Paths					5/25/2023
+
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Example 1:
+// 		Input: m = 3, n = 7
+// 		Output: 28
+
+// Example 2:
+// 		Input: m = 3, n = 2
+// 		Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 		1. Right -> Down -> Down
+// 		2. Down -> Down -> Right
+// 		3. Down -> Right -> Down
+
+// Constraints:
+//		1 <= m, n <= 100
+
+const uniquePaths = (m, n) => {
+  let count = 0;
+
+  const backtrack = (x, y) => {
+    if (x == m - 1 && y == n - 1) return count++;
+    if (x < m) backtrack(x + 1, y);
+    if (y < n) backtrack(x, y + 1);
+  };
+  backtrack(0, 0);
+
+  return count;
+};
+
+console.log(uniquePaths(3, 7)); // 28
+console.log(uniquePaths(3, 2)); // 3
+
+// Exceeds runtime limit on bigger test cases
+
+var topVotedUniquePaths = function (m, n) {
+  let perRow = Array(n).fill(1);
+  let pathCount = Array.from(Array(m).fill(perRow));
+
+  for (let y = 1; y < m; y++) {
+    for (let x = 1; x < n; x++) {
+      // path count = number of path reach to one step left + number of path reach to one step up
+      pathCount[y][x] = pathCount[y][x - 1] + pathCount[y - 1][x];
+    }
+  }
+
+  return pathCount[m - 1][n - 1];
+};
+
+// Much better
+// Also a mathematical solution
+
+var mathUniquePaths = function (m, n) {
+  let pathCount =
+    factorial(m - 1 + n - 1) / (factorial(m - 1) * factorial(n - 1));
+
+  return pathCount;
+};
+
+var factorial = function (k) {
+  if (k == 0 || k == 1) {
+    return 1;
+  } else {
+    return k * factorial(k - 1);
+  }
 };
