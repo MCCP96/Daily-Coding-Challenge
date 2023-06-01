f43512ff530d2db97ea041cf810989a8caf3fe26
MCCP96 <petermccomb@live.com>
2023-06-01 12:45:15 -0400

    Day 714 - Separate the Digits in an Array

--- 2023.js
+++ 2023.js
@@ -10640,7 +10640,7 @@ var topVotedRotate = function (M) {
 // https://imgur.com/LbkYex3 */
 
 // Group Anagrams					5/31/2023
-
+/* 
 // Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -10676,4 +10676,48 @@ console.log(groupAnagrams([""])); // [[""]]
 console.log(groupAnagrams(["a"])); // [["a"]]
 
 // Good runtime
-// Done from YOW airport, headed to Dominican Republic :)
+// Done from YOW airport, headed to Dominican Republic :) */
+
+// Separate the Digits in an Array					6/1/2023
+
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+// To separate the digits of an integer is to get all the digits it has in the same order.
+
+// For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+
+// Example 1:
+// 		Input: nums = [13,25,83,77]
+// 		Output: [1,3,2,5,8,3,7,7]
+// Explanation:
+// 		- The separation of 13 is [1,3].
+// 		- The separation of 25 is [2,5].
+// 		- The separation of 83 is [8,3].
+// 		- The separation of 77 is [7,7].
+// 		answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+
+// Example 2:
+// 		Input: nums = [7,1,3,9]
+// 		Output: [7,1,3,9]
+// Explanation: The separation of each integer in nums is itself.
+// 		answer = [7,1,3,9].
+
+// Constraints:
+//		1 <= nums.length <= 1000
+//		1 <= nums[i] <= 105
+
+const separateDigits = (nums) =>
+  nums.reduce((res, n) => {
+    if (n < 10) res.push(n);
+    else for (let x of [...String(n)]) res.push(+x);
+    return res;
+  }, []);
+
+console.log(separateDigits([13, 25, 83, 77])); // [1,3,2,5,8,3,7,7]
+console.log(separateDigits([7, 1, 3, 9])); // [7,1,3,9]
+
+// Pretty OK
+
+const topVotedSeparateDigits = (nums) => Array.from(nums.join(""), Number);
+
+// Nice!
