0e5f694ac1e4dce6bc44375c6704c3c49f93e941
MCCP96 <petermccomb@live.com>
2023-06-04 08:20:22 -0400

    Day 717 - Execute Cancellable Function With Delay

--- 2023.js
+++ 2023.js
@@ -10794,7 +10794,7 @@ const revisedTwoSum = (nums, t) => {
 }; */
 
 // Single Number II					6/3/2023
-
+/* 
 // Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
 
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -10836,4 +10836,44 @@ var topVotedSingleNumber = function (nums) {
   return ones;
 };
 
-// https://leetcode.com/problems/single-number-ii/solutions/3527570/mastering-bitwise-operations-a-guide-to-boost-your-interview-preparation/
+// https://leetcode.com/problems/single-number-ii/solutions/3527570/mastering-bitwise-operations-a-guide-to-boost-your-interview-preparation/ */
+
+// Execute Cancellable Function With Delay					6/4/2023
+
+// Given a function fn, an array or arguments args, and a timeout t in milliseconds, return a cancel function cancelFn.
+
+// After a delay of t, fn should be called with args passed as parameters unless cancelFn was called first. In that case, fn should never be called.
+
+// Example 1:
+// 		Input: fn = (x) => x * 5, args = [2], t = 20, cancelTime = 50
+// 		Output: [{"time": 20, "returned": 10}]
+// Explanation:
+// 		const cancel = cancellable(fn, [2], 20); // fn(2) called at t=20ms
+// 		setTimeout(cancel, 50);
+// 		the cancelTime (50ms) is after the delay time (20ms), so fn(2) should be called at t=20ms. The value returned from fn is 10.
+
+// Example 2:
+// 		Input: fn = (x) => x**2, args = [2], t = 100, cancelTime = 50
+// 		Output: []
+// Explanation: fn(2) was never called because cancelTime (50ms) is before the delay time (100ms).
+
+// Example 3:
+// 		Input: fn = (x1, x2) => x1 * x2, args = [2,4], t = 30, cancelTime = 100
+// 		Output: [{"time": 30, "returned": 8}]
+// Explanation: fn(2, 4) was called at t=30ms because cancelTime > t.
+
+// Constraints:
+//		fn is a function
+//		args is a valid JSON array
+//		1 <= args.length <= 10
+//		20 <= t <= 1000
+//		10 <= cancelT <= 1000
+
+const cancellable = (fn, args, t) => {
+  const timer = setTimeout(() => fn(...args), t);
+  return () => clearTimeout(timer);
+};
+
+const ex1 = cancellable((x) => x * 5, [2], 20);
+const ex2 = cancellable((x) => x ** 2, [2], 100);
+const ex3 = cancellable((x1, x2) => x1 * x2, [2, 4], 30);
