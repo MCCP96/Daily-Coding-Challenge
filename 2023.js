10a1a06ab9c286ef4e6c143a94acb32bb028dfe8
MCCP96 <petermccomb@live.com>
2023-06-06 08:31:02 -0400

    Day 719 - Minimum Path Sum

--- 2023.js
+++ 2023.js
@@ -10879,7 +10879,7 @@ const ex2 = cancellable((x) => x ** 2, [2], 100);
 const ex3 = cancellable((x1, x2) => x1 * x2, [2, 4], 30); */
 
 // Count and Say					6/5/2023
-
+/* 
 // The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
 
 // countAndSay(1) = "1"
@@ -10948,4 +10948,95 @@ var topVotedCountAndSay = function (n) {
     }
   }
   return str;
+}; */
+
+// Minimum Path Sum					6/6/2023
+
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+// Note: You can only move either down or right at any point in time.
+
+// Example 1:
+// 		Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+// 		Output: 7
+// Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+// Example 2:
+// 		Input: grid = [[1,2,3],[4,5,6]]
+// 		Output: 12
+
+// Constraints:
+//		m == grid.length
+//		n == grid[i].length
+//		1 <= m, n <= 200
+//		0 <= grid[i][j] <= 200
+
+const minPathSum = (grid) => {
+  const [m, n] = [grid.length, grid[0].length];
+  let res = Number.MAX_SAFE_INTEGER;
+
+  const backtrack = (x, y, sum) => {
+    sum += grid[y][x];
+
+    if (x == n - 1 && y == m - 1) {
+      if (sum < res) res = sum;
+      return;
+    }
+
+    if (x < n - 1) backtrack(x + 1, y, sum);
+    if (y < m - 1) backtrack(x, y + 1, sum);
+  };
+  backtrack(0, 0, 0);
+
+  return res;
+};
+
+console.log(
+  minPathSum([
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1],
+  ])
+); // 7
+console.log(
+  minPathSum([
+    [1, 2, 3],
+    [4, 5, 6],
+  ])
+); // 12
+
+// Exceeds runtime, but was good backtracking practice
+
+var topVotedMinPathSum = function (grid) {
+  const n = grid.length;
+  const m = grid[0].length;
+
+  // Calculate the distance travelled within the first column
+  // This is because each square depends on the one above
+  // And the one to the left. However there is nothing left
+  // of the first column so we can calculate it by adding
+  // the current square to the square above it
+  for (let i = 1; i < n; i++) {
+    grid[i][0] += grid[i - 1][0];
+  }
+
+  // The same goes for the first row. There is nothing above the
+  // first row. So we just calculate the distance by what is to the left
+  // of it
+  for (let j = 1; j < m; j++) {
+    grid[0][j] += grid[0][j - 1];
+  }
+
+  // Start one row and one column in because we've precomputed
+  // those above
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      // The distance to the grid at i,j is equal to itself plus the minimum
+      // of the two grid spaces (one above, one to the left)
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+
+  // Return the distance bottom right corner
+  return grid[n - 1][m - 1];
 };
