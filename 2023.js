04ca399dc9100bd78b74d7ee31f28527bf4d5a56
MCCP96 <petermccomb@live.com>
2023-06-12 10:34:01 -0400

    Day 725 - Run Asynchronous Functions in Parallel

--- 2023.js
+++ 2023.js
@@ -11279,7 +11279,7 @@ var topVotedCancellable = function (fn, args, t) {
 // Thought about nested timeouts but looked up intervals instead */
 
 // Trapping Rain Water					6/11/2023
-
+/* 
 // Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
 // Example 1:
@@ -11379,4 +11379,60 @@ const revisedTrap = (heights) => {
   }
 
   return heights.reduce((water, h, i) => water + Math.min(l[i], r[i]) - h, 0);
+}; */
+
+// Execute Asynchronous Functions in Parallel					6/12/2023
+
+// Given an array of asynchronous functions functions, return a new promise promise. Each function in the array accepts no arguments and returns a promise.
+
+// promise resolves:
+
+// When all the promises returned from functions were resolved successfully. The resolved value of promise should be an array of all the resolved values of promises in the same order as they were in the functions.
+
+// promise rejects:
+
+// When any of the promises returned from functions were rejected. promise should also reject with the reason of the first rejection.
+
+// Please solve it without using the built-in Promise.all function.
+
+// Example 1:
+// 		Input: functions = [
+// 		() => new Promise(resolve => setTimeout(() => resolve(5), 200))
+// 		]
+// 		Output: {"t": 200, "resolved": [5]}
+// Explanation:
+// 		promiseAll(functions).then(console.log); // [5]
+// 		The single function was resolved at 200ms with a value of 5.
+
+// Example 2:
+// 		Input: functions = [
+// 		() => new Promise(resolve => setTimeout(() => resolve(1), 200)),
+// 		() => new Promise((resolve, reject) => setTimeout(() => reject("Error"), 100))
+// 		]
+// 		Output: {"t": 100, "rejected": "Error"}
+// Explanation: Since one of the promises rejected, the returned promise also rejected with the same error at the same time.
+
+// Example 3:
+// 		Input: functions = [
+// 		() => new Promise(resolve => setTimeout(() => resolve(4), 50)),
+// 		() => new Promise(resolve => setTimeout(() => resolve(10), 150)),
+// 		() => new Promise(resolve => setTimeout(() => resolve(16), 100))
+// 		]
+// 		Output: {"t": 150, "resolved": [4, 10, 16]}
+// Explanation: All the promises resolved with a value. The returned promise resolved when the last promise resolved.
+
+// Constraints:
+//		functions is an array of functions that returns promises
+//		1 <= functions.length <= 10
+
+const promiseAll = async (fns) => {
+  let promises = fns.map((cur) => cur());
+  return new Promise((res) => res(Promise.all(promises)));
 };
+
+let ex3 = promiseAll([
+  () => new Promise((resolve) => setTimeout(() => resolve(4), 50)),
+  () => new Promise((resolve) => setTimeout(() => resolve(10), 150)),
+  () => new Promise((resolve) => setTimeout(() => resolve(16), 100)),
+]);
+console.log(await ex3);
