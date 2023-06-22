19c54c29938fe2bd8f0385857ba3f5a6f0fb28fc
MCCP96 <petermccomb@live.com>
2023-06-22 11:02:49 -0400

    Day 735 - Reveal Cards In Increasing Order

--- 2023.js
+++ 2023.js
@@ -12203,7 +12203,7 @@ const topVotedFindTheWinner = (n, k) => {
 // Same idea */
 
 // Maximum Number of Coins You Can Get					6/21/2023
-
+/* 
 // There are 3n piles of coins of varying size, you and your friends will take piles of coins as follows:
 
 // In each step, you will choose any 3 piles of coins (not necessarily consecutive).
@@ -12270,4 +12270,88 @@ const revisedMaxCoins = (params) => {
     count += piles[i];
   }
   return count;
+}; */
+
+// Reveal Cards In Increasing Order					6/22/2023
+
+// You are given an integer array deck. There is a deck of cards where every card has a unique integer. The integer on the ith card is deck[i].
+
+// You can order the deck in any order you want. Initially, all the cards start face down (unrevealed) in one deck.
+
+// You will do the following steps repeatedly until all cards are revealed:
+
+// Take the top card of the deck, reveal it, and take it out of the deck.
+
+// If there are still cards in the deck then put the next top card of the deck at the bottom of the deck.
+
+// If there are still unrevealed cards, go back to step 1. Otherwise, stop.
+
+// Return an ordering of the deck that would reveal the cards in increasing order.
+
+// Note that the first entry in the answer is considered to be the top of the deck.
+
+// Example 1:
+// 		Input: deck = [17,13,11,2,3,5,7]
+// 		Output: [2,13,3,11,5,17,7]
+// Explanation:
+// 		We get the deck in the order [17,13,11,2,3,5,7] (this order does not matter), and reorder it.
+// 		After reordering, the deck starts as [2,13,3,11,5,17,7], where 2 is the top of the deck.
+// 		We reveal 2, and move 13 to the bottom.  The deck is now [3,11,5,17,7,13].
+// 		We reveal 3, and move 11 to the bottom.  The deck is now [5,17,7,13,11].
+// 		We reveal 5, and move 17 to the bottom.  The deck is now [7,13,11,17].
+// 		We reveal 7, and move 13 to the bottom.  The deck is now [11,17,13].
+// 		We reveal 11, and move 17 to the bottom.  The deck is now [13,17].
+// 		We reveal 13, and move 17 to the bottom.  The deck is now [17].
+// 		We reveal 17.
+// 		Since all the cards revealed are in increasing order, the answer is correct.
+
+// Example 2:
+// 		Input: deck = [1,1000]
+// 		Output: [1,1000]
+
+// Constraints:
+//		1 <= deck.length <= 1000
+//		1 <= deck[i] <= 106
+//		All the values of deck are unique.
+
+const deckRevealedIncreasing = (deck) => {
+  deck.sort((a, b) => a - b);
+  const len = deck.length;
+
+  let m;
+  if (len % 2) m = deck.splice(~~(len / 2), 1)[0];
+  let l = deck.splice(0, len / 2);
+  let r = deck;
+  if (r.length > 2) r.unshift(r.pop());
+  if (len % 2) r.reverse();
+
+  let res = new Array(len);
+  for (let i = 0; i < l.length; i++) {
+    res[i * 2] = l[i];
+    res[i * 2 + 1] = r[i];
+  }
+  if (m) res[len - 1] = m;
+
+  return res;
 };
+
+console.log(deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7])); // [2,13,3,11,5,17,7]
+console.log(deckRevealedIncreasing([1, 1000])); // [1,1000]
+console.log(deckRevealedIncreasing([1, 2, 3, 4, 5])); // [1,5,2,4,3]
+console.log(deckRevealedIncreasing([1, 2, 3, 4])); // [1,3,2,4]
+
+// Stuck in a rabbit hole here
+// Definitely not the way
+
+const topVotedDeckRevealedIncreasing = (deck) => {
+  deck.sort((a, b) => a - b);
+  const ans = [];
+  while (deck.length) {
+    ans.unshift(deck.pop());
+    ans.unshift(ans.pop());
+  }
+  ans.push(ans.shift());
+  return ans;
+};
+
+// Very clean
