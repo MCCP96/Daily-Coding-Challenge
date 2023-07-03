c38f7a1362e321e7086b0954f759b15f495ddf58
MCCP96 <petermccomb@live.com>
2023-07-03 18:17:06 -0400

    Day 746 - Evaluate Reverse Polish Notation

--- 2023.js
+++ 2023.js
@@ -13217,7 +13217,7 @@ var topVotedFindNonMinOrMax = function (nums) {
 // ah, no need for Set */
 
 // Check if The Number is Fascinating					7/2/2023
-
+/* 
 // You are given an integer n that consists of exactly 3 digits.
 
 // We call the number n fascinating if, after the following modification, the resulting number contains all the digits from 1 to 9 exactly once and does not contain any 0's:
@@ -13264,4 +13264,102 @@ function topVotedIsFascinating(n) {
 // Nice
 
 // Yesterday's climbing route was Regular Route at Chapel Pond!
-// 780ft of trad :)
+// 780ft of trad :) */
+
+// Evaluate Reverse Polish Notation					7/3/2023
+
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+// Evaluate the expression. Return an integer that represents the value of the expression.
+
+// Note that:
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+// Example 1:
+// 		Input: tokens = ["2","1","+","3","*"]
+// 		Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+
+// Example 2:
+// 		Input: tokens = ["4","13","5","/","+"]
+// 		Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+
+// Example 3:
+// 		Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// 		Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// 		= ((10 * (6 / (12 * -11))) + 17) + 5
+// 		= ((10 * (6 / -132)) + 17) + 5
+// 		= ((10 * 0) + 17) + 5
+// 		= (0 + 17) + 5
+// 		= 17 + 5
+// 		= 22
+
+// Constraints:
+//		1 <= tokens.length <= 10^4
+//		tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+const evalRPN = (tokens) => {
+  let opIndex = tokens.findIndex((x) => /^(\+|-|\*|\/)$/.test(x));
+  if (opIndex === -1) return tokens[0];
+
+  let [a, b, op] = tokens.splice(opIndex - 2, 3);
+  a = Number(a);
+  b = Number(b);
+  let cur;
+
+  switch (op) {
+    case "+":
+      cur = a + b;
+      break;
+    case "-":
+      cur = a - b;
+      break;
+    case "*":
+      cur = a * b;
+      break;
+    case "/":
+      cur = a / b;
+      if (cur >= 0) cur = Math.floor(cur);
+      else cur = Math.ceil(cur);
+      break;
+  }
+
+  tokens.splice(opIndex - 2, 0, cur);
+  return evalRPN(tokens);
+};
+
+console.log(evalRPN(["2", "1", "+", "3", "*"])); // 9
+console.log(evalRPN(["4", "13", "5", "/", "+"])); // 6
+// prettier-ignore
+console.log(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])) // 22
+
+// Bad runtime, but easy to follow
+
+function evalRPN(tokens) {
+  let stack = [];
+  let ops = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => (a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b)),
+  };
+  for (let t of tokens) {
+    if (ops[t]) {
+      let top = stack.pop();
+      let second = stack.pop();
+      stack.push(ops[t](second, top));
+    } else {
+      stack.push(Number(t));
+    }
+  }
+  return stack.pop();
+}
+
+// Very nice use of the ops object full of equations
