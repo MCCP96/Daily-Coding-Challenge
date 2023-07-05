e9119d0be931e203a3e8f7ef8eef5e1d48648852
MCCP96 <petermccomb@live.com>
2023-07-05 10:38:59 -0400

    Day 748 - Palindrome Partitioning

--- 2023.js
+++ 2023.js
@@ -13365,7 +13365,7 @@ function evalRPN(tokens) {
 // Very nice use of the ops object full of equations */
 
 // Spiral Matrix II					7/4/2023
-
+/* 
 // Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
 
 // Example 1:
@@ -13454,4 +13454,95 @@ var topVotedGenerateMatrix = function (n) {
   }
 
   return output;
+}; */
+
+// Palindrome Partitioning					7/5/2023
+
+// Given a string s, partition s such that every substring of the partition is a palindrome
+// Return all possible palindrome partitioning of s.
+
+// Example 1:
+// 		Input: s = "aab"
+// 		Output: [["a","a","b"],["aa","b"]]
+
+// Example 2:
+// 		Input: s = "a"
+// 		Output: [["a"]]
+
+// Constraints:
+//		1 <= s.length <= 16
+//		s contains only lowercase English letters.
+
+const isPalindrome = (str) => str === [...str].reverse().join("");
+
+const partition = (s) => {
+  let res = [];
+  let seen = {};
+
+  const go = (arr, i) => {
+    if (i === s.length) {
+      if (!seen[arr]) {
+        seen[arr] = true; // avoid duplicates
+        res.push(arr);
+      }
+      return;
+    }
+
+    go([...arr, s[i]], i + 1);
+
+    let curArr = [...arr];
+    let curS = curArr.pop();
+
+    for (let j = i; j <= s.length; j++) {
+      curS += s[j];
+
+      if (isPalindrome(curS)) {
+        go([...curArr, curS], j + 1);
+      }
+    }
+  };
+  go([], 0);
+
+  return res;
 };
+
+console.log(partition("aab")); // [["a","a","b"],["aa","b"]]
+console.log(partition("a")); // [["a"]]
+console.log(partition("efe")); // [["e","f","e"],["efe"]]
+console.log(partition("fff")); // [["e","f","e"],["efe"]]
+
+// Could probably omit the 'seen' object with better logic
+
+var dfs = function (s, partition, result) {
+  // Base case:
+  // Empty string must be palindrome
+  if (0 == s.length) {
+    result.push([...partition]);
+    return;
+  }
+
+  // General cases:
+  for (let i = 1; i <= s.length; i++) {
+    let prefix = s.substring(0, i);
+    let postfix = s.substring(i);
+
+    // Current prefix is palindrome, keep trying to make more partition in postfix by DFS
+    if (isPalindrome(prefix)) {
+      partition.push(prefix);
+      dfs(postfix, partition, result);
+      partition.pop();
+    }
+  }
+  return;
+};
+
+var topVotedPartition = function (s) {
+  // buffer for partition in DFS
+  let partition = [];
+  // final output of palindrome substrings
+  let result = [];
+  dfs(s, partition, result);
+  return result;
+};
+
+// Similar idea
