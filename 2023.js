d8c7964b6b7acabdaea18fa3d6a84077722ba0f9
MCCP96 <petermccomb@live.com>
2023-07-06 08:57:17 -0400

    Day 749 - Best Time to Buy and Sell Stock II

--- 2023.js
+++ 2023.js
@@ -13457,7 +13457,7 @@ var topVotedGenerateMatrix = function (n) {
 }; */
 
 // Palindrome Partitioning					7/5/2023
-
+/* 
 // Given a string s, partition s such that every substring of the partition is a palindrome
 // Return all possible palindrome partitioning of s.
 
@@ -13545,4 +13545,92 @@ var topVotedPartition = function (s) {
   return result;
 };
 
-// Similar idea
+// Similar idea */
+
+// Best Time to Buy and Sell Stock II					7/6/2023
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+// Example 1:
+// 		Input: prices = [7,1,5,3,6,4]
+// 		Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// 		Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// 		Total profit is 4 + 3 = 7.
+
+// Example 2:
+// 		Input: prices = [1,2,3,4,5]
+// 		Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+// 		Total profit is 4.
+
+// Example 3:
+// 		Input: prices = [7,6,4,3,1]
+// 		Output: 0
+// Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+
+// Constraints:
+//		1 <= prices.length <= 3 * 104
+//		0 <= prices[i] <= 104
+
+const maxProfit = (prices) => {
+  let profit = 0;
+
+  if (prices.join() === [...prices].sort((a, b) => b - a).join()) {
+    // if decreasing
+    return profit;
+  }
+
+  let startIdx = 0;
+  while (prices[startIdx] >= prices[startIdx + 1]) startIdx++;
+  let buy = prices[startIdx];
+
+  for (let i = startIdx + 1; i < prices.length - 1; i++) {
+    if (prices[i] <= prices[i + 1]) {
+      if (buy === undefined) {
+        buy = prices[i];
+      }
+    } else if (buy !== undefined) {
+      // sell
+      profit += prices[i] - buy;
+      buy = undefined;
+    }
+  }
+
+  const lastPrice = prices.pop();
+  if (buy !== undefined && buy < lastPrice) {
+    profit += lastPrice - buy;
+  }
+
+  return profit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 7
+console.log(maxProfit([1, 2, 3, 4, 5])); // 4
+console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+console.log(maxProfit([2, 1, 2, 0, 1])); // 2
+console.log(maxProfit([3, 2, 6, 5, 0, 3])); // 7
+
+// Ça marche
+
+var topVotedMaxProfit = function (prices) {
+  // It is impossible to sell stock on first day, set -infinity as initial value for curHold
+  let [curHold, curNotHold] = [-Infinity, 0];
+
+  for (const stockPrice of prices) {
+    let [prevHold, prevNotHold] = [curHold, curNotHold];
+
+    // either keep hold, or buy in stock today at stock price
+    curHold = Math.max(prevHold, prevNotHold - stockPrice);
+
+    // either keep not-hold, or sell out stock today at stock price
+    curNotHold = Math.max(prevNotHold, prevHold + stockPrice);
+  }
+
+  // Max profit must come from notHold state finally.
+  return curNotHold;
+};
