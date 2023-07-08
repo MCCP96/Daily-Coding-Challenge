fd98b5f13b9c59b64da66138af98ddd0dbfda355
MCCP96 <petermccomb@live.com>
2023-07-08 11:09:49 -0400

    Day 751 - Kth Largest Element in a Stream

--- 2023.js
+++ 2023.js
@@ -13636,7 +13636,7 @@ var topVotedMaxProfit = function (prices) {
 }; */
 
 // Search a 2D Matrix					7/7/2023
-
+/* 
 // You are given an m x n integer matrix matrix with the following two properties:
 // Each row is sorted in non-decreasing order.
 // The first integer of each row is greater than the last integer of the previous row.
@@ -13688,4 +13688,56 @@ function topVotedSearchMatrix(matrix, target) {
   }
 
   return false;
+} */
+
+// Kth Largest Element in a Stream					7/8/2023
+
+// Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Implement KthLargest class:
+
+// KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+
+// int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+// Example 1:
+// 		Input
+// 		["KthLargest", "add", "add", "add", "add", "add"]
+// 		[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+// 		Output
+// 		[null, 4, 5, 5, 8, 8]
+// 		Explanation
+// 		KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+// 		kthLargest.add(3);   // return 4
+// 		kthLargest.add(5);   // return 5
+// 		kthLargest.add(10);  // return 5
+// 		kthLargest.add(9);   // return 8
+// 		kthLargest.add(4);   // return 8
+
+// Constraints:
+//		1 <= k <= 104
+//		0 <= nums.length <= 104
+//		-104 <= nums[i] <= 104
+//		-104 <= val <= 104
+//		At most 104 calls will be made to add.
+//		It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+
+class KthLargest {
+  constructor(k, nums) {
+    this.k = k;
+    this.nums = nums.sort((a, b) => b - a);
+  }
+
+  add(n) {
+    this.nums.push(n);
+    this.nums.sort((a, b) => b - a);
+    return this.nums[this.k - 1];
+  }
 }
+
+const ex1 = new KthLargest(3, [4, 5, 8, 2]);
+console.log(ex1.add(3)); // return 4
+ex1.add(5); // return 5
+ex1.add(10); // return 5
+ex1.add(9); // return 8
+ex1.add(4); // return 8
