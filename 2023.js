39a6472409997219da777581f68a4d83808f4caa
MCCP96 <petermccomb@live.com>
2023-07-17 09:31:16 -0400

    Day 760 - Decode Ways

--- 2023.js
+++ 2023.js
@@ -14247,7 +14247,7 @@ console.log(topVotedNew21Game(21, 17, 10)); // 0.73278
 // I got stuck looking for a mathematical solution */
 
 // My Calendar I					7/16/2023
-
+/* 
 // You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
 
 // A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
@@ -14326,4 +14326,96 @@ TopVotedMyCalendar.prototype.book = function (start, end) {
     }
   }
   return true;
+}; */
+
+// Decode Ways					7/17/2023
+
+// A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+// 'A' -> "1"
+// 'B' -> "2"
+// ...
+// 'Z' -> "26"
+
+// To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+// "AAJF" with the grouping (1 1 10 6)
+
+// "KJF" with the grouping (11 10 6)
+
+// Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+// Given a string s containing only digits, return the number of ways to decode it.
+
+// The test cases are generated so that the answer fits in a 32-bit integer.
+
+// Example 1:
+// 		Input: s = "12"
+// 		Output: 2
+// Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+
+// Example 2:
+// 		Input: s = "226"
+// 		Output: 3
+// Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+// Example 3:
+// 		Input: s = "06"
+// 		Output: 0
+// Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+
+// Constraints:
+//		1 <= s.length <= 100
+//		s contains only digits and may contain leading zero(s).
+
+const numDecodings = (s) => {
+  let count = 0;
+
+  const dp = (arr) => {
+    if (arr.length === 0) {
+      count++;
+      return;
+    }
+
+    if (arr[0] == "0") return;
+    else dp(arr.slice(1));
+
+    if (arr[0] + arr[1] <= 26) dp(arr.slice(2));
+  };
+  dp([...s]);
+
+  return count;
 };
+
+console.log(numDecodings("12")); // 2
+console.log(numDecodings("226")); // 3
+console.log(numDecodings("06")); // 0
+
+// Too slow
+
+var topVotedNumDecodings = function (s) {
+  if (s.length < 1) return 0;
+  let memo = [];
+  var recur = function (index) {
+    let result = 0;
+    if (index == s.length) return 1; // base case 1: reach the end = correct path
+    if (memo[index] != null) return memo[index]; // base case 2: if we already have the information on this index, return it
+    if (s[index] > 0) {
+      // as long as its bigger than 0, we can at least  use it as a single digit
+      result += recur(index + 1); // move forward by 1
+    }
+
+    // as long as 1. we are not starting with 0 etc. `04`
+    //            2. our next digit isn't null
+    //            3. we can form a two digit number thats smaller than 27
+    if (s[index] != 0 && s[index + 1] != null && s[index] + s[index + 1] < 27) {
+      // lets make this two digit together and move forward by 2
+      result += recur(index + 2);
+    }
+    memo[index] = result; // given the current index, store how many different ways we can decode
+    return result;
+  };
+  return recur(0);
+};
+
+// Saves runtime using memo and index
