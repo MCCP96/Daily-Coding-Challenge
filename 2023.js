11208146344491c1ab7099bbb2ec530c18f85c32
MCCP96 <petermccomb@live.com>
2023-07-18 09:30:08 -0400

    Day 761 - Card Flipping Game

--- 2023.js
+++ 2023.js
@@ -14329,7 +14329,7 @@ TopVotedMyCalendar.prototype.book = function (start, end) {
 }; */
 
 // Decode Ways					7/17/2023
-
+/* 
 // A message containing letters from A-Z can be encoded into numbers using the following mapping:
 
 // 'A' -> "1"
@@ -14418,4 +14418,57 @@ var topVotedNumDecodings = function (s) {
   return recur(0);
 };
 
-// Saves runtime using memo and index
+// Saves runtime using memo and index */
+
+// Card Flipping Game					7/18/2023
+
+// You are given two 0-indexed integer arrays fronts and backs of length n, where the ith card has the positive integer fronts[i] printed on the front and backs[i] printed on the back. Initially, each card is placed on a table such that the front number is facing up and the other is facing down. You may flip over any number of cards (possibly zero).
+
+// After flipping the cards, an integer is considered good if it is facing down on some card and not facing up on any card.
+
+// Return the minimum possible good integer after flipping the cards. If there are no good integers, return 0.
+
+// Example 1:
+// 		Input: fronts = [1,2,4,4,7], backs = [1,3,4,1,3]
+// 		Output: 2
+// Explanation:
+// 		If we flip the second card, the face up numbers are [1,3,4,4,7] and the face down are [1,2,4,1,3].
+// 		2 is the minimum good integer as it appears facing down but not facing up.
+// 		It can be shown that 2 is the minimum possible good integer obtainable after flipping some cards.
+
+// Example 2:
+// 		Input: fronts = [1], backs = [1]
+// 		Output: 0
+// Explanation:
+// 		There are no good integers no matter how we flip the cards, so we return 0.
+
+// Constraints:
+//		n == fronts.length == backs.length
+//		1 <= n <= 1000
+//		1 <= fronts[i], backs[i] <= 2000
+
+var topVotedFlipgame = (fronts, backs) => {
+  var dictionary = new Set();
+  let size = fronts.length;
+
+  for (let i = 0; i < size; ++i) {
+    if (fronts[i] == backs[i]) {
+      dictionary.add(fronts[i]);
+    }
+  }
+
+  let min_num = Number.MAX_VALUE;
+  for (let num of fronts) {
+    if (!dictionary.has(num)) min_num = Math.min(min_num, num);
+  }
+  for (let num of backs) {
+    if (!dictionary.has(num)) min_num = Math.min(min_num, num);
+  }
+
+  return min_num == Number.MAX_VALUE ? 0 : min_num;
+};
+
+// Got stuck with poor sorting logic
+// This is much cleaner
+
+// Basically make list of impossibilities then find smallest possibility
