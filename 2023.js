7daec40551f76c1938336e191c6538e6feb1c96b
MCCP96 <petermccomb@live.com>
2023-07-19 08:33:35 -0400

    Day 762 - Min Num Operations Move All Balls to Box

--- 2023.js
+++ 2023.js
@@ -14421,7 +14421,7 @@ var topVotedNumDecodings = function (s) {
 // Saves runtime using memo and index */
 
 // Card Flipping Game					7/18/2023
-
+/* 
 // You are given two 0-indexed integer arrays fronts and backs of length n, where the ith card has the positive integer fronts[i] printed on the front and backs[i] printed on the back. Initially, each card is placed on a table such that the front number is facing up and the other is facing down. You may flip over any number of cards (possibly zero).
 
 // After flipping the cards, an integer is considered good if it is facing down on some card and not facing up on any card.
@@ -14471,4 +14471,90 @@ var topVotedFlipgame = (fronts, backs) => {
 // Got stuck with poor sorting logic
 // This is much cleaner
 
-// Basically make list of impossibilities then find smallest possibility
+// Basically make list of impossibilities then find smallest possibility */
+
+// Minimum Number of Operations to Move All Balls to Each Box					7/19/2023
+
+// You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+
+// In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+
+// Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+
+// Each answer[i] is calculated considering the initial state of the boxes.
+
+// Example 1:
+// 		Input: boxes = "110"
+// 		Output: [1,1,3]
+// Explanation: The answer for each box is as follows:
+// 		1) First box: you will have to move one ball from the second box to the first box in one operation.
+// 		2) Second box: you will have to move one ball from the first box to the second box in one operation.
+// 		3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+
+// Example 2:
+// 		Input: boxes = "001011"
+// 		Output: [11,8,5,4,3,4]
+
+// Constraints:
+//		n == boxes.length
+//		1 <= n <= 2000
+//		boxes[i] is either '0' or '1'.
+
+const minOperations = (boxes) => {
+  let moves = new Array(boxes.length - 1);
+
+  for (let i = 0; i < boxes.length; i++) {
+    let count = 0;
+    for (let j = 0; j < boxes.length; j++) {
+      if (boxes[j] == 1) {
+        count += Math.abs(j - i);
+      }
+    }
+    moves[i] = count;
+  }
+
+  return moves;
+};
+
+console.log(minOperations("110")); // [1,1,3]
+console.log(minOperations("001011")); // [11,8,5,4,3,4]
+
+function topVotedMinOperations(boxes) {
+  const result = Array(boxes.length).fill(0);
+
+  // First we make one pass through the array (left to right).
+  // For each index, we calculate the moves needed to get every
+  // non-empty box on the left of the current index to the current index.
+
+  // At each i in boxes:
+  //   - add the running sum to result[i]
+  //   - increment the notEmpty box count if the current box is '1'
+  //   - add the previously seen notEmpty boxes (including current index) to the runningSum
+
+  let notEmpty = 0;
+  let runningSum = 0;
+
+  for (let i = 0; i < boxes.length; ++i) {
+    result[i] += runningSum;
+    if (boxes[i] === "1") ++notEmpty;
+    runningSum += notEmpty;
+  }
+
+  // Make one more pass through the array (right to left).
+  // The operations are identical to the first loop, except that
+  // this pass calculates the moves needed to get every non-empty box
+  // on the right of each index to the current index.
+
+  notEmpty = 0;
+  runningSum = 0;
+
+  for (let i = boxes.length - 1; i >= 0; --i) {
+    result[i] += runningSum;
+    if (boxes[i] === "1") ++notEmpty;
+    runningSum += notEmpty;
+  }
+
+  return result;
+}
+
+// Much faster using memo
