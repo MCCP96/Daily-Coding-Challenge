96d5533edc090db9c73a91f2f472fd956ad3a4e3
MCCP96 <petermccomb@live.com>
2023-07-21 09:28:56 -0400

    Day 764 - Minimize Maximum Pair Sum in Array

--- 2023.js
+++ 2023.js
@@ -14560,7 +14560,7 @@ function topVotedMinOperations(boxes) {
 // Much faster using memo */
 
 // Arithmetic Subarrays					7/20/2023
-
+/* 
 // A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
 
 // For example, these are arithmetic sequences:
@@ -14636,4 +14636,54 @@ const topVotedCheckArithmeticSubarrays = (nums, l, r) =>
       )
   );
 
-// Great use of .every!
+// Great use of .every! */
+
+// Minimize Maximum Pair Sum in Array					7/21/2023
+
+// The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+// For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+
+// Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+// Each element of nums is in exactly one pair, and
+
+// The maximum pair sum is minimized.
+
+// Return the minimized maximum pair sum after optimally pairing up the elements.
+
+// Example 1:
+// 		Input: nums = [3,5,2,3]
+// 		Output: 7
+// Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+// 		The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+
+// Example 2:
+// 		Input: nums = [3,5,4,2,4,6]
+// 		Output: 8
+// Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+// 		The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
+
+// Constraints:
+//		n == nums.length
+//		2 <= n <= 105
+//		n is even.
+//		1 <= nums[i] <= 105
+
+const minPairSum = (nums) => {
+  const len = nums.length;
+  let max = -Infinity;
+
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < len / 2; i++) {
+    const pair = nums[i] + nums[len - 1 - i];
+    if (pair > max) max = pair;
+  }
+
+  return max;
+};
+
+console.log(minPairSum([3, 5, 2, 3])); // 7
+console.log(minPairSum([3, 5, 4, 2, 4, 6])); // 8
+
+// Same as top voted
