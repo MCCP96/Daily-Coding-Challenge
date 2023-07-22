bea67dd13a1206b011a0fe641648f4b5ce3b458d
MCCP96 <petermccomb@live.com>
2023-07-22 10:08:38 -0400

    Day 765 - Finding the Users Active Minutes

--- 2023.js
+++ 2023.js
@@ -14639,7 +14639,7 @@ const topVotedCheckArithmeticSubarrays = (nums, l, r) =>
 // Great use of .every! */
 
 // Minimize Maximum Pair Sum in Array					7/21/2023
-
+/* 
 // The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
 
 // For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
@@ -14686,4 +14686,85 @@ const minPairSum = (nums) => {
 console.log(minPairSum([3, 5, 2, 3])); // 7
 console.log(minPairSum([3, 5, 4, 2, 4, 6])); // 8
 
-// Same as top voted
+// Same as top voted */
+
+// Finding the Users Active Minutes					7/22/2023
+
+// You are given the logs for users' actions on LeetCode, and an integer k. The logs are represented by a 2D integer array logs where each logs[i] = [IDi, timei] indicates that the user with IDi performed an action at the minute timei.
+
+// Multiple users can perform actions simultaneously, and a single user can perform multiple actions in the same minute.
+
+// The user active minutes (UAM) for a given user is defined as the number of unique minutes in which the user performed an action on LeetCode. A minute can only be counted once, even if multiple actions occur during it.
+
+// You are to calculate a 1-indexed array answer of size k such that, for each j (1 <= j <= k), answer[j] is the number of users whose UAM equals j.
+
+// Return the array answer as described above.
+
+// Example 1:
+// 		Input: logs = [[0,5],[1,2],[0,2],[0,5],[1,3]], k = 5
+// 		Output: [0,2,0,0,0]
+// Explanation:
+// 		The user with ID=0 performed actions at minutes 5, 2, and 5 again. Hence, they have a UAM of 2 (minute 5 is only counted once).
+// 		The user with ID=1 performed actions at minutes 2 and 3. Hence, they have a UAM of 2.
+// 		Since both users have a UAM of 2, answer[2] is 2, and the remaining answer[j] values are 0.
+
+// Example 2:
+// 		Input: logs = [[1,1],[2,2],[2,3]], k = 4
+// 		Output: [1,1,0,0]
+// Explanation:
+// 		The user with ID=1 performed a single action at minute 1. Hence, they have a UAM of 1.
+// 		The user with ID=2 performed actions at minutes 2 and 3. Hence, they have a UAM of 2.
+// 		There is one user with a UAM of 1 and one with a UAM of 2.
+// 		Hence, answer[1] = 1, answer[2] = 1, and the remaining values are 0.
+
+// Constraints:
+//		1 <= logs.length <= 104
+//		0 <= IDi <= 109
+//		1 <= timei <= 105
+//		k is in the range [The maximum UAM for a user, 105].
+
+const findingUsersActiveMinutes = (logs, k) => {
+  let seen = new Map();
+  const count = logs.reduce((map, [id, min]) => {
+    if (!map.has(id)) {
+      // new user
+      map.set(id, 1);
+      seen.set(id, new Set().add(min));
+    } else if (!seen.get(id).has(min)) {
+      // old user, new minute
+      map.set(id, map.get(id) + 1);
+      seen.set(id, seen.get(id).add(min));
+    }
+    return map;
+  }, new Map());
+
+  let res = new Array(k).fill(0);
+  for (const UAM of count.values()) {
+    res[UAM - 1]++;
+  }
+  return res;
+};
+
+// prettier-ignore
+console.log(findingUsersActiveMinutes([[0,5],[1,2],[0,2],[0,5],[1,3]], 5)) // [0,2,0,0,0]
+// prettier-ignore
+console.log(findingUsersActiveMinutes([[1,1],[2,2],[2,3]], 4)) // [1,1,0,0]
+
+var topVotedFindingUsersActiveMinutes = function (logs, k) {
+  const map = new Map();
+
+  for (const [userID, minute] of logs) {
+    if (!map.has(userID)) map.set(userID, new Set());
+    map.get(userID).add(minute);
+  }
+
+  const count = new Array(k).fill(0);
+
+  for (const actions of map.values()) {
+    count[actions.size - 1]++;
+  }
+
+  return count;
+};
+
+// Same idea, much more concise
