348b591a1c2ebcc9e143edd1366ee481eb27ecc7
MCCP96 <petermccomb@live.com>
2023-07-25 10:02:41 -0400

    Day 768 - Design Browser History

--- 2023.js
+++ 2023.js
@@ -14840,7 +14840,7 @@ var topVotedPartitionLabels = function (s) {
 // Same runtime, but much more comprehensible */
 
 // Optimal Partition of String					7/24/2023
-
+/* 
 // Given a string s, partition the string into one or more substrings such that the characters in each substring are unique. That is, no letter appears in a single substring more than once.
 
 // Return the minimum number of substrings in such a partition.
@@ -14895,4 +14895,62 @@ const topVotedPartitionString = (s, count = 1, map = new Map()) => {
   return count;
 };
 
-// same same
+// same same */
+
+// Design Browser History					7/25/2023
+
+// You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+// Implement the BrowserHistory class:
+// BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+// void visit(string url) Visits url from the current page. It clears up all the forward history.
+// string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
+// string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+
+// Constraints:
+//		1 <= homepage.length <= 20
+//		1 <= url.length <= 20
+//		1 <= steps <= 100
+//		homepage and url consist of  '.' or lower case English letters.
+//		At most 5000 calls will be made to visit, back, and forward.
+
+class BrowserHistory {
+  constructor(homepage) {
+    this.page = homepage;
+    this.historyBack = [];
+    this.historyFwd = [];
+  }
+
+  visit(url) {
+    this.historyBack.push(this.page);
+    this.page = url;
+    this.historyFwd = [];
+  }
+  back(steps) {
+    while (steps > 0 && this.historyBack.length > 0) {
+      this.historyFwd.push(this.page);
+      this.page = this.historyBack.pop();
+      steps--;
+    }
+    return this.page;
+  }
+  forward(steps) {
+    while (steps > 0 && this.historyFwd.length > 0) {
+      this.historyBack.push(this.page);
+      this.page = this.historyFwd.pop();
+      steps--;
+    }
+    return this.page;
+  }
+}
+
+const browserHistory = new BrowserHistory("leetcode.com");
+browserHistory.visit("google.com"); // You are in "leetcode.com". Visit "google.com"
+browserHistory.visit("facebook.com"); // You are in "google.com". Visit "facebook.com"
+browserHistory.visit("youtube.com"); // You are in "facebook.com". Visit "youtube.com"
+console.log(browserHistory.back(1)); // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+console.log(browserHistory.back(1)); // You are in "facebook.com", move back to "google.com" return "google.com"
+console.log(browserHistory.forward(1)); // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+browserHistory.visit("linkedin.com"); // You are in "facebook.com". Visit "linkedin.com"
+console.log(browserHistory.forward(2)); // You are in "linkedin.com", you cannot move forward any steps.
+console.log(browserHistory.back(2)); // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+console.log(browserHistory.back(7)); // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
