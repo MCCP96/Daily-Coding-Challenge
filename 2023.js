beaa8a0c800c7241afa88f6bb1a3f04c2028a012
MCCP96 <petermccomb@live.com>
2023-08-03 12:51:56 -0400

    Day 777 - Find Maximum Number of String Pairs

--- 2023.js
+++ 2023.js
@@ -15348,7 +15348,7 @@ function topVotedCountSubstrings(s) {
 } */
 
 // Smallest String With A Given Numeric Value					8/2/2023
-
+/* 
 // The numeric value of a lowercase character is defined as its position (1-indexed) in the alphabet, so the numeric value of a is 1, the numeric value of b is 2, the numeric value of c is 3, and so on.
 
 // The numeric value of a string consisting of lowercase characters is defined as the sum of its characters' numeric values. For example, the numeric value of the string "abe" is equal to 1 + 2 + 5 = 8.
@@ -15401,4 +15401,79 @@ var topVotedGetSmallestString = function (n, k) {
   return ans.padStart(n, "a");
 };
 
-// Even better!
+// Even better! */
+
+// Find Maximum Number of String Pairs					8/3/2023
+
+// You are given a 0-indexed array words consisting of distinct strings.
+
+// The string words[i] can be paired with the string words[j] if:
+// The string words[i] is equal to the reversed string of words[j].
+// 0 <= i < j < words.length.
+
+// Return the maximum number of pairs that can be formed from the array words.
+// Note that each string can belong in at most one pair.
+
+// Example 1:
+// 		Input: words = ["cd","ac","dc","ca","zz"]
+// 		Output: 2
+// Explanation: In this example, we can form 2 pair of strings in the following way:
+// 		- We pair the 0th string with the 2nd string, as the reversed string of word[0] is "dc" and is equal to words[2].
+// 		- We pair the 1st string with the 3rd string, as the reversed string of word[1] is "ca" and is equal to words[3].
+// 		It can be proven that 2 is the maximum number of pairs that can be formed.
+
+// Example 2:
+// 		Input: words = ["ab","ba","cc"]
+// 		Output: 1
+// Explanation: In this example, we can form 1 pair of strings in the following way:
+// 		- We pair the 0th string with the 1st string, as the reversed string of words[1] is "ab" and is equal to words[0].
+// 		It can be proven that 1 is the maximum number of pairs that can be formed.
+
+// Example 3:
+// 		Input: words = ["aa","ab"]
+// 		Output: 0
+// Explanation: In this example, we are unable to form any pair of strings.
+
+// Constraints:
+//		1 <= words.length <= 50
+//		words[i].length == 2
+//		words consists of distinct strings.
+//		words[i] contains only lowercase English letters.
+
+const maximumNumberOfStringPairs = (words) => {
+  let pairs = 0;
+  let seen = new Set();
+  for (const word of words) {
+    if (seen.has(word)) {
+      pairs++;
+      seen.delete(word);
+    } else {
+      seen.add([...word].reverse().join(""));
+    }
+  }
+  return pairs;
+};
+
+console.log(maximumNumberOfStringPairs(["cd", "ac", "dc", "ca", "zz"])); // 2
+console.log(maximumNumberOfStringPairs(["ab", "ba", "cc"])); // 1
+console.log(maximumNumberOfStringPairs(["aa", "ab"])); // 0
+
+// Good runtime
+
+var topVotedMaximumNumberOfStringPairs = function (words) {
+  let word_dict = {};
+  let pairs = 0;
+  for (let word of words) {
+    let reversed_word = word.split("").reverse().join("");
+    if (word_dict[reversed_word]) {
+      word_dict[reversed_word]--;
+      pairs++;
+    } else {
+      word_dict[word] = (word_dict[word] || 0) + 1;
+    }
+  }
+  return pairs;
+};
+
+// Same same
+// Day 🎰
