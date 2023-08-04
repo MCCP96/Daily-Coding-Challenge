ed152658af6160a07ca954b1cbf5255ffee774cf
MCCP96 <petermccomb@live.com>
2023-08-04 12:01:02 -0400

    Day 778 - Sort Integers by The Power Value

--- 2023.js
+++ 2023.js
@@ -15404,7 +15404,7 @@ var topVotedGetSmallestString = function (n, k) {
 // Even better! */
 
 // Find Maximum Number of String Pairs					8/3/2023
-
+/* 
 // You are given a 0-indexed array words consisting of distinct strings.
 
 // The string words[i] can be paired with the string words[j] if:
@@ -15476,4 +15476,82 @@ var topVotedMaximumNumberOfStringPairs = function (words) {
 };
 
 // Same same
-// Day 🎰
+// Day 🎰 */
+
+// Sort Integers by The Power Value					8/4/2023
+
+// The power of an integer x is defined as the number of steps needed to transform x into 1 using the following steps:
+
+// if x is even then x = x / 2
+// if x is odd then x = 3 * x + 1
+
+// For example, the power of x = 3 is 7 because 3 needs 7 steps to become 1 (3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1).
+
+// Given three integers lo, hi and k. The task is to sort all integers in the interval [lo, hi] by the power value in ascending order, if two or more integers have the same power value sort them by ascending order.
+
+// Return the kth integer in the range [lo, hi] sorted by the power value.
+
+// Notice that for any integer x (lo <= x <= hi) it is guaranteed that x will transform into 1 using these steps and that the power of x is will fit in a 32-bit signed integer.
+
+// Example 1:
+// 		Input: lo = 12, hi = 15, k = 2
+// 		Output: 13
+// Explanation: The power of 12 is 9 (12 --> 6 --> 3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1)
+// 		The power of 13 is 9
+// 		The power of 14 is 17
+// 		The power of 15 is 17
+// 		The interval sorted by the power value [12,13,14,15]. For k = 2 answer is the second element which is 13.
+// 		Notice that 12 and 13 have the same power value and we sorted them in ascending order. Same for 14 and 15.
+
+// Example 2:
+// 		Input: lo = 7, hi = 11, k = 4
+// 		Output: 7
+// Explanation: The power array corresponding to the interval [7, 8, 9, 10, 11] is [16, 3, 19, 6, 14].
+// 		The interval sorted by power is [8, 10, 11, 7, 9].
+// 		The fourth number in the sorted array is 7.
+
+// Constraints:
+//		1 <= lo <= hi <= 1000
+//		1 <= k <= hi - lo + 1
+
+const getKth = (lo, hi, k) => {
+  let arr = [];
+
+  for (let i = lo; i <= hi; i++) {
+    let x = i;
+    let steps = 0;
+    while (x != 1) {
+      x = x % 2 ? 3 * x + 1 : x / 2;
+      steps++;
+    }
+    arr.push([i, steps]);
+  }
+
+  return arr.sort((a, b) => a[1] - b[1])[k - 1][0];
+};
+
+console.log(getKth(12, 15, 2)); // 13
+console.log(getKth(7, 11, 4)); // 7
+
+// Beats 95% runtime
+
+var topVotedGetKth = function (lo, hi, k) {
+  const findPower = (num) => {
+    let power = 0;
+    while (num > 1) {
+      power++;
+      num = num % 2 ? 3 * num + 1 : num / 2;
+    }
+    return power;
+  };
+  const result = [],
+    map = {};
+  for (let i = lo; i <= hi; i++) {
+    result.push(i);
+    map[i] = findPower(i);
+  }
+  result.sort((a, b) => map[a] - map[b]);
+  return result[k - 1];
+};
+
+// Same idea
