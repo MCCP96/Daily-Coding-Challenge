5e34e94beaeb5f402066f259cfcf2a39b5a683a2
MCCP96 <petermccomb@live.com>
2023-08-06 10:58:42 -0400

    Day 780 - Check if Array is Good

--- 2023.js
+++ 2023.js
@@ -15557,7 +15557,7 @@ var topVotedGetKth = function (lo, hi, k) {
 // Same idea */
 
 // Longest Even Odd Subarray With Threshold					8/5/2023
-
+/* 
 // You are given a 0-indexed integer array nums and an integer threshold.
 
 // Find the length of the longest subarray of nums starting at index l and ending at index r (0 <= l <= r < nums.length) that satisfies the following conditions:
@@ -15645,4 +15645,67 @@ var topVotedLongestAlternatingSubarray = function (nums, threshold) {
   return maxLength;
 };
 
-// Skip elements that have already been considered is what I'm missing
+// Skip elements that have already been considered is what I'm missing */
+
+// Check if Array is Good					8/6/2023
+
+// You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+
+// base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+
+// Return true if the given array is good, otherwise return false.
+
+// Note: A permutation of integers represents an arrangement of these numbers.
+
+// Example 1:
+// 		Input: nums = [2, 1, 3]
+// 		Output: false
+// Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. However, base[3] has four elements but array nums has three. Therefore, it can not be a permutation of base[3] = [1, 2, 3, 3]. So the answer is false.
+
+// Example 2:
+// 		Input: nums = [1, 3, 3, 2]
+// 		Output: true
+// Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. It can be seen that nums is a permutation of base[3] = [1, 2, 3, 3] (by swapping the second and fourth elements in nums, we reach base[3]). Therefore, the answer is true.
+
+// Example 3:
+// 		Input: nums = [1, 1]
+// 		Output: true
+// Explanation: Since the maximum element of the array is 1, the only candidate n for which this array could be a permutation of base[n], is n = 1. It can be seen that nums is a permutation of base[1] = [1, 1]. Therefore, the answer is true.
+
+// Example 4:
+// 		Input: nums = [3, 4, 4, 1, 2, 1]
+// 		Output: false
+// Explanation: Since the maximum element of the array is 4, the only candidate n for which this array could be a permutation of base[n], is n = 4. However, base[4] has five elements but array nums has six. Therefore, it can not be a permutation of base[4] = [1, 2, 3, 4, 4]. So the answer is false.
+
+// Constraints:
+//		1 <= nums.length <= 100
+//		1 <= num[i] <= 200
+
+const isGood = (nums) =>
+  nums.sort((a, b) => a - b).join() ===
+  new Array(nums.length - 1)
+    .fill(0)
+    .map((_, i) => i + 1)
+    .join() +
+    "," +
+    (nums.length - 1);
+
+console.log(isGood([2, 1, 3])); // false
+console.log(isGood([1, 3, 3, 2])); // true
+console.log(isGood([1, 1])); // true
+console.log(isGood([3, 4, 4, 1, 2, 1])); // false
+
+// Quick and easy
+
+const topVotedIsGood = (a) => {
+  a.sort((x, y) => x - y);
+  let n = a.length,
+    v = 1;
+  if (a[n - 1] != a[n - 2]) return false;
+  for (let i = 0; i < n - 1; i++) {
+    if (a[i] != v++) return false;
+  }
+  return true;
+};
+
+// Better runtime
