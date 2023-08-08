c4d8a686e55c72ea6552c443fb11a17ee70b396b
MCCP96 <petermccomb@live.com>
2023-08-08 15:30:33 -0400

    Day 782 - Evaluate the Bracket Pairs of a String

--- 2023.js
+++ 2023.js
@@ -15711,7 +15711,7 @@ const topVotedIsGood = (a) => {
 // Better runtime */
 
 // Rearrange Words in a Sentence					8/7/2023
-
+/* 
 // Given a sentence text (A sentence is a string of space-separated words) in the following format:
 
 // First letter is in upper case.
@@ -15761,4 +15761,92 @@ console.log(arrangeWords("To be or not to be")); // "To be or to be not"
 
 // 95% runtime
 // Others used maps saying .sort didn't work for equal length words
-// 'a.length == b.length ? 0' works around that
+// 'a.length == b.length ? 0' works around that */
+
+// Evaluate the Bracket Pairs of a String					8/8/2023
+
+// You are given a string s that contains some bracket pairs, with each pair containing a non-empty key.
+
+// For example, in the string "(name)is(age)yearsold", there are two bracket pairs that contain the keys "name" and "age".
+
+// You know the values of a wide range of keys. This is represented by a 2D string array knowledge where each knowledge[i] = [keyi, valuei] indicates that key keyi has a value of valuei.
+
+// You are tasked to evaluate all of the bracket pairs. When you evaluate a bracket pair that contains some key keyi, you will:
+
+// Replace keyi and the bracket pair with the key's corresponding valuei.
+
+// If you do not know the value of the key, you will replace keyi and the bracket pair with a question mark "?" (without the quotation marks).
+
+// Each key will appear at most once in your knowledge. There will not be any nested brackets in s.
+
+// Return the resulting string after evaluating all of the bracket pairs.
+
+// Example 1:
+// 		Input: s = "(name)is(age)yearsold", knowledge = [["name","bob"],["age","two"]]
+// 		Output: "bobistwoyearsold"
+// Explanation:
+// 		The key "name" has a value of "bob", so replace "(name)" with "bob".
+// 		The key "age" has a value of "two", so replace "(age)" with "two".
+
+// Example 2:
+// 		Input: s = "hi(name)", knowledge = [["a","b"]]
+// 		Output: "hi?"
+// Explanation: As you do not know the value of the key "name", replace "(name)" with "?".
+
+// Example 3:
+// 		Input: s = "(a)(a)(a)aaa", knowledge = [["a","yes"]]
+// 		Output: "yesyesyesaaa"
+// Explanation: The same key can appear multiple times.
+// 		The key "a" has a value of "yes", so replace all occurrences of "(a)" with "yes".
+// 		Notice that the "a"s not in a bracket pair are not evaluated.
+
+// Constraints:
+//		1 <= s.length <= 105
+//		0 <= knowledge.length <= 105
+//		knowledge[i].length == 2
+//		1 <= keyi.length, valuei.length <= 10
+//		s consists of lowercase English letters and round brackets '(' and ')'.
+//		Every open bracket '(' in s will have a corresponding close bracket ')'.
+//		The key in each bracket pair of s will be non-empty.
+//		There will not be any nested bracket pairs in s.
+//		keyi and valuei consist of lowercase English letters.
+//		Each keyi in knowledge is unique.
+
+const evaluate = (s, knowledge) => {
+  const key = knowledge.reduce(
+    (map, [key, val]) => map.set(key, val),
+    new Map()
+  );
+
+  let res = "";
+  while (s.length > 0) {
+    if (s.indexOf("(") !== -1) {
+      res += s.substring(0, s.indexOf("("));
+      res += key.get(s.substring(s.indexOf("(") + 1, s.indexOf(")"))) || "?";
+      s = s.substring(s.indexOf(")") + 1);
+    } else {
+      res += s;
+      break;
+    }
+  }
+  return res;
+};
+
+console.log(
+  evaluate("(name)is(age)yearsold", [
+    ["name", "bob"],
+    ["age", "two"],
+  ])
+); // "bobistwoyearsold"
+console.log(evaluate("hi(name)", [["a", "b"]])); // "hi?"
+console.log(evaluate("(a)(a)(a)aaa", [["a", "yes"]])); // "yesyesyesaaa"
+
+// Beats 95% Runtime
+
+var topVotedEvaluate = function (s, knowledge) {
+  const map = new Map(knowledge);
+  return s.replace(/\(([a-z]+)\)/g, (_, p1) => map.get(p1) ?? "?");
+};
+
+// Didn't know you could build Map like that
+// Beauty!
