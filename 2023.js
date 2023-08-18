cbf81a65cbeb6049ec993b161408c01228f98ed3
MCCP96 <petermccomb@live.com>
2023-08-18 12:15:23 -0400

    Day 792 - Find the Value of the Partition

--- 2023.js
+++ 2023.js
@@ -16342,7 +16342,7 @@ var topVotedReverseParentheses = function (s) {
 }; */
 
 // Number of Zero-Filled Subarrays					8/17/2023
-
+/* 
 // Given an integer array nums, return the number of subarrays filled with 0.
 // A subarray is a contiguous non-empty sequence of elements within an array.
 
@@ -16410,4 +16410,61 @@ var topVotedZeroFilledSubarray = function (nums) {
   return count;
 };
 
-// Cleaner, but less fast
+// Cleaner, but less fast */
+
+// Find the Value of the Partition					8/18/2023
+
+// You are given a positive integer array nums.
+
+// Partition nums into two arrays, nums1 and nums2, such that:
+
+// Each element of the array nums belongs to either the array nums1 or the array nums2.
+
+// Both arrays are non-empty.
+
+// The value of the partition is minimized.
+
+// The value of the partition is |max(nums1) - min(nums2)|.
+
+// Here, max(nums1) denotes the maximum element of the array nums1, and min(nums2) denotes the minimum element of the array nums2.
+
+// Return the integer denoting the value of such partition.
+
+// Example 1:
+// 		Input: nums = [1,3,2,4]
+// 		Output: 1
+// Explanation: We can partition the array nums into nums1 = [1,2] and nums2 = [3,4].
+// 		- The maximum element of the array nums1 is equal to 2.
+// 		- The minimum element of the array nums2 is equal to 3.
+// 		The value of the partition is |2 - 3| = 1.
+// 		It can be proven that 1 is the minimum value out of all partitions.
+
+// Example 2:
+// 		Input: nums = [100,1,10]
+// 		Output: 9
+// Explanation: We can partition the array nums into nums1 = [10] and nums2 = [100,1].
+// 		- The maximum element of the array nums1 is equal to 10.
+// 		- The minimum element of the array nums2 is equal to 1.
+// 		The value of the partition is |10 - 1| = 9.
+// 		It can be proven that 9 is the minimum value out of all partitions.
+
+// Constraints:
+//		2 <= nums.length <= 105
+//		1 <= nums[i] <= 109
+
+const findValueOfPartition = (nums) => {
+  nums.sort((a, b) => a - b);
+
+  let min = Infinity;
+  for (let i = 1; i < nums.length; i++) {
+    min = Math.min(min, nums[i] - nums[i - 1]);
+  }
+  return min;
+};
+
+console.log(findValueOfPartition([1, 3, 2, 4])); // 1
+console.log(findValueOfPartition([100, 1, 10])); // 9
+console.log(findValueOfPartition([59, 51, 1, 98, 73])); // 8
+
+// 97% runtime
+// Same as top voted
