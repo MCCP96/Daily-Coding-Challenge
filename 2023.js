017487bd716b0f3c70319e65a3eba6bb9d62cc0a
MCCP96 <petermccomb@live.com>
2023-08-23 15:33:14 -0400

    Day 797 - Throne Inheritance

--- 2023.js
+++ 2023.js
@@ -16732,7 +16732,7 @@ console.log(bank.withdraw(10, 50)); // return false, it is invalid because accou
 // Other top voteds were much bulkier */
 
 // The kth Factor of n					8/22/2023
-
+/* 
 // You are given two positive integers n and k. A factor of an integer n is defined as an integer i where n % i == 0.
 
 // Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
@@ -16787,4 +16787,116 @@ var topVotedKthFactor = function (n, k) {
 };
 
 // "If our entry number (N) is an odd number, we DONT need to look the past EVEN numbers of it, because not a single number that is EVEN multiplied by another number will result on an ODD number."
-// "Making it O(n/2) sometime"
+// "Making it O(n/2) sometime" */
+
+// Throne Inheritance					8/23/2023
+
+// A kingdom consists of a king, his children, his grandchildren, and so on. Every once in a while, someone in the family dies or a child is born.
+
+// The kingdom has a well-defined order of inheritance that consists of the king as the first member. Let's define the recursive function Successor(x, curOrder), which given a person x and the inheritance order so far, returns who should be the next person after x in the order of inheritance.
+
+// Successor(x, curOrder):
+// if x has no children or all of x's children are in curOrder:
+// if x is the king return null
+// else return Successor(x's parent, curOrder)
+// else return x's oldest child who's not in curOrder
+
+// For example, assume we have a kingdom that consists of the king, his children Alice and Bob (Alice is older than Bob), and finally Alice's son Jack.
+
+// In the beginning, curOrder will be ["king"].
+// Calling Successor(king, curOrder) will return Alice, so we append to curOrder to get ["king", "Alice"].
+// Calling Successor(Alice, curOrder) will return Jack, so we append to curOrder to get ["king", "Alice", "Jack"].
+// Calling Successor(Jack, curOrder) will return Bob, so we append to curOrder to get ["king", "Alice", "Jack", "Bob"].
+// Calling Successor(Bob, curOrder) will return null. Thus the order of inheritance will be ["king", "Alice", "Jack", "Bob"].
+
+// Using the above function, we can always obtain a unique order of inheritance.
+
+// Implement the ThroneInheritance class:
+// ThroneInheritance(string kingName) Initializes an object of the ThroneInheritance class. The name of the king is given as part of the constructor.
+// void birth(string parentName, string childName) Indicates that parentName gave birth to childName.
+// void death(string name) Indicates the death of name. The death of the person doesn't affect the Successor function nor the current inheritance order. You can treat it as just marking the person as dead.
+// string[] getInheritanceOrder() Returns a list representing the current order of inheritance excluding dead people.
+
+// Example 1:
+// 		Input
+// 		["ThroneInheritance", "birth", "birth", "birth", "birth", "birth", "birth", "getInheritanceOrder", "death", "getInheritanceOrder"]
+// 		[["king"], ["king", "andy"], ["king", "bob"], ["king", "catherine"], ["andy", "matthew"], ["bob", "alex"], ["bob", "asha"], [null], ["bob"], [null]]
+// 		Output
+// 		[null, null, null, null, null, null, null, ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"], null, ["king", "andy", "matthew", "alex", "asha", "catherine"]]
+// 		Explanation
+// 		ThroneInheritance t= new ThroneInheritance("king"); // order: king
+// 		t.birth("king", "andy"); // order: king > andy
+// 		t.birth("king", "bob"); // order: king > andy > bob
+// 		t.birth("king", "catherine"); // order: king > andy > bob > catherine
+// 		t.birth("andy", "matthew"); // order: king > andy > matthew > bob > catherine
+// 		t.birth("bob", "alex"); // order: king > andy > matthew > bob > alex > catherine
+// 		t.birth("bob", "asha"); // order: king > andy > matthew > bob > alex > asha > catherine
+// 		t.getInheritanceOrder(); // return ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"]
+// 		t.death("bob"); // order: king > andy > matthew > bob > alex > asha > catherine
+// 		t.getInheritanceOrder(); // return ["king", "andy", "matthew", "alex", "asha", "catherine"]
+
+// Constraints:
+//		1 <= kingName.length, parentName.length, childName.length, name.length <= 15
+//		kingName, parentName, childName, and name consist of lowercase English letters only.
+//		All arguments childName and kingName are distinct.
+//		All name arguments of death will be passed to either the constructor or as childName to birth first.
+//		For each call to birth(parentName, childName), it is guaranteed that parentName is alive.
+//		At most 10^5 calls will be made to birth and death.
+//		At most 10 calls will be made to getInheritanceOrder.
+
+const ThroneInheritance = class {
+  constructor(king) {
+    this.kingdom = [king, []];
+    this.deceased = [];
+  }
+  birth(parent, child) {}
+  death(name) {
+    this.deceased.push(name);
+  }
+  getInheritanceOrder() {
+    return this.kingdom.flat().filter((name) => !this.deceased.includes(name));
+  }
+};
+
+const ex1 = new ThroneInheritance("king");
+ex1.birth("king", "andy");
+ex1.birth("king", "bob");
+ex1.birth("king", "catherine");
+ex1.birth("andy", "matthew");
+ex1.birth("bob", "alex");
+ex1.birth("bob", "asha");
+console.log(ex1.getInheritanceOrder()); // ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"]
+ex1.death("bob");
+console.log(ex1.getInheritanceOrder()); // ["king", "andy", "matthew", "alex", "asha", "catherine"]
+
+// Tried for a while but couldn't get birth method down
+
+const TopVotedThroneInheritance = function (kingName) {
+  this.dead = new Map();
+  this.map = new Map();
+
+  this.map.set("king", []);
+};
+
+TopVotedThroneInheritance.prototype.birth = function (parentName, childName) {
+  this.map.get(parentName).push(childName);
+  this.map.set(childName, []);
+};
+
+TopVotedThroneInheritance.prototype.death = function (name) {
+  this.dead.set(name, true);
+};
+
+TopVotedThroneInheritance.prototype.getInheritanceOrder = function () {
+  const r = (parent, order) => {
+    if (!this.dead.has(parent)) order.push(parent);
+
+    this.map.get(parent).forEach((child) => r(child, order));
+
+    return order;
+  };
+
+  return r("king", []);
+};
+
+// Ahhhhhh
