b4c219848ca384ba6231723721ce54be27733bd7
MCCP96 <petermccomb@live.com>
2023-08-26 10:49:32 -0400

    Day 800 - Random Pick Index

--- 2023.js
+++ 2023.js
@@ -16953,7 +16953,7 @@ var topVotedGetSumAbsoluteDifferences = function (nums) {
 }; */
 
 // Count Complete Subarrays in an Array					8/25/2023
-
+/* 
 // You are given an array nums consisting of positive integers.
 
 // We call a subarray of an array complete if the following condition is satisfied:
@@ -17015,4 +17015,80 @@ var topVotedCountCompleteSubarrays = function (nums) {
     right++;
   }
   return count;
+}; */
+
+// Random Pick Index					8/26/2023
+
+// Given an integer array nums with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+// Implement the Solution class:
+
+// Solution(int[] nums) Initializes the object with the array nums.
+
+// int pick(int target) Picks a random index i from nums where nums[i] == target. If there are multiple valid i's, then each index should have an equal probability of returning.
+
+// Example 1:
+// 		Input
+// 		["Solution", "pick", "pick", "pick"]
+// 		[[[1, 2, 3, 3, 3]], [3], [1], [3]]
+// 		Output
+// 		[null, 4, 0, 2]
+// 		Explanation
+// 		Solution solution = new Solution([1, 2, 3, 3, 3]);
+// 		solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+// 		solution.pick(1); // It should return 0. Since in the array only nums[0] is equal to 1.
+// 		solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+
+// Constraints:
+//		1 <= nums.length <= 2 * 104
+//		-231 <= nums[i] <= 231 - 1
+//		target is an integer from nums.
+//		At most 104 calls will be made to pick.
+
+const Solution = class {
+  constructor(nums) {
+    this.idx = nums.reduce(
+      (map, num, i) => map.set(num, map.has(num) ? [...map.get(num), i] : [i]),
+      new Map()
+    );
+  }
+
+  pick(t) {
+    const len = this.idx.get(t).length;
+    return this.idx.get(t)[Math.floor(Math.random() * len)];
+  }
 };
+
+const ex = new Solution([1, 2, 3, 3, 3]);
+console.log(ex.pick(3)); // 2, 3, or 4
+console.log(ex.pick(1)); // 0
+console.log(ex.pick(3)); // 2, 3, or 4
+
+class TopVotedSolution {
+  constructor(nums) {
+    this.nums = nums;
+    this.map = this.createMap(); // O(n)
+  }
+  createMap() {
+    // O(n)
+    const map = new Map();
+    this.nums.forEach((num, idx) => {
+      const numIdxs = map.has(num) ? map.get(num) : [];
+      numIdxs.push(idx);
+      map.set(num, numIdxs);
+    });
+    return map;
+  }
+  pick(target) {
+    // O(1)
+    const targetIdxOptions = this.map.get(target);
+    const randomIdx = Math.floor(Math.random() * targetIdxOptions.length);
+    return targetIdxOptions[randomIdx];
+  }
+}
+
+// same same
+
+// Day 800!
+// Only 200 away from 1000!
+// Going climbing at montagne d'argent :-)
