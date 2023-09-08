f1882b798938a9332a30bcf558b9a4ff1a6b627b
MCCP96 <petermccomb@live.com>
2023-09-08 10:11:26 -0400

    Day 813 - Online Stock Span

--- 2023.js
+++ 2023.js
@@ -18300,7 +18300,7 @@ const topVotedMaskPII = (s) => {
 // same same */
 
 // Find And Replace in String					9/7/2023
-
+/* 
 // You are given a 0-indexed string s that you must perform k replacement operations on. The replacement operations are given as three 0-indexed parallel arrays, indices, sources, and targets, all of length k.
 
 // To complete the ith replacement operation:
@@ -18409,4 +18409,88 @@ var topVotedFindReplaceString = function (S, indexes, sources, targets) {
 };
 
 // That for loop within replaceChars is what's making this all work
-// Very nice
+// Very nice */
+
+// Online Stock Span					9/8/2023
+
+// Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+// The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+
+// For example, if the prices of the stock in the last four days is [7,2,1,2] and the price of the stock today is 2, then the span of today is 4 because starting from today, the price of the stock was less than or equal 2 for 4 consecutive days.
+
+// Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+
+// Implement the StockSpanner class:
+
+// StockSpanner() Initializes the object of the class.
+
+// int next(int price) Returns the span of the stock's price given that today's price is price.
+
+// Example 1:
+// 		Input
+// 		["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+// 		[[], [100], [80], [60], [70], [60], [75], [85]]
+// 		Output
+// 		[null, 1, 1, 1, 2, 1, 4, 6]
+// 		Explanation
+// 		StockSpanner stockSpanner = new StockSpanner();
+// 		stockSpanner.next(100); // return 1
+// 		stockSpanner.next(80);  // return 1
+// 		stockSpanner.next(60);  // return 1
+// 		stockSpanner.next(70);  // return 2
+// 		stockSpanner.next(60);  // return 1
+// 		stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+// 		stockSpanner.next(85);  // return 6
+
+// Constraints:
+//		1 <= price <= 105
+//		At most 104 calls will be made to next.
+
+const StockSpanner = class {
+  constructor() {
+    this.hist = [];
+  }
+
+  next(price) {
+    this.hist.unshift(price);
+    const count = this.hist.findIndex((x) => x > price);
+    return count === -1 ? this.hist.length : count;
+  }
+};
+
+const ex = new StockSpanner();
+console.log(ex.next(100)); // 1
+console.log(ex.next(80)); // 1
+console.log(ex.next(60)); // 1
+console.log(ex.next(70)); // 2
+console.log(ex.next(60)); // 1
+console.log(ex.next(75)); // 4
+console.log(ex.next(85)); // 6
+
+// Short and sweet
+
+var TopVotedStockSpanner = function () {
+  this.stack = [];
+};
+
+TopVotedStockSpanner.prototype.next = function (price) {
+  let [curPrice, curSpan] = [price, 1];
+
+  // Compute price span in stock data with monotonic stack
+  while (
+    this.stack.length &&
+    this.stack[this.stack.length - 1].price <= curPrice
+  ) {
+    prev = this.stack.pop();
+    let [prevPrice, prevSpan] = [prev.price, prev.span];
+
+    // update current price span with history data in stack
+    curSpan += prevSpan;
+  }
+  this.stack.push({ price: curPrice, span: curSpan });
+
+  return curSpan;
+};
+
+// Much faster
