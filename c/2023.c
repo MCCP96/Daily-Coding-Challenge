b911e8a01e26cbac3ad0dd62267d8599532422e0
MCCP96 <petermccomb@live.com>
2023-09-17 08:52:12 -0400

    Day 822 - Palindrome Number

--- c/2023.c
+++ c/2023.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
 // Hello World					9/16/2023
 /*
@@ -9,7 +12,7 @@ int main(void)
 } */
 
 // Two Sum					9/16/2023
-
+/*
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -83,4 +86,80 @@ int *topVotedTwoSum(int *nums, int numsSize, int target, int *returnSize)
 // Definitely need to study.
 
 // https://www.tutorialspoint.com/c_standard_library/c_function_malloc.htm
-// "The C library function void *malloc(size_t size) allocates the requested memory and returns a pointer to it."
+// "The C library function void *malloc(size_t size) allocates the requested memory and returns a pointer to it." */
+
+// Palindrome Number					9/17/2023
+
+// Given an integer x, return true if x is a palindrome and false otherwise.
+
+// Example 1:
+// 		Input: x = 121
+// 		Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+// Example 2:
+// 		Input: x = -121
+// 		Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+// Example 3:
+// 		Input: x = 10
+// 		Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+// Constraints:
+//		-231 <= x <= 231 - 1
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+bool isPalindrome(int x)
+{
+  if (x < 0)
+  {
+    return false;
+  }
+
+  char str[(int)((ceil(log10(x)) + 1) * sizeof(char))]; // from stackoverflow
+  sprintf(str, "%d", x);
+
+  for (int i = 0, j = strlen(str) - 1; i < strlen(str) / 2; i++, j--)
+  {
+    if (str[i] != str[j])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Leetcode gives a runtime error
+// Probably string conversion or sprintf
+
+// Memory allocation is something I've really taken for granted in javascript
+
+bool topVotedIsPalindrome(int x)
+{
+  if (x < 0 || x != 0 && x % 10 == 0)
+    return false;
+
+  int check = 0;
+  while (x > check)
+  {
+    check = check * 10 + x % 10;
+    x /= 10;
+  }
+
+  return (x == check || x == check / 10);
+}
+
+int main(void)
+{
+  topVotedIsPalindrome(121);  // true
+  topVotedIsPalindrome(-121); // false
+  topVotedIsPalindrome(10);   // false
+  return EXIT_SUCCESS;
+}
+
+// Better to use the math approach
+// While loop cuts it in half and "... || x == check / 10" accounts for odd amount of digits
