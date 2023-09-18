13557409d134beeef9cfb27f158e4fb3589ef24d
MCCP96 <petermccomb@live.com>
2023-09-18 09:07:38 -0400

    Day 823 - Roman to Integer

--- c/2023.c
+++ c/2023.c
@@ -89,7 +89,7 @@ int *topVotedTwoSum(int *nums, int numsSize, int target, int *returnSize)
 // "The C library function void *malloc(size_t size) allocates the requested memory and returns a pointer to it." */
 
 // Palindrome Number					9/17/2023
-
+/*
 // Given an integer x, return true if x is a palindrome and false otherwise.
 
 // Example 1:
@@ -162,4 +162,163 @@ int main(void)
 }
 
 // Better to use the math approach
-// While loop cuts it in half and "... || x == check / 10" accounts for odd amount of digits
+// While loop cuts it in half and "... || x == check / 10" accounts for odd amount of digits */
+
+// Roman to Integer					9/18/2023
+
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+
+// Given a roman numeral, convert it to an integer.
+
+// Example 1:
+// 		Input: s = "III"
+// 		Output: 3
+// Explanation: III = 3.
+
+// Example 2:
+// 		Input: s = "LVIII"
+// 		Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+
+// Example 3:
+// 		Input: s = "MCMXCIV"
+// 		Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+// Constraints:
+//		1 <= s.length <= 15
+//		s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+//		It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+int romanToInt(char *s)
+{
+  int res = 0;
+
+  for (unsigned short i = 0; s[i]; i++)
+  {
+    switch (s[i])
+    {
+    case 'I':
+      // I can be placed before V (5) and X (10) to make 4 and 9.
+      if (s[i + 1] == 'V')
+      {
+        res += 4;
+        i++;
+      }
+      else if (s[i + 1] == 'X')
+      {
+        res += 9;
+        i++;
+      }
+      else
+        res += 1;
+      break;
+
+    case 'V':
+      res += 5;
+      break;
+
+    case 'X':
+      // X can be placed before L (50) and C (100) to make 40 and 90.
+      if (s[i + 1] == 'L')
+      {
+        res += 40;
+        i++;
+      }
+      else if (s[i + 1] == 'C')
+      {
+        res += 90;
+        i++;
+      }
+      else
+        res += 10;
+      break;
+
+    case 'L':
+      res += 50;
+      break;
+
+    case 'C':
+      // C can be placed before D (500) and M (1000) to make 400 and 900.
+      if (s[i + 1] == 'D')
+      {
+        res += 400;
+        i++;
+      }
+      else if (s[i + 1] == 'M')
+      {
+        res += 900;
+        i++;
+      }
+      else
+        res += 100;
+      break;
+
+    case 'D':
+      res += 500;
+      break;
+
+    case 'M':
+      res += 1000;
+      break;
+
+    default:
+      break;
+    }
+  }
+
+  return res;
+}
+
+int main(void)
+{
+  romanToInt("III");     // 3
+  romanToInt("LVIII");   // 58
+  romanToInt("MCMXCIV"); // 1994
+}
+
+// Passes
+// Having s[i] as the 2nd parameter of the for loop doesn't feel intuitive but works to find length of array
+
+int topVotedRomanToInt(char *s)
+{
+  int t['X' + 1] = {
+      ['I'] = 1,
+      ['V'] = 5,
+      ['X'] = 10,
+      ['L'] = 50,
+      ['C'] = 100,
+      ['D'] = 500,
+      ['M'] = 1000,
+  };
+
+  int res = 0;
+  for (int i = 0; s[i]; i++)
+  {
+    if (t[s[i]] < t[s[i + 1]])
+      res -= t[s[i]];
+    else
+      res += t[s[i]];
+  }
+  return res;
+}
+
+// Much more compact
+// First time seeing objects in C
