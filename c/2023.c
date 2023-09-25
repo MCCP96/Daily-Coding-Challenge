eade928ec856d7cb55e38a298b7aaeb4d713b1e0
MCCP96 <petermccomb@live.com>
2023-09-25 09:33:15 -0400

    Day 830 - Merge Sorted Array

--- c/2023.c
+++ c/2023.c
@@ -696,7 +696,7 @@ int topVotedLengthOfLastWord(char *s)
 // Big day of climbing in Calabogie! */
 
 // Add Binary					9/24/2023
-
+/*
 // Given two binary strings a and b, return their sum as a binary string.
 
 // Example 1:
@@ -786,4 +786,108 @@ char *topVotedAddBinary(char *a, char *b)
 }
 
 // char *output = (char *)malloc(sizeOutput + 1);
-// I've got to work on memory allocation and dynamically filling arrays
+// I've got to work on memory allocation and dynamically filling arrays */
+
+// Merge Sorted Array					9/25/2023
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Example 1:
+// 		Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// 		Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// 		The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+// Example 2:
+// 		Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// 		Output: [1]
+// Explanation: The arrays we are merging are [1] and [].
+// 		The result of the merge is [1].
+
+// Example 3:
+// 		Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+// 		Output: [1]
+// Explanation: The arrays we are merging are [] and [1].
+// 		The result of the merge is [1].
+// 		Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+// Constraints:
+//		nums1.length == m + n
+//		nums2.length == n
+//		0 <= m, n <= 200
+//		1 <= m + n <= 200
+//		-109 <= nums1[i], nums2[j] <= 109
+//		Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+
+void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    nums1[m + i] = nums2[i];
+  }
+
+  bool sorted = false;
+  while (!sorted)
+  {
+    sorted = true;
+
+    for (int i = 0; i < m + n - 1; i++)
+    {
+      if (nums1[i] > nums1[i + 1])
+      {
+        sorted = false;
+        int temp = nums1[i + 1];
+        nums1[i + 1] = nums1[i];
+        nums1[i] = temp;
+        break;
+      }
+    }
+  }
+}
+
+int main(void)
+{
+  int ex1nums1[6] = {1, 2, 3, 0, 0, 0};
+  int ex1nums2[3] = {2, 5, 6};
+  merge(ex1nums1, 6, 3, ex1nums2, 3, 3); // [1,2,2,3,5,6]
+
+  int ex2nums1[1] = {1};
+  int ex2nums2[0] = {};
+  merge(ex2nums1, 1, 1, ex2nums2, 0, 0); // [1]
+
+  int ex3nums1[1] = {0};
+  int ex3nums2[1] = {1};
+  merge(ex3nums1, 0, 0, ex3nums2, 1, 1); // [1]
+}
+
+// Ça marche
+
+void topVotedMerge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
+{
+  int i, i1, i2;
+
+  i1 = m - 1;
+  i2 = n - 1;
+  i = nums1Size - 1;
+
+  while (i1 >= 0 && i2 >= 0)
+  {
+    if (nums1[i1] > nums2[i2])
+    {
+      nums1[i--] = nums1[i1--];
+    }
+    else
+    {
+      nums1[i--] = nums2[i2--];
+    }
+  }
+
+  while (i2 >= 0)
+  {
+    nums1[i--] = nums2[i2--];
+  }
+}
