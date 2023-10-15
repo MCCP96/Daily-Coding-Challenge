687bd60042e6bef073a121a740b8aa688bc592ce
MCCP96 <petermccomb@live.com>
2023-10-14 21:16:03 -0400

    Day 849 - Nim Game

--- c/2023.c
+++ c/2023.c
@@ -2302,7 +2302,7 @@ void topVotedMoveZeroes(int *nums, int numsSize)
 // 3. Time Complexity: O(n) | Space Complexity: O(1) */
 
 // Word Pattern					10/13/2023
-
+/*
 // Given a pattern and a string s, find if s follows the same pattern.
 
 // Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
@@ -2428,4 +2428,52 @@ bool topVotedWordPattern(char *pattern, char *s)
   return true;
 }
 
-// Easy Leetcode questions are something else in C
+// Easy Leetcode questions are something else in C */
+
+// Nim Game					10/14/2023
+
+// You are playing the following Nim Game with your friend:
+
+// Initially, there is a heap of stones on the table.
+
+// You and your friend will alternate taking turns, and you go first.
+
+// On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+
+// The one who removes the last stone is the winner.
+
+// Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.
+
+// Example 1:
+// 		Input: n = 4
+// 		Output: false
+// Explanation: These are the possible outcomes:
+// 		1. You remove 1 stone. Your friend removes 3 stones, including the last stone. Your friend wins.
+// 		2. You remove 2 stones. Your friend removes 2 stones, including the last stone. Your friend wins.
+// 		3. You remove 3 stones. Your friend removes the last stone. Your friend wins.
+// 		In all outcomes, your friend wins.
+
+// Example 2:
+// 		Input: n = 1
+// 		Output: true
+
+// Example 3:
+// 		Input: n = 2
+// 		Output: true
+
+// Constraints:
+//		1 <= n <= 231 - 1
+
+bool canWinNim(int n)
+{
+  return n % 4 != 0;
+}
+
+int main(void)
+{
+  bool ex1 = canWinNim(4); // false
+  bool ex2 = canWinNim(1); // true
+  bool ex3 = canWinNim(2); // true
+}
+
+// Same as top voted
