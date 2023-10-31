89f94a1bad7b4992cf3ba2a7f3670ad4539cd3f2
MCCP96 <petermccomb@live.com>
2023-10-31 08:48:27 -0400

    Day 866 - Arranging Coins

--- c/2023.c
+++ c/2023.c
@@ -3600,7 +3600,7 @@ int topVotedThirdMax(int *nums, int numsSize)
 // Simpler is better */
 
 // Number of Segments in a String					10/30/2023
-
+/*
 // Given a string s, return the number of segments in the string.
 
 // A segment is defined to be a contiguous sequence of non-space characters.
@@ -3665,4 +3665,45 @@ int topVotedCountSegments(char *s)
   return Segementcount;
 }
 
-// "The C library function char *strtok(char *str, const char *delim) breaks string str into a series of tokens using the delimiter delim."
+// "The C library function char *strtok(char *str, const char *delim) breaks string str into a series of tokens using the delimiter delim." */
+
+// Arranging Coins					10/31/2023
+
+// You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+// Given the integer n, return the number of complete rows of the staircase you will build.
+
+// Example 1:
+// 		Input: n = 5
+// 		Output: 2
+// Explanation: Because the 3rd row is incomplete, we return 2.
+
+// Example 2:
+// 		Input: n = 8
+// 		Output: 3
+// Explanation: Because the 4th row is incomplete, we return 3.
+
+// Constraints:
+//		1 <= n <= 231 - 1
+
+int arrangeCoins(int n)
+{
+  int rows = 0;
+  for (int i = 1; i <= n; i++)
+  {
+    n -= i;
+    rows++;
+  }
+  return rows;
+}
+
+int main(void)
+{
+  int ex1 = arrangeCoins(5); // 2
+  int ex2 = arrangeCoins(8); // 3
+}
+
+int topVotedArrangeCoins(int n)
+{
+  return (int)floor(-0.5 + sqrt((double)2 * n + 0.25));
+}
