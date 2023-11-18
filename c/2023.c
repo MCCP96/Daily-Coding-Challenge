0208bf18314854ffb5595099b5d624e73ac75dba
MCCP96 <petermccomb@live.com>
2023-11-18 11:27:24 -0500

    Day 884 - Longest Uncommon Subsequence I

--- c/2023.c
+++ c/2023.c
@@ -5017,7 +5017,7 @@ int topVotedFib(int n)
 }*/
 
 // Detect Capital					11/17/2023
-
+/*
 // We define the usage of capitals in a word to be right when one of the following cases holds:
 
 // All letters in this word are capitals, like "USA".
@@ -5062,4 +5062,66 @@ int main(void)
   printf("%d", detectCapitalUse("FlaG")); // false
 }
 
-// Beats 100% Runtime
+// Beats 100% Runtime */
+
+// Longest Uncommon Subsequence I					11/18/2023
+
+// Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+// An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+
+// A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+
+// For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
+
+// Example 1:
+// 		Input: a = "aba", b = "cdc"
+// 		Output: 3
+// Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
+// 		Note that "cdc" is also a longest uncommon subsequence.
+
+// Example 2:
+// 		Input: a = "aaa", b = "bbb"
+// 		Output: 3
+// Explanation: The longest uncommon subsequences are "aaa" and "bbb".
+
+// Example 3:
+// 		Input: a = "aaa", b = "aaa"
+// 		Output: -1
+// Explanation: Every subsequence of string a is also a subsequence of string b. Similarly, every subsequence of string b is also a subsequence of string a.
+
+// Constraints:
+//		1 <= a.length, b.length <= 100
+//		a and b consist of lower-case English letters.
+
+int findLUSlength(char *a, char *b)
+{
+  return strstr(a, b) && strstr(b, a) ? -1 : fmax(strlen(a), strlen(b));
+}
+
+int main(void)
+{
+  printf("%d", findLUSlength("aba", "cdc")); // 3
+  printf("%d", findLUSlength("aaa", "bbb")); // 3
+  printf("%d", findLUSlength("aaa", "aaa")); // -1
+}
+
+// One line
+
+#define MAX(x, y) x > y ? x : y
+
+int topVotedFindLUSlength(char *a, char *b)
+{
+  if (strlen(a) == strlen(b))
+  {
+    for (int i = 0; i < strlen(a); i++)
+    {
+      if (a[i] != b[i])
+      {
+        return strlen(a);
+      }
+    }
+    return -1;
+  }
+  return MAX(strlen(a), strlen(b));
+}
