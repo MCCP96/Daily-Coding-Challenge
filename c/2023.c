1e2c3035e01ca15f6be73124871122d0d8b9263b
MCCP96 <petermccomb@live.com>
2023-11-20 09:16:35 -0500

    Day 886 - Student Attendance Record I

--- c/2023.c
+++ c/2023.c
@@ -5127,7 +5127,7 @@ int topVotedFindLUSlength(char *a, char *b)
 } */
 
 // Reverse String II					11/19/2023
-
+/*
 // Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
 
 // If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
@@ -5210,4 +5210,90 @@ char *topVotedReverseStr(char *s, int k)
 }
 
 // Final 'if' covers trailing chars to swap
-// Not of length k
+// Not of length k */
+
+// Student Attendance Record I					11/20/2023
+
+// You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+
+// 'A': Absent.
+// 'L': Late.
+// 'P': Present.
+
+// The student is eligible for an attendance award if they meet both of the following criteria:
+
+// The student was absent ('A') for strictly fewer than 2 days total.
+
+// The student was never late ('L') for 3 or more consecutive days.
+
+// Return true if the student is eligible for an attendance award, or false otherwise.
+
+// Example 1:
+// 		Input: s = "PPALLP"
+// 		Output: true
+// Explanation: The student has fewer than 2 absences and was never late 3 or more consecutive days.
+
+// Example 2:
+// 		Input: s = "PPALLL"
+// 		Output: false
+// Explanation: The student was late 3 consecutive days in the last 3 days, so is not eligible for the award.
+
+// Constraints:
+//		1 <= s.length <= 1000
+//		s[i] is either 'A', 'L', or 'P'.
+
+bool checkRecord(char *s)
+{
+  int A = 0, L = 0;
+
+  while (*s)
+  {
+    if (*s == 'A')
+      A++;
+    if (*s == 'L')
+      L++;
+    if (*s != 'L')
+      L = 0;
+
+    if (A >= 2 || L >= 3)
+      return false;
+
+    s++;
+  }
+
+  return true;
+}
+
+int main(void)
+{
+  printf("%d", checkRecord("PPALLP")); // true
+  printf("%d", checkRecord("PPALLL")); // false
+}
+
+bool topVotedCheckRecord(char *s)
+{
+  int absum = 0, lasum = 0;
+
+  for (int i = 0; s[i] != '\0'; i++)
+  {
+    if (s[i] == 'A')
+    {
+      absum++;
+      if (absum >= 2)
+        return false;
+    }
+    if (s[i] == 'L')
+    {
+      lasum++;
+      if (lasum >= 3)
+        return false;
+    }
+    else
+    {
+      lasum = 0;
+    }
+  }
+  return true;
+}
+
+// same same
