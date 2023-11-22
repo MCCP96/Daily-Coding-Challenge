e50ba2427b6c3ff01171b39a8b313507bd5cf916
MCCP96 <petermccomb@live.com>
2023-11-22 09:48:27 -0500

    Day 888 - Maximum Depth of N-ary Tree

--- c/2023.c
+++ c/2023.c
@@ -5299,7 +5299,7 @@ bool topVotedCheckRecord(char *s)
 // same same */
 
 // Reverse Words in a String III					11/21/2023
-
+/*
 // Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 
 // Example 1:
@@ -5376,4 +5376,64 @@ char *topVotedReverseWords(char *s)
   return s;
 }
 
-// That reverse!
+// That reverse! */
+
+// Maximum Depth of N-ary Tree					11/22/2023
+
+// Given a n-ary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+// Example 1:
+// 		Input: root = [1,null,3,2,4,null,5,6]
+// 		Output: 3
+
+// Example 2:
+// 		Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+// 		Output: 5
+
+// Constraints:
+//		The total number of nodes is in the range [0, 10^4].
+//		The depth of the n-ary tree is less than or equal to 1000.
+
+struct Node
+{
+  int val;
+  int numChildren;
+  struct Node **children;
+};
+
+int maxDepth(struct Node *root)
+{
+  if (root == NULL)
+    return 0;
+  if (root->numChildren == 0)
+    return 1;
+
+  int max = 0;
+  for (int i = 0; i < root->numChildren; i++)
+    max = fmax(max, maxDepth(root->children[i]) + 1);
+
+  return max;
+}
+
+// We've been seeing binary trees in class :)
+
+int topVotedMaxDepth(struct Node *root)
+{
+  if (!root)
+    return 0;
+  if (root->numChildren == 0)
+    return 1;
+  int max = 0;
+  for (int i = 0; i < root->numChildren; i++)
+  {
+    int tmp = maxDepth(root->children[i]) + 1;
+    max = tmp > max ? tmp : max;
+  }
+  return max;
+}
+
+// same same
