1bb0d8abb28a462a9f94541073cdca626c6bf0f2
MCCP96 <petermccomb@live.com>
2023-11-23 11:43:58 -0500

    Day 889 - Array Partition

--- c/2023.c
+++ c/2023.c
@@ -5379,7 +5379,7 @@ char *topVotedReverseWords(char *s)
 // That reverse! */
 
 // Maximum Depth of N-ary Tree					11/22/2023
-
+/*
 // Given a n-ary tree, find its maximum depth.
 
 // The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
@@ -5436,4 +5436,51 @@ int topVotedMaxDepth(struct Node *root)
   return max;
 }
 
-// same same
+// same same */
+
+// Array Partition					11/23/2023
+
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+// Example 1:
+// 		Input: nums = [1,4,3,2]
+// 		Output: 4
+// Explanation: All possible pairings (ignoring the ordering of elements) are:
+// 		1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+// 		2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+// 		3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+// 		So the maximum possible sum is 4.
+
+// Example 2:
+// 		Input: nums = [6,2,6,5,1,2]
+// 		Output: 9
+// Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+
+// Constraints:
+//		1 <= n <= 104
+//		nums.length == 2 * n
+//		-104 <= nums[i] <= 104
+
+int inc(const void *a, const void *b)
+{
+  return *(int *)a - *(int *)b;
+}
+
+int arrayPairSum(int *nums, int numsSize)
+{
+  qsort(nums, numsSize, sizeof(int), inc);
+
+  int sum = 0;
+  for (int i = 0; i < numsSize; i += 2)
+    sum += nums[i];
+  return sum;
+}
+
+int main(void)
+{
+  printf("%d", arrayPairSum((int[]){1, 4, 3, 2}, 4));       // 4
+  printf("%d", arrayPairSum((int[]){6, 2, 6, 5, 1, 2}, 6)); // 9
+}
+
+// ça marche
+// same as top voted
