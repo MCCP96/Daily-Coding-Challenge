b1d80b1ceb2d5460d0cec5664c16fe0b6d4c14a4
MCCP96 <petermccomb@live.com>
2023-12-07 12:11:25 -0500

    Day 903 - Longest Continuous Inc Subsequence

--- c/2023.c
+++ c/2023.c
@@ -6315,7 +6315,7 @@ bool topVotedFindTarget(struct TreeNode *root, int k)
 // keep it simple */
 
 // Robot Return to Origin					12/6/2023
-
+/*
 // There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
 
 // You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
@@ -6362,4 +6362,44 @@ int main(void)
   printf("%d", judgeCircle("LL")); // false
 }
 
-// same as top voted
+// same as top voted */
+
+// Longest Continuous Increasing Subsequence					12/7/2023
+
+// Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.
+
+// A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].
+
+// Example 1:
+// 		Input: nums = [1,3,5,4,7]
+// 		Output: 3
+// Explanation: The longest continuous increasing subsequence is [1,3,5] with length 3.
+// 		Even though [1,3,5,7] is an increasing subsequence, it is not continuous as elements 5 and 7 are separated by element 4.
+
+// Example 2:
+// 		Input: nums = [2,2,2,2,2]
+// 		Output: 1
+// Explanation: The longest continuous increasing subsequence is [2] with length 1. Note that it must be strictly increasing.
+
+// Constraints:
+//		1 <= nums.length <= 104
+//		-109 <= nums[i] <= 109
+
+int findLengthOfLCIS(int *nums, int numsSize)
+{
+  int max = 1, cur = 1;
+  for (int i = 1; i < numsSize; i++)
+  {
+    nums[i - 1] >= nums[i] ? cur = 1 : cur++;
+    max = fmax(max, cur);
+  }
+  return max;
+}
+
+int main(void)
+{
+  printf("%d\n", findLengthOfLCIS((int[]){1, 3, 5, 4, 7}, 5)); // 3
+  printf("%d\n", findLengthOfLCIS((int[]){2, 2, 2, 2, 2}, 5)); // 1
+}
+
+// same logic as top voted
