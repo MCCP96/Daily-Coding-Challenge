5b375ac4bdf2747c0d02d9cc49a7e5668566a5ba
MCCP96 <petermccomb@live.com>
2023-12-17 10:27:03 -0500

    Day 913 - Construct String from Binary Tree

--- javascript/2023.js
+++ javascript/2023.js
@@ -18946,7 +18946,7 @@ var topVotedFindMaxForm = function (S, M, N) {
 }; */
 
 // Minimum Moves to Equal Array Elements II					9/15/2023
-
+/* 
 // Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
 
 // In one move, you can increment or decrement an element of the array by 1.
@@ -18997,4 +18997,74 @@ const revisedMinMoves2 = (nums) => {
   return nums.reduce((moves, n) => (moves += Math.abs(n - median)), 0);
 };
 
-// Beats 99%
+// Beats 99% */
+
+// Construct String from Binary Tree					12/17/2023
+
+// Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+
+// Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+
+// Example 1:
+// 		Input: root = [1,2,3,4]
+// 		Output: "1(2(4))(3)"
+// Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+
+// Example 2:
+// 		Input: root = [1,2,3,null,4]
+// 		Output: "1(2()(4))(3)"
+// Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+
+// Constraints:
+//		The number of nodes in the tree is in the range [1, 104].
+//		-1000 <= Node.val <= 1000
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const tree2str = function (root) {
+  let str = root.val + "";
+
+  if (root.left) {
+    str += "(" + tree2str(root.left) + ")";
+  }
+  if (root.right) {
+    if (!root.left) str += "()";
+    str += "(" + tree2str(root.right) + ")";
+  }
+
+  return str;
+};
+
+console.log(tree2str([1, 2, 3, 4])); // "1(2(4))(3)"
+console.log(tree2str([1, 2, 3, null, 4])); // "1(2()(4))(3)"
+
+// Back to javascript where malloc doesn't exist :)
+// Strings are toooooo easy to work with
+
+var topVotedTree2str = function (t) {
+  let res = [];
+  dfs(t, res);
+  return res.join("");
+
+  function dfs(t, res) {
+    if (t === null) return;
+
+    res.push(t.val);
+
+    if (t.left === null && t.right === null) return;
+
+    res.push("(");
+    dfs(t.left, res);
+    res.push(")");
+
+    if (t.right !== null) {
+      res.push("(");
+      dfs(t.right, res);
+      res.push(")");
+    }
+  }
+};
