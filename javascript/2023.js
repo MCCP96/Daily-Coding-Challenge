301d2c7eca9cc68dbd0c724e01efdc42cb2752af
MCCP96 <petermccomb@live.com>
2023-12-19 11:22:16 -0500

    Day 915 - Balanced Binary Tree

--- javascript/2023.js
+++ javascript/2023.js
@@ -19070,7 +19070,7 @@ var topVotedTree2str = function (t) {
 }; */
 
 // Maximum Depth of Binary Tree					12/18/2023
-
+/* 
 // Given the root of a binary tree, return its maximum depth.
 
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
@@ -19106,4 +19106,68 @@ var maxDepth = function (root, depth = 0) {
 var topVotedMaxDepth = function (root) {
   if (root === undefined || root === null) return 0;
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}; */
+
+// Balanced Binary Tree					12/19/2023
+
+// Given a binary tree, determine if it is height-balanced
+
+// Example 1:
+// 		Input: root = [3,9,20,null,null,15,7]
+// 		Output: true
+
+// Example 2:
+// 		Input: root = [1,2,2,3,3,null,null,4,4]
+// 		Output: false
+
+// Example 3:
+// 		Input: root = []
+// 		Output: true
+
+// Constraints:
+//		The number of nodes in the tree is in the range [0, 5000].
+//		-104 <= Node.val <= 104
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const depth = (root) => {
+  if (root == null) return 0;
+  const l = depth(root.left);
+  const r = depth(root.right);
+  if (l == -1 || r == -1 || Math.abs(l - r) > 1) return -1;
+  return Math.max(l, r) + 1;
+};
+
+const isBalanced = (root) => {
+  return depth(root) != -1;
+};
+
+// Looked at top voted
+
+var topVotedIsBalanced = function (root) {
+  // If the tree is empty, we can say it’s balanced...
+  if (root == null) return true;
+  // Height Function will return -1, when it’s an unbalanced tree...
+  if (Height(root) == -1) return false;
+  return true;
+};
+
+// Create a function to return the “height” of a current subtree using recursion...
+var Height = function (root) {
+  // Base case...
+  if (root == null) return 0;
+  // Height of left subtree...
+  let leftHeight = Height(root.left);
+  // Height of height subtree...
+  let rightHight = Height(root.right);
+  // In case of left subtree or right subtree unbalanced, return -1...
+  if (leftHeight == -1 || rightHight == -1) return -1;
+  // If their heights differ by more than ‘1’, return -1...
+  if (Math.abs(leftHeight - rightHight) > 1) return -1;
+  // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+  return Math.max(leftHeight, rightHight) + 1;
 };
