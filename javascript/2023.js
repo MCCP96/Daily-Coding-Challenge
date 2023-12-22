a82d34f660a5b58c90ac0fa5b84fff87502f712f
MCCP96 <petermccomb@live.com>
2023-12-22 10:17:27 -0500

    Day 918 - Linked List Cycle

--- javascript/2023.js
+++ javascript/2023.js
@@ -19227,7 +19227,7 @@ var topVotedMinDepth = function (root) {
 }; */
 
 // Path Sum					12/21/2023
-
+/* 
 // Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
 
 // A leaf is a node with no children.
@@ -19286,4 +19286,67 @@ var topVotedHasPathSum = function (root, sum) {
 };
 
 // same same
-// managed to omit acc by subtracting val from target
+// managed to omit acc by subtracting val from target */
+
+// Linked List Cycle					12/22/2023
+
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+// Example 1:
+// 		Input: head = [3,2,0,-4], pos = 1
+// 		Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+// Example 2:
+// 		Input: head = [1,2], pos = 0
+// 		Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+// Example 3:
+// 		Input: head = [1], pos = -1
+// 		Output: false
+// Explanation: There is no cycle in the linked list.
+
+// Constraints:
+//		The number of the nodes in the list is in the range [0, 104].
+//		-105 <= Node.val <= 105
+//		pos is -1 or a valid index in the linked-list.
+
+// Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const hasCycle = (head) => {
+  let seen = new Set();
+  while (head) {
+    if (seen.has(head.next)) return true;
+    seen.add(head.next);
+    head = head.next;
+  }
+  return false;
+};
+
+// Ça marche
+
+const topVotedHasCycle = (head) => {
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+  return false;
+};
+
+// "We just need to follow the strategy above - make a slower pointer and a faster pointer. Then we loop and loop again:
+
+// if the fast pointer catch up with slow pointer, then it's a circular linked list
+
+// if the fast pointer get to the end, then it's not a circular linked list"
