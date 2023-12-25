4e18a9ac283782287dc5bac77143270c5d61396d
MCCP96 <petermccomb@live.com>
2023-12-25 17:10:59 -0500

    Day 921 - Reverse Linked List

--- javascript/2023.js
+++ javascript/2023.js
@@ -19440,7 +19440,7 @@ var topVotedGetIntersectionNode = function (headA, headB) {
 }; */
 
 // Excel Sheet Column Title					12/24/2023
-
+/* 
 // Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 
 // For example:
@@ -19488,4 +19488,53 @@ var topVotedConvertToTitle = function (n) {
 
 console.log(convertToTitle(1)); // "A"
 console.log(convertToTitle(28)); // "AB"
-console.log(convertToTitle(701)); // "ZY"
+console.log(convertToTitle(701)); // "ZY" */
+
+// Reverse Linked List					12/25/2023
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+// 		Input: head = [1,2,3,4,5]
+// 		Output: [5,4,3,2,1]
+
+// Example 2:
+// 		Input: head = [1,2]
+// 		Output: [2,1]
+
+// Example 3:
+// 		Input: head = []
+// 		Output: []
+
+// Constraints:
+//		The number of nodes in the list is the range [0, 5000].
+//		-5000 <= Node.val <= 5000
+
+// Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+const reverseList = (node, prev = null) => {
+  if (node == null) return prev;
+  const next = node.next;
+  node.next = prev;
+  if (next == null) return node;
+  return reverseList(next, node);
+};
+
+var topVotedReverseList = function (head) {
+  // Special case...
+  if (head == null || head.next == null) return head;
+  // Create a new node to call the function recursively and we get the reverse linked list...
+  var res = topVotedReverseList(head.next);
+  // Set head node as head.next.next...
+  head.next.next = head;
+  //set head's next to be null...
+  head.next = null;
+  return res; // Return the reverse linked list...
+};
+
+// Merry Christmas :^)
