f58291090da13c1b97fa87968e7dc1e45f22b46c
MCCP96 <petermccomb@live.com>
2023-12-30 09:55:47 -0500

    Day 926 - Minimum Absolute Difference in BST

--- javascript/2023.js
+++ javascript/2023.js
@@ -19757,7 +19757,7 @@ var topVotedFindMode = function (root) {
 }; */
 
 // Invert Binary Tree					12/29/2023
-
+/* 
 // Given the root of a binary tree, invert the tree, and return its root.
 
 // Example 1:
@@ -19812,4 +19812,75 @@ var topVotedInvertTree = function (root) {
   return root; // Return the root...
 };
 
-// same same
+// same same */
+
+// Minimum Absolute Difference in BST					12/30/2023
+
+// Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+// Example 1:
+// 		Input: root = [4,2,6,1,3]
+// 		Output: 1
+
+// Example 2:
+// 		Input: root = [1,0,48,null,null,12,49]
+// 		Output: 1
+
+// Constraints:
+//		The number of nodes in the tree is in the range [2, 104].
+//		0 <= Node.val <= 105
+
+// Note: This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const getMinimumDifference = (root) => {
+  let nums = new Set();
+
+  const explore = (node) => {
+    if (node == null) return;
+    nums.add(node.val);
+    explore(node.left);
+    explore(node.right);
+  };
+  explore(root);
+
+  nums = [...nums].sort((a, b) => a - b);
+  let min = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < nums.length - 1; i++)
+    min = Math.min(min, nums[i + 1] - nums[i]);
+
+  return min;
+};
+
+// Slow
+
+var topVotedGetMinimumDifference = function (root) {
+  // Converting tree to an sorted array
+  const arr = toArray(root);
+  let out = Infinity;
+  let i = 1;
+  // Now, simply finding the difference
+  while (i < arr.length) {
+    let diff = Math.abs(arr[i - 1] - arr[i]);
+    if (diff < out) out = diff;
+    i++;
+  }
+  return out;
+};
+
+function toArray(root, out = []) {
+  if (root) {
+    toArray(root.left, out);
+    out.push(root.val);
+    toArray(root.right, out);
+    return out;
+  }
+}
+
+// Ahhh BST, they're avoiding sorting the array by filling it in starting with left-most nodes
