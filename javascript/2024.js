c8293ed8c91b23308b25c9f168e17bd7f64cb951
MCCP96 <petermccomb@live.com>
2024-01-04 11:07:33 -0500

    Day 931 - Count Binary Substrings

--- javascript/2024.js
+++ javascript/2024.js
@@ -213,7 +213,7 @@ var topVotedFindSecondMinimumValue = function (root) {
 // same same */
 
 // Binary Number with Alternating Bits					1/3/2024
-
+/* 
 // Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
 
 // Example 1:
@@ -258,4 +258,63 @@ var topVotedHasAlternatingBits = function (n) {
     }
   }
   return true;
+}; */
+
+// Count Binary Substrings					1/4/2024
+
+// Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+// Substrings that occur multiple times are counted the number of times they occur.
+
+// Example 1:
+// 		Input: s = "00110011"
+// 		Output: 6
+// Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+// 		Notice that some of these substrings repeat and are counted the number of times they occur.
+// 		Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+
+// Example 2:
+// 		Input: s = "10101"
+// 		Output: 4
+// Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+
+// Constraints:
+//		1 <= s.length <= 105
+//		s[i] is either '0' or '1'.
+
+const countBinarySubstrings = (s) => {
+  let res = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let count = [0, 0];
+
+    for (let j = i; j < s.length; j++) {
+      if (s[j] == s[i]) count[s[i]]++;
+      else {
+        if ([s.slice(j, j + count[s[i]])].every((c) => c != s[i])) res++;
+        break;
+      }
+    }
+  }
+
+  return res;
+};
+
+console.log(countBinarySubstrings("00110011")); // 6
+console.log(countBinarySubstrings("10101")); // 4
+
+const topVotedCountBinarySubstrings = (s) => {
+  let count = 1,
+    sum = 0,
+    prevCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) count += 1;
+    else {
+      if (prevCount) sum += prevCount <= count ? prevCount : count;
+      prevCount = count;
+      count = 1;
+    }
+  }
+  return sum;
 };
