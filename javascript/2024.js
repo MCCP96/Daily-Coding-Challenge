a19090c19fb024a346a5fd454b261cdfd6035306
MCCP96 <petermccomb@live.com>
2024-01-07 10:23:06 -0500

    Day 935 - Design HashMap

--- javascript/2024.js
+++ javascript/2024.js
@@ -400,7 +400,7 @@ console.log(revisedSearch([-1, 0, 3, 5, 9, 12], 9)); // 4
 console.log(revisedSearch([-1, 0, 3, 5, 9, 12], 2)); // -1 */
 
 // Design HashSet					1/6/2024
-
+/* 
 // Design a HashSet without using any built-in hash table libraries.
 
 // Implement MyHashSet class:
@@ -446,4 +446,86 @@ MyHashSet.prototype.contains = function (key) {
 };
 
 // Top voteds are doing it using objects, which I didn't think was allowed
-// Easy either way
+// Easy either way */
+
+// Design HashMap					1/7/2024
+
+// Design a HashMap without using any built-in hash table libraries.
+
+// Implement the MyHashMap class:
+// MyHashMap() initializes the object with an empty map.
+// void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+// int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+// void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+
+// Example 1:
+// 		Input
+// 		["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+// 		[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+// 		Output
+// 		[null, null, null, 1, -1, null, 1, null, -1]
+// 		Explanation
+// 		MyHashMap myHashMap = new MyHashMap();
+// 		myHashMap.put(1, 1); // The map is now [[1,1]]
+// 		myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+// 		myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+// 		myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+// 		myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+// 		myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+// 		myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+// 		myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+
+// Constraints:
+//		0 <= key, value <= 106
+//		At most 104 calls will be made to put, get, and remove.
+
+var MyHashMap = function () {
+  this.arr = [];
+};
+
+MyHashMap.prototype.put = function (key, value) {
+  let exists = false;
+
+  for (let i = 0; i < this.arr.length; i++) {
+    if (this.arr[i][0] == key) {
+      this.arr[i][1] = value;
+      exists = true;
+    }
+  }
+
+  if (!exists) this.arr.push([key, value]);
+};
+
+MyHashMap.prototype.get = function (key) {
+  for (let i = 0; i < this.arr.length; i++) {
+    if (this.arr[i][0] == key) return this.arr[i][1];
+  }
+  return -1;
+};
+
+MyHashMap.prototype.remove = function (key) {
+  for (let i = 0; i < this.arr.length; i++) {
+    if (this.arr[i][0] == key) this.arr.splice(i, 1);
+  }
+};
+
+// Slow
+
+class TopVotedMyHashMap {
+  constructor() {
+    this.data = new Array(1000001);
+  }
+  put(key, val) {
+    this.data[key] = val;
+  }
+  get(key) {
+    let val = this.data[key];
+    return val !== undefined ? val : -1;
+  }
+  remove(key) {
+    delete this.data[key];
+  }
+}
+
+// Avoid looping by using a large array
+// Much better!
