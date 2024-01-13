f87e629f35638da42d6cbe1cb20cef5e51e2b0f1
MCCP96 <petermccomb@live.com>
2024-01-13 10:21:33 -0500

    Day 940 - Palindrome Number

--- Python/2024.py
+++ Python/2024.py
@@ -1,5 +1,5 @@
 # Two Sum					1/12/2024
-
+""" 
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -43,4 +43,51 @@ class Solution(object):
 # Took some googling, but we got there
 
 # Seems we will be doing both Java and Python this semester
-# Given Java is a lot like C, I'm going to study Python here as I'm least comfortable with it
+# Given Java is a lot like C, I'm going to study Python here as I'm least comfortable with it """
+
+# Palindrome Number					1/13/2024
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+# Example 1:
+# 		Input: x = 121
+# 		Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+# Example 2:
+# 		Input: x = -121
+# 		Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+# Example 3:
+# 		Input: x = 10
+# 		Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+# Constraints:
+# 		-231 <= x <= 231 - 1
+
+# Follow up: Could you solve it without converting the integer to a string?
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or x != 0 and x % 10 == 0:
+            return False
+
+        half = 0
+        while x > half:
+            half = half * 10 + x % 10
+            x /= 10
+
+        return x == half or x == half / 10
+
+    print(isPalindrome(None, 121))  # true
+    print(isPalindrome(None, -121))  # false
+    print(isPalindrome(None, 10))  # false
+
+
+# Just did this one in C
+# 'or x == half/10' covers numbers of odd length
+
+# Same as top voted
