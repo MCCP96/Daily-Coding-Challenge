5e869f403b05aa9ac82e09cfc05c2deee8f53760
MCCP96 <petermccomb@live.com>
2024-01-18 13:39:42 -0500

    Day 945 - Remove Duplicates from Sorted Array

--- Python/2024.py
+++ Python/2024.py
@@ -326,7 +326,7 @@ class Solution(object):
 # 'not stack' is a great way of checking len(stack) == 0 """
 
 # Merge Two Sorted Lists					1/17/2024
-
+""" 
 # You are given the heads of two sorted linked lists list1 and list2.
 
 # Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -431,4 +431,76 @@ def topVotedMergeTwoLists(self, l1, l2):
     return dummy.next
 
 
-# 'dummy = cur = ListNode(0)' and 'cur.next = l1 or l2' are nice
+# 'dummy = cur = ListNode(0)' and 'cur.next = l1 or l2' are nice """
+
+# Remove Duplicates from Sorted Array					1/18/2024
+
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+# Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+
+# Return k.
+
+# Custom Judge:
+
+# The judge will test your solution with the following code:
+#   int[] nums = [...]; // Input array
+#   int[] expectedNums = [...]; // The expected answer with correct length
+
+#   int k = removeDuplicates(nums); // Calls your implementation
+
+#   assert k == expectedNums.length;
+#   for (int i = 0; i < k; i++) {
+#       assert nums[i] == expectedNums[i];
+#   }
+
+# If all assertions pass, then your solution will be accepted.
+
+# Example 1:
+# 		Input: nums = [1,1,2]
+# 		Output: 2, nums = [1,2,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+# 		It does not matter what you leave beyond the returned k (hence they are underscores).
+
+# Example 2:
+# 		Input: nums = [0,0,1,1,1,2,2,3,3,4]
+# 		Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+# 		It does not matter what you leave beyond the returned k (hence they are underscores).
+
+# Constraints:
+# 		1 <= nums.length <= 3 * 104
+# 		-100 <= nums[i] <= 100
+# 		nums is sorted in non-decreasing order.
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        k = len(nums)
+        seen = []
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                nums[i] = 101
+                k -= 1
+            else:
+                seen.append(nums[i])
+        nums.sort()
+        return k
+
+    print(removeDuplicates(None, [1, 1, 2]))  #  2, nums = [1,2,_]
+    print(
+        removeDuplicates(None, [0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+    )  #  5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+
+# 101 because '-100 <= nums[i] <= 100'
+
+
+def removeDuplicates(self, nums: List[int]) -> int:
+    nums[:] = sorted(set(nums))
+    return len(nums)
+
+
+# 'sort in place using [:]'!
