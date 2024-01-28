fb371fb459f0bc7e0a2d1d3ba07db7dabf730dd3
MCCP96 <petermccomb@live.com>
2024-01-28 10:25:42 -0500

    Day 955 - Merge Sorted Array

--- Python/2024.py
+++ Python/2024.py
@@ -1021,7 +1021,7 @@ class Solution:
         return p1 """
 
 # Remove Duplicates from Sorted List					1/27/2024
-
+""" 
 # Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
 # Example 1:
@@ -1076,4 +1076,72 @@ def deleteDuplicates(self, head):
         while cur.next and cur.next.val == cur.val:
             cur.next = cur.next.next  # skip duplicated node
         cur = cur.next  # not duplicate of current node, move to next node
-    return head
+    return head """
+
+# Merge Sorted Array					1/28/2024
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+# Example 1:
+# 		Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# 		Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# 		The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+# Example 2:
+# 		Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# 		Output: [1]
+# Explanation: The arrays we are merging are [1] and [].
+# 		The result of the merge is [1].
+
+# Example 3:
+# 		Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+# 		Output: [1]
+# Explanation: The arrays we are merging are [] and [1].
+# 		The result of the merge is [1].
+# 		Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+# Constraints:
+# 		nums1.length == m + n
+# 		nums2.length == n
+# 		0 <= m, n <= 200
+# 		1 <= m + n <= 200
+# 		-109 <= nums1[i], nums2[j] <= 109
+
+# Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        if n == 0:
+            return
+
+        for i in range(m + n):
+            if n <= 0:
+                break
+            if nums2[0 - n] <= nums1[i]:
+                nums1[i + 1 :] = nums1[i:-1]
+                nums1[i] = nums2[0 - n]
+                n -= 1
+
+        if n > 0:
+            nums1[0 - n :] = nums2[0 - n :]
+
+    merge(None, [1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)  #  [1,2,2,3,5,6]
+    merge(None, [1], 1, [], 0)  #  [1]
+    merge(None, [0], 0, [1], 1)  #  [1]
+    merge(None, [2, 0], 2, [1], 1)  #  [1,2]
+
+
+# Beats 97% of runtimes
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        for j in range(n):
+            nums1[m + j] = nums2[j]
+        nums1.sort()
