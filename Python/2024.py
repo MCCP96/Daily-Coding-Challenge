cb926637c18da335bb5dd58aeb17286e88063691
MCCP96 <petermccomb@live.com>
2024-01-31 09:29:43 -0500

    Day 958 - Maximum Depth of Binary Tree

--- Python/2024.py
+++ Python/2024.py
@@ -1207,7 +1207,7 @@ def topVotedIsSameTree(self, p, q):
     return p is q """
 
 # Symmetric Tree					1/30/2024
-
+""" 
 # Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
 # Example 1:
@@ -1267,4 +1267,50 @@ class Solution(object):
         return self.isMirror(root.left, root.right)
 
 
-# same same
+# same same """
+
+# Maximum Depth of Binary Tree					1/31/2024
+
+# Given the root of a binary tree, return its maximum depth.
+
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Example 1:
+# 		Input: root = [3,9,20,null,null,15,7]
+# 		Output: 3
+
+# Example 2:
+# 		Input: root = [1,null,2]
+# 		Output: 2
+
+# Constraints:
+# 		The number of nodes in the tree is in the range [0, 104].
+# 		-100 <= Node.val <= 100
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def maxDepth(self, node, depth=0):
+        if node == None:
+            return depth
+        return max(
+            self.maxDepth(node.left, depth + 1),
+            self.maxDepth(node.right, depth + 1),
+        )
+
+
+class Solution:
+    def topVotedMaxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+# Omits depth variable
