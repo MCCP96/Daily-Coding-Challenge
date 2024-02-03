9ba5ebd7af85ba41d47154ffa33da6bd26ebe4c0
MCCP96 <petermccomb@live.com>
2024-02-03 13:16:11 -0500

    Day 961 - Minimum Depth of Binary Tree

--- Python/2024.py
+++ Python/2024.py
@@ -1369,7 +1369,7 @@ class Solution:
 # Phi is the golden ratio """
 
 # Balanced Binary Tree					2/2/2024
-
+""" 
 # Given a binary tree, determine if it is height-balanced.
 
 # Example 1:
@@ -1425,4 +1425,63 @@ class Solution(object):
                 return -1
             return 1 + max(left, right)
 
-        return check(root) != -1
+        return check(root) != -1 """
+
+# Minimum Depth of Binary Tree					2/3/2024
+
+# Given a binary tree, find its minimum depth.
+
+# The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+# Note: A leaf is a node with no children.
+
+# Example 1:
+# 		Input: root = [3,9,20,null,null,15,7]
+# 		Output: 2
+
+# Example 2:
+# 		Input: root = [2,null,3,null,4,null,5,null,6]
+# 		Output: 5
+
+# Constraints:
+# 		The number of nodes in the tree is in the range [0, 105].
+# 		-1000 <= Node.val <= 1000
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def minDepth(self, node, depth=0):
+        if not node:  # not a leaf
+            if depth == 0:
+                return 0
+            return 999999
+
+        if not node.left and not node.right:  # is a leaf
+            return 1
+
+        return min(
+            self.minDepth(node.left, depth + 1) + 1,
+            self.minDepth(node.right, depth + 1) + 1,
+        )
+
+
+# Would've rather solving it without depth, but empty binary tree wasn't passing
+
+
+def topVotedMinDepth(self, root):
+    if not root:
+        return 0
+    if None in [root.left, root.right]:
+        return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+    else:
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+
+# Cool, does max when non-leaf end is encountered, else min
