d36e18d0701ced356bbe73c80ead9c869d540f61
MCCP96 <petermccomb@live.com>
2024-02-10 09:41:32 -0500

    Day 968 - Linked List Cycle

--- Python/2024.py
+++ Python/2024.py
@@ -1793,7 +1793,7 @@ class Solution(object):
 # Same but avoids additional while loops """
 
 # Single Number					2/9/2024
-
+""" 
 # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 # You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -1836,4 +1836,74 @@ class Solution(object):
         return sum(list(set(nums))) * 2 - sum(nums)
 
 
-# 'The sum() function returns a number, the sum of all items in an iterable'
+# 'The sum() function returns a number, the sum of all items in an iterable' """
+
+# Linked List Cycle					2/10/2024
+
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+
+# Example 1:
+# 		Input: head = [3,2,0,-4], pos = 1
+# 		Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+# Example 2:
+# 		Input: head = [1,2], pos = 0
+# 		Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+# Example 3:
+# 		Input: head = [1], pos = -1
+# 		Output: false
+# Explanation: There is no cycle in the linked list.
+
+# Constraints:
+# 		The number of the nodes in the list is in the range [0, 104].
+# 		-105 <= Node.val <= 105
+# 		pos is -1 or a valid index in the linked-list.
+
+# Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        seen = []
+
+        def explore(node):
+            if node == None:
+                return False
+            if id(node) in seen:
+                return True
+            seen.append(id(node))
+            return explore(node.next)
+
+        return explore(head)
+
+
+# 'id() function gives the address of the particular object.'
+
+
+def hasCycle(self, head):
+    try:
+        slow = head
+        fast = head.next
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next.next
+        return True
+    except:
+        return False
+
+
+# right, I remember the tortoise and the hare
