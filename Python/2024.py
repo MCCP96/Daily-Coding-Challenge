403670b3da8f44d209f3067cd8193ee42a65f8fb
MCCP96 <petermccomb@live.com>
2024-02-15 11:01:25 -0500

    Day 973 - Number of 1 Bits

--- Python/2024.py
+++ Python/2024.py
@@ -2068,7 +2068,7 @@ def titleToNumber(self, s):
 # 'The ord() function returns the number representing the unicode code of a specified character.' """
 
 # Reverse Bits					2/14/2024
-
+""" 
 # Reverse bits of a given 32 bits unsigned integer.
 
 # Example 1:
@@ -2119,4 +2119,43 @@ class Solution:
         for _ in range(32):
             res = (res << 1) + (n & 1)
             n >>= 1
+        return res """
+
+# Number of 1 Bits					2/15/2024
+
+# Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+# Example 1:
+# 		Input: n = 00000000000000000000000000001011
+# 		Output: 3
+# Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+
+# Example 2:
+# 		Input: n = 00000000000000000000000010000000
+# 		Output: 1
+# Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+
+# Example 3:
+# 		Input: n = 11111111111111111111111111111101
+# 		Output: 31
+# Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+
+# Constraints:
+# 		The input must be a binary string of length 32.
+# 		Follow up: If this function is called many times, how would you optimize it?
+
+
+class Solution(object):
+    def hammingWeight(self, n):
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
         return res
+
+    print(hammingWeight(None, 1011))  #  3
+    print(hammingWeight(None, 10000000))  #  1
+    print(hammingWeight(None, 11111111111111111111111111111101))  #  31
+
+
+# same as top voted
