804f40da286984c7dd3ec2347ccb08d72bfd9d92
MCCP96 <petermccomb@live.com>
2024-02-19 10:19:09 -0500

    Day 977 - Reverse Linked List

--- Python/2024.py
+++ Python/2024.py
@@ -2279,7 +2279,7 @@ class Solution(object):
 # Same as top voted """
 
 # Isomorphic Strings					2/18/2024
-
+""" 
 # Given two strings s and t, determine if they are isomorphic.
 
 # Two strings s and t are isomorphic if the characters in s can be replaced to get t.
@@ -2345,4 +2345,61 @@ class Solution(object):
                 return False
             s2t[s[i]] = t[i]
             t2s[t[i]] = s[i]
-        return True
+        return True """
+
+# Reverse Linked List					2/19/2024
+
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Example 1:
+# 		Input: head = [1,2,3,4,5]
+# 		Output: [5,4,3,2,1]
+
+# Example 2:
+# 		Input: head = [1,2]
+# 		Output: [2,1]
+
+# Example 3:
+# 		Input: head = []
+# 		Output: []
+
+# Constraints:
+# 		The number of nodes in the list is the range [0, 5000].
+# 		-5000 <= Node.val <= 5000
+# 		Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
+
+
+# peek forward and assign backwards
+
+
+class Solution(object):
+    def topVotedReverseList(self, head):
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
+
+
+# same same
