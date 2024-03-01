1dc04b45d9f599bc0f8d4e12ac90322b7d103c98
MCCP96 <petermccomb@live.com>
2024-03-01 09:20:47 -0500

    Day 988 - Add Digits

--- Python/2024.py
+++ Python/2024.py
@@ -2944,7 +2944,7 @@ class Solution(object):
 # Python has a function for that """
 
 # Binary Tree Paths					2/29/2024
-
+""" 
 # Given the root of a binary tree, return all root-to-leaf paths in any order.
 
 # A leaf is a node with no children.
@@ -3012,4 +3012,63 @@ class Solution(object):
                 stack.append((node.right, ls + str(node.val) + "->"))
             if node.left:
                 stack.append((node.left, ls + str(node.val) + "->"))
-        return res
+        return res """
+
+# Add Digits					3/1/2024
+
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+# Example 1:
+# 		Input: num = 38
+# 		Output: 2
+# Explanation: The process is
+# 		38 --> 3 + 8 --> 11
+# 		11 --> 1 + 1 --> 2
+# 		Since 2 has only one digit, return it.
+
+# Example 2:
+# 		Input: num = 0
+# 		Output: 0
+
+# Constraints:
+# 		0 <= num <= 231 - 1
+# 		Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            acc = 0
+            while num > 0:
+                acc += num % 10
+                num //= 10
+            num = acc
+        return num
+
+    print(addDigits(None, 38))  #  2
+    print(addDigits(None, 0))  #  0
+
+
+# 95% Runtime
+
+
+class Solution:
+    def topVotedAddDigits(self, num):
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+
+
+# Any number where it's digits add to 9 is always divisible by 9. (18, 27, 36, 45, 54, 63, 72, 81, 90, etc.) Therefore the 'digital root' for any number divisible by 9 is always 9. You can see this even in larger numbers like 99 because 9 + 9 = 18, and then 1 + 8 = 9 still, so the root always becomes 9 for any numbers divisible by 9.
+
+# Additionally, 0 always has a digital root of 0 obviously.
+
+# The only other cases you need to worry about to find the digital root are when it isn't 0 or 9.
+
+# So for any number that isn't 0 and isn't divisible by 9, the root will always n % 9 for a given number n. (AKA the difference between given number n and the nearest number that is divisible by 9, since numbers divisible by 9 always have a digital root of 9).
+# For examples: 100 % 9 = 1 (one greater than 99, which is divisible by 9).
+# 101 % 9 = 2
+# 102 % 9 = 3 and so on.
