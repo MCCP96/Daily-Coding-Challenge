64bfba1a7fd60f796e62193eb2afbcab99c78c52
MCCP96 <petermccomb@live.com>
2024-03-17 10:36:23 -0400

    Day 1004 - Intersection of Two Arrays

--- Python/2024.py
+++ Python/2024.py
@@ -3986,7 +3986,7 @@ class Solution(object):
 # Same as top voted """
 
 # Find the K-or of an Array					3/16/2024
-
+""" 
 # You are given an integer array nums, and an integer k. Let's introduce K-or operation by extending the standard bitwise OR. In K-or, a bit position in the result is set to 1 if at least k numbers in nums have a 1 in that position.
 
 # Return the K-or of nums.
@@ -4071,4 +4071,38 @@ class Solution:
             if count >= k:
                 ans |= 2**i
 
-        return ans
+        return ans """
+
+# Intersection of Two Arrays					3/17/2024
+
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+# Example 1:
+# 		Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# 		Output: [2]
+
+# Example 2:
+# 		Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# 		Output: [9,4]
+# Explanation: [4,9] is also accepted.
+
+# Constraints:
+# 		1 <= nums1.length, nums2.length <= 1000
+# 		0 <= nums1[i], nums2[i] <= 1000
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        res = []
+        for n in set(nums1):
+            if n in nums2:
+                res.append(n)
+        return res
+
+    print(intersection(None, [1, 2, 2, 1], [2, 2]))  #  [2]
+    print(intersection(None, [4, 9, 5], [9, 4, 9, 8, 4]))  #  [9,4]
+
+
+class Solution(object):
+    def topVotedIntersection(self, nums1, nums2):
+        return list(set(nums1) & set(nums2))
