511519c8eeaa20cab0ca334b0db6be970b799670
MCCP96 <petermccomb@live.com>
2024-04-21 11:07:03 -0400

    Day 1039 - Island Perimeter

--- Python/2024.py
+++ Python/2024.py
@@ -6309,7 +6309,7 @@ def radixMSD(arr):
 radixMSD(["add", "cab", "fad", "fee", "bad", "bee", "fed", "bed", "ace"]) """
 
 # Diameter of Binary Tree					4/20/2024
-
+""" 
 # Given the root of a binary tree, return the length of the diameter of the tree.
 
 # The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
@@ -6352,4 +6352,76 @@ class Solution(object):
 
 
 # Algorithms & Data Structures final went well this morning
-# Sorting algorithm studied here were most of it :)
+# Sorting algorithm studied here were most of it :) """
+
+# Island Perimeter					4/21/2024
+
+# You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+# Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+# The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+# Example 1:
+# 		Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+# 		Output: 16
+# Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+# Example 2:
+# 		Input: grid = [[1]]
+# 		Output: 4
+
+# Example 3:
+# 		Input: grid = [[1,0]]
+# 		Output: 4
+
+# Constraints:
+# 		row == grid.length
+# 		col == grid[i].length
+# 		1 <= row, col <= 100
+# 		grid[i][j] is 0 or 1.
+# 		There is exactly one island in grid.
+
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        perimeter = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == 1:  # current cell is land
+                    cur = 4  # max perimeter for current cell
+                    cur -= row - 1 >= 0 and grid[row - 1][col]  # check above
+                    cur -= (
+                        col + 1 < len(grid[row]) and grid[row][col + 1]
+                    )  # check right
+                    cur -= row + 1 < len(grid) and grid[row + 1][col]  # check below
+                    cur -= col - 1 >= 0 and grid[row][col - 1]  # check left
+                    perimeter += cur
+        return perimeter
+
+    print(
+        islandPerimeter(None, [[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]])
+    )  #  16
+    print(islandPerimeter(None, [[1]]))  #  4
+    print(islandPerimeter(None, [[1, 0]]))  #  4
+
+# straight forward
+    
+def topVotedIslandPerimeter(self, grid):
+    R,C = len(grid), len(grid[0])
+    perimeter = 0
+    # Traverse the grid
+    for i in range(R):
+        for j in range(C):
+            # If it is a land block increment perimeter by 4
+            if grid[i][j] == 1:
+                perimeter += 4
+            # Check whether top neighbour is a land and decrement it by 2
+            # as it intersects
+            if i>0 and grid[i-1][j] == 1:
+                perimeter -= 2
+            # Check left neighbour is a land and decrement it by 2
+            # as it intersects
+            if j>0 and grid[i][j-1] == 1:
+                perimeter -= 2
+    return perimeter
