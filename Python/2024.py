27c519a3525e59fd20ad61be61d3ad6a3164feab
MCCP96 <petermccomb@live.com>
2024-04-22 09:03:27 -0400

    Day 1040 - Number Complement

--- Python/2024.py
+++ Python/2024.py
@@ -6355,7 +6355,7 @@ class Solution(object):
 # Sorting algorithm studied here were most of it :) """
 
 # Island Perimeter					4/21/2024
-
+""" 
 # You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
 
 # Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
@@ -6424,4 +6424,49 @@ def topVotedIslandPerimeter(self, grid):
             # as it intersects
             if j>0 and grid[i][j-1] == 1:
                 perimeter -= 2
-    return perimeter
+    return perimeter """
+
+# Number Complement					4/22/2024
+
+# The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+# For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+
+# Given an integer num, return its complement.
+
+# Example 1:
+# 		Input: num = 5
+# 		Output: 2
+# Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+# Example 2:
+# 		Input: num = 1
+# 		Output: 0
+# Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+# Constraints:
+# 		1 <= num < 2^31
+
+
+class Solution(object):
+    def findComplement(self, num):
+        val = 0
+        p = 0
+        while num > 0:
+            val += 2**p if num % 2 == 0 else 0
+            num >>= 1
+            p += 1
+        return val
+
+    print(findComplement(None, 5))  #  2
+    print(findComplement(None, 1))  #  0
+
+
+# Probably some one-liner for this, but good to practice bit manipulation
+
+
+def topVotedFindComplement(self, num):
+    return num ^ (2 ** num.bit_length() - 1)
+
+
+# Et voilà
