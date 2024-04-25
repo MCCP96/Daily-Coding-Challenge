f403e12d9fd1e3279f4d9ca1f84a558f969e3708
MCCP96 <petermccomb@live.com>
2024-04-25 13:28:43 -0400

    Day 1043 - Construct the Rectangle

--- Python/2024.py
+++ Python/2024.py
@@ -6537,7 +6537,7 @@ class Solution:
         return "-".join(grouping).upper() """
 
 # Max Consecutive Ones					4/24/2024
-
+""" 
 # Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
 # Example 1:
@@ -6577,4 +6577,48 @@ class Solution:
             if zeros > 0:
                 zeros -= nums[l] == 0
                 l += 1
-        return r - l + 1
+        return r - l + 1 """
+
+# Construct the Rectangle					4/25/2024
+
+# A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+
+# The area of the rectangular web page you designed must equal to the given target area.
+
+# The width W should not be larger than the length L, which means L >= W.
+
+# The difference between length L and width W should be as small as possible.
+
+# Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.
+
+# Example 1:
+# 		Input: area = 4
+# 		Output: [2,2]
+# Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1].
+# 		But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+
+# Example 2:
+# 		Input: area = 37
+# 		Output: [37,1]
+
+# Example 3:
+# 		Input: area = 122122
+# 		Output: [427,286]
+
+# Constraints:
+# 		1 <= area <= 107
+
+
+class Solution(object):
+    def topVotedConstructRectangle(self, area):
+        for l in range(int(area**0.5), 0, -1):
+            if area % l == 0:
+                return [area // l, l]
+
+
+class Solution(object):
+    def anotherTopVotedConstructRectangle(self, area):
+        mid = int(math.sqrt(area))
+        while area % mid != 0:
+            mid -= 1
+        return [int(area / mid), mid]
