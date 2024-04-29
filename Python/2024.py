2273ff506c46bf4a11401305191290539b18a179
MCCP96 <petermccomb@live.com>
2024-04-29 08:48:33 -0400

    Day 1047 - Divide Two Integers

--- Python/2024.py
+++ Python/2024.py
@@ -6796,7 +6796,7 @@ class Solution:
         return res """
 
 # Base 7					4/28/2024
-
+""" 
 # Given an integer num, return a string of its base 7 representation.
 
 # Example 1:
@@ -6832,4 +6832,84 @@ class Solution(object):
     print(convertToBase7(None, -7))  #  "-10"
 
 
-# same as top voted
+# same as top voted """
+
+# Divide Two Integers					4/29/2024
+
+# Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+# The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+# Return the quotient after dividing dividend by divisor.
+
+# Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+# Example 1:
+# 		Input: dividend = 10, divisor = 3
+# 		Output: 3
+# Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+# Example 2:
+# 		Input: dividend = 7, divisor = -3
+# 		Output: -2
+# Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+
+# Constraints:
+# 		-2^31 <= dividend, divisor <= 2^31 - 1
+# 		divisor != 0
+
+
+class Solution(object):
+    def divide(self, dividend, divisor):
+        negative = False
+        if dividend < 0 and divisor > 0:  # negative dividend
+            negative = True
+            dividend = -dividend
+        elif dividend > 0 and divisor < 0:  # negative divisor
+            negative = True
+            divisor = -divisor
+        elif dividend < 0 and divisor < 0:  # both negative
+            dividend = -dividend
+            divisor = -divisor
+
+        if divisor == 1:
+            return dividend if not negative else -dividend
+
+        count = 0
+        while dividend >= divisor:
+            dividend -= divisor
+            count += 1
+
+        return count if not negative else -count
+
+    print(divide(None, 10, 3))  #  3
+    print(divide(None, 7, -3))  #  -2
+
+
+# Breaks on edge cases
+
+
+class Solution:
+    def divide(self, dividend, divisor):
+        is_negative = (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+
+        quotient = 0
+        while dividend >= divisor:
+            curr_divisor, num_divisors = divisor, 1
+            while dividend >= curr_divisor:
+                dividend -= curr_divisor
+                quotient += num_divisors
+
+                curr_divisor = curr_divisor << 1
+                num_divisors = num_divisors << 1
+
+        quotient = -quotient if is_negative else quotient
+        return min(max(-(2**31), quotient), 2**31 - 1)  # description's 'note' paragraph
+
+
+# Commented explanation:
+# "To not over-complicate, here is a simple explanation. A naive method here is to repeatedly subtract divisor from dividend, until there is none enough left. Then the count of subtractions will be the answer. Yet this takes linear time and is thus slow. A better method is to subtract divisor in a more efficient way. We can subtract divisor, 2divisor, 4divisor, 8divisor... as is implemented above. Now the subtracting process only takes log-time."
+
+# Better is_negative, better logic
+# People are calling the bitshifts cheating, but it beats the linear approach
