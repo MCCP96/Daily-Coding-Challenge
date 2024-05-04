9e0173d05aff02abf8dd8ef94041975a9c66c4d7
MCCP96 <petermccomb@live.com>
2024-05-04 10:26:34 -0400

    Day 1052 - Univalued Binary Tree

--- Python/2024.py
+++ Python/2024.py
@@ -7150,7 +7150,7 @@ class Solution:
     print(topVotedIsAdditiveNumber(None, "199100199"))  #  true """
 
 # Binary Prefix Divisible By 5					5/3/2024
-
+""" 
 # You are given a binary array nums (0-indexed).
 
 # We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).
@@ -7195,4 +7195,64 @@ class Solution:
         for n in nums:
             num = (num * 2 + n) % 5
             res.append(num == 0)
-        return res
+        return res """
+
+# Univalued Binary Tree					5/4/2024
+
+# A binary tree is uni-valued if every node in the tree has the same value.
+
+# Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+
+# Example 1:
+# 		Input: root = [1,1,1,1,1,null,1]
+# 		Output: true
+
+# Example 2:
+# 		Input: root = [2,2,2,5,2]
+# 		Output: false
+
+# Constraints:
+# 		The number of nodes in the tree is in the range [1, 100].
+# 		0 <= Node.val < 100
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def isUnivalTree(self, root):
+        def explore(node, val):
+            if node == None:
+                return True
+            if node.val != val:
+                return False
+            return explore(node.left, val) and explore(node.right, val)
+
+        return explore(root, root.val)
+
+    print(isUnivalTree(None, [1, 1, 1, 1, 1, null, 1]))  #  true
+    print(isUnivalTree(None, [2, 2, 2, 5, 2]))  #  false
+
+
+class Solution:
+    def isUnivalTree(self, tree):
+        if not tree:
+            return True
+
+        if tree.right:
+            if tree.val != tree.right.val:
+                return False
+
+        if tree.left:
+            if tree.val != tree.left.val:
+                return False
+
+        return self.isUnivalTree(tree.right) and self.isUnivalTree(tree.left)
+
+
+# Same same
