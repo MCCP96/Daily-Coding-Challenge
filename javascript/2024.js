48964fe2c9e21bb8ab6b5eba1c1e01cf71f13ef2
MCCP96 <petermccomb@live.com>
2024-05-14 10:52:44 -0400

    Day 1062 - Insert Interval

--- javascript/2024.js
+++ javascript/2024.js
@@ -1350,7 +1350,7 @@ var topVotedCanJump = function (nums) {
 }; */
 
 // Merge Intervals					5/13/2024
-
+/* 
 // Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 // Example 1:
@@ -1417,4 +1417,107 @@ function topVotedMerge(intervals) {
     }
   }
   return res;
-}
+} */
+
+// Insert Interval					5/14/2024
+
+// You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+// Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+// Return intervals after the insertion.
+
+// Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+// Example 1:
+// 		Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// 		Output: [[1,5],[6,9]]
+
+// Example 2:
+// 		Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// 		Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+// Constraints:
+//		0 <= intervals.length <= 104
+//		intervals[i].length == 2
+//		0 <= starti <= endi <= 105
+//		intervals is sorted by starti in ascending order.
+//		newInterval.length == 2
+//		0 <= start <= end <= 105
+
+const insert = (intervals, [newS, newE]) => {
+  // insert new interval in sorted location
+  let idx = intervals.findIndex(([s, e]) => newS < s);
+  if (idx != -1) {
+    intervals.splice(idx, 0, [newS, newE]);
+  } else {
+    intervals.push([newS, newE]); // is largest
+  }
+
+  // same as question #56 "merge interval"
+  let res = [[intervals[0][0], intervals[0][0]]];
+  let lastIdx = 0;
+  for (const [s, e] of intervals) {
+    if (s <= res[lastIdx][1]) {
+      if (e > res[lastIdx][1]) {
+        res[lastIdx][1] = e;
+      }
+    } else {
+      res.push([s, e]);
+      lastIdx++;
+    }
+  }
+  return res;
+};
+
+console.log(
+  insert(
+    [
+      [1, 3],
+      [6, 9],
+    ],
+    [2, 5]
+  )
+); //  [[1,5],[6,9]]
+console.log(
+  insert(
+    [
+      [1, 2],
+      [3, 5],
+      [6, 7],
+      [8, 10],
+      [12, 16],
+    ],
+    [4, 8]
+  )
+); //  [[1,2],[3,10],[12,16]]
+console.log(insert([[1, 5]], [2, 3])); //  [[1,5]
+
+var topVotedInsert = function (intervals, newInterval) {
+  let merged = [];
+  let i = 0;
+
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    merged.push(intervals[i]);
+    i++;
+  }
+
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval = [
+      Math.min(newInterval[0], intervals[i][0]),
+      Math.max(newInterval[1], intervals[i][1]),
+    ];
+    i++;
+  }
+  merged.push(newInterval);
+
+  while (i < intervals.length) {
+    merged.push(intervals[i]);
+    i++;
+  }
+
+  return merged;
+};
+
+// nice
