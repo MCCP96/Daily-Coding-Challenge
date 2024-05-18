9509c649348d909afcb0a69cb6a6b269ca241875
MCCP96 <petermccomb@live.com>
2024-05-17 22:02:19 -0400

    Day 1065 - Binary Tree Postorder Traversal

--- javascript/2024.js
+++ javascript/2024.js
@@ -1610,7 +1610,7 @@ const topVotedRotateRight = function (head, k) {
 }; */
 
 // Binary Tree Preorder Traversal					5/16/2024
-
+/* 
 // Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
 // Example 1:
@@ -1661,4 +1661,58 @@ const topVotedPreorderTraversal = (root) => {
   return result;
 };
 
-// Stack to do it iteratively
+// Stack to do it iteratively */
+
+// Binary Tree Postorder Traversal0					5/17/2024
+
+//Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+// Example 1:
+// 		Input: root = [1,null,2,3]
+// 		Output: [3,2,1]
+
+// Example 2:
+// 		Input: root = []
+// 		Output: []
+
+// Example 3:
+// 		Input: root = [1]
+// 		Output: [1]
+
+// Constraints:
+//		The number of the nodes in the tree is in the range [0, 100].
+//		-100 <= Node.val <= 100
+
+const postorderTraversal = (root) => {
+  let res = [];
+  const explore = (node) => {
+    if (!node) return;
+    explore(node.left);
+    explore(node.right);
+    res.push(node.val);
+  };
+  explore(root);
+  return res;
+};
+
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+var topVotedPostorderTraversal = function (root) {
+  if (!root) return [];
+  let stack = [],
+    res = [];
+  stack.push(root);
+  while (stack.length) {
+    let node = stack[stack.length - 1];
+    if (node.left) {
+      stack.push(node.left);
+      node.left = null;
+    } else if (node.right) {
+      stack.push(node.right);
+      node.right = null;
+    } else res.push(stack.pop().val);
+  }
+  return res;
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+};
