2f3d0eb34464a88b701b9f0a61fc8f1ec2347172
MCCP96 <petermccomb@live.com>
2024-06-07 10:22:20 -0400

    Gas Station

--- javascript/2024.js
+++ javascript/2024.js
@@ -3131,7 +3131,7 @@ var topVotedSumNumbers = function (root) {
 // path*10 to avoid converting from string to int */
 
 // Longest Consecutive Sequence					6/6/2024
-
+/* 
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
 // You must write an algorithm that runs in O(n) time.
@@ -3193,4 +3193,89 @@ var topVotedLongestConsecutive = function (arr) {
   }
 
   return maxLen;
+}; */
+
+// Gas Station					6/7/2024
+
+// There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+// Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+// Example 1:
+// 		Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+// 		Output: 3
+// Explanation:
+// 		Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+// 		Travel to station 4. Your tank = 4 - 1 + 5 = 8
+// 		Travel to station 0. Your tank = 8 - 2 + 1 = 7
+// 		Travel to station 1. Your tank = 7 - 3 + 2 = 6
+// 		Travel to station 2. Your tank = 6 - 4 + 3 = 5
+// 		Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+// 		Therefore, return 3 as the starting index.
+
+// Example 2:
+// 		Input: gas = [2,3,4], cost = [3,4,3]
+// 		Output: -1
+// Explanation:
+// 		You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+// 		Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+// 		Travel to station 0. Your tank = 4 - 3 + 2 = 3
+// 		Travel to station 1. Your tank = 3 - 3 + 3 = 3
+// 		You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+// 		Therefore, you can't travel around the circuit once no matter where you start.
+
+// Constraints:
+//		n == gas.length == cost.length
+//		1 <= n <= 105
+//		0 <= gas[i], cost[i] <= 104
+
+const revisedCanCompleteCircuit = (gas, cost) => {
+  let absDif = 0; // final gas difference
+  let cur = 0; // current gas in tank
+  let res = 0; // valid starting index
+
+  gas.forEach((amt, i) => {
+    const dif = amt - cost[i]; // fuel required for current station
+    absDif += dif; // total gas needed
+    cur += dif; // current tank
+
+    if (cur < 0) {
+      // insufficient gas
+      res = i + 1; // update res
+      cur = 0; // reset current tank
+    }
+  });
+
+  if (absDif < 0) return -1; // not enough total gas
+  return res;
+};
+
+console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])); //  3
+console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3])); //  -1
+console.log(canCompleteCircuit([5, 1, 2, 3, 4], [4, 4, 1, 5, 1])); //  4
+console.log(canCompleteCircuit([5, 8, 2, 8], [6, 5, 6, 6])); //  3
+console.log(canCompleteCircuit([3, 1, 1], [1, 2, 2])); //  0
+console.log(canCompleteCircuit([2, 0, 1, 2, 3, 4, 0], [0, 1, 0, 0, 0, 0, 11])); //  0
+
+// Had another embarrassingly bad/bloated solution
+// Rewrote after reading top voted
+
+var topVotedCanCompleteCircuit = function (gas, cost) {
+  let totalTank = 0;
+  let currentTank = 0;
+  let startingStation = 0;
+
+  for (let i = 0; i < gas.length; i++) {
+    const netCost = gas[i] - cost[i];
+    totalTank += netCost;
+    currentTank += netCost;
+    if (currentTank < 0) {
+      startingStation = i + 1;
+      currentTank = 0;
+    }
+  }
+
+  return totalTank < 0 ? -1 : startingStation;
 };
