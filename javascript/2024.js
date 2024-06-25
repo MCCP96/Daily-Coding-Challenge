90e4845a6308be9ce996f91ca7e90a66565f73c9
MCCP96 <petermccomb@live.com>
2024-06-25 10:08:42 -0400

    Find the Maximum Divisibility Score

--- javascript/2024.js
+++ javascript/2024.js
@@ -4578,7 +4578,7 @@ function findIntersectionValues(nums1, nums2) {
 // same same */
 
 // Number of Even and Odd Bits					6/24/2024
-
+/* 
 // You are given a positive integer n.
 
 // Let even denote the number of even indices in the binary representation of n with value 1.
@@ -4635,4 +4635,76 @@ var topVotedEvenOddBit = function (n) {
   return [even, odd];
 };
 
-// toString() really impacts runtime
+// toString() really impacts runtime */
+
+// Find the Maximum Divisibility Score					6/25/2024
+
+// You are given two integer arrays nums and divisors.
+
+// The divisibility score of divisors[i] is the number of indices j such that nums[j] is divisible by divisors[i].
+
+// Return the integer divisors[i] with the maximum divisibility score. If multiple integers have the maximum score, return the smallest one.
+
+// Example 1:
+// 		Input: nums = [2,9,15,50], divisors = [5,3,7,2]
+// 		Output: 2
+// Explanation:
+// 		The divisibility score of divisors[0] is 2 since nums[2] and nums[3] are divisible by 5.
+// 		The divisibility score of divisors[1] is 2 since nums[1] and nums[2] are divisible by 3.
+// 		The divisibility score of divisors[2] is 0 since none of the numbers in nums is divisible by 7.
+// 		The divisibility score of divisors[3] is 2 since nums[0] and nums[3] are divisible by 2.
+// 		As divisors[0], divisors[1], and divisors[3] have the same divisibility score, we return the smaller one which is divisors[3].
+
+// Example 2:
+// 		Input: nums = [4,7,9,3,9], divisors = [5,2,3]
+// 		Output: 3
+// Explanation:
+// 		The divisibility score of divisors[0] is 0 since none of numbers in nums is divisible by 5.
+// 		The divisibility score of divisors[1] is 1 since only nums[0] is divisible by 2.
+// 		The divisibility score of divisors[2] is 3 since nums[2], nums[3] and nums[4] are divisible by 3.
+
+// Example 3:
+// 		Input: nums = [20,14,21,10], divisors = [10,16,20]
+// 		Output: 10
+// Explanation:
+// 		The divisibility score of divisors[0] is 2 since nums[0] and nums[3] are divisible by 10.
+// 		The divisibility score of divisors[1] is 0 since none of the numbers in nums is divisible by 16.
+// 		The divisibility score of divisors[2] is 1 since nums[0] is divisible by 20.
+
+// Constraints:
+//		1 <= nums.length, divisors.length <= 1000
+//		1 <= nums[i], divisors[i] <= 10^9
+
+const maxDivScore = (nums, divisors) => {
+  let res = [Infinity, 0]; // [divisor, maxDivisibilityScore]
+  for (const div of divisors) {
+    let divScore = 0;
+    for (const n of nums) {
+      if (n % div == 0) divScore++; // divisor found
+    }
+    if (divScore > res[1]) res = [div, divScore]; // new most divisible
+    else if (divScore == res[1] && div < res[0]) res[0] = div; // smaller most divisible
+  }
+  return res[0];
+};
+
+console.log(maxDivScore([2, 9, 15, 50], [5, 3, 7, 2])); //  2
+console.log(maxDivScore([4, 7, 9, 3, 9], [5, 2, 3])); //  3
+console.log(maxDivScore([20, 14, 21, 10], [10, 16, 20])); //  10
+
+// I wonder if there's an O(n) approach
+
+const topVotedMaxDivScore = (a, b) => {
+  let f = [];
+  b.map((d, i) => {
+    let cnt = 0;
+    for (const x of a) {
+      if (x % d == 0) cnt++;
+    }
+    f.push([cnt, d]);
+  });
+  f.sort((x, y) => y[0] - x[0] || x[1] - y[1]);
+  return f[0][1];
+};
+
+// Not great readability
