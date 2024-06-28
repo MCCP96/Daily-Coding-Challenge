6d120791dcb3ca83edb567d9f3474135a7309dee
MCCP96 <petermccomb@live.com>
2024-06-28 10:18:52 -0400

    Minimum Cost of a Path With Special Roads

--- javascript/2024.js
+++ javascript/2024.js
@@ -4763,7 +4763,7 @@ console.log(minimizedStringLength("baadccab")); //  4
 // same as top voted */
 
 // Minimum Number of Coins for Fruits					6/27/2024
-
+/* 
 // You are given an integer array prices where prices[i] denotes the number of coins needed to purchase the ith fruit.
 
 // The fruit market has the following reward for each fruit:
@@ -4852,4 +4852,164 @@ function topVotedMinimumCoins(A) {
   }
 
   return dp[N]; // Return the minimum coins needed for N elements
-}
+} */
+
+// Minimum Cost of a Path With Special Roads					6/28/2024
+
+// You are given an array start where start = [startX, startY] represents your initial position (startX, startY) in a 2D space. You are also given the array target where target = [targetX, targetY] represents your target position (targetX, targetY).
+
+// The cost of going from a position (x1, y1) to any other position in the space (x2, y2) is |x2 - x1| + |y2 - y1|.
+
+// There are also some special roads. You are given a 2D array specialRoads where specialRoads[i] = [x1i, y1i, x2i, y2i, costi] indicates that the ith special road goes in one direction from (x1i, y1i) to (x2i, y2i) with a cost equal to costi. You can use each special road any number of times.
+
+// Return the minimum cost required to go from (startX, startY) to (targetX, targetY).
+
+// Example 1:
+// 		Input: start = [1,1], target = [4,5], specialRoads = [[1,2,3,3,2],[3,4,4,5,1]]
+// 		Output: 5
+// Explanation:
+// 		(1,1) to (1,2) with a cost of |1 - 1| + |2 - 1| = 1.
+// 		(1,2) to (3,3). Use specialRoads[0] with the cost 2.
+// 		(3,3) to (3,4) with a cost of |3 - 3| + |4 - 3| = 1.
+// 		(3,4) to (4,5). Use specialRoads[1] with the cost 1.
+// 		So the total cost is 1 + 2 + 1 + 1 = 5.
+
+// Example 2:
+// 		Input: start = [3,2], target = [5,7], specialRoads = [[5,7,3,2,1],[3,2,3,4,4],[3,3,5,5,5],[3,4,5,6,6]]
+// 		Output: 7
+// Explanation:
+// 		It is optimal not to use any special edges and go directly from the starting to the ending position with a cost |5 - 3| + |7 - 2| = 7.
+// 		Note that the specialRoads[0] is directed from (5,7) to (3,2).
+
+// Example 3:
+// 		Input: start = [1,1], target = [10,4], specialRoads = [[4,2,1,1,3],[1,2,7,4,4],[10,3,6,1,2],[6,1,1,2,3]]
+// 		Output: 8
+// Explanation:
+// 		(1,1) to (1,2) with a cost of |1 - 1| + |2 - 1| = 1.
+// 		(1,2) to (7,4). Use specialRoads[1] with the cost 4.
+// 		(7,4) to (10,4) with a cost of |10 - 7| + |4 - 4| = 3.
+
+// Constraints:
+//		start.length == target.length == 2
+//		1 <= startX <= targetX <= 105
+//		1 <= startY <= targetY <= 105
+//		1 <= specialRoads.length <= 200
+//		specialRoads[i].length == 5
+//		startX <= x1i, x2i <= targetX
+//		startY <= y1i, y2i <= targetY
+//		1 <= costi <= 105
+
+const dist = (x1, y1, x2, y2) => Math.abs(x2 - x1) + Math.abs(y2 - y1);
+
+const minimumCost = ([sx, sy], [tx, ty], specialRoads) => {
+  let minCost = dist(sx, sy, tx, ty);
+
+  const dfs = (cur, x, y) => {
+    if (cur >= minCost) return;
+
+    const toTarget = dist(x, y, tx, ty);
+    minCost = Math.min(cur + toTarget, minCost);
+
+    for (const [x1, y1, x2, y2, d] of specialRoads) {
+      const toRoadEnd = dist(x, y, x1, y1) + d;
+      dfs(toRoadEnd + cur, x2, y2);
+    }
+  };
+  dfs(0, sx, sy);
+
+  return minCost;
+};
+
+console.log(
+  minimumCost(
+    [1, 1],
+    [4, 5],
+    [
+      [1, 2, 3, 3, 2],
+      [3, 4, 4, 5, 1],
+    ]
+  )
+); //  5
+console.log(
+  minimumCost(
+    [3, 2],
+    [5, 7],
+    [
+      [5, 7, 3, 2, 1],
+      [3, 2, 3, 4, 4],
+      [3, 3, 5, 5, 5],
+      [3, 4, 5, 6, 6],
+    ]
+  )
+); //  7
+console.log(
+  minimumCost(
+    [1, 1],
+    [10, 4],
+    [
+      [4, 2, 1, 1, 3],
+      [1, 2, 7, 4, 4],
+      [10, 3, 6, 1, 2],
+      [6, 1, 1, 2, 3],
+    ]
+  )
+); //  8
+
+// exceeds time limit
+// passes 1039/1045 testcases
+
+const getDist = (x1, y1, x2, y2) => Math.abs(x2 - x1) + Math.abs(y2 - y1);
+
+var topVotedMinimumCost = function (start, target, specialRoads) {
+  let cost = Infinity;
+  const map = new Map();
+
+  const dfs = (curDist, x, y) => {
+    if (curDist > cost) return;
+
+    // without using any specialRoad
+    const toTargetDist = getDist(x, y, target[0], target[1]);
+    cost = Math.min(toTargetDist + curDist, cost);
+
+    // recording the distance to get to [x, y]
+    if (map.get(`${x}, ${y}`) && map.get(`${x}, ${y}`) <= curDist) return;
+    else map.set(`${x}, ${y}`, curDist);
+
+    // using specialRoads
+    for (let j = 0; j < specialRoads.length; j++) {
+      const [x1, y1, x2, y2, d] = specialRoads[j];
+      const toSpecialEndDist = getDist(x, y, x1, y1) + d;
+      dfs(toSpecialEndDist + curDist, x2, y2);
+    }
+  };
+
+  dfs(0, start[0], start[1]);
+
+  return cost;
+};
+
+// map saves runtime
+// avoid double checking points
+
+const revisedMinimumCost = ([sx, sy], [tx, ty], specialRoads) => {
+  let minCost = dist(sx, sy, tx, ty);
+  const map = new Map();
+
+  const dfs = (cur, x, y) => {
+    if (cur >= minCost) return;
+
+    const toTarget = dist(x, y, tx, ty);
+    minCost = Math.min(cur + toTarget, minCost);
+
+    if (map?.get(`${x},${y}`) <= cur) return;
+    else map.set(`${x},${y}`, cur);
+
+    for (const [x1, y1, x2, y2, d] of specialRoads) {
+      const toRoadEnd = dist(x, y, x1, y1) + d;
+      dfs(toRoadEnd + cur, x2, y2);
+    }
+  };
+  dfs(0, sx, sy);
+
+  return minCost;
+};
