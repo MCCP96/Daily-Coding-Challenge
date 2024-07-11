25ce1aa77c6a931bc9cb80a644ad36304e2ed2d6
MCCP96 <petermccomb@live.com>
2024-07-11 09:55:03 -0400

    Count Pairs That Form a Complete Day I

--- javascript/2024.js
+++ javascript/2024.js
@@ -5833,7 +5833,7 @@ const topVotedDivideArray = (nums, k) => {
 // same same */
 
 // Find the Number of Ways to Place People I					7/10/2024
-
+/* 
 // https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/description/
 
 // You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D plane, where points[i] = [xi, yi].
@@ -5948,4 +5948,62 @@ var topVotedNumberOfPairs = function (ps) {
   return count;
 };
 
-// smart to sort
+// smart to sort */
+
+// Count Pairs That Form a Complete Day I					7/10/2024
+
+// https://leetcode.com/problems/count-pairs-that-form-a-complete-day-i/
+
+// Given an integer array hours representing times in hours, return an integer denoting the number of pairs i, j where i < j and hours[i] + hours[j] forms a complete day.
+
+// A complete day is defined as a time duration that is an exact multiple of 24 hours.
+
+// For example, 1 day is 24 hours, 2 days is 48 hours, 3 days is 72 hours, and so on.
+
+// Example 1:
+// 		Input: hours = [12,12,30,24,24]
+// 		Output: 2
+// Explanation:
+// 		The pairs of indices that form a complete day are (0, 1) and (3, 4).
+
+// Example 2:
+// 		Input: hours = [72,48,24,3]
+// 		Output: 3
+// Explanation:
+// 		The pairs of indices that form a complete day are (0, 1), (0, 2), and (1, 2).
+
+// Constraints:
+//		1 <= hours.length <= 100
+//		1 <= hours[i] <= 109
+
+const countCompleteDayPairs = (hours) => {
+  const n = hours.length;
+  let res = 0;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      res += (hours[i] + hours[j]) % 24 == 0;
+    }
+  }
+  return res;
+};
+
+console.log(countCompleteDayPairs([12, 12, 30, 24, 24])); //  2
+console.log(countCompleteDayPairs([72, 48, 24, 3])); //  3
+
+// brute force
+
+var topVotedCountCompleteDayPairs = function (hours) {
+  let remainderCount = {};
+  let count = 0;
+  for (let hour of hours) {
+    let remainder = hour % 24;
+    if (remainder === 0) {
+      count += remainderCount[0] || 0;
+    } else {
+      count += remainderCount[24 - remainder] || 0;
+    }
+    remainderCount[remainder] = (remainderCount[remainder] || 0) + 1;
+  }
+  return count;
+};
