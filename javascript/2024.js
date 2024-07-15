b1fd1d470defc5af93d6cd947382d3131faec0ef
MCCP96 <petermccomb@live.com>
2024-07-15 10:59:13 -0400

    Minimum Average of Smallest and Largest Elements

--- javascript/2024.js
+++ javascript/2024.js
@@ -6138,7 +6138,7 @@ console.log(
 ); //  0 */
 
 // Minimum Additions to Make Valid String					7/14/2024
-
+/* 
 // https://leetcode.com/problems/minimum-additions-to-make-valid-string/description/
 
 // Given a string word to which you can insert letters "a", "b" or "c" anywhere and any number of times, return the minimum number of letters that must be inserted so that word becomes valid.
@@ -6215,4 +6215,77 @@ var topVotedAddMinimum = function (word) {
   }
 
   return result;
+}; */
+
+// Minimum Average of Smallest and Largest Elements					7/15/2024
+
+// https://leetcode.com/problems/minimum-average-of-smallest-and-largest-elements/solutions/5356780/solution-by-dare2solve-detailed-solution/
+
+// You have an array of floating point numbers averages which is initially empty. You are given an array nums of n integers where n is even.
+
+// You repeat the following procedure n / 2 times:
+// - Remove the smallest element, minElement, and the largest element maxElement, from nums.
+// - Add (minElement + maxElement) / 2 to averages.
+
+// Return the minimum element in averages.
+
+// Example 1:
+// 		Input: nums = [7,8,3,4,15,13,4,1]
+// 		Output: 5.5
+// Explanation:
+// 		step	nums	averages
+// 		0	[7,8,3,4,15,13,4,1]	[]
+// 		1	[7,8,3,4,13,4]	[8]
+// 		2	[7,8,4,4]	[8,8]
+// 		3	[7,4]	[8,8,6]
+// 		4	[]	[8,8,6,5.5]
+// 		The smallest element of averages, 5.5, is returned.
+
+// Example 2:
+// 		Input: nums = [1,9,8,3,10,5]
+// 		Output: 5.5
+// Explanation:
+// 		step	nums	averages
+// 		0	[1,9,8,3,10,5]	[]
+// 		1	[9,8,3,5]	[5.5]
+// 		2	[8,5]	[5.5,6]
+// 		3	[]	[5.5,6,6.5]
+
+// Example 3:
+// 		Input: nums = [1,2,3,7,8,9]
+// 		Output: 5.0
+// Explanation:
+// 		step	nums	averages
+// 		0	[1,2,3,7,8,9]	[]
+// 		1	[2,3,7,8]	[5]
+// 		2	[3,7]	[5,5]
+// 		3	[]	[5,5,5]
+
+// Constraints:
+//		2 <= n == nums.length <= 50
+//		n is even.
+//		1 <= nums[i] <= 50
+
+const minimumAverage = (nums) => {
+  let n = nums.length;
+  let m = n / 2;
+  let i = 0;
+  nums.sort((a, b) => a - b);
+
+  let min = Infinity;
+  while (m-- > 0) min = Math.min((nums[i] + nums[n - i++ - 1]) / 2, min);
+  return min;
+};
+
+console.log(minimumAverage([7, 8, 3, 4, 15, 13, 4, 1])); //  5.5
+console.log(minimumAverage([1, 9, 8, 3, 10, 5])); //  5.5
+console.log(minimumAverage([1, 2, 3, 7, 8, 9])); //  5.0
+
+var topVotedMinimumAverage = function (nums) {
+  var res = 100;
+  nums.sort((a, b) => a - b);
+  for (var i = 0; i < nums.length >> 1; ++i) {
+    res = Math.min(res, nums[i] + nums[nums.length - i - 1]);
+  }
+  return res / 2;
 };
