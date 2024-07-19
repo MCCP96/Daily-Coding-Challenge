9ce85051c067510df023f5b7d32a76ca285469b1
MCCP96 <petermccomb@live.com>
2024-07-19 08:33:31 -0400

    Product of Array Except Self

--- javascript/2024.js
+++ javascript/2024.js
@@ -6512,7 +6512,7 @@ console.log(
 // same as top voted */
 
 // Decode Ways					7/18/2024
-
+/* 
 // https://leetcode.com/problems/decode-ways/description/
 
 // You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
@@ -6609,4 +6609,73 @@ var topVotedNumDecodings = function (s) {
   }
 
   return dp[n];
+}; */
+
+// Product of Array Except Self					7/19/2024
+
+// https://leetcode.com/problems/product-of-array-except-self/description/
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example 1:
+// 		Input: nums = [1,2,3,4]
+// 		Output: [24,12,8,6]
+
+// Example 2:
+// 		Input: nums = [-1,1,0,-3,3]
+// 		Output: [0,0,9,0,0]
+
+// Constraints:
+//		2 <= nums.length <= 105
+//		-30 <= nums[i] <= 30
+//		The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+const productExceptSelf = (nums) => {
+  const sum = nums.reduce((a, c) => a * c, 1);
+  if (
+    sum == 0 &&
+    nums.findIndex((x) => x == 0) != nums.findLastIndex((x) => x == 0)
+  )
+    return new Array(nums.length).fill(0); // 2 instances of 0
+
+  let res = nums.map((c) => {
+    if (sum != 0 && c != 0) return sum / c;
+    else if (sum == 0 && c == 0)
+      return nums.reduce((a, c) => a * (c != 0 ? c : 1), 1);
+    else return 0;
+  });
+
+  return res;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4])); //  [24,12,8,6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); //  [0,0,9,0,0]
+console.log(productExceptSelf([-1, 1, 0, -3, 0])); //  [0,0,9,0,0]
+
+var topVotedProductExceptSelf = function (nums) {
+  const n = nums.length;
+
+  const prefix = new Array(n).fill(1);
+  const suffix = new Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
+  }
+
+  for (let i = n - 2; i >= 0; i--) {
+    suffix[i] = suffix[i + 1] * nums[i + 1];
+  }
+
+  const answer = [];
+  for (let i = 0; i < n; i++) {
+    answer[i] = prefix[i] * suffix[i];
+  }
+
+  return answer;
 };
