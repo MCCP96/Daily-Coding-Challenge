b4c9602534972da944a83aa393e1d396b482807a
MCCP96 <petermccomb@live.com>
2024-08-03 12:40:18 -0400

    Find the Encrypted String

--- javascript/2024.js
+++ javascript/2024.js
@@ -7732,7 +7732,7 @@ var topVotedIncreasingTriplet = function (nums) {
 // same same */
 
 // Integer Break					8/2/2024
-
+/* 
 // https://leetcode.com/problems/integer-break/description/
 
 // Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
@@ -7777,4 +7777,53 @@ console.log(topVotedIntegerBreak(10)); //  36
 // 6 = 3 + 3 (product = 9)
 // 7 = 3 + 2 + 2 (product = 12)
 // 8 = 3 + 3 + 2 (product = 18)
-// 9 = 3 + 3 + 3 (product = 27)
+// 9 = 3 + 3 + 3 (product = 27) */
+
+// Find the Encrypted String					8/3/2024
+
+// https://leetcode.com/problems/find-the-encrypted-string/description/
+
+// You are given a string s and an integer k. Encrypt the string using the following algorithm:
+
+// For each character c in s, replace c with the kth character after c in the string (in a cyclic manner).
+
+// Return the encrypted string.
+
+// Example 1:
+// 		Input: s = "dart", k = 3
+// 		Output: "tdar"
+// Explanation:
+// 		For i = 0, the 3rd character after 'd' is 't'.
+// 		For i = 1, the 3rd character after 'a' is 'd'.
+// 		For i = 2, the 3rd character after 'r' is 'a'.
+// 		For i = 3, the 3rd character after 't' is 'r'.
+
+// Example 2:
+// 		Input: s = "aaa", k = 1
+// 		Output: "aaa"
+// Explanation:
+// 		As all the characters are the same, the encrypted string will also be the same.
+
+// Constraints:
+//		1 <= s.length <= 100
+//		1 <= k <= 104
+//		s consists only of lowercase English letters.
+
+const getEncryptedString = (s, k) => {
+  k = k % s.length;
+  return s.substring(k) + s.substring(0, k);
+};
+
+console.log(getEncryptedString("dart", 3)); //  "tdar"
+console.log(getEncryptedString("aaa", 1)); //  "aaa"
+
+var topVotedGetEncryptedString = function (s, k) {
+  const n = s.length;
+  k = k % n;
+  let result = new Array(n);
+  for (let i = 0; i < n; ++i) {
+    let idx = (i + k) % n;
+    result[idx] = s[i];
+  }
+  return result.join("");
+};
