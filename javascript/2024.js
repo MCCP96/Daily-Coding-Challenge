6cc16df4d7dc810b4cf65f9b39cb0dfd29ab07f7
MCCP96 <petermccomb@live.com>
2024-08-07 17:15:18 -0400

    Find K Pairs with Smallest Sums

--- javascript/2024.js
+++ javascript/2024.js
@@ -8031,7 +8031,7 @@ var topVotedLargestDivisibleSubset = function (nums) {
 // again iterative dp */
 
 // Sum of Two Integers					8/6/2024
-
+/* 
 // https://leetcode.com/problems/sum-of-two-integers/description/
 
 // Given two integers a and b, return the sum of the two integers without using the operators + and -.
@@ -8055,4 +8055,76 @@ var topVotedGetSum = function (a, b) {
     b = carry;
   }
   return a ^ b;
+}; */
+
+// Find K Pairs with Smallest Sums					8/7/2024
+
+// https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/
+
+// You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+
+// Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+// Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+// Example 1:
+// 		Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+// 		Output: [[1,2],[1,4],[1,6]]
+// Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+// Example 2:
+// 		Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+// 		Output: [[1,1],[1,1]]
+// Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+
+// Constraints:
+//		1 <= nums1.length, nums2.length <= 105
+//		-109 <= nums1[i], nums2[i] <= 109
+//		nums1 and nums2 both are sorted in non-decreasing order.
+//		1 <= k <= 104
+//		k <= nums1.length * nums2.length
+
+const kSmallestPairs = (nums1, nums2, k) => {
+  let [n, m] = [nums1.length, nums2.length];
+  let pairs = new Array(k * 2);
+  let idx = 0;
+
+  for (let i = 0; i < n && i < k; i++) {
+    for (let j = 0; j < m && j < k; j++) {
+      pairs[idx++] = [nums1[i], nums2[j]];
+    }
+  }
+
+  return pairs.sort(([a1, a2], [b1, b2]) => a1 + a2 - (b1 + b2)).slice(0, k);
+};
+
+console.log(kSmallestPairs([1, 7, 11], [2, 4, 6], 3)); //  [[1,2],[1,4],[1,6]]
+console.log(kSmallestPairs([1, 1, 2], [1, 2, 3], 2)); //  [[1,1],[1,1]]
+
+var topVotedKSmallestPairs = function (nums1, nums2, k) {
+  const minHeap = new MinPriorityQueue({ priority: (x) => x[0] });
+
+  for (let i = 0; i < nums1.length; i++) {
+    const num1 = nums1[i];
+    const num2 = nums2[0];
+
+    minHeap.enqueue([num1 + num2, i, 0]);
+  }
+
+  const n = nums2.length;
+  const res = [];
+
+  while (k > 0 && !minHeap.isEmpty()) {
+    const [sum, idx1, idx2] = minHeap.dequeue().element;
+
+    res.push([nums1[idx1], nums2[idx2]]);
+
+    if (res.length === k) return res;
+
+    if (idx2 < n - 1) {
+      minHeap.enqueue([nums1[idx1] + nums2[idx2 + 1], idx1, idx2 + 1]);
+    }
+  }
+
+  return res;
 };
