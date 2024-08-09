15a54a6908729656195ae784ca56949ba7a05944
MCCP96 <petermccomb@live.com>
2024-08-09 08:35:47 -0400

    Remove K Digits

--- javascript/2024.js
+++ javascript/2024.js
@@ -8130,7 +8130,7 @@ var topVotedKSmallestPairs = function (nums1, nums2, k) {
 }; */
 
 // Lexicographically Smallest String After a Swap					8/8/2024
-
+/* 
 // https://leetcode.com/problems/lexicographically-smallest-string-after-a-swap/description/
 
 // Given a string s containing only digits, return the lexicographically smallest string that can be obtained after swapping adjacent digits in s with the same parity at most once.
@@ -8174,4 +8174,82 @@ var topVotedGetSmallestString = function (s) {
     }
   }
   return arr.join("");
+}; */
+
+// Remove K Digits					8/9/2024
+
+// https://leetcode.com/problems/remove-k-digits/description/
+
+// Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+// Example 1:
+// 		Input: num = "1432219", k = 3
+// 		Output: "1219"
+// Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+// Example 2:
+// 		Input: num = "10200", k = 1
+// 		Output: "200"
+// Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+// Example 3:
+// 		Input: num = "10", k = 2
+// 		Output: "0"
+// Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+// Constraints:
+//		1 <= k <= num.length <= 10^5
+//		num consists of only digits.
+//		num does not have any leading zeros except for the zero itself.
+
+const removeKdigits = (num, k) => {
+  const n = num.length;
+  if (k == n) return "0";
+  num = num.split("");
+
+  let res = "";
+  for (let i = 1; k > 0; i++, k--) {
+    if (num[i - 1] < num[i]) {
+      num.splice(i, 1);
+    } else {
+      num.splice(i - 1, 1);
+    }
+  }
+
+  if (!num.some((x) => x != 0)) return "0"; // only zeros left
+
+  const idx = num.findIndex((x) => x > 0);
+  if (idx > 0) num.splice(0, idx); // remove leading zeros
+  return num.join("");
+};
+
+console.log(removeKdigits("1432219", 3)); //  "1219"
+console.log(removeKdigits("10200", 1)); //  "200"
+console.log(removeKdigits("10", 2)); //  "0
+console.log(removeKdigits("100", 1)); //  "0
+
+var topVotedRemoveKdigits = function (num, k) {
+  const stack = [];
+
+  let removed = 0;
+  for (let n of num) {
+    while (stack.length && n < stack[stack.length - 1] && removed < k) {
+      stack.pop();
+      removed += 1;
+    }
+    stack.push(n);
+  }
+
+  // remove all remaining large numbers
+  while (removed < k) {
+    stack.pop();
+    removed += 1;
+  }
+
+  // remove all beginning zeroes
+  while (stack.length && stack[0] === "0") {
+    stack.shift();
+  }
+
+  return stack.length ? stack.join("") : "0";
 };
