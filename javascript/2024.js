28cc90978ead986bb7c38cbc7aea8d5810231414
MCCP96 <petermccomb@live.com>
2024-08-11 10:30:09 -0400

    Arithmetic Slices

--- javascript/2024.js
+++ javascript/2024.js
@@ -8255,7 +8255,7 @@ var topVotedRemoveKdigits = function (num, k) {
 }; */
 
 // Find the Winning Player in Coin Game					8/10/2024
-
+/* 
 // https://leetcode.com/problems/find-the-winning-player-in-coin-game/description/
 
 // You are given two positive integers x and y, denoting the number of coins with values 75 and 10 respectively.
@@ -8298,4 +8298,83 @@ console.log(losingPlayer(4, 11)); //  "Bob"
 const topVotedLosingPlayer = (x, y) => {
   const turns = Math.min(x, Math.floor(y / 4));
   return turns % 2 ? "Alice" : "Bob";
+}; */
+
+// Arithmetic Slices					8/11/2024
+
+// https://leetcode.com/problems/arithmetic-slices/description/
+
+// An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+// For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+
+// Given an integer array nums, return the number of arithmetic subarrays of nums.
+
+// A subarray is a contiguous subsequence of the array.
+
+// Example 1:
+// 		Input: nums = [1,2,3,4]
+// 		Output: 3
+// Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and [1,2,3,4] itself.
+
+// Example 2:
+// 		Input: nums = [1]
+// 		Output: 0
+
+// Constraints:
+//		1 <= nums.length <= 5000
+//		-1000 <= nums[i] <= 1000
+
+const numberOfArithmeticSlices = (nums) => {
+  const n = nums.length;
+  if (n < 3) return 0;
+
+  let count = 0;
+  const dfs = (prevIdx, i, dif) => {
+    while (i < n) {
+      if (nums[prevIdx] - nums[i++] == dif) {
+        count++;
+        dfs(i - 1, i, dif);
+      }
+    }
+  };
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      dfs(j, j + 1, nums[i] - nums[j]);
+    }
+  }
+
+  return count;
+};
+
+console.log(numberOfArithmeticSlices([1, 2, 3, 4])); //  3
+console.log(numberOfArithmeticSlices([1])); //  0
+
+var topVotedNumberOfArithmeticSlices = function (A) {
+  let sum = 0,
+    dp = Array(A.length).fill(0);
+
+  for (var i = 2; i <= dp.length - 1; i++) {
+    if (A[i] - A[i - 1] === A[i - 1] - A[i - 2]) {
+      dp[i] = 1 + dp[i - 1];
+      sum += dp[i];
+    }
+  }
+
+  return sum;
+};
+
+const revisedNumberOfArithmeticSlices = (nums) => {
+  const n = nums.length;
+  let res = 0;
+  let dp = new Array(n).fill(0);
+
+  for (let i = 2; i < n; i++) {
+    if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+      dp[i] = 1 + dp[i - 1];
+      res += dp[i];
+    }
+  }
+
+  return res;
 };
