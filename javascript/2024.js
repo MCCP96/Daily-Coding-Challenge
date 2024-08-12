4470b6626022413501287d098a400f8e7a193913
MCCP96 <petermccomb@live.com>
2024-08-12 11:32:13 -0400

    Number of Bit Changes to Make Two Integers Equal

--- javascript/2024.js
+++ javascript/2024.js
@@ -8301,7 +8301,7 @@ const topVotedLosingPlayer = (x, y) => {
 }; */
 
 // Arithmetic Slices					8/11/2024
-
+/* 
 // https://leetcode.com/problems/arithmetic-slices/description/
 
 // An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
@@ -8376,5 +8376,77 @@ const revisedNumberOfArithmeticSlices = (nums) => {
     }
   }
 
+  return res;
+}; */
+
+// Number of Bit Changes to Make Two Integers Equal					8/12/2024
+
+// https://leetcode.com/problems/number-of-bit-changes-to-make-two-integers-equal/description/
+
+// You are given two positive integers n and k.
+
+// You can choose any bit in the binary representation of n that is equal to 1 and change it to 0.
+
+// Return the number of changes needed to make n equal to k. If it is impossible, return -1.
+
+// Example 1:
+// 		Input: n = 13, k = 4
+// 		Output: 2
+// Explanation:
+// 		Initially, the binary representations of n and k are n = (1101)2 and k = (0100)2.
+// 		We can change the first and fourth bits of n. The resulting integer is n = (0100)2 = k.
+
+// Example 2:
+// 		Input: n = 21, k = 21
+// 		Output: 0
+// Explanation:
+// 		n and k are already equal, so no changes are needed.
+
+// Example 3:
+// 		Input: n = 14, k = 13
+// 		Output: -1
+// Explanation:
+// 		It is not possible to make n equal to k.
+
+// Constraints:
+//		1 <= n, k <= 106
+
+const minChanges = (n, k) => {
+  let res = 0;
+  while (n > 0 || k > 0) {
+    const nbit = n % 2;
+    const kbit = k % 2;
+
+    if (kbit == 1 && nbit == 0) return -1;
+    else if (nbit == 1 && kbit == 0) res++;
+
+    n >>= 1;
+    k >>= 1;
+  }
+  return res;
+};
+
+console.log(minChanges(13, 4)); //  2
+console.log(minChanges(21, 21)); //  0
+console.log(minChanges(14, 13)); //  -1
+
+var TopVotedMinChange = function (n, k) {
+  // Step 1: Check if it's possible to convert n to k
+  if ((n & k) !== k) {
+    return -1;
+  }
+
+  // Step 2: Count the number of 1 bits in n and k
+  let res = 0;
+
+  for (let c of n.toString(2)) {
+    res += parseInt(c);
+  }
+
+  for (let c of k.toString(2)) {
+    res -= parseInt(c);
+  }
+
+  // Step 3: Return the number of changes needed
   return res;
 };
