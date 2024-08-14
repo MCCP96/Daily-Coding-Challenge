b6eb776d8e867c07af8e5e9f0d65652c15ac1e1d
MCCP96 <petermccomb@live.com>
2024-08-14 09:52:38 -0400

    Wiggle Subsequence

--- javascript/2024.js
+++ javascript/2024.js
@@ -8452,7 +8452,7 @@ var TopVotedMinChange = function (n, k) {
 }; */
 
 // Find All Duplicates in an Array					8/13/2024
-
+/* 
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
 
 // Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
@@ -8502,4 +8502,59 @@ var topVotedFindDuplicates = function (nums) {
     nums[x - 1] *= -1;
   }
   return ans;
+}; */
+
+// Wiggle Subsequence					8/14/2024
+
+// https://leetcode.com/problems/wiggle-subsequence/
+
+// A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
+
+// For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) alternate between positive and negative.
+
+// In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences. The first is not because its first two differences are positive, and the second is not because its last difference is zero.
+
+// A subsequence is obtained by deleting some elements (possibly zero) from the original sequence, leaving the remaining elements in their original order.
+
+// Given an integer array nums, return the length of the longest wiggle subsequence of nums.
+
+// Example 1:
+// 		Input: nums = [1,7,4,9,2,5]
+// 		Output: 6
+// Explanation: The entire sequence is a wiggle sequence with differences (6, -3, 5, -7, 3).
+
+// Example 2:
+// 		Input: nums = [1,17,5,10,13,15,10,5,16,8]
+// 		Output: 7
+// Explanation: There are several subsequences that achieve this length.
+// 		One is [1, 17, 10, 13, 10, 16, 8] with differences (16, -7, 3, -3, 6, -8).
+
+// Example 3:
+// 		Input: nums = [1,2,3,4,5,6,7,8,9]
+// 		Output: 2
+
+// Constraints:
+//		1 <= nums.length <= 1000
+//		0 <= nums[i] <= 1000
+
+// Follow up: Could you solve this in O(n) time?
+
+const wiggleMaxLength = (nums) => {
+  const n = nums.length;
+  let [pos, neg] = [1, 1];
+
+  for (let i = 1; i < n; i++) {
+    const dif = nums[i] - nums[i - 1];
+    if (dif > 0) pos = neg + 1;
+    else if (dif < 0) neg = pos + 1;
+  }
+
+  return Math.max(pos, neg);
 };
+
+console.log(wiggleMaxLength([1, 7, 4, 9, 2, 5])); //  6
+console.log(wiggleMaxLength([1, 17, 5, 10, 13, 15, 10, 5, 16, 8])); //  7
+console.log(wiggleMaxLength([1, 2, 3, 4, 5, 6, 7, 8, 9])); //  2
+
+// first thought dp, but was too slow
+// same as top voted
