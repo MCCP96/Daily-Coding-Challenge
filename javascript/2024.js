3348ab227b1946019dede34ad163d6415341122f
MCCP96 <petermccomb@live.com>
2024-08-22 06:46:49 -0400

    Find if Digit Game Can Be Won

--- javascript/2024.js
+++ javascript/2024.js
@@ -9066,7 +9066,7 @@ var topVotedMinimumSubstringsInPartition = function (S) {
 }; */
 
 // Taking Maximum Energy From the Mystic Dungeon					8/21/2024
-
+/* 
 // https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/
 
 // In a mystic dungeon, n magicians are standing in a line. Each magician has an attribute that gives you energy. Some magicians can give you negative energy, which means taking energy from you.
@@ -9131,4 +9131,46 @@ let topVotedMaximumEnergy = (energy, k) => {
     energy[i] += energy[i + k];
   }
   return Math.max(...energy);
+}; */
+
+// Find if Digit Game Can Be Won					8/22/2024
+
+// https://leetcode.com/problems/find-if-digit-game-can-be-won/description/
+
+// You are given an array of positive integers nums.
+
+// Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
+
+// Return true if Alice can win this game, otherwise, return false.
+
+// Example 1:
+// 		Input: nums = [1,2,3,4,10]
+// 		Output: false
+// Explanation:
+// 		Alice cannot win by choosing either single-digit or double-digit numbers.
+
+// Example 2:
+// 		Input: nums = [1,2,3,4,5,14]
+// 		Output: true
+// Explanation:
+// 		Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+// Example 3:
+// 		Input: nums = [5,5,5,25]
+// 		Output: true
+// Explanation:
+// 		Alice can win by choosing double-digit numbers which have a sum equal to 25.
+
+// Constraints:
+//		1 <= nums.length <= 100
+//		1 <= nums[i] <= 99
+
+const canAliceWin = (nums) => {
+  let [a, b] = [0, 0];
+  for (const n of nums) n < 10 ? (a += n) : (b += n);
+  return a != b;
 };
+
+console.log(canAliceWin([1, 2, 3, 4, 10])); //  false
+console.log(canAliceWin([1, 2, 3, 4, 5, 14])); //  true
+console.log(canAliceWin([5, 5, 5, 25])); //  true
