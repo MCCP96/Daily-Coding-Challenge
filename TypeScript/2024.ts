2d942f01078aa2549efa950010d965f6c5e15431
MCCP96 <petermccomb@live.com>
2024-09-15 16:43:37 -0400

    Remove Element

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -700,7 +700,7 @@ function topVotedDoubleIt(head: ListNode | null): ListNode | null {
 // recursion saves the num array */
 
 // Length of Last Word					9/14/2024
-
+/* 
 // https://leetcode.com/problems/length-of-last-word/description/
 
 //Given a string s consisting of words and spaces, return the length of the last word in the string.
@@ -740,4 +740,45 @@ const lengthOfLastWord = (s: string): number => {
 
 console.log(lengthOfLastWord("Hello World")); //  5
 console.log(lengthOfLastWord("   fly me   to   the moon  ")); //  4
-console.log(lengthOfLastWord("luffy is still joyboy")); //  6
+console.log(lengthOfLastWord("luffy is still joyboy")); //  6 */
+
+// Remove Element					9/15/2024
+
+// https://leetcode.com/problems/remove-element/description/
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+
+// Return k.
+
+// Example 1:
+// 		Input: nums = [3,2,2,3], val = 3
+// 		Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// 		It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Example 2:
+// 		Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// 		Output: 5, nums = [0,1,4,0,3,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+// 		Note that the five elements can be returned in any order.
+// 		It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Constraints:
+//		0 <= nums.length <= 100
+//		0 <= nums[i] <= 50
+//		0 <= val <= 100
+
+function removeElement(nums: number[], val: number): number {
+  let i = 0;
+  for (const n of nums) {
+    if (n != val) nums[i++] = n;
+  }
+  return i + 1;
+}
+
+console.log(removeElement([3, 2, 2, 3], 3)); //  2, nums = [2,2,_,_]
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); //  5, nums = [0,1,4,0,3,_,_,_]
