ec4f7e9d4441d2b2ffe07c9ee60709a61d3fb4a2
MCCP96 <petermccomb@live.com>
2024-09-23 07:08:27 -0400

    Combination Sum III

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -1395,7 +1395,7 @@ console.log(
 // same as top voted */
 
 // Minimum Flips to Make a OR b Equal to c					9/22/2024
-
+/* 
 // https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/description/
 
 // Given 3 positives numbers a, b and c. Return the minimum flips required in some bits of a and b to make ( a OR b == c ). (bitwise OR operation).
@@ -1465,4 +1465,88 @@ function topVotedMinFlips(a: number, b: number, c: number): number {
     check *= 2;
   }
   return result;
+} */
+
+// Combination Sum III					9/23/2024
+
+// https://leetcode.com/problems/combination-sum-iii/description/
+
+// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+// - Only numbers 1 through 9 are used.
+// - Each number is used at most once.
+
+// Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+// Example 1:
+// 		Input: k = 3, n = 7
+// 		Output: [[1,2,4]]
+// Explanation:
+// 		1 + 2 + 4 = 7
+// 		There are no other valid combinations.
+
+// Example 2:
+// 		Input: k = 3, n = 9
+// 		Output: [[1,2,6],[1,3,5],[2,3,4]]
+// Explanation:
+// 		1 + 2 + 6 = 9
+// 		1 + 3 + 5 = 9
+// 		2 + 3 + 4 = 9
+// 		There are no other valid combinations.
+
+// Example 3:
+// 		Input: k = 4, n = 1
+// 		Output: []
+// Explanation: There are no valid combinations.
+// 		Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+
+// Constraints:
+//		2 <= k <= 9
+//		1 <= n <= 60
+
+const combinationSum3 = (k: number, n: number): number[][] => {
+  let min = 0;
+  for (let i = 1; i <= k; i++) min += i;
+  if (min > n) return [];
+
+  let res: number[][] = [];
+  const dfs = (i: number, sum: number, count: number, arr: number[]) => {
+    if (sum > n || count > k) return;
+    if (sum == n && count == k) {
+      res.push(arr);
+      return;
+    }
+    while (++i <= 9) {
+      dfs(i, sum + i, count + 1, [...arr, i]);
+    }
+  };
+  dfs(0, 0, 0, []);
+
+  return res;
+};
+
+console.log(combinationSum3(3, 7)); //  [[1,2,4]]
+console.log(combinationSum3(3, 9)); //  [[1,2,6],[1,3,5],[2,3,4]]
+console.log(combinationSum3(4, 1)); //  []
+
+function topVotedCombinationSum3(k: number, n: number): number[][] {
+  const ans: number[][] = [];
+  const t: number[] = [];
+  const dfs = (i: number, s: number) => {
+    if (s === 0) {
+      if (t.length === k) {
+        ans.push(t.slice());
+      }
+      return;
+    }
+    if (i > 9 || i > s || t.length >= k) {
+      return;
+    }
+    for (let j = i; j <= 9; j++) {
+      t.push(j);
+      dfs(j + 1, s - j);
+      t.pop();
+    }
+  };
+  dfs(1, n);
+  return ans;
 }
