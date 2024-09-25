00f7889fb749450c8273acb9bf60894d11d42429
MCCP96 <petermccomb@live.com>
2024-09-25 08:05:12 -0400

    Queue Reconstruction by Height

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -1552,7 +1552,7 @@ function topVotedCombinationSum3(k: number, n: number): number[][] {
 } */
 
 // Count Number of Teams					9/24/2024
-
+/* 
 // https://leetcode.com/problems/count-number-of-teams/description/
 
 // There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
@@ -1634,4 +1634,92 @@ const numTeams = (rating: number[]): number => {
 
 console.log(numTeams([2, 5, 3, 4, 1])); //  3
 console.log(numTeams([2, 1, 3])); //  0
-console.log(numTeams([1, 2, 3, 4])); //  4
+console.log(numTeams([1, 2, 3, 4])); //  4 */
+
+// Queue Reconstruction by Height					9/25/2024
+
+// https://leetcode.com/problems/queue-reconstruction-by-height/description/
+
+// You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
+
+// Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+
+// Example 1:
+// 		Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+// 		Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+// Explanation:
+// 		Person 0 has height 5 with no other people taller or the same height in front.
+// 		Person 1 has height 7 with no other people taller or the same height in front.
+// 		Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+// 		Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+// 		Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+// 		Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+// 		Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
+
+// Example 2:
+// 		Input: people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+// 		Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+
+// Constraints:
+//		1 <= people.length <= 2000
+//		0 <= hi <= 106
+//		0 <= ki < people.length
+//		It is guaranteed that the queue can be reconstructed.
+
+const reconstructQueue = (people: number[][]): number[][] => {
+  const n = people.length;
+
+  let inFrontsMap = new Map<number, number[]>();
+  for (const [h, k] of people) {
+    if (inFrontsMap.has(k)) {
+      inFrontsMap.set(k, [...inFrontsMap.get(k)!, h]);
+    } else {
+      inFrontsMap.set(k, [h]);
+    }
+  }
+
+  return [...inFrontsMap.entries()]
+    .sort()
+    .map(([k, hArr]) => hArr.sort().map((h) => [h, k]))
+    .flat();
+};
+
+console.log(
+  reconstructQueue([
+    [7, 0],
+    [4, 4],
+    [7, 1],
+    [5, 0],
+    [6, 1],
+    [5, 2],
+  ])
+); //  [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+console.log(
+  reconstructQueue([
+    [6, 0],
+    [5, 0],
+    [4, 0],
+    [3, 2],
+    [2, 2],
+    [1, 4],
+  ])
+); //  [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+
+// missed constraint: "'exactly' ki other people in front"
+// instead did 'at most' ki people
+
+function topVotedReconstructQueue(people: number[][]): number[][] {
+  people = people.sort((a, b) => {
+    if (a[0] !== b[0]) {
+      return b[0] - a[0];
+    } else {
+      return a[1] - b[1];
+    }
+  });
+  let result: number[][] = [];
+  for (let person of people) {
+    result.splice(person[1], 0, person);
+  }
+
+  return result;
+}
