97cdf793926f9593a44e48da05d63434ef1d40e9
MCCP96 <petermccomb@live.com>
2024-10-03 07:50:25 -0400

    Append Characters to String to Make Subsequence

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -2030,7 +2030,7 @@ console.log(itr.next()); // "bc"
 console.log(itr.hasNext()); // False */
 
 // Average Waiting Time					10/2/2024
-
+/* 
 // https://leetcode.com/problems/average-waiting-time/
 
 // There is a restaurant with a single chef. You are given an array customers, where customers[i] = [arrivali, timei]:
@@ -2109,4 +2109,54 @@ function topVotedAverageWaitingTime(customers: number[][]): number {
     waitingTime += cookTime - arrival;
   }
   return waitingTime / customers.length;
-}
+} */
+
+// Append Characters to String to Make Subsequence					10/3/2024
+
+// https://leetcode.com/problems/append-characters-to-string-to-make-subsequence
+
+// You are given two strings s and t consisting of only lowercase English letters.
+
+// Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+
+// A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+// Example 1:
+// 		Input: s = "coaching", t = "coding"
+// 		Output: 4
+// Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+// 		Now, t is a subsequence of s ("coachingding").
+// 		It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+
+// Example 2:
+// 		Input: s = "abcde", t = "a"
+// 		Output: 0
+// Explanation: t is already a subsequence of s ("abcde").
+
+// Example 3:
+// 		Input: s = "z", t = "abcde"
+// 		Output: 5
+// Explanation: Append the characters "abcde" to the end of s so that s = "zabcde".
+// 		Now, t is a subsequence of s ("zabcde").
+// 		It can be shown that appending any 4 characters to the end of s will never make t a subsequence.
+
+// Constraints:
+//		1 <= s.length, t.length <= 105
+//		s and t consist only of lowercase English letters.
+
+const appendCharacters = (s: string, t: string): number => {
+  let i = 0;
+  for (const c of s) {
+    if (c == t[i]) i++;
+  }
+  return t.length - i;
+};
+
+console.log(appendCharacters("coaching", "coding")); //  4
+console.log(appendCharacters("abcde", "a")); //  0
+console.log(appendCharacters("z", "abcde")); //  5
+
+// this shouldn't be a medium
+
+const oneLineAppendCharacters = (s: string, t: string): number =>
+  t.length - [...s].reduce((i, c) => (i += +(c == t[i])), 0);
