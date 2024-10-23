59dec4122511bb11a4fe5422fedbd73947100ec0
MCCP96 <petermccomb@live.com>
2024-10-23 08:42:12 -0400

    Find the Longest Substring Containing Vowels in Even Counts

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -2975,7 +2975,7 @@ function topVotedXorQueries(arr: number[], queries: number[][]): number[] {
 } */
 
 // Letter Tile Possibilities					10/22/2024
-
+/* 
 // https://leetcode.com/problems/letter-tile-possibilities/description/
 
 // You have n  tiles, where each tile has one letter tiles[i] printed on it.
@@ -3021,4 +3021,64 @@ console.log(numTilePossibilities("AAABBC")); //  188
 console.log(numTilePossibilities("V")); //  1
 
 // surely a math solution to this
-// still good recursion practice
+// still good recursion practice */
+
+// Find the Longest Substring Containing Vowels in Even Counts					10/23/2024
+
+// https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/description/
+
+// Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
+
+// Example 1:
+// 		Input: s = "eleetminicoworoep"
+// 		Output: 13
+// Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
+
+// Example 2:
+// 		Input: s = "leetcodeisgreat"
+// 		Output: 5
+// Explanation: The longest substring is "leetc" which contains two e's.
+
+// Example 3:
+// 		Input: s = "bcbcbc"
+// 		Output: 6
+// Explanation: In this case, the given string "bcbcbc" is the longest because all vowels: a, e, i, o and u appear zero times.
+
+// Constraints:
+//		1 <= s.length <= 5 x 10^5
+//		s contains only lowercase English letters.
+
+function topVotedFindTheLongestSubstring(s: string): number {
+  let bitmask = 0;
+  let maxLength = 0;
+  const bitmaskFirstOccurrence = new Map<number, number>();
+  bitmaskFirstOccurrence.set(0, -1);
+
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    if (ch === "a") {
+      bitmask ^= 1 << 0;
+    } else if (ch === "e") {
+      bitmask ^= 1 << 1;
+    } else if (ch === "i") {
+      bitmask ^= 1 << 2;
+    } else if (ch === "o") {
+      bitmask ^= 1 << 3;
+    } else if (ch === "u") {
+      bitmask ^= 1 << 4;
+    }
+
+    if (bitmaskFirstOccurrence.has(bitmask)) {
+      const length = i - bitmaskFirstOccurrence.get(bitmask)!;
+      maxLength = Math.max(maxLength, length);
+    } else {
+      bitmaskFirstOccurrence.set(bitmask, i);
+    }
+  }
+
+  return maxLength;
+}
+
+console.log(topVotedFindTheLongestSubstring("eleetminicoworoep")); //  13
+console.log(topVotedFindTheLongestSubstring("leetcodeisgreat")); //  5
+console.log(topVotedFindTheLongestSubstring("bcbcbc")); //  6
