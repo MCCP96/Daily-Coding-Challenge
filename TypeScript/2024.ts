aa0337c57c60fe3a10d0b2059ef007967396a62b
MCCP96 <petermccomb@live.com>
2024-10-25 10:49:19 -0400

    Display Table of Food Orders in a Restaurant

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -3084,7 +3084,7 @@ console.log(topVotedFindTheLongestSubstring("leetcodeisgreat")); //  5
 console.log(topVotedFindTheLongestSubstring("bcbcbc")); //  6 */
 
 // Matrix Block Sum					10/24/2024
-
+/* 
 // https://leetcode.com/problems/matrix-block-sum/description/
 
 // Given a m x n matrix mat and an integer k, return a matrix answer where each answer[i][j] is the sum of all elements mat[r][c] for:
@@ -3183,4 +3183,174 @@ function topVotedMatrixBlockSum(mat: number[][], K: number): number[][] {
   }
 
   return mat;
+} */
+
+// Display Table of Food Orders in a Restaurant					10/25/2024
+
+// https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/description/
+
+// Given the array orders, which represents the orders that customers have done in a restaurant. More specifically orders[i]=[customerNamei,tableNumberi,foodItemi] where customerNamei is the name of the customer, tableNumberi is the table customer sit at, and foodItemi is the item customer orders.
+
+// Return the restaurant's “display table”. The “display table” is a table whose row entries denote how many of each food item each table ordered. The first column is the table number and the remaining columns correspond to each food item in alphabetical order. The first row should be a header whose first column is “Table”, followed by the names of the food items. Note that the customer names are not part of the table. Additionally, the rows should be sorted in numerically increasing order.
+
+// Example 1:
+// 		Input: orders = [["David","3","Ceviche"],["Corina","10","Beef Burrito"],["David","3","Fried Chicken"],["Carla","5","Water"],["Carla","5","Ceviche"],["Rous","3","Ceviche"]]
+// 		Output: [["Table","Beef Burrito","Ceviche","Fried Chicken","Water"],["3","0","2","1","0"],["5","0","1","0","1"],["10","1","0","0","0"]]
+// Explanation:
+// 		The displaying table looks like:
+// 		Table,Beef Burrito,Ceviche,Fried Chicken,Water
+// 		3    ,0           ,2      ,1            ,0
+// 		5    ,0           ,1      ,0            ,1
+// 		10   ,1           ,0      ,0            ,0
+// 		For the table 3: David orders "Ceviche" and "Fried Chicken", and Rous orders "Ceviche".
+// 		For the table 5: Carla orders "Water" and "Ceviche".
+// 		For the table 10: Corina orders "Beef Burrito".
+
+// Example 2:
+// 		Input: orders = [["James","12","Fried Chicken"],["Ratesh","12","Fried Chicken"],["Amadeus","12","Fried Chicken"],["Adam","1","Canadian Waffles"],["Brianna","1","Canadian Waffles"]]
+// 		Output: [["Table","Canadian Waffles","Fried Chicken"],["1","2","0"],["12","0","3"]]
+// Explanation:
+// 		For the table 1: Adam and Brianna order "Canadian Waffles".
+// 		For the table 12: James, Ratesh and Amadeus order "Fried Chicken".
+
+// Example 3:
+// 		Input: orders = [["Laura","2","Bean Burrito"],["Jhon","2","Beef Burrito"],["Melissa","2","Soda"]]
+// 		Output: [["Table","Bean Burrito","Beef Burrito","Soda"],["2","1","1","1"]]
+
+// Constraints:
+//		1 <= orders.length <= 5 * 10^4
+//		orders[i].length == 3
+//		1 <= customerNamei.length, foodItemi.length <= 20
+//		customerNamei and foodItemi consist of lowercase and uppercase English letters and the space character.
+//		tableNumberi is a valid integer between 1 and 500.
+
+const displayTable = (orders: string[][]): string[][] => {
+  // orders[i]=[customerNamei,tableNumberi,foodItemi]
+  let history = new Map<number, Map<string, number>>();
+  let foodItems = new Set<string>();
+
+  // track orders
+  for (const [_, tableNumStr, foodItem] of orders) {
+    const tableNum = +tableNumStr; // sorting purposes
+    if (!history.has(tableNum)) {
+      history.set(tableNum, new Map<string, number>());
+    }
+
+    // track count of food item per table
+    const curTable = history.get(tableNum)!;
+    if (!curTable.has(foodItem)) {
+      curTable.set(foodItem, 0);
+    }
+    curTable.set(foodItem, curTable.get(foodItem)! + 1);
+
+    foodItems.add(foodItem); // count distinct food items
+  }
+
+  let res = new Array(history.size + 1)
+    .fill(-1)
+    .map((_) => new Array(foodItems.size + 1));
+
+  // header row
+  const sortedFoodItems = [...foodItems.entries()].sort();
+  res[0][0] = "Table";
+  let i = 1;
+  for (const [foodItem] of sortedFoodItems) {
+    res[0][i++] = foodItem;
+  }
+
+  // table row data
+  const sortedTableNums = [...history.keys()].sort((a, b) => a - b);
+  let row = 1;
+  for (const tableNum of sortedTableNums) {
+    res[row][0] = tableNum + "";
+
+    const curTable = history.get(tableNum)!;
+    let i = 1;
+    for (const [foodItem] of sortedFoodItems) {
+      if (curTable.has(foodItem)) {
+        res[row][i] = curTable.get(foodItem) + "";
+      } else {
+        res[row][i] = "0";
+      }
+      i++;
+    }
+    row++;
+  }
+
+  return res;
+};
+
+console.log(
+  displayTable([
+    ["David", "3", "Ceviche"],
+    ["Corina", "10", "Beef Burrito"],
+    ["David", "3", "Fried Chicken"],
+    ["Carla", "5", "Water"],
+    ["Carla", "5", "Ceviche"],
+    ["Rous", "3", "Ceviche"],
+  ])
+); //  [["Table","Beef Burrito","Ceviche","Fried Chicken","Water"],["3","0","2","1","0"],["5","0","1","0","1"],["10","1","0","0","0"]]
+console.log(
+  displayTable([
+    ["James", "12", "Fried Chicken"],
+    ["Ratesh", "12", "Fried Chicken"],
+    ["Amadeus", "12", "Fried Chicken"],
+    ["Adam", "1", "Canadian Waffles"],
+    ["Brianna", "1", "Canadian Waffles"],
+  ])
+); //  [["Table","Canadian Waffles","Fried Chicken"],["1","2","0"],["12","0","3"]]
+console.log(
+  displayTable([
+    ["Laura", "2", "Bean Burrito"],
+    ["Jhon", "2", "Beef Burrito"],
+    ["Melissa", "2", "Soda"],
+  ])
+); //  [["Table","Bean Burrito","Beef Burrito","Soda"],["2","1","1","1"]]
+
+function topVotedDisplayTable(orders: string[][]): string[][] {
+  const ordersLength = orders.length;
+  let columnNames: string[] = [];
+
+  for (let i = 0; i < ordersLength; i++) {
+    columnNames.push(orders[i][2]);
+  }
+
+  // Removing duplicates and ordering columns
+  columnNames = Array.from(new Set(columnNames));
+  columnNames.sort();
+
+  const columnNamesMap: Record<string, number> = {};
+
+  // Map tables index
+  columnNames.forEach((column, index) => (columnNamesMap[column] = index + 1));
+  columnNames.unshift("Table");
+
+  const columnNamesLength = columnNames.length;
+
+  const tablesMap: Record<string, string[]> = {};
+
+  // Creating table arrays
+  for (let i = 0; i < ordersLength; i++) {
+    const tableNumber = orders[i][1];
+    const index = columnNamesMap[orders[i][2]];
+
+    if (!!tablesMap[tableNumber]) {
+      tablesMap[tableNumber][index] = (
+        Number(tablesMap[tableNumber][index]) + 1
+      ).toString();
+    } else {
+      tablesMap[tableNumber] = new Array(columnNamesLength).fill("0");
+      tablesMap[tableNumber][0] = tableNumber;
+      tablesMap[tableNumber][index] = (
+        Number(tablesMap[tableNumber][index]) + 1
+      ).toString();
+    }
+  }
+
+  // Sort table by number
+  const tableOrders = Object.values(tablesMap)
+    .map((x) => x)
+    .sort((a, b) => Number(a[0]) - Number(b[0]));
+
+  return [columnNames, ...tableOrders];
 }
