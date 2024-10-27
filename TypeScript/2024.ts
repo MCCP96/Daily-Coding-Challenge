37f5191568e9dfd52212e2d9fd1a17a5cad37e8c
MCCP96 <petermccomb@live.com>
2024-10-27 11:20:22 -0400

    Restore the Array From Adjacent Pairs

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -3356,7 +3356,7 @@ function topVotedDisplayTable(orders: string[][]): string[][] {
 } */
 
 // Remove Sub-Folders from the Filesystem					10/26/2024
-
+/* 
 // https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/description/
 
 // Given a list of folders folder, return the folders after removing all sub-folders in those folders. You may return the answer in any order.
@@ -3451,4 +3451,95 @@ const revisedRemoveSubfolders = (folder: string[]): string[] => {
   }
 
   return parents;
+}; */
+
+// Restore the Array From Adjacent Pairs					10/27/2024
+
+// https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/
+
+// There is an integer array nums that consists of n unique elements, but you have forgotten it. However, you do remember every pair of adjacent elements in nums.
+
+// You are given a 2D integer array adjacentPairs of size n - 1 where each adjacentPairs[i] = [ui, vi] indicates that the elements ui and vi are adjacent in nums.
+
+// It is guaranteed that every adjacent pair of elements nums[i] and nums[i+1] will exist in adjacentPairs, either as [nums[i], nums[i+1]] or [nums[i+1], nums[i]]. The pairs can appear in any order.
+
+// Return the original array nums. If there are multiple solutions, return any of them.
+
+// Example 1:
+// 		Input: adjacentPairs = [[2,1],[3,4],[3,2]]
+// 		Output: [1,2,3,4]
+// Explanation: This array has all its adjacent pairs in adjacentPairs.
+// 		Notice that adjacentPairs[i] may not be in left-to-right order.
+
+// Example 2:
+// 		Input: adjacentPairs = [[4,-2],[1,4],[-3,1]]
+// 		Output: [-2,4,1,-3]
+// Explanation: There can be negative numbers.
+// 		Another solution is [-3,1,4,-2], which would also be accepted.
+
+// Example 3:
+// 		Input: adjacentPairs = [[100000,-100000]]
+// 		Output: [100000,-100000]
+
+// Constraints:
+//		nums.length == n
+//		adjacentPairs.length == n - 1
+//		adjacentPairs[i].length == 2
+//		2 <= n <= 105
+//		-105 <= nums[i], ui, vi <= 105
+//		There exists some nums that has adjacentPairs as its pairs.
+
+const restoreArray = (pairs: number[][]): number[] => {
+  // map out adjacents for an element
+  let map = new Map<number, Set<number>>();
+  for (const [a, b] of pairs) {
+    if (!map.has(a)) map.set(a, new Set());
+    if (!map.has(b)) map.set(b, new Set());
+    map.get(a)?.add(b);
+    map.get(b)?.add(a);
+  }
+
+  // find start
+  let leading: number;
+  for (const [num, adj] of map) {
+    if (adj.size == 1) {
+      leading = num; // only 1 adjacent, therefore leading
+      break;
+    }
+  }
+
+  // setup first entry
+  let res = new Array<number>(map.size);
+  let i = 0;
+  res[i++] = leading!;
+
+  // add adjacents following previous element
+  let prev = res[0];
+  while (map.size > 1) {
+    const adj = map.get(prev); // adjacents based on prev
+    const cur = [...adj?.values()!][0]; // always only 1 adjacent
+    res[i++] = cur;
+
+    map.delete(prev); // remove prev, no more adjacents
+    map.get(cur)?.delete(prev); // remove prev from cur, only 1 adj left
+    prev = cur;
+  }
+
+  return res;
 };
+
+console.log(
+  restoreArray([
+    [2, 1],
+    [3, 4],
+    [3, 2],
+  ])
+); //  [1,2,3,4]
+console.log(
+  restoreArray([
+    [4, -2],
+    [1, 4],
+    [-3, 1],
+  ])
+); //  [-2,4,1,-3]
+console.log(restoreArray([[100000, -100000]])); //  [100000,-100000]
