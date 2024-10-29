14978cf1294b6ae6f43895c16e27c652b098f198
MCCP96 <petermccomb@live.com>
2024-10-29 07:36:57 -0400

    Find the Sequence of Strings Appeared on the Screen

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -3545,7 +3545,7 @@ console.log(
 console.log(restoreArray([[100000, -100000]])); //  [100000,-100000] */
 
 // Design Underground System					10/28/2024
-
+/* 
 // https://leetcode.com/problems/design-underground-system/description/
 
 // An underground railway system is keeping track of customer travel times between different stations. They are using this data to calculate the average time it takes to travel from one station to another.
@@ -3668,4 +3668,70 @@ class TopVotedUndergroundSystem {
   }
 }
 
-// same same
+// same same */
+
+// Find the Sequence of Strings Appeared on the Screen					10/29/2024
+
+// https://leetcode.com/problems/find-the-sequence-of-strings-appeared-on-the-screen/description/
+
+// You are given a string target.
+
+// Alice is going to type target on her computer using a special keyboard that has only two keys:
+// - Key 1 appends the character "a" to the string on the screen.
+// - Key 2 changes the last character of the string on the screen to its next character in the English alphabet. For example, "c" changes to "d" and "z" changes to "a".
+
+// Note that initially there is an empty string "" on the screen, so she can only press key 1.
+
+// Return a list of all strings that appear on the screen as Alice types target, in the order they appear, using the minimum key presses.
+
+// Example 1:
+// 		Input: target = "abc"
+// 		Output: ["a","aa","ab","aba","abb","abc"]
+// Explanation:
+// 		The sequence of key presses done by Alice are:
+// 		Press key 1, and the string on the screen becomes "a".
+// 		Press key 1, and the string on the screen becomes "aa".
+// 		Press key 2, and the string on the screen becomes "ab".
+// 		Press key 1, and the string on the screen becomes "aba".
+// 		Press key 2, and the string on the screen becomes "abb".
+// 		Press key 2, and the string on the screen becomes "abc".
+
+// Example 2:
+// 		Input: target = "he"
+// 		Output: ["a","b","c","d","e","f","g","h","ha","hb","hc","hd","he"]
+
+// Constraints:
+//		1 <= target.length <= 400
+//		target consists only of lowercase English letters.
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+const stringSequence = (t: string): string[] => {
+  // convert to idxs for quicker ALPHABET lookup
+  let size = 0;
+  const idxs = [...t].map((c) => {
+    const idx = c.charCodeAt(0) - 97;
+    size += idx + 1; // calculate res array size
+    return idx;
+  });
+
+  // build res array
+  let res = new Array(size);
+  let pos = 0;
+  let prev = "";
+  for (const i of idxs) {
+    let j = 0;
+    while (j <= i) {
+      res[pos++] = prev + ALPHABET[j++];
+    }
+    prev = res[pos - 1];
+  }
+
+  return res;
+};
+
+console.log(stringSequence("abc")); //  ["a","aa","ab","aba","abb","abc"]
+console.log(stringSequence("he")); //  ["a","b","c","d","e","f","g","h","ha","hb","hc","hd","he"]
+
+// 5ms runtime
+// no other submitted TypeScript solutions
