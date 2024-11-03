7bb68908c7fb8895b4f022683e543fd86028541e
MCCP96 <petermccomb@live.com>
2024-11-03 09:53:21 -0500

    Sort the Matrix Diagonally

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -3808,7 +3808,7 @@ function topVotedRemoveLeafNodes(
 } */
 
 // Number of Employees Who Met the Target					11/2/2024
-
+/* 
 // https://leetcode.com/problems/number-of-employees-who-met-the-target/description/
 
 // There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
@@ -3844,4 +3844,85 @@ const numberOfEmployeesWhoMetTarget = (hours: number[], t: number): number =>
   hours.filter((x) => x >= t).length;
 
 console.log(numberOfEmployeesWhoMetTarget([0, 1, 2, 3, 4], 2)); //  3
-console.log(numberOfEmployeesWhoMetTarget([5, 1, 4, 2, 2], 6)); //  0
+console.log(numberOfEmployeesWhoMetTarget([5, 1, 4, 2, 2], 6)); //  0 */
+
+// Sort the Matrix Diagonally					11/3/2024
+
+// https://leetcode.com/problems/sort-the-matrix-diagonally/description/
+
+// A matrix diagonal is a diagonal line of cells starting from some cell in either the topmost row or leftmost column and going in the bottom-right direction until reaching the matrix's end. For example, the matrix diagonal starting from mat[2][0], where mat is a 6 x 3 matrix, includes cells mat[2][0], mat[3][1], and mat[4][2].
+
+// Given an m x n matrix mat of integers, sort each matrix diagonal in ascending order and return the resulting matrix.
+
+// Example 1:
+// 		Input: mat = [[3,3,1,1],[2,2,1,2],[1,1,1,2]]
+// 		Output: [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+
+// Example 2:
+// 		Input: mat = [[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]]
+// 		Output: [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]
+
+// Constraints:
+//		m == mat.length
+//		n == mat[i].length
+//		1 <= m, n <= 100
+//		1 <= mat[i][j] <= 100
+
+const diagonalSort = (mat: number[][]): number[][] => {
+  const n = mat.length;
+  const m = mat[0].length;
+
+  // start at bottom-left, move to top-right
+  let res = new Array(n).fill(0).map((_) => new Array(m));
+  let row = n - 1;
+  let col = 0;
+
+  while (row != 0 || col != m) {
+    // get nums
+    let diagonalNums = [];
+    let j = col;
+    for (let i = row; i < n && j < m; i++, j++) {
+      diagonalNums.push(mat[i][j]);
+    }
+
+    // sort
+    diagonalNums.sort((a, b) => a - b);
+    console.log(diagonalNums);
+
+    // place in res
+    let idx = 0;
+    j = col;
+    for (let i = row; i < n && j < m; i++, j++) {
+      res[i][j] = diagonalNums[idx++];
+    }
+
+    if (row == 0) col++; // move right, through cols
+    if (row != 0) row--; // move up rows
+  }
+
+  return res;
+};
+
+console.log(
+  diagonalSort([
+    [3, 3, 1, 1],
+    [2, 2, 1, 2],
+    [1, 1, 1, 2],
+  ])
+); //  [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+console.log(
+  diagonalSort([
+    [11, 25, 66, 1, 69, 7],
+    [23, 55, 17, 45, 15, 52],
+    [75, 31, 36, 44, 58, 8],
+    [22, 27, 33, 25, 68, 4],
+    [84, 28, 14, 11, 5, 50],
+  ])
+);
+//  [[5,17,4,1,52,7],
+//   [11,11,25,45,8,69],
+//   [14,23,25,44,58,15],
+//   [22,27,31,36,50,66],
+//   [84,28,75,33,55,68]]
+
+// 100% Runtime
