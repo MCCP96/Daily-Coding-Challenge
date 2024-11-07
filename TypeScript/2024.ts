054f9cf537668f6b044736fc761731b0a3989270
MCCP96 <petermccomb@live.com>
2024-11-07 08:16:47 -0500

    Merge In Between Linked Lists

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -4146,7 +4146,7 @@ const revisedFindMatrix = (nums: number[]): number[][] => {
 }; */
 
 // Delete Node in a Linked List					11/6/2024
-
+/* 
 // https://leetcode.com/problems/delete-node-in-a-linked-list/description/
 
 // There is a singly-linked list head and we want to delete a node node in it.
@@ -4203,4 +4203,71 @@ const revisedDeleteNode = (node: ListNode | null): void => {
 };
 
 // "The node to be deleted is in the list and is not a tail node"
-// somehow much slower
+// somehow much slower */
+
+// Merge In Between Linked Lists					11/7/2024
+
+// https://leetcode.com/problems/merge-in-between-linked-lists/description/
+
+// You are given two linked lists: list1 and list2 of sizes n and m respectively.
+
+// Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+
+// The blue edges and nodes in the following figure indicate the result:
+
+// Build the result list and return its head.
+
+// Example 1:
+// 		Input: list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+// 		Output: [10,1,13,1000000,1000001,1000002,5]
+// Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+
+// Example 2:
+// 		Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+// 		Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
+// Explanation: The blue edges and nodes in the above figure indicate the result.
+
+// Constraints:
+//		3 <= list1.length <= 104
+//		1 <= a <= b < list1.length - 1
+//		1 <= list2.length <= 104
+
+const mergeInBetween = (
+  list1: ListNode | null,
+  a: number,
+  b: number,
+  list2: ListNode | null
+): ListNode | null => {
+  let nodeA = list1;
+  for (let i = 1; i < a; i++) nodeA = nodeA!.next; // nav to idx a
+
+  let nodeB = nodeA!.next; // save pos
+  nodeA!.next = list2; // insert list2 at idx a of list1
+
+  let tailList2 = list2;
+  while (tailList2!.next != null) tailList2 = tailList2!.next; // nav to end of list2
+
+  for (let i = 0; i <= b - a; i++) nodeB = nodeB!.next; // nav to idx b
+  tailList2!.next = nodeB; // insert remaining list1 to tail of list2
+
+  return list1;
+};
+
+printLinkedList(
+  mergeInBetween(
+    linkedList([10, 1, 13, 6, 9, 5]),
+    3,
+    4,
+    linkedList([1000000, 1000001, 1000002])
+  )
+); //  [10,1,13,1000000,1000001,1000002,5]
+printLinkedList(
+  mergeInBetween(
+    linkedList([0, 1, 2, 3, 4, 5, 6]),
+    2,
+    5,
+    linkedList([1000000, 1000001, 1000002, 1000003, 1000004])
+  )
+); //  [0,1,1000000,1000001,1000002,1000003,1000004,6]
+
+// 100% Runtime
