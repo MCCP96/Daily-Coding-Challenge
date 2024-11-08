35f984d8bf50d7f27a953713e7469a0c1439be40
MCCP96 <petermccomb@live.com>
2024-11-08 07:17:08 -0500

    Maximum Twin Sum of a Linked List

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -4206,7 +4206,7 @@ const revisedDeleteNode = (node: ListNode | null): void => {
 // somehow much slower */
 
 // Merge In Between Linked Lists					11/7/2024
-
+/* 
 // https://leetcode.com/problems/merge-in-between-linked-lists/description/
 
 // You are given two linked lists: list1 and list2 of sizes n and m respectively.
@@ -4270,4 +4270,102 @@ printLinkedList(
   )
 ); //  [0,1,1000000,1000001,1000002,1000003,1000004,6]
 
-// 100% Runtime
+// 100% Runtime */
+
+// Maximum Twin Sum of a Linked List					11/8/2024
+
+// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
+
+// In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+
+// For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+
+// The twin sum is defined as the sum of a node and its twin.
+
+// Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+// Example 1:
+// 		Input: head = [5,4,2,1]
+// 		Output: 6
+// Explanation:
+// 		Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+// 		There are no other nodes with twins in the linked list.
+// 		Thus, the maximum twin sum of the linked list is 6.
+
+// Example 2:
+// 		Input: head = [4,2,2,3]
+// 		Output: 7
+// Explanation:
+// 		The nodes with twins present in this linked list are:
+// 		- Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+// 		- Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+// 		Thus, the maximum twin sum of the linked list is max(7, 4) = 7.
+
+// Example 3:
+// 		Input: head = [1,100000]
+// 		Output: 100001
+// Explanation:
+// 		There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
+
+// Constraints:
+//		The number of nodes in the list is an even integer in the range [2, 105].
+//		1 <= Node.val <= 105
+
+const pairSum = (head: ListNode | null): number => {
+  // find n
+  let node = head;
+  let n = 0;
+  while (node != null) {
+    node = node.next;
+    n++;
+  }
+
+  // find middle
+  let i = 0;
+  node = head;
+  while (i++ < n / 2 - 1) node = node!.next;
+  let middle = node?.next;
+  node!.next = null;
+
+  // flip middle (head2)
+  let head2: ListNode | null = null;
+  while (middle != null) {
+    head2 = new ListNode(middle.val, head2);
+    middle = middle.next;
+  }
+
+  // find largest pair
+  let max = 0;
+  while (head != null && head2 != null) {
+    max = Math.max(max, head.val + head2.val);
+    head = head.next;
+    head2 = head2.next;
+  }
+  return max;
+};
+
+console.log(pairSum(linkedList([5, 4, 2, 1]))); //  6
+console.log(pairSum(linkedList([4, 2, 2, 3]))); //  7
+console.log(pairSum(linkedList([1, 100000]))); //  100001
+
+// technically O(n)
+
+function topVotedPairSum(head: ListNode | null): number {
+  const stack: number[] = [];
+  let slow = head;
+  let fast = head.next;
+
+  while (fast != null) {
+    stack.push(slow.val);
+    slow = slow.next;
+    fast = fast.next?.next;
+  }
+
+  let maxTwinSum = 0;
+  while (slow != null) {
+    maxTwinSum = Math.max(maxTwinSum, slow.val + stack.pop());
+    slow = slow.next;
+  }
+
+  return maxTwinSum;
+}
