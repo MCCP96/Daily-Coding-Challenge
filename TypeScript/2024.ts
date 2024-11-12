4ca10a9fe296055911ffa74405272daf7158a5b4
MCCP96 <petermccomb@live.com>
2024-11-12 07:46:11 -0500

    Merge Nodes in Between Zeros

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -4563,7 +4563,7 @@ class TopVotedCustomStack {
 } */
 
 // Generate Binary Strings Without Adjacent Zeros					11/11/2024
-
+/* 
 // https://leetcode.com/problems/generate-binary-strings-without-adjacent-zeros/description/
 
 // You are given a positive integer n.
@@ -4607,4 +4607,61 @@ const validStrings = (n: number): string[] => {
 };
 
 console.log(validStrings(3)); //  ["010","011","101","110","111"]
-console.log(validStrings(1)); //  ["0","1"]
+console.log(validStrings(1)); //  ["0","1"] */
+
+// Merge Nodes in Between Zeros					11/12/2024
+
+// https://leetcode.com/problems/merge-nodes-in-between-zeros/description/
+
+// You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+// For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+// Return the head of the modified linked list.
+
+// Example 1:
+// 		Input: head = [0,3,1,0,4,5,2,0]
+// 		Output: [4,11]
+// Explanation:
+// 		The above figure represents the given linked list. The modified list contains
+// 		- The sum of the nodes marked in green: 3 + 1 = 4.
+// 		- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+
+// Example 2:
+// 		Input: head = [0,1,0,3,0,2,2,0]
+// 		Output: [1,3,4]
+// Explanation:
+// 		The above figure represents the given linked list. The modified list contains
+// 		- The sum of the nodes marked in green: 1 = 1.
+// 		- The sum of the nodes marked in red: 3 = 3.
+// 		- The sum of the nodes marked in yellow: 2 + 2 = 4.
+
+// Constraints:
+//		The number of nodes in the list is in the range [3, 2 * 105].
+//		0 <= Node.val <= 1000
+//		There are no two consecutive nodes with Node.val == 0.
+//		The beginning and end of the linked list have Node.val == 0.
+
+const mergeNodes = (head: ListNode | null): ListNode | null => {
+  let res: ListNode | null = new ListNode(0, null);
+  let newHead = res;
+  head = head!.next; // skip leading 0
+
+  while (head!.next) {
+    if (head!.val == 0) {
+      res!.next = new ListNode(0, null);
+      res = res!.next;
+    } else {
+      res!.val += head!.val;
+    }
+    head = head!.next;
+  }
+
+  return newHead;
+};
+
+printLinkedList(mergeNodes(linkedList([0, 3, 1, 0, 4, 5, 2, 0]))); //  [4,11]
+printLinkedList(mergeNodes(linkedList([0, 1, 0, 3, 0, 2, 2, 0]))); //  [1,3,4]
+printLinkedList(mergeNodes(linkedList([0, 24, 14, 0]))); //  [38]
+
+// I had to merge them instead of making a new list
