d8655cfd6b3582663f23bceabe7252cb830d3aa5
MCCP96 <petermccomb@live.com>
2024-11-16 07:47:39 -0500

    Check Balanced String

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -4796,7 +4796,7 @@ console.log(numSplits("aacaba")); //  2
 console.log(numSplits("abcd")); //  1 */
 
 // Split a String Into the Max Number of Unique Substrings					11/15/2024
-
+/* 
 // https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/
 
 // Given a string s, return the maximum number of unique substrings that the given string can be split into.
@@ -4864,4 +4864,44 @@ function dfs(s: string, minI: number, maxI: number, arr: string[]): number {
   arr.pop();
   // Return the answer
   return ans;
-}
+} */
+
+// Check Balanced String					11/16/2024
+
+// https://leetcode.com/problems/check-balanced-string/description/
+
+// You are given a string num consisting of only digits. A string of digits is called balanced if the sum of the digits at even indices is equal to the sum of digits at odd indices.
+
+// Return true if num is balanced, otherwise return false.
+
+// Example 1:
+// 		Input: num = "1234"
+// 		Output: false
+// Explanation:
+// 		The sum of digits at even indices is 1 + 3 == 4, and the sum of digits at odd indices is 2 + 4 == 6.
+// 		Since 4 is not equal to 6, num is not balanced.
+
+// Example 2:
+// 		Input: num = "24123"
+// 		Output: true
+// Explanation:
+// 		The sum of digits at even indices is 2 + 1 + 3 == 6, and the sum of digits at odd indices is 4 + 2 == 6.
+// 		Since both are equal the num is balanced.
+
+// Constraints:
+//		2 <= num.length <= 100
+//		num consists of digits only
+
+const isBalanced = (num: string): boolean => {
+  const n = num.length;
+  let even = 0;
+  let odd = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 2 == 0) even += +num[i];
+    else odd += +num[i];
+  }
+  return even == odd;
+};
+
+console.log(isBalanced("1234")); //  false
+console.log(isBalanced("24123")); //  true
