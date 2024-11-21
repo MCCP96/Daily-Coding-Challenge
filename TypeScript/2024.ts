5c965948096b7c0d1c8b2d83031db35ab6c5acf4
MCCP96 <petermccomb@live.com>
2024-11-21 08:25:44 -0500

    Maximum Score From Removing Stones

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -5113,7 +5113,7 @@ function topVotedMaxConsecutiveAnswers(answerKey: string, k: number): number {
 } */
 
 // Find Duplicate File in System					11/20/2024
-
+/* 
 // https://leetcode.com/problems/find-duplicate-file-in-system/description/
 
 // Given a list paths of directory info, including the directory path, and all the files with contents in this directory, return all the duplicate files in the file system in terms of their paths. You may return the answer in any order.
@@ -5211,4 +5211,68 @@ function topVotedFindDuplicate(paths: string[]): string[][] {
   }
 
   return Object.values(res);
-}
+} */
+
+// Maximum Score From Removing Stones					11/21/2024
+
+// https://leetcode.com/problems/maximum-score-from-removing-stones/
+
+// You are playing a solitaire game with three piles of stones of sizes a​​​​​​, b,​​​​​​ and c​​​​​​ respectively. Each turn you choose two different non-empty piles, take one stone from each, and add 1 point to your score. The game stops when there are fewer than two non-empty piles (meaning there are no more available moves).
+
+// Given three integers a​​​​​, b,​​​​​ and c​​​​​, return the maximum score you can get.
+
+// Example 1:
+// 		Input: a = 2, b = 4, c = 6
+// 		Output: 6
+// Explanation: The starting state is (2, 4, 6). One optimal set of moves is:
+// 		- Take from 1st and 3rd piles, state is now (1, 4, 5)
+// 		- Take from 1st and 3rd piles, state is now (0, 4, 4)
+// 		- Take from 2nd and 3rd piles, state is now (0, 3, 3)
+// 		- Take from 2nd and 3rd piles, state is now (0, 2, 2)
+// 		- Take from 2nd and 3rd piles, state is now (0, 1, 1)
+// 		- Take from 2nd and 3rd piles, state is now (0, 0, 0)
+// 		There are fewer than two non-empty piles, so the game ends. Total: 6 points.
+
+// Example 2:
+// 		Input: a = 4, b = 4, c = 6
+// 		Output: 7
+// Explanation: The starting state is (4, 4, 6). One optimal set of moves is:
+// 		- Take from 1st and 2nd piles, state is now (3, 3, 6)
+// 		- Take from 1st and 3rd piles, state is now (2, 3, 5)
+// 		- Take from 1st and 3rd piles, state is now (1, 3, 4)
+// 		- Take from 1st and 3rd piles, state is now (0, 3, 3)
+// 		- Take from 2nd and 3rd piles, state is now (0, 2, 2)
+// 		- Take from 2nd and 3rd piles, state is now (0, 1, 1)
+// 		- Take from 2nd and 3rd piles, state is now (0, 0, 0)
+// 		There are fewer than two non-empty piles, so the game ends. Total: 7 points.
+
+// Example 3:
+// 		Input: a = 1, b = 8, c = 8
+// 		Output: 8
+// Explanation: One optimal set of moves is to take from the 2nd and 3rd piles for 8 turns until they are empty.
+// 		After that, there are fewer than two non-empty piles, so the game ends.
+
+// Constraints:
+//		1 <= a, b, c <= 105
+
+const maximumScore = (a: number, b: number, c: number): number => {
+  // keep the 2 largest piles for the end
+  // take from the smallest, trying to equalize the other 2 before running out
+
+  // sort (a: smallest -> c: largest)
+  [a, b, c] = [a, b, c].sort((a, b) => a - b);
+
+  let res = 0;
+  while (a-- > 0) {
+    // take from smallest, equalizing
+    if (c > b) c--;
+    else b--;
+    res++;
+  }
+
+  return res + Math.min(b, c);
+};
+
+console.log(maximumScore(2, 4, 6)); //  6
+console.log(maximumScore(4, 4, 6)); //  7
+console.log(maximumScore(1, 8, 8)); //  8
