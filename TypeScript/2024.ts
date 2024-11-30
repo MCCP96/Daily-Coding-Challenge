9f34b2ccb4349dbf8e2949236e96389dd1c44826
MCCP96 <petermccomb@live.com>
2024-11-30 10:09:15 -0500

    Merge Triplets to Form Target Triplet

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -5613,7 +5613,7 @@ console.log(canSortArray([1, 2, 3, 4, 5])); //  true
 console.log(canSortArray([3, 16, 8, 4, 2])); //  false */
 
 // Number of Wonderful Substrings					11/26/2024
-
+/* 
 // https://leetcode.com/problems/number-of-wonderful-substrings/description/
 
 // A wonderful string is a string where at most one letter appears an odd number of times.
@@ -5712,4 +5712,113 @@ function topVotedWonderfulSubstrings(word: string): number {
 // Checking the current bitmask (currentMask).
 // Checking all bitmasks that differ from the currentMask by exactly one bit (since a wonderful substring can have at most one character that appears an odd number of times).
 // Using a hashmap to keep track of all previously seen bitmasks and their frequencies.
-// Count Substrings: For each position in the string, calculate how many substrings ending at that position are wonderful by looking up in the hashmap.
+// Count Substrings: For each position in the string, calculate how many substrings ending at that position are wonderful by looking up in the hashmap. */
+
+// Merge Triplets to Form Target Triplet					11/30/2024
+
+// https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+
+// A triplet is an array of three integers. You are given a 2D integer array triplets, where triplets[i] = [ai, bi, ci] describes the ith triplet. You are also given an integer array target = [x, y, z] that describes the triplet you want to obtain.
+
+// To obtain target, you may apply the following operation on triplets any number of times (possibly zero):
+
+// Choose two indices (0-indexed) i and j (i != j) and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
+
+// For example, if triplets[i] = [2, 5, 3] and triplets[j] = [1, 7, 5], triplets[j] will be updated to [max(2, 1), max(5, 7), max(3, 5)] = [2, 7, 5].
+
+// Return true if it is possible to obtain the target triplet [x, y, z] as an element of triplets, or false otherwise.
+
+// Example 1:
+// 		Input: triplets = [[2,5,3],[1,8,4],[1,7,5]], target = [2,7,5]
+// 		Output: true
+// Explanation: Perform the following operations:
+// 		- Choose the first and last triplets [[2,5,3],[1,8,4],[1,7,5]]. Update the last triplet to be [max(2,1), max(5,7), max(3,5)] = [2,7,5]. triplets = [[2,5,3],[1,8,4],[2,7,5]]
+// 		The target triplet [2,7,5] is now an element of triplets.
+
+// Example 2:
+// 		Input: triplets = [[3,4,5],[4,5,6]], target = [3,2,5]
+// 		Output: false
+// Explanation: It is impossible to have [3,2,5] as an element because there is no 2 in any of the triplets.
+
+// Example 3:
+// 		Input: triplets = [[2,5,3],[2,3,4],[1,2,5],[5,2,3]], target = [5,5,5]
+// 		Output: true
+// Explanation: Perform the following operations:
+// 		- Choose the first and third triplets [[2,5,3],[2,3,4],[1,2,5],[5,2,3]]. Update the third triplet to be [max(2,1), max(5,2), max(3,5)] = [2,5,5]. triplets = [[2,5,3],[2,3,4],[2,5,5],[5,2,3]].
+// 		- Choose the third and fourth triplets [[2,5,3],[2,3,4],[2,5,5],[5,2,3]]. Update the fourth triplet to be [max(2,5), max(5,2), max(5,3)] = [5,5,5]. triplets = [[2,5,3],[2,3,4],[2,5,5],[5,5,5]].
+// 		The target triplet [5,5,5] is now an element of triplets.
+
+// Constraints:
+//		1 <= triplets.length <= 105
+//		triplets[i].length == target.length == 3
+//		1 <= ai, bi, ci, x, y, z <= 1000
+
+const mergeTriplets = (triplets: number[][], t: number[]): boolean => {
+  // keep triplets where max doesn't exceed t
+  triplets = triplets.filter(
+    ([a, b, c]) => a <= t[0] && b <= t[1] && c <= t[2]
+  );
+
+  // find at least one of each t
+  let t1Found = false;
+  let t2Found = false;
+  let t3Found = false;
+  for (const trip of triplets) {
+    if (trip[0] == t[0]) t1Found = true;
+    if (trip[1] == t[1]) t2Found = true;
+    if (trip[2] == t[2]) t3Found = true;
+  }
+
+  return t1Found && t2Found && t3Found;
+};
+
+console.log(
+  mergeTriplets(
+    [
+      [2, 5, 3],
+      [1, 8, 4],
+      [1, 7, 5],
+    ],
+    [2, 7, 5]
+  )
+); //  true
+console.log(
+  mergeTriplets(
+    [
+      [3, 4, 5],
+      [4, 5, 6],
+    ],
+    [3, 2, 5]
+  )
+); //  false
+console.log(
+  mergeTriplets(
+    [
+      [2, 5, 3],
+      [2, 3, 4],
+      [1, 2, 5],
+      [5, 2, 3],
+    ],
+    [5, 5, 5]
+  )
+); //  true
+
+function topVotedMergeTriplets(
+  triplets: number[][],
+  target: number[]
+): boolean {
+  const firstTargetDigitIdx = triplets.findIndex(
+    (t) => t[0] === target[0] && t[1] <= target[1] && t[2] <= target[2]
+  );
+  const secondTargetDigitIdx = triplets.findIndex(
+    (t) => t[1] === target[1] && t[0] <= target[0] && t[2] <= target[2]
+  );
+  const thirdTargetDigitIdx = triplets.findIndex(
+    (t) => t[2] === target[2] && t[0] <= target[0] && t[1] <= target[1]
+  );
+  return ![
+    firstTargetDigitIdx,
+    secondTargetDigitIdx,
+    thirdTargetDigitIdx,
+  ].includes(-1);
+}
