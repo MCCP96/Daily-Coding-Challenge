5677483d0a17c34f972604c53114a8efe560e266
MCCP96 <petermccomb@live.com>
2024-12-03 08:35:58 -0500

    Advent of Code Day 3: Mull It Over

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -5898,7 +5898,7 @@ const day1 = async () => {
 console.log(await day1()); */
 
 // Advent of Code Day 2: Red-Nosed Reports          12/2/2024
-
+/* 
 // Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
 
 // While the Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no sign of the Chief Historian, the engineers there run up to you as soon as they see you. Apparently, they still talk about the time Rudolph was saved through molecular synthesis from a single electron.
@@ -5969,4 +5969,57 @@ const day2 = async () => {
   return res;
 };
 
-console.log(await day2());
+console.log(await day2()); */
+
+// Advent of Code Day 3: Mull It Over          12/3/2024
+
+// https://adventofcode.com/2024/day/3
+
+// "Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the North Pole Toboggan Rental Shop. The Historians head out to take a look.
+
+// The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"
+
+// The computer appears to be trying to run a program, but its memory (your puzzle input) is corrupted. All of the instructions have been jumbled up!
+
+// It seems like the goal of the program is just to multiply some numbers. It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.
+
+// However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
+
+// For example, consider the following section of corrupted memory:
+
+// xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+// Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
+
+// Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+
+const getData = async (filename: string): Promise<string> => {
+  let res: string = "";
+
+  await fetch(`/TypeScript/inputs/${filename}.txt`)
+    .then((res) => res.text())
+    .then((data) => {
+      res = data;
+    });
+
+  return res;
+};
+
+const day3 = async (): Promise<number> => {
+  const memStr = await getData("3");
+  let res = 0;
+
+  for (let i = 0; i < memStr.length; i++) {
+    if (memStr.substring(i, i + 4) == "mul(") {
+      let cur = memStr.substring(i + 4, memStr.indexOf(")", i));
+
+      if (/^\d{1,3},\d{1,3}$/.test(cur)) {
+        let [n1, n2] = cur.split(",").map((n) => +n);
+        res += n1 * n2;
+      }
+    }
+  }
+
+  return res;
+};
+
+console.log(await day3()); // 183669043
