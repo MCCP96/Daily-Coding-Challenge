03ed8e9ac8f084f0f06c38abedca065e11fac771
MCCP96 <petermccomb@live.com>
2024-12-13 08:58:29 -0500

    Advent of Code Day 13: Claw Contraption

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -7069,7 +7069,7 @@ console.log(
 ); // 189547 */
 
 // Advent of Code Day 12: Garden Groups
-
+/* 
 // https://adventofcode.com/2024/day/12
 
 // Why not search for the Chief Historian near the gardener and his massive farm? There's plenty of food, so The Historians grab something to eat while they search.
@@ -7216,4 +7216,129 @@ const day12 = async () => {
   return totalFencePrice;
 };
 
-console.log(await day12()); // 1461806
+console.log(await day12()); // 1461806 */
+
+// Advent of Code Day 13: Claw Contraption
+
+// https://adventofcode.com/2024/day/13
+
+// Next up: the lobby of a resort on a tropical island. The Historians take a moment to admire the hexagonal floor tiles before spreading out.
+
+// Fortunately, it looks like the resort has a new arcade! Maybe you can win some prizes from the claw machines?
+
+// The claw machines here are a little unusual. Instead of a joystick or directional buttons to control the claw, these machines have two buttons labeled A and B. Worse, you can't just put in a token and play; it costs 3 tokens to push the A button and 1 token to push the B button.
+
+// With a little experimentation, you figure out that each machine's buttons are configured to move the claw a specific amount to the right (along the X axis) and a specific amount forward (along the Y axis) each time that button is pressed.
+
+// Each machine contains one prize; to win the prize, the claw must be positioned exactly above the prize on both the X and Y axes.
+
+// You wonder: what is the smallest number of tokens you would have to spend to win as many prizes as possible? You assemble a list of every machine's button behavior and prize location (your puzzle input). For example:
+
+// Button A: X+94, Y+34
+// Button B: X+22, Y+67
+// Prize: X=8400, Y=5400
+
+// Button A: X+26, Y+66
+// Button B: X+67, Y+21
+// Prize: X=12748, Y=12176
+
+// Button A: X+17, Y+86
+// Button B: X+84, Y+37
+// Prize: X=7870, Y=6450
+
+// Button A: X+69, Y+23
+// Button B: X+27, Y+71
+// Prize: X=18641, Y=10279
+// This list describes the button configuration and prize location of four different claw machines.
+
+// For now, consider just the first claw machine in the list:
+
+// Pushing the machine's A button would move the claw 94 units along the X axis and 34 units along the Y axis.
+// Pushing the B button would move the claw 22 units along the X axis and 67 units along the Y axis.
+// The prize is located at X=8400, Y=5400; this means that from the claw's initial position, it would need to move exactly 8400 units along the X axis and exactly 5400 units along the Y axis to be perfectly aligned with the prize in this machine.
+// The cheapest way to win the prize is by pushing the A button 80 times and the B button 40 times. This would line up the claw along the X axis (because 80*94 + 40*22 = 8400) and along the Y axis (because 80*34 + 40*67 = 5400). Doing this would cost 80*3 tokens for the A presses and 40*1 for the B presses, a total of 280 tokens.
+
+// For the second and fourth claw machines, there is no combination of A and B presses that will ever win a prize.
+
+// For the third claw machine, the cheapest way to win the prize is by pushing the A button 38 times and the B button 86 times. Doing this would cost a total of 200 tokens.
+
+// So, the most prizes you could possibly win is two; the minimum tokens you would have to spend to win all (two) prizes is 480.
+
+// You estimate that each button would need to be pressed no more than 100 times to win a prize. How else would someone be expected to play?
+
+// Figure out how to win as many prizes as possible. What is the fewest tokens you would have to spend to win all possible prizes?
+
+type ClawMachine = {
+  a: { x: number; y: number };
+  b: { x: number; y: number };
+  t: { x: number; y: number };
+};
+
+const getData = async (filename: string): Promise<ClawMachine[]> => {
+  let res: ClawMachine[] = [];
+
+  await fetch(`/TypeScript/inputs/${filename}.txt`)
+    .then((response) => response.text())
+    .then((data) => {
+      for (const machineData of data.split("\r\n\r\n")) {
+        const [a, b, t] = machineData.split("\r\n");
+
+        let data: ClawMachine = {
+          a: { x: 0, y: 0 },
+          b: { x: 0, y: 0 },
+          t: { x: 0, y: 0 },
+        };
+
+        data.a.x = Number(a.substring(a.indexOf("X+") + 2, a.indexOf(", ")));
+        data.a.y = Number(a.substring(a.indexOf("Y+") + 2));
+        data.b.x = Number(b.substring(b.indexOf("X+") + 2, b.indexOf(", ")));
+        data.b.y = Number(b.substring(b.indexOf("Y+") + 2));
+        data.t.x = Number(t.substring(t.indexOf("X=") + 2, t.indexOf(", ")));
+        data.t.y = Number(t.substring(t.indexOf("Y=") + 2));
+
+        res.push(data);
+      }
+    });
+
+  return res;
+};
+
+const analyzeMachine = ({ a, b, t }: ClawMachine) => {
+  // track A and B presses
+  let [A, B] = [0, 0];
+
+  // Best case scenario: Max B presses
+  B = Math.min(~~(t.x / b.x), ~~(t.y / b.y), 100); // no more than 100
+  let curPos = { x: B * b.x, y: B * b.y };
+
+  // Balance until t is found using A presses
+  let isWinner = curPos.x == t.x && curPos.y == t.y;
+
+  while (!isWinner && A++ < 100) {
+    curPos.x += a.x;
+    curPos.y += a.y;
+
+    while (curPos.x > t.x || curPos.y > t.y) {
+      // exceeded t, remove B press
+      curPos.x -= b.x;
+      curPos.y -= b.y;
+      B--;
+    }
+
+    isWinner = curPos.x == t.x && curPos.y == t.y;
+  }
+
+  // return min token cost
+  return isWinner ? A * 3 + B : 0;
+};
+
+const day13 = (data: ClawMachine[]) => {
+  console.log(data);
+  let tokens = 0;
+  for (const machineData of data) {
+    tokens += analyzeMachine(machineData);
+  }
+  return tokens;
+};
+
+console.log(day13(await getData("13")));
