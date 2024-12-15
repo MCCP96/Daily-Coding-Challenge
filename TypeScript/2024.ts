ba1417b1fc349da78155a24819f949374f34de49
MCCP96 <petermccomb@live.com>
2024-12-15 10:31:24 -0500

    Advent of Code Day 15: Warehouse Woes

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -7343,7 +7343,7 @@ const day13 = (data: ClawMachine[]) => {
 console.log(day13(await getData("13"))); // 36758 */
 
 // Advent of Code Day 14: Restroom Redoubt          12/14/2024
-
+/* 
 // https://adventofcode.com/2024/day/14
 
 // One of The Historians needs to use the bathroom; fortunately, you know there's a bathroom near an unvisited location on their list, and so you're all quickly teleported directly to the lobby of Easter Bunny Headquarters.
@@ -7531,4 +7531,385 @@ const day14 = (robots: Robot[]) => {
   return quadrants.reduce((a, c) => a * c, 1);
 };
 
-console.log(day14(await getData("14"))); // 226548000
+console.log(day14(await getData("14"))); // 226548000 */
+
+// Advent of Code Day 15: Warehouse Woes          12/15/2024
+
+// https://adventofcode.com/2024/day/15
+
+// You appear back inside your own mini submarine! Each Historian drives their mini submarine in a different direction; maybe the Chief has his own submarine down here somewhere as well?
+
+// You look up to see a vast school of lanternfish swimming past you. On closer inspection, they seem quite anxious, so you drive your mini submarine over to see if you can help.
+
+// Because lanternfish populations grow rapidly, they need a lot of food, and that food needs to be stored somewhere. That's why these lanternfish have built elaborate warehouse complexes operated by robots!
+
+// These lanternfish seem so anxious because they have lost control of the robot that operates one of their most important warehouses! It is currently running amok, pushing around boxes in the warehouse with no regard for lanternfish logistics or lanternfish inventory management strategies.
+
+// Right now, none of the lanternfish are brave enough to swim up to an unpredictable robot so they could shut it off. However, if you could anticipate the robot's movements, maybe they could find a safe option.
+
+// The lanternfish already have a map of the warehouse and a list of movements the robot will attempt to make (your puzzle input). The problem is that the movements will sometimes fail as boxes are shifted around, making the actual movements of the robot difficult to predict.
+
+// For example:
+
+// ##########
+// #..O..O.O#
+// #......O.#
+// #.OO..O.O#
+// #..O@..O.#
+// #O#..O...#
+// #O..O..O.#
+// #.OO.O.OO#
+// #....O...#
+// ##########
+
+// <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+// vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+// ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+// <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+// ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+// ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+// >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+// <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+// ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+// v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+
+// As the robot (@) attempts to move, if there are any boxes (O) in the way, the robot will also attempt to push those boxes. However, if this action would cause the robot or a box to move into a wall (#), nothing moves instead, including the robot. The initial positions of these are shown on the map at the top of the document the lanternfish gave you.
+
+// The rest of the document describes the moves (^ for up, v for down, < for left, > for right) that the robot will attempt to make, in order. (The moves form a single giant sequence; they are broken into multiple lines just to make copy-pasting easier. Newlines within the move sequence should be ignored.)
+
+// Here is a smaller example to get started:
+
+// ########
+// #..O.O.#
+// ##@.O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// <^^>>>vv<v>>v<<
+// Were the robot to attempt the given sequence of moves, it would push around the boxes as follows:
+
+// Initial state:
+// ########
+// #..O.O.#
+// ##@.O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move <:
+// ########
+// #..O.O.#
+// ##@.O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move ^:
+// ########
+// #.@O.O.#
+// ##..O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move ^:
+// ########
+// #.@O.O.#
+// ##..O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move >:
+// ########
+// #..@OO.#
+// ##..O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move >:
+// ########
+// #...@OO#
+// ##..O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move >:
+// ########
+// #...@OO#
+// ##..O..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #......#
+// ########
+
+// Move v:
+// ########
+// #....OO#
+// ##..@..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move v:
+// ########
+// #....OO#
+// ##..@..#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move <:
+// ########
+// #....OO#
+// ##.@...#
+// #...O..#
+// #.#.O..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move v:
+// ########
+// #....OO#
+// ##.....#
+// #..@O..#
+// #.#.O..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move >:
+// ########
+// #....OO#
+// ##.....#
+// #...@O.#
+// #.#.O..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move >:
+// ########
+// #....OO#
+// ##.....#
+// #....@O#
+// #.#.O..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move v:
+// ########
+// #....OO#
+// ##.....#
+// #.....O#
+// #.#.O@.#
+// #...O..#
+// #...O..#
+// ########
+
+// Move <:
+// ########
+// #....OO#
+// ##.....#
+// #.....O#
+// #.#O@..#
+// #...O..#
+// #...O..#
+// ########
+
+// Move <:
+// ########
+// #....OO#
+// ##.....#
+// #.....O#
+// #.#O@..#
+// #...O..#
+// #...O..#
+// ########
+// The larger example has many more moves; after the robot has finished those moves, the warehouse would look like this:
+
+// ##########
+// #.O.O.OOO#
+// #........#
+// #OO......#
+// #OO@.....#
+// #O#.....O#
+// #O.....OO#
+// #O.....OO#
+// #OO....OO#
+// ##########
+// The lanternfish use their own custom Goods Positioning System (GPS for short) to track the locations of the boxes. The GPS coordinate of a box is equal to 100 times its distance from the top edge of the map plus its distance from the left edge of the map. (This process does not stop at wall tiles; measure all the way to the edges of the map.)
+
+// So, the box shown below has a distance of 1 from the top edge of the map and 4 from the left edge of the map, resulting in a GPS coordinate of 100 * 1 + 4 = 104.
+
+// #######
+// #...O..
+// #......
+// The lanternfish would like to know the sum of all boxes' GPS coordinates after the robot finishes moving. In the larger example, the sum of all boxes' GPS coordinates is 10092. In the smaller example, the sum is 2028.
+
+// Predict the motion of the robot and boxes in the warehouse. After the robot is finished moving, what is the sum of all boxes' GPS coordinates?
+
+const getData = async (filename: string): Promise<[string[][], string]> => {
+  let map: string[][] = [];
+  let moves: string = "";
+
+  await fetch(`/TypeScript/inputs/${filename}.txt`)
+    .then((response) => response.text())
+    .then((data) => {
+      const [mapData, movesData] = data.split("\r\n\r\n");
+
+      map = mapData.split("\r\n").map((c) => c.split(""));
+      moves = movesData.split("\r\n").join("");
+    });
+
+  return [map, moves];
+};
+
+let map: string[][];
+let n: number;
+let m: number;
+
+const handleMove = (
+  posX: number,
+  posY: number,
+  sol: { validMove: boolean },
+  move: string
+) => {
+  // not my proudest
+  // *implementing a newPos variable instead of repeated code would be more readable
+
+  switch (move) {
+    case "^":
+      if (map[posY - 1][posX] == ".") {
+        // valid move
+        sol.validMove = true;
+      } else if (map[posY - 1][posX] == "O") {
+        // box, check further
+        handleMove(posX, posY - 1, sol, move);
+      }
+
+      if (sol.validMove) {
+        map[posY - 1][posX] = map[posY][posX];
+        map[posY][posX] = map[posY + 1][posX];
+      }
+      break;
+    case ">":
+      if (map[posY][posX + 1] == ".") {
+        // valid move
+        sol.validMove = true;
+      } else if (map[posY][posX + 1] == "O") {
+        // box, check further
+        handleMove(posX + 1, posY, sol, move);
+      }
+
+      if (sol.validMove) {
+        map[posY][posX + 1] = map[posY][posX];
+        map[posY][posX] = map[posY][posX - 1];
+      }
+      break;
+    case "v":
+      if (map[posY + 1][posX] == ".") {
+        // valid move
+        sol.validMove = true;
+      } else if (map[posY + 1][posX] == "O") {
+        // box, check further
+        handleMove(posX, posY + 1, sol, move);
+      }
+
+      if (sol.validMove) {
+        map[posY + 1][posX] = map[posY][posX];
+        map[posY][posX] = map[posY - 1][posX];
+      }
+      break;
+    case "<":
+      if (map[posY][posX - 1] == ".") {
+        // valid move
+        sol.validMove = true;
+      } else if (map[posY][posX - 1] == "O") {
+        // box, check further
+        handleMove(posX - 1, posY, sol, move);
+      }
+
+      if (sol.validMove) {
+        map[posY][posX - 1] = map[posY][posX];
+        map[posY][posX] = map[posY][posX + 1];
+      }
+      break;
+  }
+};
+
+const day15 = ([mapData, moves]: [string[][], string]): number => {
+  map = mapData;
+  n = map.length;
+  m = map[0].length;
+
+  // find starting pos
+  let [posX, posY] = [-1, 0];
+
+  while (posX == -1) {
+    posY++;
+    for (let col = 1; col < m - 1; col++) {
+      if (map[posY][col] == "@") {
+        posX = col;
+        break;
+      }
+    }
+  }
+
+  // handle each move
+  for (const move of moves) {
+    let sol = { validMove: false };
+
+    handleMove(posX, posY, sol, move);
+
+    if (sol.validMove) {
+      // empty prev pos
+      map[posY][posX] = ".";
+
+      // update cur pos
+      if (move == "^") posY--;
+      if (move == ">") posX++;
+      if (move == "v") posY++;
+      if (move == "<") posX--;
+    }
+  }
+
+  // sum box GPS values
+  let sum = 0;
+
+  for (let row = 1; row < n - 1; row++) {
+    for (let col = 1; col < m - 1; col++) {
+      if (map[row][col] == "O") {
+        sum += 100 * row + col;
+      }
+    }
+  }
+
+  return sum;
+};
+
+console.log(day15(await getData("15"))); // 1463512
