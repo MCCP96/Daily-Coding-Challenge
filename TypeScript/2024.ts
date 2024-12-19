71504a23c9a42fb03411ee1175bc1633253f4cd5
MCCP96 <petermccomb@live.com>
2024-12-19 09:29:21 -0500

    Advent of Code Day 19: Linen Layout

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -8245,7 +8245,7 @@ const day17 = ({ a, b, c, program }: Computer) => {
 console.log(day17(await getData("17"))); // 7,1,5,2,4,0,7,6,1 */
 
 // Advent of Code Day 18: RAM Run         12/18/2024
-
+/* 
 // https://adventofcode.com/2024/day/18
 
 // You and The Historians look a lot more pixelated than you remember. You're inside a computer at the North Pole!
@@ -8360,4 +8360,114 @@ const day18 = (bytes: number[][]) => {
   return minSteps;
 };
 
-console.log(day18(await getData("18"))); // 364
+console.log(day18(await getData("18"))); // 364 */
+
+// Advent of Code Day 19: Linen Layout          12/19/2024
+
+// https://adventofcode.com/2024/day/19
+
+// Today, The Historians take you up to the hot springs on Gear Island! Very suspiciously, absolutely nothing goes wrong as they begin their careful search of the vast field of helixes.
+
+// Could this finally be your chance to visit the onsen next door? Only one way to find out.
+
+// After a brief conversation with the reception staff at the onsen front desk, you discover that you don't have the right kind of money to pay the admission fee. However, before you can leave, the staff get your attention. Apparently, they've heard about how you helped at the hot springs, and they're willing to make a deal: if you can simply help them arrange their towels, they'll let you in for free!
+
+// Every towel at this onsen is marked with a pattern of colored stripes. There are only a few patterns, but for any particular pattern, the staff can get you as many towels with that pattern as you need. Each stripe can be white (w), blue (u), black (b), red (r), or green (g). So, a towel with the pattern ggr would have a green stripe, a green stripe, and then a red stripe, in that order. (You can't reverse a pattern by flipping a towel upside-down, as that would cause the onsen logo to face the wrong way.)
+
+// The Official Onsen Branding Expert has produced a list of designs - each a long sequence of stripe colors - that they would like to be able to display. You can use any towels you want, but all of the towels' stripes must exactly match the desired design. So, to display the design rgrgr, you could use two rg towels and then an r towel, an rgr towel and then a gr towel, or even a single massive rgrgr towel (assuming such towel patterns were actually available).
+
+// To start, collect together all of the available towel patterns and the list of desired designs (your puzzle input). For example:
+
+// r, wr, b, g, bwu, rb, gb, br
+
+// brwrr
+// bggr
+// gbbr
+// rrbgbr
+// ubwu
+// bwurrg
+// brgr
+// bbrgwb
+
+// The first line indicates the available towel patterns; in this example, the onsen has unlimited towels with a single red stripe (r), unlimited towels with a white stripe and then a red stripe (wr), and so on.
+
+// After the blank line, the remaining lines each describe a design the onsen would like to be able to display. In this example, the first design (brwrr) indicates that the onsen would like to be able to display a black stripe, a red stripe, a white stripe, and then two red stripes, in that order.
+
+// Not all designs will be possible with the available towels. In the above example, the designs are possible or impossible as follows:
+
+// brwrr can be made with a br towel, then a wr towel, and then finally an r towel.
+// bggr can be made with a b towel, two g towels, and then an r towel.
+// gbbr can be made with a gb towel and then a br towel.
+// rrbgbr can be made with r, rb, g, and br.
+// ubwu is impossible.
+// bwurrg can be made with bwu, r, r, and g.
+// brgr can be made with br, g, and r.
+// bbrgwb is impossible.
+// In this example, 6 of the eight designs are possible with the available towel patterns.
+
+// To get into the onsen as soon as possible, consult your list of towel patterns and desired designs carefully. How many designs are possible?
+
+const getData = async (filename: string): Promise<string[], string[]> => {
+  let patterns: string[] = [];
+  let designs: string[] = [];
+
+  await fetch(`/TypeScript/inputs/${filename}.txt`)
+    .then((response) => response.text())
+    .then((data) => {
+      const [patternStr, designStr] = data.split("\r\n\r\n");
+      patterns = patternStr.split(", ");
+      designs = designStr.split("\r\n");
+    });
+
+  return [patterns, designs];
+};
+
+const day19 = ([patterns, designs]: [string[], string[]]) => {
+  let possibleDesigns = 0;
+
+  for (const design of designs) {
+    const n = design.length;
+
+    // track all pattern indexes within design and their lengths
+    let idxData = new Map<number, number[]>();
+
+    for (const pattern of patterns) {
+      const len = pattern.length;
+
+      let idx = design.indexOf(pattern);
+      let pos = 1;
+
+      while (idx != -1) {
+        if (!idxData.has(idx)) idxData.set(idx, []);
+        idxData.get(idx)?.push(len);
+
+        idx = design.indexOf(pattern, pos++);
+      }
+    }
+
+    // recursively build different pattern combinations, trying to build design
+    let impossibleIdx = new Set(); // avoid repeated checking of idxs (deadend)
+
+    const buildCombo = (idx: number, sol: { isValid: boolean }) => {
+      if (sol.isValid || impossibleIdx.has(idx)) return; // deadend
+
+      if (idx == n) {
+        sol.isValid = true; // valid combo found
+        possibleDesigns++;
+        return;
+      }
+
+      for (const patternLen of idxData.get(idx) ?? []) {
+        buildCombo(idx + patternLen, sol);
+      }
+
+      impossibleIdx.add(idx); // no solution for this idx (deadend)
+    };
+
+    buildCombo(0, { isValid: false });
+  }
+
+  return possibleDesigns;
+};
+
+console.log(day19(await getData("19"))); // 226
