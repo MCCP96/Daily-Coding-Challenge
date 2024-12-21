214064486d62e735dd64286320b7022f94798e03
MCCP96 <petermccomb@live.com>
2024-12-21 16:51:38 -0500

    Advent of Code Day 21: Keypad Conundrum

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -8473,7 +8473,7 @@ const day19 = ([patterns, designs]: [string[], string[]]) => {
 console.log(day19(await getData("19"))); // 226 */
 
 // Advent of Code Day 20: Race Condition          12/20/2024
-
+/* 
 // https://adventofcode.com/2024/day/20
 
 // The Historians are quite pixelated again. This time, a massive, black building looms over you - you're right outside the CPU!
@@ -8681,4 +8681,218 @@ const day20 = (map: (string | number)[][]): number => {
   return bestCheats;
 };
 
-console.log(day20(await getData("20"))); // 1441
+console.log(day20(await getData("20"))); // 1441 */
+
+// Advent of Code Day 21: Keypad Conundrum          12/21/2024
+
+// https://adventofcode.com/2024/day/21
+
+// As you teleport onto Santa's Reindeer-class starship, The Historians begin to panic: someone from their search party is missing. A quick life-form scan by the ship's computer reveals that when the missing Historian teleported, he arrived in another part of the ship.
+
+// The door to that area is locked, but the computer can't open it; it can only be opened by physically typing the door codes (your puzzle input) on the numeric keypad on the door.
+
+// The numeric keypad has four rows of buttons: 789, 456, 123, and finally an empty gap followed by 0A. Visually, they are arranged like this:
+
+// +---+---+---+
+// | 7 | 8 | 9 |
+// +---+---+---+
+// | 4 | 5 | 6 |
+// +---+---+---+
+// | 1 | 2 | 3 |
+// +---+---+---+
+//     | 0 | A |
+//     +---+---+
+// Unfortunately, the area outside the door is currently depressurized and nobody can go near the door. A robot needs to be sent instead.
+
+// The robot has no problem navigating the ship and finding the numeric keypad, but it's not designed for button pushing: it can't be told to push a specific button directly. Instead, it has a robotic arm that can be controlled remotely via a directional keypad.
+
+// The directional keypad has two rows of buttons: a gap / ^ (up) / A (activate) on the first row and < (left) / v (down) / > (right) on the second row. Visually, they are arranged like this:
+
+//     +---+---+
+//     | ^ | A |
+// +---+---+---+
+// | < | v | > |
+// +---+---+---+
+// When the robot arrives at the numeric keypad, its robotic arm is pointed at the A button in the bottom right corner. After that, this directional keypad remote control must be used to maneuver the robotic arm: the up / down / left / right buttons cause it to move its arm one button in that direction, and the A button causes the robot to briefly move forward, pressing the button being aimed at by the robotic arm.
+
+// For example, to make the robot type 029A on the numeric keypad, one sequence of inputs on the directional keypad you could use is:
+
+// < to move the arm from A (its initial position) to 0.
+// A to push the 0 button.
+// ^A to move the arm to the 2 button and push it.
+// >^^A to move the arm to the 9 button and push it.
+// vvvA to move the arm to the A button and push it.
+// In total, there are three shortest possible sequences of button presses on this directional keypad that would cause the robot to type 029A: <A^A>^^AvvvA, <A^A^>^AvvvA, and <A^A^^>AvvvA.
+
+// Unfortunately, the area containing this directional keypad remote control is currently experiencing high levels of radiation and nobody can go near it. A robot needs to be sent instead.
+
+// When the robot arrives at the directional keypad, its robot arm is pointed at the A button in the upper right corner. After that, a second, different directional keypad remote control is used to control this robot (in the same way as the first robot, except that this one is typing on a directional keypad instead of a numeric keypad).
+
+// There are multiple shortest possible sequences of directional keypad button presses that would cause this robot to tell the first robot to type 029A on the door. One such sequence is v<<A>>^A<A>AvA<^AA>A<vAAA>^A.
+
+// Unfortunately, the area containing this second directional keypad remote control is currently -40 degrees! Another robot will need to be sent to type on that directional keypad, too.
+
+// There are many shortest possible sequences of directional keypad button presses that would cause this robot to tell the second robot to tell the first robot to eventually type 029A on the door. One such sequence is <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A.
+
+// Unfortunately, the area containing this third directional keypad remote control is currently full of Historians, so no robots can find a clear path there. Instead, you will have to type this sequence yourself.
+
+// Were you to choose this sequence of button presses, here are all of the buttons that would be pressed on your directional keypad, the two robots' directional keypads, and the numeric keypad:
+
+// <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+// v<<A>>^A<A>AvA<^AA>A<vAAA>^A
+// <A^A>^^AvvvA
+// 029A
+// In summary, there are the following keypads:
+
+// One directional keypad that you are using.
+// Two directional keypads that robots are using.
+// One numeric keypad (on a door) that a robot is using.
+// It is important to remember that these robots are not designed for button pushing. In particular, if a robot arm is ever aimed at a gap where no button is present on the keypad, even for an instant, the robot will panic unrecoverably. So, don't do that. All robots will initially aim at the keypad's A key, wherever it is.
+
+// To unlock the door, five codes will need to be typed on its numeric keypad. For example:
+
+// 029A
+// 980A
+// 179A
+// 456A
+// 379A
+// For each of these, here is a shortest sequence of button presses you could type to cause the desired code to be typed on the numeric keypad:
+
+// 029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+// 980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
+// 179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+// 456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
+// 379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+// The Historians are getting nervous; the ship computer doesn't remember whether the missing Historian is trapped in the area containing a giant electromagnet or molten lava. You'll need to make sure that for each of the five codes, you find the shortest sequence of button presses necessary.
+
+// The complexity of a single code (like 029A) is equal to the result of multiplying these two values:
+
+// The length of the shortest sequence of button presses you need to type on your directional keypad in order to cause the code to be typed on the numeric keypad; for 029A, this would be 68.
+// The numeric part of the code (ignoring leading zeroes); for 029A, this would be 29.
+// In the above example, complexity of the five codes can be found by calculating 68 * 29, 60 * 980, 68 * 179, 64 * 456, and 64 * 379. Adding these together produces 126384.
+
+// Find the fewest number of button presses you'll need to perform in order to cause the robot in front of the door to type each code. What is the sum of the complexities of the five codes on your list?
+
+const getData = async (filename: string): Promise<string[]> => {
+  // return ["029A", "980A", "179A", "456A", "379A"]; // example (126384)
+  return ["869A", "170A", "319A", "349A", "489A"];
+};
+
+const nKeys = "7894561230A";
+const nLayout = [
+  ["7", "8", "9"],
+  ["4", "5", "6"],
+  ["1", "2", "3"],
+  ["_", "0", "A"],
+];
+const dKeys = "^A<v>";
+const dLayout = [
+  ["_", "^", "A"],
+  ["<", "v", ">"],
+];
+
+const dir = [
+  { dx: 0, dy: -1, btn: "^" }, // up
+  { dx: 1, dy: 0, btn: ">" }, // right
+  { dx: 0, dy: 1, btn: "v" }, // down
+  { dx: -1, dy: 0, btn: "<" }, // left
+];
+
+const findPaths = (s: string, e: string, layout: string[][]): string => {
+  const n = layout.length;
+  const m = layout[0].length;
+
+  // return strings of all shortest equal paths
+  let res: string = "N/A";
+
+  // find coords of starting pos
+  let [x, y] = [-1, -1];
+  while (x == -1) x = layout[++y].indexOf(s);
+
+  // recursively search
+  let leastMoves = new Map<string, number>(); // avoid repeated sub-optimal paths
+
+  const explore = (x: number, y: number, moves: number, str: string) => {
+    if (leastMoves.has(layout[y][x]) && leastMoves.get(layout[y][x])! < moves)
+      return; // not shortest
+    leastMoves.set(layout[y][x], moves);
+
+    if (layout[y][x] == e) {
+      // target button reached
+      if (res == "N/A" || moves < res.length) res = str;
+      return;
+    }
+
+    // try all directions
+    for (const { dx, dy, btn } of dir) {
+      if (x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n) {
+        // within bounds
+        explore(x + dx, y + dy, moves + 1, str + btn);
+      }
+    }
+  };
+  explore(x, y, 0, "");
+
+  return res + "A";
+};
+
+const day21 = (codes: string[]) => {
+  // map all shortest paths between all buttons
+  const nMap = new Map<string, string>();
+
+  for (let i = 0; i < nKeys.length; i++) {
+    for (let j = 0; j < nKeys.length; j++) {
+      const s = nKeys[i];
+      const e = nKeys[j];
+      nMap.set(`${s}->${e}`, findPaths(s, e, nLayout));
+    }
+  }
+
+  const dMap = new Map<string, string>();
+
+  for (let i = 0; i < dKeys.length; i++) {
+    for (let j = 0; j < dKeys.length; j++) {
+      const s = dKeys[i];
+      const e = dKeys[j];
+      dMap.set(`${s}->${e}`, findPaths(s, e, dLayout));
+    }
+  }
+
+  // find total
+  let res = 0;
+
+  for (let code of codes) {
+    let n = Number(code.substring(0, 3));
+
+    // num pad
+    let newCode = "";
+    let p = "A";
+    for (const c of code) {
+      newCode += nMap.get(`${p}->${c}`);
+      p = c;
+    }
+    code = newCode;
+
+    // dir pads
+    let depth = 2;
+    while (depth-- > 0) {
+      let newCode = "";
+      p = "A";
+      for (const c of code) {
+        newCode += dMap.get(`${p}->${c}`);
+        p = c;
+      }
+      code = newCode;
+    }
+
+    // input sol
+    res += n * code.length;
+  }
+
+  return res;
+};
+
+console.log(day21(await getData("21"))); // 165062 (too high)
+
+// yeahhhhhhhhhhhhhh, didnt manage to solve this one
+// could seem to hit the example either
