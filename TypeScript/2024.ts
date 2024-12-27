5acce4058db6920bd0577e056bdccd01254285f8
MCCP96 <petermccomb@live.com>
2024-12-27 09:16:57 -0500

    Find the Peaks

--- TypeScript/2024.ts
+++ TypeScript/2024.ts
@@ -9503,7 +9503,7 @@ console.log(day25(await getData("25"))); // 3360
 // 🎅🎄 */
 
 // Fraction Addition and Subtraction					12/26/2024
-
+/* 
 // https://leetcode.com/problems/fraction-addition-and-subtraction/
 
 // Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
@@ -9588,4 +9588,51 @@ const fractionAddition = (exp: string): string => {
 
 console.log(fractionAddition("-1/2+1/2")); //  "0/1"
 console.log(fractionAddition("-1/2+1/2+1/3")); //  "1/3"
-console.log(fractionAddition("1/3-1/2")); //  "-1/6"
+console.log(fractionAddition("1/3-1/2")); //  "-1/6" */
+
+// Find the Peaks					12/27/2024
+
+// https://leetcode.com/problems/find-the-peaks/description/
+
+// You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+
+// Return an array that consists of indices of peaks in the given array in any order.
+
+// Notes:
+// A peak is defined as an element that is strictly greater than its neighboring elements.
+// The first and last elements of the array are not a peak.
+
+// Example 1:
+// 		Input: mountain = [2,4,4]
+// 		Output: []
+// Explanation: mountain[0] and mountain[2] can not be a peak because they are first and last elements of the array.
+// 		mountain[1] also can not be a peak because it is not strictly greater than mountain[2].
+// 		So the answer is [].
+
+// Example 2:
+// 		Input: mountain = [1,4,3,8,5]
+// 		Output: [1,3]
+// Explanation: mountain[0] and mountain[4] can not be a peak because they are first and last elements of the array.
+// 		mountain[2] also can not be a peak because it is not strictly greater than mountain[3] and mountain[1].
+// 		But mountain [1] and mountain[3] are strictly greater than their neighboring elements.
+// 		So the answer is [1,3].
+
+// Constraints:
+//		3 <= mountain.length <= 100
+//		1 <= mountain[i] <= 100
+
+const findPeaks = (mountains: number[]): number[] => {
+  const n = mountains.length;
+  let peaks: number[] = [];
+
+  for (let i = 1; i < n - 1; i++)
+    if (mountains[i - 1] < mountains[i] && mountains[i + 1] < mountains[i])
+      peaks.push(i);
+
+  return peaks;
+};
+
+console.log(findPeaks([2, 4, 4])); //  []
+console.log(findPeaks([1, 4, 3, 8, 5])); //  [1,3]
+
+// doing easys til the end of the year
