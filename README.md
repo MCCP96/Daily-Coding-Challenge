8593320cdbcab72d37465f1d4f63edc6184f8bb0
MCCP96 <petermccomb@live.com>
2025-01-01 10:10:44 -0500

    Happy New Year!

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Daily Coding Challenges
 
 These are my [daily coding challenges](https://mccp.app/)<br/>
-Problems are found and solved on [LeetCode](https://leetcode.com/MCCP96/)
+
+Previously [LeetCode](https://leetcode.com/MCCP96/) problems, starting in 2025, they focus on React and the implementation of different technologies within that framework.
 
 "Success is the product of daily habit - not once-in-a-lifetime transformations."
 
---
+++ React/2025/src/app/page.tsx
@@ -0,0 +1,31 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  // Day 1294 - Happy New Year 🎇
+
+  /* 
+    My first internship starts Jan 6th, working in React/TypeScript.
+    This year I plan on moving away from LeetCode to practice relevant technologies.
+
+    Some ideas are:
+      - basic components/css
+      - NEXTjs documentation
+      - React Native documentation
+      - React Redux
+      - localStorage
+      - firebase (database/auth)
+      - maps
+      - email
+      - stripe
+    
+    I'm sure I'll also revist LeetCode occasionally.
+
+    Happy new year!
+   */
+
+  return (
+    <div className={styles.page}>
+      <h1>2025</h1>
+    </div>
+  );
+}
